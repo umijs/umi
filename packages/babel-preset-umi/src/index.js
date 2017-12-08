e994928e780ff2f1355946bf7f4f95108da1ef57
chencheng <sorrycc@gmail.com>
2017-12-08 10:04:47 +0800

    feat(babel-preset-umi): enable async-generator-functions since chrome 63 enabled it

--- packages/babel-preset-umi/src/index.js
+++ packages/babel-preset-umi/src/index.js
@@ -7,6 +7,7 @@ export default function(context, opts = {}) {
     require.resolve('@babel/plugin-syntax-dynamic-import'),
     require.resolve('@babel/plugin-proposal-object-rest-spread'),
     require.resolve('@babel/plugin-proposal-optional-catch-binding'),
+    require.resolve('@babel/plugin-proposal-async-generator-functions'),
     require.resolve('@babel/plugin-proposal-decorators'),
     [
       require.resolve('@babel/plugin-proposal-class-properties'),
