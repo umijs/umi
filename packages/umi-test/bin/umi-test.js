3ca5287b4e3e3db25f6e546fb25146f30b2d4e64
chencheng <sorrycc@gmail.com>
2017-12-15 13:35:08 +0800

    feat: umi test with --watch and --coverage params

--- packages/umi-test/bin/umi-test.js
+++ packages/umi-test/bin/umi-test.js
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
 
-const test = require('../lib/test');
+const test = require('../lib');
 
 test();
---
+++ packages/umi/src/scripts/test.js
@@ -0,0 +1,11 @@
+import test from '../test';
+
+const args = process.argv.slice(2);
+
+const watch = args.indexOf('-w') > -1 || args.indexOf('--watch') > -1;
+const coverage = args.indexOf('--coverage') > -1;
+
+test({
+  watch,
+  coverage,
+});
