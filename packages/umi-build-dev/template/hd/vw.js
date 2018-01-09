22db9ff42892db3a93920cc2f66e6cd040fe5757
chencheng <sorrycc@gmail.com>
2018-01-09 11:29:24 +0800

    use es5 for hd files

--- packages/umi-build-dev/template/hd/vw.js
+++ packages/umi-build-dev/template/hd/vw.js
@@ -1,33 +1,33 @@
 export default function(baseFontSize, psdWidth) {
-  const _baseFontSize = baseFontSize || 100;
-  const _psdWidth = psdWidth || 750;
+  var _baseFontSize = baseFontSize || 100;
+  var _psdWidth = psdWidth || 750;
 
-  const win = window;
-  const doc = win.document;
-  const ua = navigator.userAgent;
-  const matches = ua.match(/Android[\S\s]+AppleWebkit\/(\d{3})/i);
-  const UCversion = ua.match(/U3\/((\d+|\.){5,})/i);
-  const isUCHd =
+  var win = window;
+  var doc = win.document;
+  var ua = navigator.userAgent;
+  var matches = ua.match(/Android[\S\s]+AppleWebkit\/(\d{3})/i);
+  var UCversion = ua.match(/U3\/((\d+|\.){5,})/i);
+  var isUCHd =
     UCversion && parseInt(UCversion[1].split('.').join(''), 10) >= 80;
-  const isIos = navigator.appVersion.match(/(iphone|ipad|ipod)/gi);
-  const dpr = win.devicePixelRatio || 1;
-  const docEl = doc.documentElement;
+  var isIos = navigator.appVersion.match(/(iphone|ipad|ipod)/gi);
+  var dpr = win.devicePixelRatio || 1;
+  var docEl = doc.documentElement;
   // 为了消除安卓dpr乱标的比例
-  let rate = 1;
-  let scale = 1 / dpr;
+  var rate = 1;
+  var scale = 1 / dpr;
   if (isIos) {
     // iOS下不用做什么
   } else if ((matches && matches[1] > 534) || isUCHd) {
     // 有些兼容环境下, fontSize为100px的时候, 结果1rem=86px; 需要纠正viewport;
     docEl.style.fontSize = _baseFontSize + 'px';
-    const div = doc.createElement('div');
+    var div = doc.createElement('div');
     div.setAttribute('style', 'width: 1rem;display:none');
     docEl.appendChild(div);
-    const trueWidth = win.getComputedStyle(div).width;
+    var trueWidth = win.getComputedStyle(div).width;
     docEl.removeChild(div);
     // 如果1rem的真实px跟html.fontSize不符. 那么就要加一个rate缩放了;
     if (trueWidth !== docEl.style.fontSize) {
-      const trueWidthVal = parseInt(trueWidth, 10);
+      var trueWidthVal = parseInt(trueWidth, 10);
       rate = _baseFontSize / trueWidthVal;
       scale = scale * rate;
     }
@@ -36,7 +36,7 @@ export default function(baseFontSize, psdWidth) {
     scale = 1;
   }
 
-  let metaEl = doc.querySelector('meta[name="viewport"]');
+  var metaEl = doc.querySelector('meta[name="viewport"]');
   if (!metaEl) {
     metaEl = doc.createElement('meta');
     metaEl.setAttribute('name', 'viewport');
