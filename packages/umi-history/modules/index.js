835ee0e6cd990d96a357695d2e88389287cb8866
chencheng <sorrycc@gmail.com>
2018-01-13 06:33:58 +0800

    refact: move umi-history outside of umijs/umi

--- packages/umi-history/modules/index.js
+++
@@ -1,5 +0,0 @@
-export createBrowserHistory from './createBrowserHistory'
-export createHashHistory from './createHashHistory'
-export createMemoryHistory from './createMemoryHistory'
-export { createLocation, locationsAreEqual } from './LocationUtils'
-export { parsePath, createPath } from './PathUtils'
