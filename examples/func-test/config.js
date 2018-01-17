a22ea645be6c20c1232f70190987059db8057926
chencheng <sorrycc@gmail.com>
2018-01-17 15:05:53 +0800

    example: add context.title config

--- examples/func-test/config.js
+++ examples/func-test/config.js
@@ -1,8 +1,8 @@
 export default {
   spma: 'a111111111111',
   pages: {
-    '/': { spmb: 'b2222222222222' },
-    '/list.html': { spmb: 'b3333333333333' },
+    '/index.html': { spmb: 'b2222222222222', context: { title: '首页' } },
+    '/list.html': { spmb: 'b3333333333333', context: { title: '列表页' } },
   },
   plugins: ['./plugin', 'umi-plugin-yunfengdie'],
   // disableServiceWorker: true,
