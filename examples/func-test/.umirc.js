903d3555df009e06e65e560364d5df17b7db8ea9
chencheng <sorrycc@gmail.com>
2018-02-06 11:39:24 +0800

    update example

--- examples/func-test/.umirc.js
+++ examples/func-test/.umirc.js
@@ -1,8 +1,7 @@
 export default {
-  spma: 'a111111111111',
   pages: {
-    '/': { spmb: 'b2222222222222', context: { title: '首页' } },
-    'list.html': { spmb: 'b3333333333333', context: { title: '列表页' } },
+    '/': { context: { title: '首页' } },
+    'list.html': { context: { title: '列表页' } },
   },
   plugins: [
     './plugin1',
