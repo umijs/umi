28b6228a582814d8bc62e46dc94309f645972156
chencheng <sorrycc@gmail.com>
2018-02-07 21:45:42 +0800

    fix: path match problem in middleware, Close #90

--- packages/umi-build-dev/src/createRouteMiddleware.js
+++ packages/umi-build-dev/src/createRouteMiddleware.js
@@ -1,3 +1,4 @@
+import matchPath from 'react-router-dom/matchPath';
 import { setRequest } from './requestCache';
 import HtmlGenerator from './HtmlGenerator';
 
@@ -8,7 +9,7 @@ export default function createRouteMiddleware(service, opts = {}) {
   return (req, res, next) => {
     const { path } = req;
     const route = service.routes.filter(r => {
-      return r.path === path;
+      return matchPath(path, r);
     })[0];
     if (route) {
       service.applyPlugins('onRouteRequest', {
@@ -20,16 +21,19 @@ export default function createRouteMiddleware(service, opts = {}) {
 
       // 尝试解决 Compiling... 不消失的问题
       setTimeout(() => {
-        setRequest(path, {
+        setRequest(route.path, {
           onChange: opts.rebuildEntry,
         });
       }, 300);
 
       const htmlGenerator = new HtmlGenerator(service);
-      const content = htmlGenerator.getContent({
-        pageConfig: (config.pages || {})[path],
-        route,
-      });
+      const gcOpts = config.exportStatic
+        ? {
+            pageConfig: (config.pages || {})[path],
+            route,
+          }
+        : {};
+      const content = htmlGenerator.getContent(gcOpts);
       res.setHeader('Content-Type', 'text/html');
       res.send(content);
     } else {
