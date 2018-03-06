44cdf419d1e2224ec7a07baeafafaa1eb889af85
oliver <oliver@pengfei.local>
2018-03-06 21:48:02 +0800

    add .eslintrc commit

--- packages/af-webpack/src/readRc.js
+++ packages/af-webpack/src/readRc.js
@@ -1,6 +1,6 @@
 import { readFileSync } from 'fs';
-
+import stripJsonComments from 'strip-json-comments';
 export default function(path) {
   const content = readFileSync(path, 'utf-8');
-  return JSON.parse(content);
+  return JSON.parse(stripJsonComments(content));
 }
