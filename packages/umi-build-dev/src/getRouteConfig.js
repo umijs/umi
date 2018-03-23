2ce569b9399bf5a4c20c989d9ce5dc2da6c203c1
boxz <lmfmcbo@gmail.com>
2018-03-23 23:42:05 +0800

    fix nested routes winpath bug

--- packages/umi-build-dev/src/getRouteConfig.js
+++ packages/umi-build-dev/src/getRouteConfig.js
@@ -147,7 +147,7 @@ function getRoutesByPagesDir(paths, dirPath = '') {
           const childRoutes = getRoutesByPagesDir(paths, fullPath);
           if (layoutFile) {
             ret = ret.concat({
-              path: `/${fullPath}`,
+              path: winPath(`/${fullPath}`),
               exact: false,
               component: `./${relative(cwd, layoutFile)}`,
               routes: childRoutes,
