3b943a1dae7b36e398a272ee5f0080b89d5dff52
chencheng <sorrycc@gmail.com>
2018-03-28 08:29:39 +0800

    add testcase

--- packages/umi-build-dev/src/plugins/404/index.js
+++ packages/umi-build-dev/src/plugins/404/index.js
@@ -1,53 +1,34 @@
 import { join, basename, extname } from 'path';
-import { existsSync, readFileSync } from 'fs';
+import { readFileSync } from 'fs';
 
-const EXTNAMES = ['.js', '.jsx', '.ts', '.tsx'];
+export function patchRoutes(routes) {
+  let index = null;
+  for (const [i, value] of routes.entries()) {
+    const { component } = value;
+    if (basename(component, extname(component)) === '404') {
+      index = i;
+    }
+    if (value.routes) {
+      value.routes = patchRoutes(value.routes);
+    }
+  }
+  if (index !== null) {
+    const route = routes.splice(index, 1)[0];
+    routes = routes.concat({
+      component: route.component,
+    });
+  }
+  return routes;
+}
 
 export default function(api) {
   const { paths } = api.service;
-  const { winPath } = api.utils;
 
-  // function get404JS() {
-  //   for (const extname of EXTNAMES) {
-  //     const filePath = winPath(join(paths.absPagesPath, `404${extname}`));
-  //     if (existsSync(filePath)) {
-  //       return filePath;
-  //     }
-  //   }
-  // }
-
-  function patchRoutes(routes) {
-    let index = null;
-    for (const [i, value] of routes.entries()) {
-      const { component } = value;
-      if (basename(component, extname(component)) === '404') {
-        index = i;
-      }
-      if (value.routes) {
-        value.routes = patchRoutes(value.routes);
-      }
-    }
-    if (index !== null) {
-      const route = routes.splice(index, 1)[0];
-      console.log('spliced route', route);
-      routes = routes.concat({
-        component: route.component,
-      });
-    }
-    return routes;
+  if (process.env.NODE_ENV === 'production') {
+    api.register('modifyRoutes', ({ memo }) => {
+      return patchRoutes(memo);
+    });
   }
-
-  api.register('modifyRoutes', ({ memo }) => {
-    return patchRoutes(memo);
-  });
-
-  // api.register('modifyRoutesContent', ({ memo }) => {
-  //   const filePath = get404JS();
-  //   if (filePath) {
-  //     memo.push(`    <Route component={require('${filePath}').default} />`);
-  //   }
-  //   return memo;
-  // });
 
   api.register('beforeServer', ({ args: { devServer } }) => {
     function UMI_PLUGIN_404(req, res, next) {
