4a34b819c24788b77723f4959904d7395350c17b
chencheng (云谦) <sorrycc@gmail.com>
2018-04-11 15:34:48 +0800

    feat(umi-build-dev): support disable fastclick via opts (#307)
    * feat(umi-build-dev): support disable fastclick via opts
    
    * fix typo

--- packages/umi-build-dev/src/plugins/fastclick.js
+++ packages/umi-build-dev/src/plugins/fastclick.js
@@ -1,10 +1,24 @@
 export default function(api) {
+  const { config } = api.service;
   const { IMPORT } = api.placeholder;
 
-  api.register('modifyEntryFile', ({ memo }) => {
-    memo = memo.replace(
-      IMPORT,
-      `
+  api.register('modifyConfigPlugins', ({ memo }) => {
+    memo.push(api => {
+      return {
+        name: 'disableFastClick',
+        onChange() {
+          api.service.restart(/* why */ 'Config disableFastClick Changed');
+        },
+      };
+    });
+    return memo;
+  });
+
+  if (!config.disableFastClick) {
+    api.register('modifyEntryFile', ({ memo }) => {
+      memo = memo.replace(
+        IMPORT,
+        `
 import FastClick from 'umi-fastclick';
 ${IMPORT}
 
@@ -16,13 +30,14 @@ document.addEventListener(
   false,
 );
     `.trim(),
-    );
-    return memo;
-  });
+      );
+      return memo;
+    });
 
-  api.register('modifyAFWebpackOpts', ({ memo }) => {
-    // 期一个带 umi 前缀的别名，是为了后面可以修改他
-    memo.alias['umi-fastclick'] = require.resolve('fastclick');
-    return memo;
-  });
+    api.register('modifyAFWebpackOpts', ({ memo }) => {
+      // 取一个带 umi 前缀的别名，是为了后面可以修改他
+      memo.alias['umi-fastclick'] = require.resolve('fastclick');
+      return memo;
+    });
+  }
 }
