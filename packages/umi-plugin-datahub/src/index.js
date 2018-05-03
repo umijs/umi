78b96f3150935489de1abce6389f16665232ca51
chencheng <sorrycc@gmail.com>
2018-05-03 18:27:38 +0800

    feat(umi-plugin-datahub): support config datahub

--- packages/umi-plugin-datahub/src/index.js
+++ packages/umi-plugin-datahub/src/index.js
@@ -2,23 +2,20 @@ import { join } from 'path';
 import DataHub from 'macaca-datahub';
 import datahubMiddleware from 'datahub-proxy-middleware';
 
-const datahubConfig = {
-  port: 5678,
-  hostname: '127.0.0.1',
-  store: join(__dirname, 'data'),
-  proxy: {
-    '^/api': {
-      hub: 'ifccustmng',
-    },
-  },
-  showBoard: false,
-};
+export default (api, opts = {}) => {
+  const datahubConfig = {
+    port: 5678,
+    hostname: '127.0.0.1',
+    store: join(__dirname, 'data'),
+    proxy: {},
+    showBoard: false,
+    ...opts,
+  };
 
-const defaultDatahub = new DataHub({
-  port: datahubConfig.port,
-});
+  const defaultDatahub = new DataHub({
+    port: datahubConfig.port,
+  });
 
-export default api => {
   api.register('beforeServerWithApp', ({ args: { app } }) => {
     datahubMiddleware(app)(datahubConfig);
     defaultDatahub.startServer(datahubConfig).then(() => {
