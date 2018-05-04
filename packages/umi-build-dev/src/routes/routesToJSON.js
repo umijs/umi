b14d6cded1cf45b9ab3210a1a834275db5b66d00
chencheng <sorrycc@gmail.com>
2018-05-04 20:55:07 +0800

    add routesToJSON

--- packages/umi-build-dev/src/routes/routesToJSON.js
+++ packages/umi-build-dev/src/routes/routesToJSON.js
@@ -1,19 +1,67 @@
-import { join } from 'path';
+import { join, relative } from 'path';
 import winPath from '../winPath';
 import normalizeEntry from '../normalizeEntry';
 
-// WIP
-
-export default (routes, paths, requested, env, applyPlugins) => {
-  // TODO: loading
+export default (routes, { config, applyPlugins, paths }, requestedMap, env) => {
   patchRoutes(routes);
 
-  JSON.stringify(
+  const { loading } = config;
+  let loadingOpts = '';
+  if (loading) {
+    loadingOpts = ` loading: require('${winPath(
+      join(paths.cwd, loading),
+    )}').default `;
+  }
+
+  return JSON.stringify(
     routes,
     (key, value) => {
       switch (key) {
         case 'component':
-          break;
+          const [component, webpackChunkName, path] = value.split('^^');
+          const importPath =
+            component.charAt(0) === '/'
+              ? component
+              : winPath(relative(paths.tmpDirPath, component));
+
+          let ret;
+          let isCompiling = false;
+          const compilingPath = winPath(paths.absCompilingComponentPath);
+
+          if (env === 'production' && !config.disabledynamicimport) {
+            // 按需加载
+            ret = `dynamic(() => import(/* webpackChunkName: ${webpackChunkName} */'${importPath}'), {${loadingOpts}})`;
+          } else {
+            // 非按需加载
+            if (
+              env === 'production' ||
+              // 无 socket 时按需编译体验很差，所以禁用
+              process.env.SOCKET_SERVER === 'none' ||
+              process.env.COMPILE_ON_DEMAND === 'none' ||
+              !path ||
+              requestedMap[path]
+            ) {
+              ret = `require('${importPath}').default`;
+            } else {
+              isCompiling = true;
+              ret = `() => React.createElement(require('${compilingPath}').default, { route: '${path}' })`;
+            }
+          }
+
+          if (applyPlugins) {
+            applyPlugins('modifyRouteComponent', {
+              initialValue: ret,
+              args: {
+                isCompiling,
+                pageJSFile: importPath,
+                importPath,
+                webpackChunkName,
+                config,
+              },
+            });
+          }
+
+          return ret;
         case 'Route':
           return `require('${winPath(join(paths.cwd, value))}').default`;
         default:
@@ -31,11 +79,18 @@ function patchRoutes(routes, webpackChunkName) {
 }
 
 function patchRoute(route, webpackChunkName) {
-  if (!webpackChunkName) {
-    webpackChunkName = normalizeEntry(route.component || 'common_component');
+  if (route.component) {
+    if (!webpackChunkName) {
+      webpackChunkName = normalizeEntry(route.component || 'common_component');
+    }
+    route.component = [
+      route.component || 'common_component',
+      webpackChunkName,
+      route.path,
+    ].join('^^');
   }
-  route.component = [route.component || 'common_component'].join('^^');
   if (route.routes) {
+    // 只在一级路由做按需编译
     patchRoutes(route.routes, webpackChunkName);
   }
 }
---
+++ packages/umi-build-dev/src/routes/routesToJSON.test.js
@@ -0,0 +1,186 @@
+import routesToJSON from './routesToJSON';
+
+const service = {
+  config: {},
+  paths: {
+    cwd: '$CWD$',
+    tmpDirPath: './pages/.umi',
+    absCompilingComponentPath: '$COMPILING$',
+  },
+};
+
+describe('routesToJSON', () => {
+  it('normal', () => {
+    const json = routesToJSON([{ component: './pages/A' }], service, {});
+    expect(JSON.parse(json)).toEqual([
+      { component: "require('../A').default" },
+    ]);
+  });
+
+  it('relative component path', () => {
+    const json = routesToJSON(
+      [
+        { component: './pages/A' },
+        { component: './pages/A/A' },
+        { component: './A' },
+        { component: './B/A' },
+      ],
+      service,
+      {},
+    );
+    expect(JSON.parse(json)).toEqual([
+      { component: "require('../A').default" },
+      { component: "require('../A/A').default" },
+      { component: "require('../../A').default" },
+      { component: "require('../../B/A').default" },
+    ]);
+  });
+
+  it('dynamic load when env is production', () => {
+    const json = routesToJSON(
+      [{ component: './pages/A' }],
+      service,
+      {},
+      'production',
+    );
+    expect(JSON.parse(json)).toEqual([
+      {
+        component:
+          "dynamic(() => import(/* webpackChunkName: pages__A */'../A'), {})",
+      },
+    ]);
+  });
+
+  it('dynamic load when env is production (with loading)', () => {
+    const json = routesToJSON(
+      [{ component: './pages/A' }],
+      {
+        ...service,
+        config: { loading: './LoadingComponent' },
+      },
+      {},
+      'production',
+    );
+    expect(JSON.parse(json)).toEqual([
+      {
+        component:
+          "dynamic(() => import(/* webpackChunkName: pages__A */'../A'), { loading: require('$CWD$/LoadingComponent').default })",
+      },
+    ]);
+  });
+
+  it('dynamic load when env is production (with loading) (winPath)', () => {
+    const json = routesToJSON(
+      [{ component: './pages/A' }],
+      {
+        ...service,
+        config: { loading: 'AAA\\LoadingComponent' },
+      },
+      {},
+      'production',
+    );
+    expect(JSON.parse(json)).toEqual([
+      {
+        component:
+          "dynamic(() => import(/* webpackChunkName: pages__A */'../A'), { loading: require('$CWD$/AAA/LoadingComponent').default })",
+      },
+    ]);
+  });
+
+  it('disable dyamic load if config.disabledynamicimport is set', () => {
+    const json = routesToJSON(
+      [{ component: './pages/A' }],
+      {
+        ...service,
+        config: { disabledynamicimport: true },
+      },
+      {},
+      'production',
+    );
+    expect(JSON.parse(json)).toEqual([
+      { component: "require('../A').default" },
+    ]);
+  });
+
+  it('show compiling if have path', () => {
+    const json = routesToJSON(
+      [{ component: './pages/A', path: '/a' }],
+      service,
+      {},
+    );
+    expect(JSON.parse(json)).toEqual([
+      {
+        component:
+          "() => React.createElement(require('$COMPILING$').default, { route: '/a' })",
+        path: '/a',
+      },
+    ]);
+  });
+
+  it('do not show compiling if have requested', () => {
+    const json = routesToJSON(
+      [
+        { component: './pages/A', path: '/a' },
+        { component: './pages/B', path: '/b' },
+      ],
+      service,
+      {
+        '/a': 1,
+      },
+    );
+    expect(JSON.parse(json)).toEqual([
+      { component: "require('../A').default", path: '/a' },
+      {
+        component:
+          "() => React.createElement(require('$COMPILING$').default, { route: '/b' })",
+        path: '/b',
+      },
+    ]);
+  });
+
+  it('applyPlugins', () => {
+    let applyPluginName = null;
+    let applyPluginOpts = null;
+    let applied = false;
+    routesToJSON(
+      [{ component: './pages/A' }],
+      {
+        ...service,
+        applyPlugins(name, opts) {
+          applyPluginName = name;
+          applyPluginOpts = opts;
+          applied = true;
+        },
+      },
+      {},
+      'production',
+    );
+    expect(applied).toEqual(true);
+    expect(applyPluginName).toEqual('modifyRouteComponent');
+    expect(applyPluginOpts).toEqual({
+      initialValue:
+        "dynamic(() => import(/* webpackChunkName: pages__A */'../A'), {})",
+      args: {
+        isCompiling: false,
+        pageJSFile: '../A',
+        importPath: '../A',
+        webpackChunkName: 'pages__A',
+        config: {},
+      },
+    });
+  });
+
+  it('Route', () => {
+    const json = routesToJSON([{ Route: './routes/A' }], service, {});
+    expect(JSON.parse(json)).toEqual([
+      { Route: "require('$CWD$/routes/A').default" },
+    ]);
+  });
+
+  it('Route with winPath', () => {
+    const json = routesToJSON([{ Route: 'routes\\A' }], service, {});
+    expect(JSON.parse(json)).toEqual([
+      { Route: "require('$CWD$/routes/A').default" },
+    ]);
+  });
+});
