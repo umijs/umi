aed3158b8b2227da17aee4c2738cb85b61a4a666
chencheng <sorrycc@gmail.com>
2018-05-14 15:59:56 +0800

    feat(umi-build-dev): add ROUTES_MODIFIER placeholder for runtime routes update

--- packages/umi-build-dev/src/constants.js
+++ packages/umi-build-dev/src/constants.js
@@ -2,6 +2,7 @@ export const CONFIG_FILES = ['.umirc.js', 'config/config.js'];
 
 export const PLACEHOLDER_IMPORT = '<%= IMPORT %>';
 export const PLACEHOLDER_ROUTER_MODIFIER = '<%= ROUTER_MODIFIER %>';
+export const PLACEHOLDER_ROUTES_MODIFIER = '<%= ROUTES_MODIFIER %>';
 export const PLACEHOLDER_RENDER = '<%= RENDER %>';
 export const PLACEHOLDER_ROUTER = '<%= ROUTER %>';
 export const PLACEHOLDER_HISTORY_MODIFIER = '<%= HISTORY_MODIFIER %>';
