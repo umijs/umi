1a76f031bd50ba8a33884e85155552d54bce88fa
chencheng <sorrycc@gmail.com>
2018-05-28 17:51:06 +0800

    refact(umi-plugin-dva): refact and add testcases

---
+++ packages/umi-plugin-dva/src/fixtures/normal/models/global.js
@@ -0,0 +1,5 @@
+
+export default {
+  namespace: 'global',
+  state: 'global',
+};
---
+++ packages/umi-plugin-dva/src/utils.js
@@ -0,0 +1,43 @@
+import winPath from 'slash';
+import { join } from 'path';
+import { existsSync } from 'fs';
+
+const JS_EXTNAMES = ['.js', '.jsx', '.ts', '.tsx'];
+
+function stripFirstSlash(path) {
+  if (path.charAt(0) === '/') {
+    return path.slice(1);
+  } else {
+    return path;
+  }
+}
+
+export function chunkName(cwd, path) {
+  return stripFirstSlash(winPath(path).replace(winPath(cwd), '')).replace(
+    /\//g,
+    '__',
+  );
+}
+
+export function optsToArray(item) {
+  if (!item) return [];
+  if (Array.isArray(item)) {
+    return item;
+  } else {
+    return [item];
+  }
+}
+
+export function endWithSlash(path) {
+  return path.slice(-1) !== '/' ? `${path}/` : path;
+}
+
+export function findJSFile(baseDir, fileNameWithoutExtname) {
+  for (const extname of JS_EXTNAMES) {
+    const fileName = `${fileNameWithoutExtname}${extname}`;
+    const absFilePath = join(baseDir, fileName);
+    if (existsSync(absFilePath)) {
+      return absFilePath;
+    }
+  }
+}
