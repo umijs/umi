1ddf00fe4f1abaf46ad905020072d0dcf9dd54dd
xiaohu <448627663@qq.com>
2018-06-01 00:37:22 +0800

    fix Naming with Chinese Close #214 https://github.com/sindresorhus/slash/pull/9

--- packages/umi-build-dev/src/winPath.js
+++ packages/umi-build-dev/src/winPath.js
@@ -1,5 +1,6 @@
 import slash from 'slash';
 
 export default function(path) {
-  return slash(path);
+  const hasChinese = /[^\u4e00-\u9fa5]+/.test(path);
+  return hasChinese ? path.replace(/\\/g, '/') : slash(path);
 }
