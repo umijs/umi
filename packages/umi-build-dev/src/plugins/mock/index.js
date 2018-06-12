26232e00bb52259c1f9fc9359715279cf75cf4b2
chencheng <sorrycc@gmail.com>
2018-06-12 22:04:50 +0800

    feat(umi-build-dev): alias HTTP_MOCK to MOCK

--- packages/umi-build-dev/src/plugins/mock/index.js
+++ packages/umi-build-dev/src/plugins/mock/index.js
@@ -2,7 +2,7 @@ import createMockMiddleware from './createMockMiddleware';
 
 export default function(api) {
   api.register('beforeServerWithApp', ({ args: { app } }) => {
-    if (process.env.MOCK !== 'none') {
+    if (process.env.MOCK !== 'none' && process.env.HTTP_MOCK !== 'none') {
       app.use(createMockMiddleware(api));
     }
   });
