1b3acb03a671446aa91599363e76a4701b081c4e
chencheng <sorrycc@gmail.com>
2018-06-26 13:18:33 +0800

    feat(umi-plugin-polyfill): improve polyfill, remove extend option

--- packages/umi-plugin-polyfill/src/index.js
+++ packages/umi-plugin-polyfill/src/index.js
@@ -1,36 +1,7 @@
-import { join } from 'path';
-import { existsSync } from 'fs';
-import assert from 'assert';
-export default function(api, opts = {}) {
-  const { paths } = api.service;
-  const { winPath } = api.utils;
-  function checkFile(file) {
-    assert(typeof file === 'string', 'polyfill extend must be a string');
-    if (!file.startsWith('.')) {
-      return file;
-    } else if (existsSync(file)) {
-      return file;
-    } else if (existsSync(join(paths.cwd, file))) {
-      return join(paths.cwd, file);
-    }
-    return '';
-  }
-  let extendFile = '';
-  if (opts.extend) {
-    assert(
-      typeof opts.extend === 'object' && opts.extend.length,
-      'extend must be an array',
-    );
-    opts.extend.map(item => {
-      let fileName = checkFile(item);
-      if (fileName) {
-        extendFile += `import '${winPath(fileName)}';\r\n`;
-      }
-      return item;
-    });
-  }
+export default function(api) {
   api.register('modifyEntryFile', ({ memo }) => {
-    memo = `import 'umi-plugin-polyfill/lib/global.js';\r\n${extendFile}${memo}`;
-    return memo;
+    return `
+import 'umi-plugin-polyfill/lib/global.js';
+${memo}`.trim();
   });
 }
