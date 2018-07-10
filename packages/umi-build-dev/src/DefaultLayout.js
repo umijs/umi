066cf21bf6e95fdf42f4dbacb46677aa70f2855d
paranoidjk <hust2012jiangkai@gmail.com>
2018-07-10 14:47:18 +0800

    feat: redirect to 404 if none router exact match

--- packages/umi-build-dev/src/DefaultLayout.js
+++ packages/umi-build-dev/src/DefaultLayout.js
@@ -1,5 +1,13 @@
 import React from 'react';
+import { matchRoutes } from 'react-router-config';
 
 export default function(props) {
+  if (process.env.NODE_ENV !== 'development') {
+    const match = matchRoutes(props.route.routes, window.location.pathname);
+    if (!match.find(m => m.match.isExact)) {
+      window.g_history.replace('/404');
+      return '';
+    }
+  }
   return <div>{props.children}</div>;
 }
