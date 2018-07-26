f2faddc43d690e6f604cffe9d072fd56b5f7f2b1
niaoge <78493244@qq.com>
2018-07-26 17:12:10 +0800

    修复roobBase中没有判断是否对BASE_URL中的结尾 /的匹配，service-worker.js 查找应该根根据 window.roobBase 定位，而不应该根据 BASE_URL

--- packages/umi-build-dev/template/registerServiceWorker.js
+++ packages/umi-build-dev/template/registerServiceWorker.js
@@ -1,7 +1,9 @@
 
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
-    navigator.serviceWorker.register(`${process.env.BASE_URL || ''}service-worker.js`)
+    // navigator.serviceWorker.register(`${process.env.BASE_URL || ''}service-worker.js`)
+    //从window.routerBase位置获取更准确
+    navigator.serviceWorker.register(window.routerBase + 'service-worker.js')
       .then((reg) => {})
       .catch(e => {});
   });
