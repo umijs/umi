f3b7445248a6afd42aaf03b91bdfea1c345907a4
chencheng <sorrycc@gmail.com>
2018-07-27 20:39:23 +0800

    fix travis

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  testEnvironment: 'node',
+  testURL: 'http://localhost/',
   testMatch: ['**/?*.(spec|test|e2e).(j|t)s?(x)'],
   testPathIgnorePatterns: [
     '/node_modules/',
