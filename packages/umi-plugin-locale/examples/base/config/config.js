ecb9029f7fabb8e398cc4ab7ba3dd392ebc138a3
愚道 <tingzhao.ytz@antfin.com>
2018-08-02 16:44:22 +0800

    refact umi-plugin-locale to new umi api

--- packages/umi-plugin-locale/examples/base/config/config.js
+++ packages/umi-plugin-locale/examples/base/config/config.js
@@ -1,9 +1,13 @@
 export default {
-  plugins: ['umi-plugin-locale'],
-  locale: {
-    enable: true,
-    baseNavigator: false,
-    default: 'en-US',
-  },
+  plugins: [
+    [
+      '../../src/index',
+      {
+        enable: true,
+        baseNavigator: false,
+        default: 'en-US',
+      },
+    ],
+  ],
   singular: true,
 };
