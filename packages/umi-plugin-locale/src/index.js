dfe276973229317255608dcbc811c7c811ca3683
愚道 <tingzhao.ytz@antfin.com>
2018-08-02 16:44:22 +0800

    refact umi-plugin-locale to new umi api

--- packages/umi-plugin-locale/src/index.js
+++ packages/umi-plugin-locale/src/index.js
@@ -1,6 +1,19 @@
+//
+// TODOLIST:
+//   - 单元测试
+//   - example 里面的 antd 依赖改为 umi 插件
+//
+
 import { join, dirname } from 'path';
-import { existsSync, statSync, readdirSync } from 'fs';
+import {
+  existsSync,
+  statSync,
+  readdirSync,
+  readFileSync,
+  writeFileSync,
+} from 'fs';
 import { winPath } from 'umi-utils';
+import Mustache from 'mustache';
 
 // export for test
 export function getLocaleFileList(absSrcPath, singular) {
@@ -17,7 +30,7 @@ export function getLocaleFileList(absSrcPath, singular) {
           lang: fileInfo[1],
           country: fileInfo[2],
           name: `${fileInfo[1]}-${fileInfo[2]}`,
-          path: fullname,
+          path: winPath(fullname),
         });
       }
     }
@@ -26,150 +39,49 @@ export function getLocaleFileList(absSrcPath, singular) {
 }
 
 export default function(api, options = {}) {
-  const { service, placeholder } = api;
-  const { IMPORT } = placeholder;
-  const { paths } = service;
+  const { service, paths } = api;
 
-  api.register('_modifyConfigPlugins', ({ memo }) => {
-    memo.push(api => {
-      return {
-        name: 'locale',
-        onChange() {
-          api.service.rebuildFiles();
-        },
-      };
+  api.addPageWatcher(
+    join(paths.absSrcPath, service.config.singular ? 'locale' : 'locales'),
+  );
+
+  api.onOptionChange(newOpts => {
+    options = newOpts;
+    api.rebuildTmpFiles();
+  });
+
+  api.addRendererWrapperWithComponent(() => {
+    const localeFileList = getLocaleFileList(
+      paths.absSrcPath,
+      service.config.singular,
+    );
+    const wrapperTpl = readFileSync(
+      join(__dirname, '../template/wrapper.jsx.tpl'),
+      'utf-8',
+    );
+    const defaultLocale = options.defaultLocale || 'zh-CN';
+    const wrapperContent = Mustache.render(wrapperTpl, {
+      localeList: localeFileList,
+      antd: options.antd === undefined ? true : options.antd,
+      baseNavigator:
+        options.baseNavigator === undefined ? true : options.baseNavigator,
+      useLocalStorage: true,
+      defaultLocale,
+      defaultAntdLocale: defaultLocale.replace('-', '_'),
     });
-    return memo;
+    const wrapperPath = join(paths.absTmpDirPath, './LocaleWrapper.jsx');
+    writeFileSync(wrapperPath, wrapperContent, 'utf-8');
+    return wrapperPath;
   });
 
-  api.register('modifyPageWatchers', ({ memo }) => {
-    return [
+  api.modifyAFWebpackOpts(memo => {
+    return {
       ...memo,
-      join(paths.absSrcPath, service.config.singular ? 'locale' : 'locales'),
-    ];
-  });
-
-  api.register('modifyRouterContent', ({ memo }) => {
-    const localeFileList = getLocaleFileList(
-      paths.absSrcPath,
-      service.config.singular,
-    );
-    return getLocaleWrapper(localeFileList, memo, options.antd);
-  });
-
-  api.register('modifyRouterFile', ({ memo }) => {
-    const localeFileList = getLocaleFileList(
-      paths.absSrcPath,
-      service.config.singular,
-    );
-    return memo.replace(
-      IMPORT,
-      `
-          ${getInitCode(
-            localeFileList,
-            options.default,
-            options.baseNavigator,
-            options.antd,
-          )}
-          ${IMPORT}
-        `,
-    );
-  });
-
-  api.register('modifyAFWebpackOpts', ({ memo }) => {
-    memo.alias = {
-      ...(memo.alias || {}),
-      'umi/locale': join(__dirname, './locale.js'),
-      'react-intl': dirname(require.resolve('react-intl/package.json')),
+      alias: {
+        ...(memo.alias || {}),
+        'umi/locale': join(__dirname, './locale.js'),
+        'react-intl': dirname(require.resolve('react-intl/package.json')),
+      },
     };
-    return memo;
   });
-
-  function getLocaleWrapper(localeList, inner, antd = true) {
-    let ret = inner;
-    if (localeList.length) {
-      ret = `<IntlProvider locale={appLocale.locale} messages={appLocale.messages}>
-        <InjectedWrapper>${ret}</InjectedWrapper>
-      </IntlProvider>`;
-    }
-    if (antd) {
-      ret = `<LocaleProvider locale={appLocale.antd || defaultAntd}>
-        ${ret}
-      </LocaleProvider>`;
-    }
-    return ret;
-  }
-
-  function getInitCode(
-    localeList,
-    defaultLocale = 'zh-CN',
-    baseNavigator = true,
-    antd = true,
-    useLocalStorage = true,
-  ) {
-    // 初始化依赖模块的引入
-    // 把 locale 文件夹下的所有的模块的数据添加到 localeInfo 中
-    // 然后按照优先级依次从 localStorage, 浏览器 Navigator（baseNavigator 为 true 时），default 配置中取语言设置
-    // 如果 locale 下没有符合规则的文件，那么也可以仅仅针对 antd 做国际化
-    return `
-      ${
-        localeList.length
-          ? "import { addLocaleData, IntlProvider, injectIntl } from 'react-intl';"
-          : ''
-      }
-      ${localeList.length ? "import { _setIntlObject } from 'umi/locale';" : ''}
-      ${
-        localeList.length
-          ? `const InjectedWrapper = injectIntl(function(props) {
-        _setIntlObject(props.intl);
-        return props.children;
-      })`
-          : ''
-      }
-      const baseNavigator = ${baseNavigator};
-      ${antd ? `import { LocaleProvider } from 'antd';` : ''}
-      ${
-        antd
-          ? `const defaultAntd = require('antd/lib/locale-provider/${defaultLocale.replace(
-              '-',
-              '_',
-            )}');`
-          : ''
-      }
-      const localeInfo = {
-        ${localeList
-          .map(
-            locale => `'${locale.name}': {
-            messages: require('${winPath(locale.path)}').default,
-            locale: '${locale.name}',
-            ${
-              antd
-                ? `antd: require('antd/lib/locale-provider/${locale.lang}_${
-                    locale.country
-                  }'),`
-                : ''
-            }
-            data: require('react-intl/locale-data/${locale.lang}'),
-          }`,
-          )
-          .join(',\n')}
-      };
-      let appLocale = {
-        locale: '${defaultLocale}',
-        messages: {},
-      };
-      if (${useLocalStorage} && localStorage.getItem('umi_locale') && localeInfo[localStorage.getItem('umi_locale')]) {
-        appLocale = localeInfo[localStorage.getItem('umi_locale')];
-      } else if (localeInfo[navigator.language] && baseNavigator){
-        appLocale = localeInfo[navigator.language];
-      } else {
-        appLocale = localeInfo['${defaultLocale}'] || appLocale;
-      }
-      ${
-        localeList.length
-          ? 'appLocale.data && addLocaleData(appLocale.data);'
-          : ''
-      }
-    `;
-  }
 }
---
+++ packages/umi-plugin-locale/template/wrapper.jsx.tpl
@@ -0,0 +1,59 @@
+
+{{#localeList.length}}
+import { addLocaleData, IntlProvider, injectIntl } from 'react-intl';
+import { _setIntlObject } from 'umi/locale';
+
+const InjectedWrapper = injectIntl(function(props) {
+  _setIntlObject(props.intl);
+  return props.children;
+})
+{{/localeList.length}}
+
+const baseNavigator = {{{baseNavigator}}};
+const useLocalStorage = {{{useLocalStorage}}};
+
+{{#antd}}
+import { LocaleProvider } from 'antd';
+const defaultAntd = require('antd/lib/locale-provider/{{defaultAntdLocale}}');
+{{/antd}}
+
+const localeInfo = {
+  {{#localeList}}
+  '{{name}}': {
+    messages: require('{{{path}}}').default,
+    locale: '{{name}}',
+    {{#antd}}antd: require('antd/lib/locale-provider/{{lang}}_{{country}}'),{{/antd}}
+    data: require('react-intl/locale-data/{{lang}}'),
+  },
+  {{/localeList}}
+};
+
+let appLocale = {
+  locale: '{{defaultLocale}}',
+  messages: {},
+};
+if (useLocalStorage && localStorage.getItem('umi_locale') && localeInfo[localStorage.getItem('umi_locale')]) {
+  appLocale = localeInfo[localStorage.getItem('umi_locale')];
+} else if (localeInfo[navigator.language] && baseNavigator){
+  appLocale = localeInfo[navigator.language];
+} else {
+  appLocale = localeInfo['${defaultLocale}'] || appLocale;
+}
+{{#localeList.length}}
+appLocale.data && addLocaleData(appLocale.data);
+{{/localeList.length}}
+
+export default (props) => {
+  let ret = props.children;
+  {{#localeList.length}}
+  ret = (<IntlProvider locale={appLocale.locale} messages={appLocale.messages}>
+    <InjectedWrapper>{ret}</InjectedWrapper>
+  </IntlProvider>)
+  {{/localeList.length}}
+  {{#antd}}
+  ret = (<LocaleProvider locale={appLocale.antd || defaultAntd}>
+    {ret}
+  </LocaleProvider>);
+  {{/antd}}
+  return ret;
+}
