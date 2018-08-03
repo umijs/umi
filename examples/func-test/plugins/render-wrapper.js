400eebac243123d65505944dfa95b34c7f6268b5
愚道 <tingzhao.ytz@antfin.com>
2018-08-03 11:30:00 +0800

    feat: add plugin api addRendererWrapperWithModule

--- examples/func-test/plugins/render-wrapper.js
+++ examples/func-test/plugins/render-wrapper.js
@@ -2,4 +2,5 @@ import { join } from 'path';
 
 export default api => {
   api.addRendererWrapperWithComponent(join(__dirname, './wrapper.js'));
+  api.addRendererWrapperWithModule(join(__dirname, './wrapper-module.js'));
 };
