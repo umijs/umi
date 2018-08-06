7296397040670a24056e19bd3ba537297ac92d6a
愚道 <tingzhao.ytz@antfin.com>
2018-08-06 17:19:06 +0800

    fix: add UMI_DIR when build

--- packages/umi/src/scripts/build.js
+++ packages/umi/src/scripts/build.js
@@ -1,6 +1,8 @@
 import yParser from 'yargs-parser';
+import { dirname } from 'path';
 import buildDevOpts from '../buildDevOpts';
 
+process.env.UMI_DIR = dirname(require.resolve('../../package'));
 process.env.NODE_ENV = 'production';
 
 const argv = yParser(process.argv.slice(2));
