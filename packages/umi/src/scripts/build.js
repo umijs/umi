af8d299e20ac655a1a50977d00f2286191fdf09d
chencheng <sorrycc@gmail.com>
2018-08-14 16:34:12 +0800

    fix(umi-test): webpack alias don't work in umi-test properly

--- packages/umi/src/scripts/build.js
+++ packages/umi/src/scripts/build.js
@@ -1,8 +1,6 @@
 import yParser from 'yargs-parser';
-import { dirname } from 'path';
 import buildDevOpts from '../buildDevOpts';
 
-process.env.UMI_DIR = dirname(require.resolve('../../package'));
 process.env.NODE_ENV = 'production';
 
 const argv = yParser(process.argv.slice(2));
