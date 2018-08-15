f73d7d232ad92f4d29d197e56743ff4efcea0161
chencheng <sorrycc@gmail.com>
2018-08-15 16:56:07 +0800

    feat: generate html files with webpack plugin in umi build

---
+++ packages/umi-build-dev/src/plugins/commands/getHtmlGeneratorPlugin.js
@@ -0,0 +1,21 @@
+import getHtmlGenerator from './getHtmlGenerator';
+import chunksToMap from './build/chunksToMap';
+
+export default service => {
+  return class {
+    apply(compiler) {
+      compiler.hooks.emit.tap('generate-html-files', compilation => {
+        const chunksMap = chunksToMap(compilation.chunks);
+        const hg = getHtmlGenerator(service, {
+          chunksMap,
+        });
+        hg.generate().forEach(({ filePath, content }) => {
+          compilation.assets[filePath] = {
+            source: () => content,
+            size: () => content.length,
+          };
+        });
+      });
+    }
+  };
+};
