bd36f6bbf6b35f8bdb279fb04ea635c80e199e55
诸岳 <fuping.dfp@antfin.com>
2018-08-17 15:16:36 +0800

    feat: Upgrade atool-monitor to report more data

--- packages/umi-build-dev/src/plugins/atoolMonitor.js
+++ packages/umi-build-dev/src/plugins/atoolMonitor.js
@@ -1,3 +1,4 @@
+const { join } = require('path');
 const { reportData, reportBuildData } = require('atool-monitor');
 
 export default function(api, opts = {}) {
@@ -5,16 +6,19 @@ export default function(api, opts = {}) {
   //  上报 app + dev 数据
   api.onDevCompileDone(({ isFirstCompile, stats }) => {
     if (isFirstCompile && process.env.REPEAT_REPORT !== 'none') {
-      // 只在 dev 的第一次编译上报数据
+      // 只在 dev 的第一次编译上报数据 + 避免重复上报
+      // 以 umi 为底层工具的框架上报数据后，umi 这边不再上报
       const appData = {
         reportFrom: 'umi',
-      };
-      const appExtraData = {
         umiVersion,
-        antdVersion: require(require.resolve('antd/package.json')).version,
+        antdVersion: require(join(
+          api.webpackConfig.resolve.alias.antd,
+          'package.json',
+        )).version,
       };
       const devData = {
         reportFrom: 'umi',
+        umiVersion,
         env: {
           NODE_ENV: process.env.NODE_ENV,
           UMI_ENV: process.env.UMI_ENV,
@@ -23,50 +27,49 @@ export default function(api, opts = {}) {
         startTime: stats.startTime,
         endTime: stats.endTime,
       };
-      const devExtraData = {
-        umiVersion,
-      };
-      reportData(appData, appExtraData, devData, devExtraData);
+      reportData({ appData, devData });
     }
   });
 
   // build 成功上报数据
   api.onBuildSuccess(({ stats }) => {
-    const buildData = {
-      reportFrom: 'umi',
-      env: {
-        NODE_ENV: process.env.NODE_ENV,
-        UMI_ENV: process.env.UMI_ENV,
-      },
-      status: 'success',
-      exception: '',
-      errorName: '',
-      startTime: stats.startTime,
-      endTime: stats.endTime,
-    };
-    const extraData = {
-      umiVersion,
-    };
-    reportBuildData(buildData, extraData);
+    if (process.env.REPEAT_REPORT !== 'none') {
+      // 以 umi 为底层工具的框架上报数据后，umi 这边不再上报
+      const buildData = {
+        reportFrom: 'umi',
+        umiVersion,
+        env: {
+          NODE_ENV: process.env.NODE_ENV,
+          UMI_ENV: process.env.UMI_ENV,
+        },
+        status: 'success',
+        exception: '',
+        errorName: '',
+        startTime: stats.startTime,
+        endTime: stats.endTime,
+      };
+      reportBuildData({ buildData });
+    }
   });
 
   // build 失败上报数据
   api.onBuildFail(({ err, stats }) => {
-    const buildData = {
-      reportFrom: 'umi',
-      env: {
-        NODE_ENV: process.env.NODE_ENV,
-        UMI_ENV: process.env.UMI_ENV,
-      },
-      status: 'fail',
-      exception: err instanceof Error ? err.stack : '',
-      errorName: err instanceof Error ? err.name : '',
-      startTime: stats.startTime,
-      endTime: stats.endTime,
-    };
-    const extraData = {
-      umiVersion: process.env.UMI_VERSION,
-    };
-    reportBuildData(buildData, extraData);
+    if (process.env.REPEAT_REPORT !== 'none') {
+      // 以 umi 为底层工具的框架上报数据后，umi 这边不再上报
+      const buildData = {
+        reportFrom: 'umi',
+        umiVersion,
+        env: {
+          NODE_ENV: process.env.NODE_ENV,
+          UMI_ENV: process.env.UMI_ENV,
+        },
+        status: 'fail',
+        exception: err instanceof Error ? err.stack : '',
+        errorName: err instanceof Error ? err.name : '',
+        startTime: stats.startTime,
+        endTime: stats.endTime,
+      };
+      reportBuildData({ buildData });
+    }
   });
 }
