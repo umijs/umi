314fbe8f10223bc2449589cf0c9e54abb3f760e2
黄文鉴 <concefly@foxmail.com>
2018-08-23 18:23:23 +0800

    feat: tsConfigFile path support in af-webpack userConfig (#894)
    af-webpack 支持用户用 tsConfigFile 指定 tsconfig.json 路径

---
+++ packages/af-webpack/src/getUserConfig/configs/tsConfigFile.js
@@ -0,0 +1,13 @@
+import assert from 'assert';
+
+export default function() {
+  return {
+    name: 'tsConfigFile',
+    validate(val) {
+      assert(
+        typeof val === 'string',
+        `The tsConfigFile config must be String, but got ${val}`,
+      );
+    },
+  };
+}
