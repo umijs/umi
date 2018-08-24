2b5ba05b750151786ac45f0b3ee1d782c47d9e44
黄文鉴 <concefly@foxmail.com>
2018-08-24 11:54:54 +0800

    feat: af-webpack 支持 chainConfig 配置 (#901)

---
+++ packages/af-webpack/src/getUserConfig/configs/chainConfig.js
@@ -0,0 +1,13 @@
+import assert from 'assert';
+
+export default function() {
+  return {
+    name: 'chainConfig',
+    validate(val) {
+      assert(
+        typeof val === 'function',
+        `The chainConfig config must be Function, but got ${val}`,
+      );
+    },
+  };
+}
