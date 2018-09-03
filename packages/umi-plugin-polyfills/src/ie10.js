e38064b5a997330071f2761860a0181d7eeb5c02
chencheng <sorrycc@gmail.com>
2018-09-03 19:28:39 +0800

    fix: ie10 don't have Map

--- packages/umi-plugin-polyfills/src/ie10.js
+++ packages/umi-plugin-polyfills/src/ie10.js
@@ -4,5 +4,6 @@ import 'core-js/es6/promise';
 // React depends on set/map/requestAnimationFrame
 // https://reactjs.org/docs/javascript-environment-requirements.html
 import 'core-js/es6/set';
+import 'core-js/es6/map';
 
 import 'url-polyfill';
