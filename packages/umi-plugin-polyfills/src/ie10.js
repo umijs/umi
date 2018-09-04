0a2c6416ee788c1ed04b2f677a0329f98be78d50
chencheng (云谦) <sorrycc@gmail.com>
2018-09-04 10:30:37 +0800

    feat: add Object polyfills for ie (#988)

--- packages/umi-plugin-polyfills/src/ie10.js
+++ packages/umi-plugin-polyfills/src/ie10.js
@@ -5,5 +5,6 @@ import 'core-js/es6/promise';
 // https://reactjs.org/docs/javascript-environment-requirements.html
 import 'core-js/es6/set';
 import 'core-js/es6/map';
+import 'core-js/es6/object';
 
 import 'url-polyfill';
