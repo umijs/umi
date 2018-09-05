3a2f764a60b12cff52e1d0171ba944aa959d6054
chencheng <sorrycc@gmail.com>
2018-09-05 16:14:04 +0800

    update

--- packages/umi-plugin-routes/src/index.js
+++ packages/umi-plugin-routes/src/index.js
@@ -13,12 +13,10 @@ function optsToArray(item) {
 export default function(api, opts) {
   api.onOptionChange(newOpts => {
     opts = newOpts;
-    console.log('set new opts', newOpts);
     api.rebuildTmpFiles();
   });
 
   api.modifyRoutes(routes => {
-    console.log('do modifyRoutes');
     routes = exclude(routes, optsToArray(opts.exclude));
 
     if (opts.update) {
