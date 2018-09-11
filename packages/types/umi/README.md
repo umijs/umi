72944b453d615ab52f82c3598bb473e7721dea44
Jetsly <lingjie.li@huijie-inc.com>
2018-09-11 11:03:34 +0800

    add types file to umijs

---
+++ packages/types/umi/README.md
@@ -0,0 +1,11 @@
+# Installation
+> `npm install --save @types/umi`
+
+# Summary
+This package contains type definitions for umijs (https://github.com/umijs/umi).
+
+# Details
+Files were exported from 
+https://github.com/umijs/umi/tree/master/types/umi
+
+
--- packages/types/umi/locale.d.ts
+++ packages/types/umi/locale.d.ts
@@ -1,5 +1,5 @@
 import * as React from 'react';
- type formatValues = { [key: string]: string | number }
+type formatValues = { [key: string]: string | number }
 declare const formatMessage: ({ id }: { id: string }, values?: formatValues) => string;
 declare const getLocale: () => string;
 declare const setLocale: (lang: string) => string;
