047464ba74a85f2e385d621b7806a0d4d8145ec1
chencheng <sorrycc@gmail.com>
2018-09-18 11:27:47 +0800

    feat: support config scripts, headScripts, metas and links

--- packages/umi-plugin-react/test/normal.e2e.js
+++ packages/umi-plugin-react/test/normal.e2e.js
@@ -43,6 +43,22 @@ describe('normal', () => {
       () => document.querySelector('title').innerHTML,
     );
     expect(titleText).toEqual('默认标题');
+
+    // scripts
+    const scripts = await page.evaluate(() => window.scripts);
+    expect(scripts).toEqual(['headScript1', 'script1', 'script2']);
+
+    // links
+    const link = await page.evaluate(() =>
+      document.querySelector('#link1').getAttribute('foo'),
+    );
+    expect(link).toEqual('bar');
+
+    // metas
+    const meta = await page.evaluate(() =>
+      document.querySelector('#meta1').getAttribute('foo'),
+    );
+    expect(meta).toEqual('bar');
   });
 
   it('a page', async () => {
