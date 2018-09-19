d9a16a0844451ccf71800a82c937188cbded3a17
chencheng <sorrycc@gmail.com>
2018-09-19 17:01:02 +0800

    fix: service.__chunks may be not ready

--- packages/umi-build-dev/src/plugins/commands/dev/createRouteMiddleware.js
+++ packages/umi-build-dev/src/plugins/commands/dev/createRouteMiddleware.js
@@ -5,10 +5,11 @@ export default function createRouteMiddleware(service) {
   return (req, res) => {
     const { path } = req;
 
-    if (path === '/__umiDev/routes') {
-      res.setHeader('Content-Type', 'text/json');
-      res.send(JSON.stringify(service.routes));
-    } else {
+    function sendHtml() {
+      if (!service.__chunks) {
+        setTimeout(sendHtml, 300);
+        return;
+      }
       const chunksMap = chunksToMap(service.__chunks);
       const htmlGenerator = getHtmlGenerator(service, {
         chunksMap,
@@ -16,6 +17,13 @@ export default function createRouteMiddleware(service) {
       const content = htmlGenerator.getMatchedContent(path);
       res.setHeader('Content-Type', 'text/html');
       res.send(content);
+    }
+
+    if (path === '/__umiDev/routes') {
+      res.setHeader('Content-Type', 'text/json');
+      res.send(JSON.stringify(service.routes));
+    } else {
+      sendHtml();
     }
   };
 }
