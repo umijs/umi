ed186958ab60fa8c291a201165ed9fe5bea5961f
chencheng <sorrycc@gmail.com>
2018-09-19 14:20:03 +0800

    add testcase for runtimePlugin

--- packages/umi/src/runtimePlugin.js
+++ packages/umi/src/runtimePlugin.js
@@ -1,11 +1,12 @@
 import assert from 'assert';
+import isPlainObject from 'is-plain-object';
 
 let plugins = null;
 let validKeys = [];
 
 export function init(opts = {}) {
   plugins = [];
-  validKeys = opts.validKeys;
+  validKeys = opts.validKeys || [];
 }
 
 export function use(plugin) {
@@ -16,6 +17,7 @@ export function use(plugin) {
 }
 
 export function getItem(key) {
+  assert(validKeys.includes(key), `Invalid key ${key}`);
   return plugins.filter(plugin => key in plugin).map(plugin => plugin[key]);
 }
 
@@ -26,13 +28,13 @@ function _compose(...funcs) {
   if (funcs.length === 1) {
     return funcs[0];
   }
-  return funcs.reduce((a, b) => (...args) => a(b(...args)));
+  return funcs.reduceRight((a, b) => (...args) => a(b(...args)));
 }
 
 export function compose(item, { initialValue }) {
   if (typeof item === 'string') item = getItem(item);
   return () => {
-    _compose(...item)(initialValue);
+    return _compose(...item)(initialValue);
   };
 }
 
@@ -58,11 +60,8 @@ export function applyForEach(item, { initialValue }) {
 export function mergeConfig(item) {
   if (typeof item === 'string') item = getItem(item);
   assert(Array.isArray(item), `item must be Array`);
-  return item.reduce(
-    item,
-    (memo, config) => {
-      return { ...memo, ...config };
-    },
-    {},
-  );
+  return item.reduce((memo, config) => {
+    assert(isPlainObject(config), `Config is not plain object`);
+    return { ...memo, ...config };
+  }, {});
 }
