0729aac7f4adefee7d20e1a57d9e6546be690021
chencheng <sorrycc@gmail.com>
2018-09-23 10:02:45 +0800

    add docs

--- packages/umi-build-dev/src/plugins/targets.js
+++ packages/umi-build-dev/src/plugins/targets.js
@@ -2,6 +2,7 @@ import { join, relative } from 'path';
 import { writeFileSync, readFileSync } from 'fs';
 import Mustache from 'mustache';
 import assert from 'assert';
+import mkdirp from 'mkdirp';
 import isPlainObject from 'is-plain-object';
 
 export default function(api) {
@@ -25,8 +26,9 @@ export default function(api) {
   });
 
   function writeTmpFile() {
+    console.log('write tmp file');
     const tpl = readFileSync(
-      join(__dirname, '../../../template/polyfills.js.tpl'),
+      join(__dirname, '../../template/polyfills.js.tpl'),
       'utf-8',
     );
     const result = Mustache.render(tpl, {
@@ -38,6 +40,8 @@ export default function(api) {
         relative(paths.absTmpDirPath, require.resolve('url-polyfill')),
       ),
     });
+    console.log(2, join(paths.absTmpDirPath, 'polyfills.js'), result);
+    mkdirp.sync(paths.absTmpDirPath);
     writeFileSync(join(paths.absTmpDirPath, 'polyfills.js'), result, 'utf-8');
   }
   writeTmpFile();
