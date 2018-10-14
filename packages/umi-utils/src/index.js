e08edf30f51d8f30a95da78ca2b177b5d95991b4
xiaohuoni <448627663@qq.com>
2018-10-14 13:10:59 +0800

    umi-utils add chunkName endWithSlash optsToArray

--- packages/umi-utils/src/index.js
+++ packages/umi-utils/src/index.js
@@ -1,3 +1,6 @@
 export { default as findJS } from './findJS';
 export { default as compatDirname } from './compatDirname';
 export { default as winPath } from './winPath';
+export { default as optsToArray } from './optsToArray';
+export { default as chunkName } from './chunkName';
+export { default as endWithSlash } from './endWithSlash';
