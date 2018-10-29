eac3527250aeefba75c293810a7c63549bd49ae5
xiaohu <448627663@qq.com>
2018-10-29 20:35:00 +0800

    location.origin is undefined [ie10] Close:#1179

--- packages/umi-build-dev/src/html/HTMLGenerator.js
+++ packages/umi-build-dev/src/html/HTMLGenerator.js
@@ -202,7 +202,7 @@ export default class HTMLGenerator {
 
   getContent(route) {
     const { cwd } = this.paths;
-    const { exportStatic, runtimePublicPath } = this.config;
+    const { exportStatic, runtimePublicPath, targets } = this.config;
 
     let context = {
       route,
@@ -274,6 +274,11 @@ export default class HTMLGenerator {
       runtimePublicPath || (exportStatic && exportStatic.dynamicRoot);
     headScripts.push({
       content: [
+        ...(targets && targets.ie && targets.ie <= 10
+          ? [
+              `if (window["context"] == undefined) {if (!window.location.origin) {window.location.origin = window.location.protocol + "//" + window.location.hostname + (window.location.port ? ':' + window.location.port: '');}window["context"] = location.origin+"/V6.0";}`,
+            ]
+          : []),
         `window.routerBase = ${routerBaseStr};`,
         ...(setPublicPath ? [`window.publicPath = ${publicPathStr};`] : []),
       ].join('\n'),
