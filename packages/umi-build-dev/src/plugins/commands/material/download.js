5279c0ada008a83613a7d3239445c3a4e4926160
愚道 <tingzhao.ytz@antfin.com>
2018-11-02 18:09:55 +0800

    import user-home

--- packages/umi-build-dev/src/plugins/commands/material/download.js
+++ packages/umi-build-dev/src/plugins/commands/material/download.js
@@ -1,8 +1,32 @@
 import { join } from 'path';
+import { existsSync } from 'fs';
+import { spawn } from 'child_process';
 
-export function downloadNpmPackage(url, log) {}
+const userHome = require('user-home');
+const materailsTempPath = join(userHome, '.umi-materials');
 
-export function downloadFromGit(url, branch = 'master', log) {}
+export function downloadNpmPackage(url, log) {
+  // TODO support
+  log(`${url} is a npm package, not support now.`);
+}
+
+export function getDirNameByUrl(url) {
+  return url.replace(/\//g);
+}
+
+export function downloadFromGit(url, branch = 'master', log) {
+  const dirName = getDirNameByUrl(url);
+  const templateTmpDirPath = join(materailsTempPath, dirName);
+  if (existsSync(templateTmpDirPath)) {
+    // git repo already exist, pull it
+    // cd dirName && git pull
+  } else {
+    // new git repo, clone
+    // git clone url dirName
+  }
+  // git checkout branch
+  // TODO
+}
 
 export function isNpmPackage(url) {
   return /^@?([\w\-]+\/?)+$/.test(url);
@@ -12,13 +36,12 @@ export function isGitUrl(url) {
   return /\.git$/.test(url);
 }
 
-const gitSiteParser = /^(https?)\:\/\/((github|gitlab)[\.\w\-]+)\/([\w\-]+)\/([\w\-]+)(\/tree\/([\w\.\-]+)([\w\-\/]+))?$/;
-// test is a github or gitlab url
 // git site url maybe like: http://gitlab.alitest-inc.com/bigfish/bigfish-materials/tree/master/demo
 // or http://gitlab.alitest-inc.com/bigfish/testmaterials/tree/master
 // or http://gitlab.alitest-inc.com/bigfish/testmaterials
 // or https://github.com/umijs/umi-materials/tree/master/demo
 // or https://github.com/alibaba/ice/tree/master/react-materials/blocks/AbilityIntroduction
+const gitSiteParser = /^(https?)\:\/\/((github|gitlab)[\.\w\-]+)\/([\w\-]+)\/([\w\-]+)(\/tree\/([\w\.\-]+)([\w\-\/]+))?$/;
 export function isGitSiteUrl(url) {
   return gitSiteParser.test(url);
 }
@@ -26,13 +49,16 @@ export function isGitSiteUrl(url) {
 export function parseGitSiteUrl(url) {
   // (http|s)://(host)/(group)/(name)/tree/(branch)/(path)
   const [
-    ,
+    // eslint-disable-next-line
+    all,
     protocol,
     host,
-    ,
+    // eslint-disable-next-line
+    site,
     group,
     name,
-    ,
+    // eslint-disable-next-line
+    allpath,
     branch = 'master',
     path = '/',
   ] = gitSiteParser.exec(url);
--- packages/umi-build-dev/src/plugins/commands/material/material.js
+++ packages/umi-build-dev/src/plugins/commands/material/material.js
@@ -7,11 +7,12 @@ export default api => {
 
   return class Generator extends api.Generator {
     writing() {
-      const path = this.args[0].toString();
-      const name = basename(path);
-      this.fs.copyTpl(join(paths.absPagesPath, `${path}.js`), {
-        name,
-      });
+      // TODO
+      // const path = this.args[0].toString();
+      // const name = basename(path);
+      // this.fs.copyTpl(join(paths.absPagesPath, `${path}.js`), {
+      //   name,
+      // });
     }
   };
 };
