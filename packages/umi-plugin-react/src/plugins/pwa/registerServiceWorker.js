388d06779e2dc0bf06b297784235d58613969815
xiaoiver <pyqiverson@sina.com>
2018-11-09 10:40:50 +0800

    feat: put registration into event data (#1434)

--- packages/umi-plugin-react/src/plugins/pwa/registerServiceWorker.js
+++ packages/umi-plugin-react/src/plugins/pwa/registerServiceWorker.js
@@ -1,19 +1,39 @@
 import { register } from 'register-service-worker';
 
-function dispathServiceWorkerEvent(eventName) {
-  const event = document.createEvent('Event');
-  event.initEvent(eventName, true, true);
+// polyfill the CustomEvent in ie9/10/11
+// https://developer.mozilla.org/en-US/docs/Web/API/CustomEvent/CustomEvent#Polyfill
+(function() {
+  if (typeof window.CustomEvent === 'function') return false;
+
+  function CustomEvent(event, params) {
+    params = params || { bubbles: false, cancelable: false, detail: undefined };
+    var evt = document.createEvent('CustomEvent');
+    evt.initCustomEvent(
+      event,
+      params.bubbles,
+      params.cancelable,
+      params.detail,
+    );
+    return evt;
+  }
+
+  CustomEvent.prototype = window.Event.prototype;
+  window.CustomEvent = CustomEvent;
+})();
+
+function dispatchServiceWorkerEvent(eventName, eventData) {
+  const event = new CustomEvent(eventName, { detail: eventData });
   window.dispatchEvent(event);
 }
 
 export default function(swDest) {
   register(`${process.env.BASE_URL}${swDest}`, {
-    updated() {
-      dispathServiceWorkerEvent('sw.updated');
+    updated(registration) {
+      dispatchServiceWorkerEvent('sw.updated', registration);
     },
 
     offline() {
-      dispathServiceWorkerEvent('sw.offline');
+      dispatchServiceWorkerEvent('sw.offline', {});
     },
   });
 }
