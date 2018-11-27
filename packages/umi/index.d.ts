e112144333c9464583163623f9032036c276bfea
amily4555 <amily4555@gmail.com>
2018-11-27 20:03:14 +0800

    remove .idea in .gitignore

--- packages/umi/index.d.ts
+++ packages/umi/index.d.ts
@@ -2,6 +2,6 @@ export {default as Link} from './link';
 export {default as NavLink} from './navlink';
 export {default as Redirect} from './redirect';
 
-export {default as Dynamic} from './dynamic';
+export {default as dynamic} from './dynamic';
 export {default as router} from './router';
 export {default as withRouter} from './withRouter';
