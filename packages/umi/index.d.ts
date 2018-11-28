0fad35cbb881a63efcb54a59b91b786c2038877f
amily4555 <amily4555@gmail.com>
2018-11-28 13:47:55 +0800

    ts 调用umi组件或方法的时候更加友好

---
+++ packages/umi/index.d.ts
@@ -0,0 +1,7 @@
+export {default as Link} from './link';
+export {default as NavLink} from './navlink';
+export {default as Redirect} from './redirect';
+
+export {default as dynamic} from './dynamic';
+export {default as router} from './router';
+export {default as withRouter} from './withRouter';
