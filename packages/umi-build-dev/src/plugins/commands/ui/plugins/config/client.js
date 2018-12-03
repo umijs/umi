258536da75ba1881c2775bfd096c0a0b30290b67
chencheng (云谦) <sorrycc@gmail.com>
2018-12-03 16:38:40 +0800

    feat(umi ui): support config (#1553)
    Close #1569

--- packages/umi-build-dev/src/plugins/commands/ui/plugins/config/client.js
+++ packages/umi-build-dev/src/plugins/commands/ui/plugins/config/client.js
@@ -1,12 +1,68 @@
 import React from 'react';
 import { connect } from 'dva';
 
+function toString(data) {
+  if (typeof data === 'string') return data;
+  else if (typeof data === 'object') return JSON.stringify(data);
+  else if (typeof data === 'boolean') return JSON.stringify(data);
+  else throw new Error(`unsupport data type: ${typeof data}`);
+}
+
+function ConfigItem(props) {
+  return (
+    <>
+      <li>
+        {props.name}
+        {do {
+          if (props.name === 'plugins') {
+            <PluginList data={props.data} />;
+          } else {
+            <ConfigPropertyItem {...props} />;
+          }
+        }}
+      </li>
+    </>
+  );
+}
+
+function ConfigPropertyItem({ name, data }) {
+  function blurHandler(e) {
+    window.send('config', ['set', name, `${e.target.value}`]);
+  }
+  return <input defaultValue={toString(data)} onBlur={blurHandler} />;
+}
+
+function PluginList(props) {
+  return (
+    <ul>
+      {props.data.map((item, i) => (
+        <PluginItem key={i} data={item} />
+      ))}
+    </ul>
+  );
+}
+
+function PluginItem(props) {
+  const [name, opts] = Array.isArray(props.data) ? props.data : [props.data];
+  return (
+    <li>
+      {name}
+      {opts ? <input defaultValue={JSON.stringify(opts)} /> : ''}
+    </li>
+  );
+}
+
 const ConfigManager = connect(state => ({
   config: state.config,
 }))(props => {
   return (
     <div>
       <h3>config mangager page</h3>
+      <ul>
+        {Object.keys(props.config.data).map(key => (
+          <ConfigItem key={key} name={key} data={props.config.data[key]} />
+        ))}
+      </ul>
     </div>
   );
 });
