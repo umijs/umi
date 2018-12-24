5c0c988cfdee76e4a3977c5bb07fdce2fddac432
Yu <yutingzhao1991@sina.com>
2018-12-24 13:06:50 +0800

    fix proxy when there are some proxy has same prefix (#1699)

--- packages/umi/test/normal/mock/users.js
+++ packages/umi/test/normal/mock/users.js
@@ -2,4 +2,6 @@ export default {
   'GET /api/users': (req, res) => {
     res.json({ name: 'cc' });
   },
+  'GET /api/proxytest': { data: 'proxytest' },
+  'GET /api/proxytest1': { data: 'proxytest1' },
 };
