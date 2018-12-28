d39567c1b80bb87546d76bdeb165f707a1f569b4
imhele <work@imhele.com>
2018-12-28 13:57:47 +0800

    fix umi-plugin-locale index.d.ts

--- packages/umi-plugin-locale/index.d.ts
+++ packages/umi-plugin-locale/index.d.ts
@@ -1,3 +1,4 @@
+import * as React from 'react';
 declare function setLocale(lang: string): void;
 declare function getLocale(): string;
 interface Params {
@@ -5,6 +6,6 @@ interface Params {
   defaultMessage?: string;
 }
 declare function formatMessage(params: Params, data?: Object): void;
-declare const FormattedMessage: React.Component;
+declare class FormattedMessage extends React.Component<Params> { }
 
 export { setLocale, getLocale, formatMessage, FormattedMessage };
