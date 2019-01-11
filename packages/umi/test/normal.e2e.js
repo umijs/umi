26d14eadd7cccc749dbe0450cc9df9374664e87d
chencheng (云谦) <sorrycc@gmail.com>
2019-01-11 09:04:41 +0800

    chore: add testcase for mock #1791 (#1806)

--- packages/umi/test/normal.e2e.js
+++ packages/umi/test/normal.e2e.js
@@ -1,5 +1,6 @@
 import puppeteer from 'puppeteer';
 import got from 'got';
+import FormData from 'form-data';
 
 describe('normal', () => {
   let browser;
@@ -67,8 +68,72 @@ describe('normal', () => {
   });
 
   it('mock', async () => {
-    const res = await got(`http://localhost:${port}/api/users`);
+    let res;
+
+    // GET
+    res = await got(`http://localhost:${port}/api/users`);
     expect(res.body).toEqual('{"name":"cc"}');
+
+    // POST
+    res = await got(`http://localhost:${port}/api/users`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: '{"a":"b"}',
+    });
+    expect(res.body).toEqual('{"body":{"a":"b"}}');
+
+    // POST with form
+    res = await got(`http://localhost:${port}/api/users`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+      },
+      body: 'a=b',
+      form: true,
+    });
+    expect(res.body).toEqual('{"body":{"a":"b"}}');
+
+    // POST with form-data (multipart)
+    const form = new FormData();
+    form.append('foo', 'bar');
+    form.append('ccc', 'ddd');
+    res = await got(`http://localhost:${port}/api/users`, {
+      method: 'POST',
+      body: form,
+    });
+    expect(res.body).toEqual('{"body":{"foo":"bar","ccc":"ddd"}}');
+
+    // PUT
+    res = await got(`http://localhost:${port}/api/users`, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: '{"a":"b"}',
+    });
+    expect(res.body).toEqual('{"body":{"a":"b"}}');
+
+    // DELETE
+    res = await got(`http://localhost:${port}/api/users`, {
+      method: 'DELETE',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: '{"a":"b"}',
+    });
+    expect(res.body).toEqual('{"body":{"a":"b"}}');
+
+    // PATCH
+    res = await got(`http://localhost:${port}/api/users`, {
+      method: 'PATCH',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: '{"a":"b"}',
+    });
+    expect(res.body).toEqual('{"body":{"a":"b"}}');
   });
 
   it('use proxy when not find mock', async () => {
