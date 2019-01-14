6cd10d07ac06cc228c7e809d3d666c276dd3560c
chencheng (云谦) <sorrycc@gmail.com>
2019-01-14 21:30:12 +0800

    feat: expose routes as window.g_routes (#1840)

--- packages/umi/test/fixtures/build/export-static-htmlSuffix/expected/umi.js
+++ packages/umi/test/fixtures/build/export-static-htmlSuffix/expected/umi.js
@@ -2019,6 +2019,7 @@ var routes = [{
   "exact": true,
   "component": __webpack_require__(39).default
 }];
+window.g_routes = routes;
 window.g_plugins.applyForEach('patchRoutes', {
   initialValue: routes
 });
--- packages/umi/test/fixtures/build/simple/expected/umi.js
+++ packages/umi/test/fixtures/build/simple/expected/umi.js
@@ -2003,6 +2003,7 @@ var routes = [{
   "exact": true,
   "component": __webpack_require__(38).default
 }];
+window.g_routes = routes;
 window.g_plugins.applyForEach('patchRoutes', {
   initialValue: routes
 });
