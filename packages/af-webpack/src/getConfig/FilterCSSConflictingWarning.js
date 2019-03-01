d2a4f0111dce5186a6df5ede80c96a9e82c91c27
kale-io <cos+a@iuvo.ai>
2019-03-01 21:59:39 +0000

    Convert checks for missing indexOf to use includes ECMAScript 7 introduced the includes function for arrays so that bitwise and comparisons to -1 are no longer needed

--- packages/af-webpack/src/getConfig/FilterCSSConflictingWarning.js
+++ packages/af-webpack/src/getConfig/FilterCSSConflictingWarning.js
@@ -2,7 +2,7 @@ class FilterCSSConflictingWarning {
   apply(compiler) {
     compiler.hooks.afterEmit.tap('FilterWarning', compilation => {
       compilation.warnings = (compilation.warnings || []).filter(warning => {
-        return warning.message.indexOf('Conflicting order between:') === -1;
+        return !warning.message.includes('Conflicting order between:');
       });
     });
   }
