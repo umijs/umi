03ab0787614886f662c687f5bad0a0ab32f3543a
ycjcl868 <45808948@qq.com>
2019-03-16 15:06:56 +0800

    feat: add umi router types

--- packages/umi/withRouter.d.ts
+++ packages/umi/withRouter.d.ts
@@ -1,2 +1,13 @@
-import { withRouter } from 'react-router-dom';
+import { withRouter, RouteComponentProps, RouteProps, match } from 'react-router-dom';
+
+interface RouteTypes extends Pick<RouteProps, 'component' | 'exact' | 'path'> {
+  _title?: string;
+  _title_default?: string;
+}
+
+export interface RouterTypes extends RouteComponentProps {
+  computedMatch?: match;
+  route?: RouteTypes;
+}
+
 export default withRouter;
