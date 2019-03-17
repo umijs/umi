e6fde9e6429758b6837a1a7eb86d7b8671821279
ycjcl868 <45808948@qq.com>
2019-03-17 08:39:50 +0800

    feat: routerTypes

--- packages/umi/index.d.ts
+++ packages/umi/index.d.ts
@@ -1,5 +1,3 @@
-import { withRouter, RouteComponentProps, RouteProps, match } from 'react-router-dom';
-
 export { default as Link } from './link';
 export { default as NavLink } from './navlink';
 export { default as Redirect } from './redirect';
@@ -8,14 +6,4 @@ export { default as dynamic } from './dynamic';
 export { default as router } from './router';
 export { default as withRouter } from './withRouter';
 
-type IncludeRoute = 'component' | 'exact' | 'path';
-
-interface RouteType extends Pick<RouteProps, IncludeRoute> {
-  _title?: string;
-  _title_default?: string;
-}
-
-export interface RouterTypes extends RouteComponentProps {
-  computedMatch?: match;
-  route?: RouteType;
-}
+export { default as RouterTypes } from './routerTypes';
---
+++ packages/umi/routerTypes.d.ts
@@ -0,0 +1,13 @@
+import { withRouter, RouteComponentProps, RouteProps, match } from 'react-router-dom';
+
+type IncludeRoute = 'component' | 'exact' | 'path';
+
+interface RouteType extends Pick<RouteProps, IncludeRoute> {
+  _title?: string;
+  _title_default?: string;
+}
+
+export interface RouterTypes extends RouteComponentProps {
+  computedMatch?: match;
+  route?: RouteType;
+}
