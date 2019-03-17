48cf46b51fdce281247b3533353393da1e21f3a0
ycjcl868 <45808948@qq.com>
2019-03-17 13:43:02 +0800

    alias: basic router props

--- packages/umi/routerTypes.d.ts
+++ packages/umi/routerTypes.d.ts
@@ -1,10 +1,10 @@
-import { RouteComponentProps, RouteProps, match } from 'react-router-dom';
+import { RouteComponentProps as BasicRouteProps, RouteProps, match } from 'react-router-dom';
 
 type IncludeRoute = 'component' | 'exact' | 'path';
 
 type RouteType = Pick<RouteProps, IncludeRoute>;
 
-export default interface RouterTypes<T extends Object = {}> extends RouteComponentProps {
+export default interface RouterTypes<T extends Object = {}> extends BasicRouteProps {
   computedMatch?: match;
   route?: RouteType & T;
 }
