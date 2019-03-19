2b73a4da3da2319d7a8e25aaa1e39a5f0db7bc9c
chencheng <sorrycc@gmail.com>
2019-03-19 22:35:38 +0800

    fix ci

--- packages/umi-library/src/fixtures/e2e/normal/expected/index.esm.js
+++ packages/umi-library/src/fixtures/e2e/normal/expected/index.esm.js
@@ -1,12 +1,10 @@
 import React from 'react';
 
 function styleInject(css, ref) {
-  if (ref === void 0) ref = {};
+  if ( ref === void 0 ) ref = {};
   var insertAt = ref.insertAt;
 
-  if (!css || typeof document === 'undefined') {
-    return;
-  }
+  if (!css || typeof document === 'undefined') { return; }
 
   var head = document.head || document.getElementsByTagName('head')[0];
   var style = document.createElement('style');
