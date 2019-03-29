0fd314cb5a35fdc45c3067783c0d68e0870d269e
信鑫-King <45808948@qq.com>
2019-03-30 00:36:41 +0800

    Update packages/umi/routerTypes.d.ts
    Co-Authored-By: imhele <work@imhele.com>

--- packages/umi/routerTypes.d.ts
+++ packages/umi/routerTypes.d.ts
@@ -8,7 +8,7 @@ type IncludeRoute = 'component' | 'exact' | 'path';
 
 type RouteType = Pick<RouteProps, IncludeRoute>;
 
-export default interface RouterTypes<T extends Object = {}, P extends { [K in keyof P]?: string } = {}>
+export default interface RouterTypes<T extends Object = {}, P = {}>
   extends BasicRouteProps {
   computedMatch?: match<P>;
   route?: RouteType & T;
