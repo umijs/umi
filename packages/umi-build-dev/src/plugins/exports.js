cdace63d99754d0055ba9cadc6adfc5eaa35aa74
xiaohuoni <448627663@qq.com>
2019-05-08 16:25:01 +0800

    暂存

--- packages/umi-build-dev/src/plugins/exports.js
+++ packages/umi-build-dev/src/plugins/exports.js
@@ -5,6 +5,50 @@ const reserveLibrarys = ['umi'];
 const reserveExportsNames = ['umi'];
 const umiExportsHook = {};
 
+export const generateStringsByConfiguration = item => {
+  assert(item.source, 'source should be supplied.');
+  assert(
+    item.exportAll || item.specifiers,
+    'exportAll or specifiers should be supplied.',
+  );
+  assert(
+    !reserveLibrarys.includes(item.source),
+    `${item.source} is reserve library, Please don't use it.`,
+  );
+  if (item.exportAll) {
+    return `export * from '${item.source}';`;
+  }
+  assert(
+    Array.isArray(item.specifiers),
+    `specifiers should be Array, but got ${item.specifiers.toString()}.`,
+  );
+  const specifiersStrArr = item.specifiers.map(specifier => {
+    if (typeof specifier === 'string') {
+      assert(
+        !reserveExportsNames.includes(specifier),
+        `${specifier} is reserve name, you can use 'exported' to set alias`,
+      );
+      assert(
+        !umiExportsHook[specifier],
+        `${specifier} is Defined, you can use 'exported' to set alias`,
+      );
+      umiExportsHook[specifier] = true;
+      return specifier;
+    } else {
+      assert(
+        isPlainObject(specifier),
+        `Configure item context should be Plain Object, but got ${specifier}.`,
+      );
+      assert(
+        specifier.local && specifier.exported,
+        'local and exported should be supplied.',
+      );
+      return `${specifier.local} as ${specifier.exported}`;
+    }
+  });
+  return `export { ${specifiersStrArr.join(',')} } from '${item.source}';`;
+};
+
 export default function(api) {
   api.onGenerateFiles(() => {
     generateUmiExports();
@@ -17,49 +61,5 @@ export default function(api) {
       })
       .map(generateStringsByConfiguration);
     api.writeTmpFile('umiExports.js', umiExports.join('\n'));
-  };
-
-  const generateStringsByConfiguration = item => {
-    assert(item.source, 'source should be supplied.');
-    assert(
-      item.exportAll || item.specifiers,
-      'exportAll or specifiers should be supplied.',
-    );
-    assert(
-      !reserveLibrarys.includes(item.source),
-      `${item.source} is reserve library, Please don't use it.`,
-    );
-    if (item.exportAll) {
-      return `export * from '${item.source}';`;
-    }
-    assert(
-      Array.isArray(item.specifiers),
-      `specifiers should be Array, but got ${item.specifiers.toString()}.`,
-    );
-    const specifiersStrArr = item.specifiers.map(specifier => {
-      if (typeof specifier === 'string') {
-        assert(
-          !reserveExportsNames.includes(specifier),
-          `${specifier} is reserve name, you can use 'exported' to set alias`,
-        );
-        assert(
-          !umiExportsHook[specifier],
-          `${specifier} is Defined, you can use 'exported' to set alias`,
-        );
-        umiExportsHook[specifier] = true;
-        return specifier;
-      } else {
-        assert(
-          isPlainObject(specifier),
-          `Configure item context should be Plain Object, but got ${specifier}.`,
-        );
-        assert(
-          specifier.local && specifier.exported,
-          'local and exported should be supplied.',
-        );
-        return `${specifier.local} as ${specifier.exported}`;
-      }
-    });
-    return `export { ${specifiersStrArr.join(',')} } from '${item.source}';`;
   };
 }
