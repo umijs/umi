b5df6f297ea8c4b79148ee58f6a26ee79de7ed71
信鑫-King <45808948@qq.com>
2019-05-26 08:27:33 +0800

    feat: locale intellisense (#2469)
    * feat: locale intellisense
    
    * fix: ternary expression judge type
    
    * feat: judge String Literal Type and string
    
    * fix: use string
    
    * fix: use @ts-ignore suppress tsc error
    
    * fix: use @ alias

--- packages/umi-plugin-locale/index.d.ts
+++ packages/umi-plugin-locale/index.d.ts
@@ -5,14 +5,20 @@
  *   - expose interfaces from `IntlShape`
  */
 import * as React from 'react';
+// @ts-ignore
+import locales_zh_CN from '@/locales/zh-CN';
+// @ts-ignore
+import locale_zh_CN from '@/locale/zh-CN';
 // Basic types and interfaces
 export declare type DateSource = Date | string | number;
 export declare type MessageValue = string | number | boolean | Date | null | undefined;
 export declare interface DateTimeFormatProps extends Intl.DateTimeFormatOptions {
   format?: string;
 }
+
+type zh_CN = (keyof typeof locales_zh_CN) & (keyof typeof locale_zh_CN);
 export interface MessageDescriptor {
-  id: string;
+  id: zh_CN extends string ? zh_CN : string;
   description?: string;
   defaultMessage?: string;
 }
