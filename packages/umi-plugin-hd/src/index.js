cc48d743b868ef63c29c906fc3b987f16c5c13c8
chencheng (云谦) <sorrycc@gmail.com>
2019-05-29 17:16:18 +0800

    fix(umi-plugin-hd): theme failed with theme file (#2498)

--- packages/umi-plugin-hd/src/index.js
+++ packages/umi-plugin-hd/src/index.js
@@ -2,11 +2,20 @@ import px2rem from 'postcss-plugin-px2rem';
 import { join, relative } from 'path';
 
 export default function(api, options = {}) {
-  const { paths, findJS } = api;
+  const { paths, cwd, findJS } = api;
+
+  function getThemeConfig(theme) {
+    // theme may be a string point to file with exported function
+    if (typeof theme === 'string') {
+      return require(join(cwd, theme))(); // eslint-disable-line
+    } else {
+      return theme || {};
+    }
+  }
 
   api.modifyAFWebpackOpts(opts => {
     opts.theme = {
-      ...(opts.theme || {}),
+      ...getThemeConfig(opts.theme),
       '@hd': '2px',
       ...(options.theme || {}),
     };
