d3f6e40bf66e54b6435ef20f054b414746814f13
chencheng <sorrycc@gmail.com>
2019-06-03 15:44:26 +0800

    chore: fix build script

--- packages/umi-plugin-react/.fatherrc.js
+++ packages/umi-plugin-react/.fatherrc.js
@@ -2,4 +2,5 @@ export default {
   target: 'node',
   cjs: 'babel',
   disableTypeCheck: true,
+  browserFiles: ['src/plugins/pwa/registerServiceWorker.js', 'src/plugins/title/TitleWrapper.js'],
 };
