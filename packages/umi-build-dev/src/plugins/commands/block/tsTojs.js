86dd2e198a6e8aa3e3c4b1e7846a9ea65049c83f
陈帅 <qixian.cs@outlook.com>
2019-06-08 00:04:02 +0800

    add ts to js

---
+++ packages/umi-build-dev/src/plugins/commands/block/tsTojs.js
@@ -0,0 +1,22 @@
+import sylvanas from 'sylvanas';
+import glob from 'glob';
+
+function globList(patternList, options) {
+  let fileList = [];
+  patternList.forEach(pattern => {
+    fileList = [...fileList, ...glob.sync(pattern, options)];
+  });
+
+  return fileList;
+}
+export default async cwd => {
+  const tsFiles = globList(['**/*.tsx', '**/*.ts'], {
+    cwd,
+    ignore: ['**/*.d.ts'],
+  });
+  sylvanas(tsFiles, {
+    cwd,
+    action: 'overwrite',
+  });
+  return true;
+};
