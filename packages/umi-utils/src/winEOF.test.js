834642a219a33a6864a1245ccddb45d80f198e10
陈帅 <qixian.cs@outlook.com>
2019-06-25 01:13:53 +0800

    fix winEOF in mac

--- packages/umi-utils/src/winEOF.test.js
+++ packages/umi-utils/src/winEOF.test.js
@@ -1,7 +1,15 @@
 import winEOF from './winEOF';
 
+const isWindows = typeof process !== 'undefined' && process.platform === 'win32';
+
 test('normal', () => {
-  expect(winEOF('a\r\nb')).toEqual('a\nb');
-  expect(winEOF('成\r\nb')).toEqual('成\nb');
-  expect(winEOF('🍟\r\n🍔')).toEqual('🍟\n🍔');
+  if (isWindows) {
+    expect(winEOF('a \r\n b')).toEqual('a \n b');
+    expect(winEOF('成 \r\n b')).toEqual('成 \n b');
+    expect(winEOF('🍟 \r\n 🍔')).toEqual('🍟 \n 🍔');
+  } else {
+    expect(winEOF('a \n b')).toEqual('a \n b');
+    expect(winEOF('成 \n b')).toEqual('成 \n b');
+    expect(winEOF('🍟 \n 🍔')).toEqual('🍟 \n 🍔');
+  }
 });
