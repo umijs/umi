dfbe4fc905b31bdbed7cabcdcf8fba4c3d4694a8
ikobe <ikobe621@gmail.com>
2019-06-26 16:18:36 +0800

    refactor: error constructor

--- packages/umi-core/src/error.js
+++ packages/umi-core/src/error.js
@@ -7,12 +7,30 @@ marked.setOptions({
   renderer: new TerminalRenderer(),
 });
 
+// 支持内部框架扩展 error code map
+const ERROR_CODE_MAP = require(process.env.ERROR_CODE_MAP_PATH || '@umijs/error-code-map');
+
 export class UmiError extends Error {
   constructor(opts, ...params) {
     const { message, code, context } = opts;
     super(message, ...params);
     this.code = code;
     this.context = context || {};
+
+    this.test();
+  }
+
+  test() {
+    if (this.code) {
+      return;
+    }
+    for (const c of Object.keys(ERROR_CODE_MAP)) {
+      const { test } = ERROR_CODE_MAP[c];
+      if (test && test({ error: this, context: this.context })) {
+        this.code = c;
+        break;
+      }
+    }
   }
 }
 
@@ -23,30 +41,21 @@ export function printUmiError(e, opts = {}) {
   }
 
   const { detailsOnly } = opts;
-  const { context } = e;
-  let { code } = e;
-  // 支持内部框架扩展 error code map
-  const errorCodeMap = require(process.env.ERROR_CODE_MAP_PATH || '@umijs/error-code-map');
-
-  if (!code) {
-    for (const c of Object.keys(errorCodeMap)) {
-      const { test } = errorCodeMap[c];
-      if (test && test({ error: e, context })) {
-        code = c;
-      }
-    }
-  }
+  const { code } = e;
 
   if (!code) return;
 
-  const { message, details } = errorCodeMap[code];
+  const { message, details } = ERROR_CODE_MAP[code];
   console.error(`\n${chalk.bgRed.black(' ERROR CODE ')} ${chalk.red(code)}`);
 
   if (!detailsOnly) {
     console.error(`\n${chalk.bgRed.black(' ERROR ')} ${chalk.red(e.message || message)}`);
   }
 
-  if (process.env.LANG.includes('zh_CN')) {
+  const osLocale = require('os-locale');
+  const lang = osLocale.sync();
+
+  if (lang === 'zh-CN') {
     console.error(`\n${chalk.bgMagenta.black(' DETAILS ')}\n\n${marked(details['zh-CN'])}`);
   } else {
     console.error(`\n${chalk.bgMagenta.black(' DETAILS ')}\n\n${marked(details.en)}`);
