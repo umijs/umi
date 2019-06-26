dcd7f5da03a4983bd258b4e8208e57bfd30edf3b
陈帅 <qixian.cs@outlook.com>
2019-06-26 18:12:05 +0800

    remove loading

--- packages/umi-build-dev/src/Service.js
+++ packages/umi-build-dev/src/Service.js
@@ -7,7 +7,7 @@ import { assign, cloneDeep } from 'lodash';
 import { parse } from 'dotenv';
 import signale from 'signale';
 import { deprecate, winPath } from 'umi-utils';
-import ora from 'ora';
+// import ora from 'ora';
 
 import { UmiError, printUmiError } from 'umi-core/lib/error';
 import getPaths from './getPaths';
@@ -19,7 +19,7 @@ import getCodeFrame from './utils/getCodeFrame';
 
 const debug = require('debug')('umi-build-dev:Service');
 
-const spinner = ora();
+// const spinner = ora();
 export default class Service {
   constructor({ cwd }) {
     //  用户传入的 cmd 不可信任 转化一下
@@ -42,21 +42,21 @@ export default class Service {
     this.UmiError = UmiError;
     this.printUmiError = printUmiError;
 
-    spinner.start('🧞  get user config');
+    // spinner.start('🧞  get user config');
     // resolve user config
     this.config = UserConfig.getConfig({
       cwd: this.cwd,
       service: this,
     });
     debug(`user config: ${JSON.stringify(this.config)}`);
-    spinner.succeed();
+    // spinner.succeed();
 
-    spinner.start('💥 resolve plugins');
+    // spinner.start('💥 resolve plugins');
     // resolve plugins
     this.plugins = this.resolvePlugins();
     this.extraPlugins = [];
     debug(`plugins: ${this.plugins.map(p => p.id).join(' | ')}`);
-    spinner.succeed();
+    // spinner.succeed();
 
     // resolve paths
     this.paths = getPaths(this);
