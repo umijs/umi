343db855ebedfb23d58f5008dd18b71e6eb3a175
chencheng <sorrycc@gmail.com>
2019-06-28 13:50:07 +0800

    chore: remove ora from cli and Service (we may add it back later)

--- packages/umi-build-dev/src/Service.js
+++ packages/umi-build-dev/src/Service.js
@@ -7,8 +7,6 @@ import { assign, cloneDeep } from 'lodash';
 import { parse } from 'dotenv';
 import signale from 'signale';
 import { deprecate, winPath } from 'umi-utils';
-// import ora from 'ora';
-
 import { UmiError, printUmiError } from 'umi-core/lib/error';
 import getPaths from './getPaths';
 import getPlugins from './getPlugins';
@@ -19,7 +17,6 @@ import getCodeFrame from './utils/getCodeFrame';
 
 const debug = require('debug')('umi-build-dev:Service');
 
-// const spinner = ora();
 export default class Service {
   constructor({ cwd }) {
     //  用户传入的 cmd 不可信任 转化一下
@@ -42,21 +39,17 @@ export default class Service {
     this.UmiError = UmiError;
     this.printUmiError = printUmiError;
 
-    // spinner.start('🧞  get user config');
     // resolve user config
     this.config = UserConfig.getConfig({
       cwd: this.cwd,
       service: this,
     });
     debug(`user config: ${JSON.stringify(this.config)}`);
-    // spinner.succeed();
 
-    // spinner.start('💥 resolve plugins');
     // resolve plugins
     this.plugins = this.resolvePlugins();
     this.extraPlugins = [];
     debug(`plugins: ${this.plugins.map(p => p.id).join(' | ')}`);
-    // spinner.succeed();
 
     // resolve paths
     this.paths = getPaths(this);
--- packages/umi/src/cli.js
+++ packages/umi/src/cli.js
@@ -2,10 +2,7 @@ import { dirname } from 'path';
 import yParser from 'yargs-parser';
 import signale from 'signale';
 import semver from 'semver';
-import ora from 'ora';
 import buildDevOpts from './buildDevOpts';
-
-const spinner = ora();
 
 const script = process.argv[2];
 const args = yParser(process.argv.slice(3));
@@ -16,7 +13,6 @@ if (semver.satisfies(nodeVersion, '<6.5')) {
   signale.error(`Node version must >= 6.5, but got ${nodeVersion}`);
   process.exit(1);
 }
-spinner.start('🏃 parsing parameters');
 
 // Notify update when process exits
 const updater = require('update-notifier');
@@ -32,8 +28,6 @@ const aliasMap = {
   '-h': 'help',
   '--help': 'help',
 };
-
-spinner.succeed();
 
 switch (script) {
   case 'build':
