aecd76dea1b8530efc8db10f5d7a6448ef78931b
信鑫-King <45808948@qq.com>
2019-07-18 14:07:52 +0800

    fix: findRoute recursive bug (#2812)

--- packages/umi-build-dev/src/findRoute.js
+++ packages/umi-build-dev/src/findRoute.js
@@ -1,7 +1,10 @@
 export default function findRoute(routes, path) {
   for (const route of routes) {
     if (route.routes) {
-      return findRoute(route.routes, path);
+      const routesMatch = findRoute(route.routes, path);
+      if (routesMatch) {
+        return routesMatch;
+      }
     } else if (require('react-router-dom').matchPath(path, route)) {
       // for get params (/news/1 => { params: { id： 1 } })
       const { params } = require('react-router-dom').matchPath(path, route);
