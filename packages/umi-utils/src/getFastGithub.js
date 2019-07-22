e4ec013d97b49e7e3c7d66f0c856e1c00aa05d06
DiamondYuan <admin@diamondyuan.com>
2019-07-22 20:27:23 +0800

    fix: delete spaces before the url (#2858)

--- packages/umi-utils/src/getFastGithub.js
+++ packages/umi-utils/src/getFastGithub.js
@@ -2,7 +2,7 @@ import fetch from 'node-fetch';
 
 const registryMap = {
   'github.com': 'https://github.com/ant-design/pro-blocks.git',
-  'github.com.cnpmjs.org': ' https://github.com.cnpmjs.org/ant-design/pro-blocks.git',
+  'github.com.cnpmjs.org': 'https://github.com.cnpmjs.org/ant-design/pro-blocks.git',
 };
 
 const getFastGithub = async () => {
