c9ca7021f77887e021b4d4ac7833bc817da3c5d6
ycjcl868 <45808948@qq.com>
2019-08-05 22:10:08 +0800

    feat: use Reload

--- packages/umi-ui/client/src/components/DirectoryForm/index.tsx
+++ packages/umi-ui/client/src/components/DirectoryForm/index.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
-import { Icon, Breadcrumb } from 'antd';
+import { Icon, Row, Col, Button } from 'antd';
 import cls from 'classnames';
+import useLoading from '@/components/hooks/useLoading';
 import { getCwd, listDirectory } from '@/services/project';
 import DirectoryItem, { DirectoryItemProps } from './item';
 
@@ -16,6 +17,8 @@ interface DirectoryFormProps {
 
 const DirectoryForm: React.FC<DirectoryFormProps> = props => {
   const { value, onChange } = props;
+  const { getLoading, setLoading } = useLoading(['reload']);
+
   const [dirPath, setDirPath] = useState<string>(value || '');
   const [directories, setDirectories] = useState<DirectoryItemProps[]>([]);
   const triggerChangeValue = (path: string) => {
@@ -23,10 +26,10 @@ const DirectoryForm: React.FC<DirectoryFormProps> = props => {
       onChange(path);
     }
   };
+  console.log('dirPath', dirPath);
+  const pathArr = dirPath === '/' ? [''] : dirPath.split('/');
 
-  const pathArr = dirPath.split('/');
-
-  const changeDirectories = async (path: string): void => {
+  const changeDirectories = async (path: string): Promise<void> => {
     const { data: files } = await listDirectory({
       dirPath: path,
     });
@@ -46,35 +49,52 @@ const DirectoryForm: React.FC<DirectoryFormProps> = props => {
   const handleDirectoryClick = async (folderName?: string) => {
     if (folderName) {
       // TODO windows Path format
-      const currDirPath = `${dirPath}/${folderName}`;
+      const currDirPath = `${dirPath === '/' ? dirPath : `${dirPath}/`}${folderName}`;
       await changeDirectories(currDirPath);
     }
   };
+
   const handleParentDirectory = async () => {
-    const currDirPath = pathArr.slice(0, -1).join('/');
-    await changeDirectories(currDirPath);
+    if (pathArr.length > 0) {
+      const currDirPath = pathArr.slice(0, -1).join('/');
+      await changeDirectories(currDirPath);
+    }
   };
 
   const handleBreadDirChange = async (i: number) => {
-    const currDirPath = pathArr.slice(0, i + 1).join('/');
+    const currDirPath = pathArr.slice(0, i + 1).join('/') || '/';
     await changeDirectories(currDirPath);
   };
+
+  const handleReload = async () => {
+    setLoading('reload', true);
+    await changeDirectories(dirPath);
+    // just test
+    setTimeout(() => {
+      setLoading('reload', false);
+    }, 1500);
+  };
+
   return (
     <div className={styles.directoryForm}>
       <div className={styles['directoryForm-toolbar']}>
-        <Icon
-          type="arrow-left"
-          className={styles['directoryForm-toolbar-back']}
-          onClick={handleParentDirectory}
-        />
-        <Breadcrumb className={styles['directoryForm-toolbar-bread']}>
+        <Button className={styles['directoryForm-toolbar-back']} onClick={handleParentDirectory}>
+          <Icon type="arrow-left" onClick={handleParentDirectory} />
+        </Button>
+        <div className={styles['directoryForm-toolbar-bread']}>{dirPath}</div>
+        {/* <Row type="flex" className={styles['directoryForm-toolbar-bread']}>
           {pathArr.map((path, i) => (
-            <Breadcrumb.Item onClick={() => i !== pathArr.length - 1 && handleBreadDirChange(i)}>
-              {path}
-            </Breadcrumb.Item>
+            <Col key={path} onClick={() => handleBreadDirChange(i)}>
+              <p className={styles['directoryForm-toolbar-bread-path']}>{path}</p>
+            </Col>
           ))}
-        </Breadcrumb>
-        {/* <p>路径：{dirPath}</p> */}
+        </Row> */}
+        <Button>
+          <Icon type="edit" />
+        </Button>
+        <Button loading={getLoading('reload')} onClick={handleReload}>
+          {!getLoading('reload') && <Icon type="reload" />}
+        </Button>
       </div>
       <div className={styles['directoryForm-list']}>
         {directories.map(item => (
---
+++ packages/umi-ui/client/src/components/hooks/useLoading.ts
@@ -0,0 +1,35 @@
+import * as React from 'react';
+import flatten from 'lodash/flatten';
+import cls from 'classnames';
+
+import styles from './index.less';
+
+const { useState, useCallback } = React;
+
+const useLoading = (key: string | string[]) => {
+  const flatKeys = flatten([key]);
+  const getKeysMap = useCallback(() => {
+    return flatKeys.reduce(
+      (curr, acc) => ({
+        ...curr,
+        [acc]: false,
+      }),
+      {},
+    );
+  }, flatKeys);
+  const [keysMap, setKeysMap] = useState<object>(getKeysMap);
+
+  const getLoading = (curKey: string): boolean => {
+    return keysMap[curKey] !== false;
+  };
+  const setLoading = (curKey: string, value: boolean) => {
+    setKeysMap(keys => ({
+      ...keys,
+      [curKey]: value,
+    }));
+  };
+
+  return { getLoading, setLoading };
+};
+
+export default useLoading;
