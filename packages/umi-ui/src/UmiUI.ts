9d4d56dcfdce9451efd5cdbb6cba4f355b764b90
chencheng <sorrycc@gmail.com>
2019-08-19 10:53:47 +0800

    add bacon script for ga

--- packages/umi-ui/src/UmiUI.ts
+++ packages/umi-ui/src/UmiUI.ts
@@ -168,7 +168,6 @@ export default class UmiUI {
 
       // catch exit
       process.on('SIGINT', () => {
-        console.log('siginint');
         if (key) {
           try {
             this.config.setCreatingProgress(key, {
@@ -489,28 +488,38 @@ export default class UmiUI {
       }
 
       // 添加埋点脚本
-      // TODO: 内外不同
       function normalizeHtml(html) {
         const ga = `
-          <!-- Global site tag (gtag.js) - Google Analytics -->
-          <script async src="https://www.googletagmanager.com/gtag/js?id=UA-145890626-1"></script>
-          <script>
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-145890626-1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
 
-            gtag('config', 'UA-145890626-1');
-          </script>
+  gtag('config', 'UA-145890626-1');
+</script>
+        `;
+        const deer = `
+<script>
+  !function(t,e,a,r,c){t.TracertCmdCache=t.TracertCmdCache||[],t[c]=window[c]||
+  {_isRenderInit:!0,call:function(){t.TracertCmdCache.push(arguments)},
+  start:function(t){this.call('start',t)}},t[c].l=new Date;
+  var n=e.createElement(a),s=e.getElementsByTagName(a)[0];
+  n.async=!0,n.src=r,s.parentNode.insertBefore(n,s);
+  n.onerror=function(){console.warn(decodeURI('Tracert%20%E8%84%9A%E6%9C%AC%E6%9C%AA%E6%88%90%E5%8A%9F%E5%8A%A0%E8%BD%BD,%20%E8%AF%B7%E6%A3%80%E6%9F%A5%E7%BD%91%E7%BB%9C%E4%BB%A5%E5%8F%8A%20A%20%E4%BD%8D%E6%98%AF%E5%90%A6%E5%9C%A8%E4%B9%9D%E8%89%B2%E9%B9%BF%E5%BB%BA%E7%AB%8B%E6%B4%9E%E5%AF%9F'));
+  var fallback=function(){console.warn(decodeURI('Tracert%20%E5%91%BD%E4%BB%A4%E6%89%A7%E8%A1%8C%E5%A4%B1%E8%B4%A5%EF%BC%8C%E8%AF%B7%E6%A3%80%E6%9F%A5%20JS%20%E6%98%AF%E5%90%A6%E6%AD%A3%E7%A1%AE%E5%BC%95%E5%85%A5'))};
+  for(var fnlist=["call","start","config","logPv","info","err","click","expo","pageName","pageState","time","timeEnd","parse","checkExpo","stringify","report","set","before"],i=0;i<fnlist.length;i++){t[c][fnlist[i]]=fallback}};
+  }(window,document,'script','https://ur.alipay.com/tracert_a1613.js','Tracert');
+  Tracert.start({
+  });
+</script>
         `;
         if (process.env.BIGFISH_COMPAT) {
           html = html.replace('<body>', '<body>\n<script>window.g_bigfish = {};</script>');
         }
-        // DISABLE_UMI_UI_ANALYTICS=true => close ga/deer
-        // LOCAL_DEBUG ? disable ?
-        if (!process.env.DISABLE_UMI_UI_ANALYTICS) {
-          const baconScript = process.env.BIGFISH_COMPAT
-            ? `<script>console.log('bacon for bigfish');</script>`
-            : ga;
+        if (!process.env.LOCAL_DEBUG) {
+          const baconScript = process.env.BIGFISH_COMPAT ? deer : ga;
           html = html.replace('</head>', `${baconScript}\n</head>`);
         }
         return html;
