e230d686d15d1f7678ccb7cb941741b600b9bb7b
Alex.Zeng <Alexzeng@dfocuspace.com>
2019-09-04 12:40:17 +0800

    fix: add judgement to typeof navigator

--- packages/umi-plugin-locale/src/locale.js
+++ packages/umi-plugin-locale/src/locale.js
@@ -34,8 +34,11 @@ function getLocale() {
   // support SSR
   const { g_langSeparator = '-', g_lang } = window;
   const lang = typeof localStorage !== 'undefined' ? window.localStorage.getItem('umi_locale') : '';
-  const browserLang =
-    typeof navigator.language === 'string' ? navigator.language.split('-').join(g_langSeparator) : '';
+  const isNavigatorLanguageValid =
+    typeof navigator !== 'undefined' && typeof navigator.language === 'string';
+  const browserLang = isNavigatorLanguageValid
+    ? navigator.language.split('-').join(g_langSeparator)
+    : '';
   return lang || g_lang || browserLang;
 }
 
