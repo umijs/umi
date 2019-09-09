bc6bfe4c99b6410e712e455cddb2293e7c9f1a1c
ycjcl868 <45808948@qq.com>
2019-09-09 16:54:08 +0800

    fix: task plugin dev watch

--- packages/umi-ui-tasks/.fatherrc.ts
+++ packages/umi-ui-tasks/.fatherrc.ts
@@ -1,12 +1,4 @@
-import glob from 'glob';
-import { join } from 'path';
-
 export default [
-  {
-    target: 'node',
-    cjs: { type: 'babel', lazy: true },
-    disableTypeCheck: true,
-  },
   {
     entry: 'ui/index.tsx',
     typescriptOpts: {
