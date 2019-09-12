edb0e81d60f4ebb68192dbef56695e35a250388f
chencheng <sorrycc@gmail.com>
2019-09-12 16:01:13 +0800

    chore: update bubble script

--- packages/umi-plugin-ui/bubble/index.js
+++ packages/umi-plugin-ui/bubble/index.js
@@ -1,11 +1,21 @@
-const el = document.createElement('div');
-el.style.position = 'fixed';
-el.style.right = '20px';
-el.style.bottom = '20px';
-el.style.width = '50px';
-el.style.height = '50px';
-el.style.background = 'red';
-el.addEventListener('click', () => {
-  alert('umi ui bubble');
-});
-document.querySelector('body').appendChild(el);
+function showUmiUIModal() {
+  alert('show umi ui modal');
+}
+
+function showBubble() {
+  const el = document.createElement('div');
+  el.style.position = 'fixed';
+  el.style.right = '20px';
+  el.style.bottom = '20px';
+  el.style.width = '50px';
+  el.style.height = '50px';
+  el.style.background = 'red';
+  el.addEventListener('click', showUmiUIModal);
+  document.querySelector('body').appendChild(el);
+}
+
+function init() {
+  showBubble();
+}
+
+init();
--- packages/umi-plugin-ui/src/index.ts
+++ packages/umi-plugin-ui/src/index.ts
@@ -2,6 +2,7 @@ import { IApi } from 'umi-types';
 import { join } from 'path';
 
 export default (api: IApi) => {
+  // TODO: 区分生产和开发环境，生产环境引打包好的，或者通过异步远程加载也可以
   api.addEntryCode(`
 // Umi UI Bubble
 require('${join(__dirname, '../bubble/index.js')}');
