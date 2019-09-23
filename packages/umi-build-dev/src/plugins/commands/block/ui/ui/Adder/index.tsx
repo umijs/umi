eed64f4e4558a54e0b94e4e6a3a67501d79d61f2
Yu <yutingzhao1991@sina.com>
2019-09-23 21:11:07 +0800

    feat: add hooks and init routetree (#3259)

--- packages/umi-build-dev/src/plugins/commands/block/ui/ui/Adder/index.tsx
+++ packages/umi-build-dev/src/plugins/commands/block/ui/ui/Adder/index.tsx
@@ -1,20 +1,43 @@
 import React, { useState } from 'react';
-import { Modal, Button, Input, Switch } from 'antd';
-import { AddBlockParams, Block } from '../../data';
+import { IUiApi } from 'umi-types';
+import { Modal, Button, Input, Switch, TreeSelect } from 'antd';
+
+import useCallData from '../hooks/useCallData';
+import { AddBlockParams, Block, Resource } from '../../../data.d';
 import styles from './index.module.less';
 
 interface Props {
   onAdded: (params: AddBlockParams) => void;
   block: Block;
+  api: IUiApi;
+  blockType: Resource['blockType'];
 }
 
 const Adder: React.FC<Props> = props => {
-  const { onAdded, block, children } = props;
+  const { onAdded, block, children, blockType, api } = props;
+  const { callRemote } = api;
+
   const [visible, setVisible] = useState<boolean>(false);
   const [path, setPath] = useState<string>(block.defaultPath);
+  const [routePath, setRoutePath] = useState<string>(block.defaultPath);
   const [name, setName] = useState<string>(block.url.split('/').pop());
   const [transformJS, setTransformJS] = useState<boolean>(false);
   const [removeLocale, setRemoveLocale] = useState<boolean>(false);
+
+  const { data: routePathTreeData } = useCallData(
+    () => {
+      return callRemote({
+        type: 'org.umi.block.routes',
+      }) as any;
+    },
+    [],
+    {
+      defaultData: [],
+    },
+  );
+
+  console.log(routePathTreeData);
+
   return (
     <>
       <Button
@@ -37,6 +60,7 @@ const Adder: React.FC<Props> = props => {
               url: block.url,
               name,
               path,
+              routePath,
               isPage: false,
               transformJS,
               removeLocale,
@@ -44,10 +68,27 @@ const Adder: React.FC<Props> = props => {
           }}
         >
           <div>
+            <div className={styles.label}>选择路由</div>
+            <TreeSelect
+              treeDataSimpleMode
+              value={routePath}
+              placeholder="请选择路由"
+              onChange={setRoutePath}
+              treeData={[
+                {
+                  value: '1213',
+                  key: '323',
+                },
+              ]}
+            />
             <div className={styles.label}>选择安装路径</div>
             <Input value={path} onChange={e => setPath(e.target.value)} />
-            <div className={styles.label}>区块名称</div>
-            <Input value={name} onChange={e => setName(e.target.value)} />
+            {blockType === 'block' && (
+              <>
+                <div className={styles.label}>区块名称</div>
+                <Input value={name} onChange={e => setName(e.target.value)} />
+              </>
+            )}
             <div className={styles.label}>编译为 JS</div>
             <Switch value={transformJS} onChange={setTransformJS} />
             <div className={styles.label}>移除国际化</div>
