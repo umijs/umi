515a171793dd849239ced77d63bf4e99d7335543
ycjcl868 <45808948@qq.com>
2019-09-25 09:46:26 +0800

    fix: windows

--- packages/umi-plugin-ui/src/index.ts
+++ packages/umi-plugin-ui/src/index.ts
@@ -2,13 +2,13 @@ import { IApi } from 'umi-types';
 import { join } from 'path';
 
 export default (api: IApi) => {
-  const { relativeToTmp } = api;
+  const { relativeToTmp, winPath } = api;
   // TODO: 区分生产和开发环境，生产环境引打包好的，或者通过异步远程加载也可以
   api.addEntryCode(`
 // Umi UI Bubble
 require('${relativeToTmp(join(__dirname, '../bubble'))}').default({
   port: ${process.env.UMI_UI_PORT},
-  path: '${api.cwd}',
+  path: '${winPath(api.cwd)}',
   currentProject: '${process.env.UMI_UI_CURRENT_PROJECT || ''}',
   isBigfish: ${process.env.BIGFISH_COMPAT},
 });
