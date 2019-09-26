a7e1910713c95f2fcee9c5313ecd81da7c697c6b
陈帅 <qixian.cs@outlook.com>
2019-09-26 20:08:14 +0800

    better code

--- packages/umi-build-dev/src/plugins/commands/block/ui/ui/BlockList/index.tsx
+++ packages/umi-build-dev/src/plugins/commands/block/ui/ui/BlockList/index.tsx
@@ -10,19 +10,29 @@ import { Block, AddBlockParams, Resource } from '../../../data.d';
 
 const { CheckableTag } = Tag;
 
-interface BlockListProps {
+interface BlockItemProps {
   api: IUiApi;
-  name?: string;
   type: Resource['blockType'];
-  list: Block[];
   addingBlock: string;
+  item: Block;
   onAddClick: (params: AddBlockParams) => void;
   onAddSuccess: (params: AddBlockParams) => void;
   loading?: boolean;
   keyword?: string;
 }
+interface BlockListProps extends Omit<BlockItemProps, 'item'> {
+  name?: string;
+  list: Block[];
+}
 
-const renderMetas = (item: any, keyword?: string) => (
+/**
+ * 子区块 的 tag
+ * @param
+ */
+const Meats: React.FC<{
+  item: Block;
+  keyword?: string;
+}> = ({ item, keyword }) => (
   <div className={styles.metas}>
     <span className={styles.tags}>
       {item.tags &&
@@ -35,18 +45,80 @@ const renderMetas = (item: any, keyword?: string) => (
   </div>
 );
 
+/**
+ * 子区块的展示逻辑
+ * @param param0
+ */
+const BlockItem: React.FC<BlockItemProps> = ({
+  type,
+  item,
+  addingBlock,
+  api,
+  onAddClick,
+  keyword,
+  onAddSuccess,
+}) => {
+  const isBlock = type === 'block';
+  const style = {
+    flex: `0 1 ${isBlock ? '20%' : '25%'}`,
+  };
+  return (
+    <Col style={style} key={item.url}>
+      <div className={isBlock ? styles.blockCard : styles.templateCard}>
+        <Spin spinning={item.url === addingBlock} tip="Adding...">
+          <div className={styles.demo}>
+            <div className={styles.addProject}>
+              <Adder
+                api={api}
+                blockType={type}
+                block={item}
+                onAddClick={onAddClick}
+                onAddSuccess={onAddSuccess}
+              >
+                添加到项目
+              </Adder>
+              {item.previewUrl && (
+                <Button className={styles.previewBtn} target="_blank" href={item.previewUrl}>
+                  预览
+                </Button>
+              )}
+            </div>
+
+            <LazyLoad
+              height={type === 'block' ? '20%' : '25%'}
+              key={item.url}
+              scrollContainer={document.getElementById('block-list-view')}
+              offset={100}
+            >
+              <img src={item.img} alt={item.url} />
+            </LazyLoad>
+          </div>
+        </Spin>
+
+        <div className={styles.content}>
+          <div className={styles.title}>
+            <HighlightedText text={item.name} highlight={keyword} />
+          </div>
+          {type === 'template' && (
+            <Typography.Paragraph
+              className={styles.description}
+              ellipsis={{ rows: 2, expandable: false }}
+            >
+              <HighlightedText text={item.description} highlight={keyword} />
+            </Typography.Paragraph>
+          )}
+          <Meats item={item} keyword={keyword} />
+        </div>
+      </div>
+    </Col>
+  );
+};
+
 const BlockList: React.FC<BlockListProps> = props => {
-  const {
-    list = [],
-    type = 'block',
-    addingBlock,
-    loading,
-    keyword,
-    onAddClick,
-    onAddSuccess,
-    api,
-  } = props;
+  const { list = [], loading, api } = props;
+
   const { uniq, flatten } = api._;
+
   const tags: string[] = useMemo<string[]>(
     () => {
       return uniq(flatten(list.map(item => item.tags)));
@@ -87,63 +159,7 @@ const BlockList: React.FC<BlockListProps> = props => {
         {list
           .filter(item => !selectedTag || item.tags.includes(selectedTag))
           .map(item => (
-            <Col
-              style={{
-                flex: `0 1 ${type === 'block' ? '20%' : '25%'}`,
-              }}
-              key={item.url}
-            >
-              <div className={type === 'block' ? styles.blockCard : styles.templateCard}>
-                <Spin spinning={item.url === addingBlock} tip="Adding...">
-                  <div className={styles.demo}>
-                    <div className={styles.addProject}>
-                      <Adder
-                        api={api}
-                        blockType={type}
-                        block={item}
-                        onAddClick={onAddClick}
-                        onAddSuccess={onAddSuccess}
-                      >
-                        添加到项目
-                      </Adder>
-                      {item.previewUrl && (
-                        <Button
-                          className={styles.previewBtn}
-                          target="_blank"
-                          href={item.previewUrl}
-                        >
-                          预览
-                        </Button>
-                      )}
-                    </div>
-
-                    <LazyLoad
-                      height={type === 'block' ? '20%' : '25%'}
-                      key={item.url}
-                      scrollContainer={document.getElementById('block-list-view')}
-                      offset={100}
-                    >
-                      <img src={item.img} alt={item.url} />
-                    </LazyLoad>
-                  </div>
-                </Spin>
-
-                <div className={styles.content}>
-                  <div className={styles.title}>
-                    <HighlightedText text={item.name} highlight={keyword} />
-                  </div>
-                  {type === 'template' && (
-                    <Typography.Paragraph
-                      className={styles.description}
-                      ellipsis={{ rows: 2, expandable: false }}
-                    >
-                      <HighlightedText text={item.description} highlight={keyword} />
-                    </Typography.Paragraph>
-                  )}
-                  {renderMetas(item, keyword)}
-                </div>
-              </div>
-            </Col>
+            <BlockItem item={item} {...props} />
           ))}
       </Row>
     );
@@ -160,23 +176,21 @@ const BlockList: React.FC<BlockListProps> = props => {
         >
           全部
         </CheckableTag>
-        {tags.map(tag => {
-          return (
-            <CheckableTag
-              checked={selectedTag === tag}
-              key={tag}
-              onChange={checked => {
-                if (checked) {
-                  setSelectedTag(tag);
-                } else {
-                  setSelectedTag('');
-                }
-              }}
-            >
-              {tag}
-            </CheckableTag>
-          );
-        })}
+        {tags.map(tag => (
+          <CheckableTag
+            checked={selectedTag === tag}
+            key={tag}
+            onChange={checked => {
+              if (checked) {
+                setSelectedTag(tag);
+              } else {
+                setSelectedTag('');
+              }
+            }}
+          >
+            {tag}
+          </CheckableTag>
+        ))}
       </div>
       <div className={styles.cardContainer}>{contents}</div>
     </>
