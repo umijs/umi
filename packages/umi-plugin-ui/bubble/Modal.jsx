08091afc9da58fab43b06cb4664c70505abfa609
ycjcl868 <45808948@qq.com>
2019-09-27 16:41:26 +0800

    fix: get scroll bar size

--- packages/umi-plugin-ui/bubble/Modal.jsx
+++ packages/umi-plugin-ui/bubble/Modal.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import styled, { keyframes } from 'styled-components';
+import { getScrollBarSize } from './Dragger/utils';
 
 const fadeInUp = keyframes`
   from {
@@ -44,46 +45,7 @@ const IframeWrapper = styled('div')`
   }
 `;
 
-let cached;
-
 class Modal extends React.Component {
-  getScrollBarSize = fresh => {
-    if (fresh || cached === undefined) {
-      const inner = document.createElement('div');
-      inner.style.width = '100%';
-      inner.style.height = '200px';
-
-      const outer = document.createElement('div');
-      const outerStyle = outer.style;
-
-      outerStyle.position = 'absolute';
-      outerStyle.top = 0;
-      outerStyle.left = 0;
-      outerStyle.pointerEvents = 'none';
-      outerStyle.visibility = 'hidden';
-      outerStyle.width = '200px';
-      outerStyle.height = '150px';
-      outerStyle.overflow = 'hidden';
-
-      outer.appendChild(inner);
-
-      document.body.appendChild(outer);
-
-      const widthContained = inner.offsetWidth;
-      outer.style.overflow = 'scroll';
-      let widthScroll = inner.offsetWidth;
-
-      if (widthContained === widthScroll) {
-        widthScroll = outer.clientWidth;
-      }
-
-      document.body.removeChild(outer);
-
-      cached = widthContained - widthScroll;
-    }
-    return cached;
-  };
-
   switchScrollingEffect = close => {
     const bodyIsOverflowing =
       document.body.scrollHeight > (window.innerHeight || document.documentElement.clientHeight) &&
@@ -96,7 +58,7 @@ class Modal extends React.Component {
       document.body.style.width = '';
       return;
     }
-    const scrollBarSize = this.getScrollBarSize();
+    const scrollBarSize = getScrollBarSize();
     if (scrollBarSize) {
       document.body.style.position = 'relative';
       document.body.style.width = `calc(100% - ${scrollBarSize}px)`;
