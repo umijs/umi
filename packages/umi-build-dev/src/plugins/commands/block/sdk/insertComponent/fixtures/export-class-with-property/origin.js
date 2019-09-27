e7a6620adfe3d25a4974eef6d7e4481d997be854
chencheng <sorrycc@gmail.com>
2019-09-27 16:21:01 +0800

    fix(block): parse failed when have class property

---
+++ packages/umi-build-dev/src/plugins/commands/block/sdk/insertComponent/fixtures/export-class-with-property/origin.js
@@ -0,0 +1,19 @@
+
+class App extends React.Component {
+  state: AccountCenterState = {
+    newTags: [],
+    inputVisible: false,
+    inputValue: '',
+    tabKey: 'articles',
+  };
+
+  render() {
+    return (
+      <div>
+        <h1>foo</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
