5fb65d1c825040da159097d8d6e55124fb2772e8
chenshuai <qixian.cs@outlook.com>
2019-10-08 20:03:23 +0800

    add serve alive

--- packages/umi-build-dev/src/plugins/commands/block/ui/ui/Adder/ResultPanel.tsx
+++ packages/umi-build-dev/src/plugins/commands/block/ui/ui/Adder/ResultPanel.tsx
@@ -1,11 +1,22 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { Result, Button } from 'antd';
 import Context from '../UIApiContext';
 import styles from './ResultPanel.module.less';
 
 export default ({ name, url, onFinish }: { name: string; url: string; onFinish: () => void }) => {
   const { api } = useContext(Context);
+  const [alive, setAlive] = useState<boolean>(false);
   const isMini = api.isMini();
+  useEffect(() => {
+    (async () => {
+      const msg = (await api.callRemote({
+        type: 'tasks/is_dev_server_alive',
+      })) as {
+        alive: boolean;
+      };
+      setAlive(msg.alive);
+    })();
+  }, []);
   return (
     <div className={styles.result}>
       <Result
@@ -13,7 +24,7 @@ export default ({ name, url, onFinish }: { name: string; url: string; onFinish: 
         title={`${name} 安装成功！`}
         subTitle="代码已插入页面。编译完成后页面将会自动刷新。"
         extra={
-          isMini
+          isMini || !alive
             ? [
                 <Button type="primary" key="preview" onClick={() => onFinish()}>
                   完成
