3c20d7ac79b7b763eb6f429ee3829155a863189d
ycjcl868 <45808948@qq.com>
2019-10-10 09:02:51 +0800

    fix: bubble default font-family

--- packages/umi-plugin-ui/src/bubble/Dragger/index.jsx
+++ packages/umi-plugin-ui/src/bubble/Dragger/index.jsx
@@ -21,6 +21,9 @@ const Container = styled.div`
 
   * {
     box-sizing: border-box;
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'PingFang SC', 'Hiragino Sans GB',
+    'Microsoft YaHei', 'Helvetica Neue', Helvetica, Arial, sans-serif, 'Apple Color Emoji',
+    'Segoe UI Emoji', 'Segoe UI Symbol';
   }
 
   &:hover {
