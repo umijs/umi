6cf6693e0180c60ce4b3f7f28ca5370e64187968
ycjcl868 <45808948@qq.com>
2019-10-11 17:51:54 +0800

    tweak: ui block icon use SVG

--- packages/umi-build-dev/src/plugins/commands/block/ui/ui/icon.tsx
+++ packages/umi-build-dev/src/plugins/commands/block/ui/ui/icon.tsx
@@ -13,3 +13,16 @@ export default props => {
   );
   return <Icon component={SVGIcon} {...props} />;
 };
+
+export const Clear = props => {
+  const SVGIcon = () => (
+    <svg width="1em" height="1em" fill="currentColor" viewBox="0 0 1024 1024" {...props}>
+      <path
+        d="M899.1 869.6l-53-305.6H864c14.4 0 26-11.6 26-26V346c0-14.4-11.6-26-26-26H618V138c0-14.4-11.6-26-26-26H432c-14.4 0-26 11.6-26 26v182H160c-14.4 0-26 11.6-26 26v192c0 14.4 11.6 26 26 26h17.9l-53 305.6c-.3 1.5-.4 3-.4 4.4 0 14.4 11.6 26 26 26h723c1.5 0 3-.1 4.4-.4 14.2-2.4 23.7-15.9 21.2-30zM204 390h272V182h72v208h272v104H204V390zm468 440V674c0-4.4-3.6-8-8-8h-48c-4.4 0-8 3.6-8 8v156H416V674c0-4.4-3.6-8-8-8h-48c-4.4 0-8 3.6-8 8v156H202.8l45.1-260H776l45.1 260H672z"
+        fill="currentColor"
+        fillRule="nonzero"
+      />
+    </svg>
+  );
+  return <Icon component={SVGIcon} {...props} />;
+};
