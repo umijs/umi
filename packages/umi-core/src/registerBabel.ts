3fec34e5c9cfd0a98485f46f3f29f18e7a305475
chencheng <sorrycc@gmail.com>
2019-10-14 15:34:08 +0800

    fix: get config failed with UMI_ENV and .umirc.{UMI_ENV}.js

--- packages/umi-core/src/registerBabel.ts
+++ packages/umi-core/src/registerBabel.ts
@@ -2,6 +2,7 @@ import { join } from 'path';
 import slash2 from 'slash2';
 
 export default function(cwd, extraFiles = []) {
+  const umiEnv = process.env.UMI_ENV;
   require('@babel/register')({
     presets: [
       require.resolve('@babel/preset-typescript'),
@@ -14,7 +15,12 @@ export default function(cwd, extraFiles = []) {
       ],
     ],
     ignore: [/node_modules/],
-    only: [join(cwd, 'config'), join(cwd, '.umirc.js'), join(cwd, '.umirc.ts')]
+    only: [
+      join(cwd, 'config'),
+      join(cwd, '.umirc.js'),
+      join(cwd, '.umirc.ts'),
+      ...(umiEnv ? [join(cwd, `.umirc.${umiEnv}.js`), join(cwd, `.umirc.${umiEnv}.ts`)] : []),
+    ]
       .concat(extraFiles)
       .map(file => slash2(file)),
     extensions: ['.es6', '.es', '.jsx', '.js', '.mjs', '.ts', '.tsx'],
