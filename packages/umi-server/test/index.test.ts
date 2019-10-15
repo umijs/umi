22c50a669e6b7a15585b50b1c9c918315d21ab49
ycjcl868 <45808948@qq.com>
2019-10-15 15:44:39 +0800

    fix: ssr test cases migrate into umi-server

--- packages/umi-server/test/index.test.ts
+++ packages/umi-server/test/index.test.ts
@@ -1,6 +1,7 @@
 import { join } from 'path';
+import { readdirSync } from 'fs';
 import { fork } from 'child_process';
-import { winEOL } from 'umi-utils';
+import { winPath } from 'umi-utils';
 import server from '..';
 
 async function build({ cwd }) {
@@ -25,25 +26,22 @@ async function build({ cwd }) {
   });
 }
 
-const fixtures = join(__dirname, 'fixtures');
+const fixtures = join(winPath(__dirname), 'fixtures');
 
 describe('build', () => {
-  beforeAll(() => {
-    require('test-build-result')({
-      root: fixtures,
-      build({ cwd, dir }) {
-        return new Promise((resolve, reject) => {
-          build({ cwd });
-        });
-      },
-      replaceContent(content) {
-        return winEOL(content.replace(/\/\/ EXTERNAL MODULE[^\n]+/g, '// $EXTERNAL_MODULE$'));
-      },
-    });
+  beforeAll(async () => {
+    const dirs = readdirSync(fixtures).filter(dir => dir.charAt(0) !== '.');
+    console.log('dirs', dirs);
+    for (const dir of dirs) {
+      // eslint-disable-next-line no-await-in-loop
+      await build({ cwd: join(fixtures, dir) });
+    }
   });
+
   afterAll(done => {
     done();
   });
+
   it('ssr', async () => {
     const render = server({
       root: join(fixtures, 'ssr', 'dist'),
@@ -54,5 +52,26 @@ describe('build', () => {
       },
     });
     expect(ssrHtml).toMatch(/Hello UmiJS SSR/);
+  });
+
+  it('ssr-styles', async () => {
+    const render = server({
+      root: join(fixtures, 'ssr-styles', 'dist'),
+    });
+
+    const { ssrHtml: indexHtml } = await render({
+      req: {
+        url: '/',
+      },
+    });
+
+    expect(indexHtml).toMatchSnapshot();
+
+    const { ssrHtml: newsHtml } = await render({
+      req: {
+        url: '/news',
+      },
+    });
+    expect(newsHtml).toMatchSnapshot();
   });
 });
