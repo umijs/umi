143b45a09ff32a45dd91bb737d42eaa5ded8dcd3
chencheng <sorrycc@gmail.com>
2019-10-17 13:44:40 +0800

    fix(umi-ui): add blacklist root node including antd and tech-ui

---
+++ packages/umi-build-dev/src/plugins/commands/block/sdk/flagBabelPlugin/fixtures/root-blacklist/expected.js
@@ -0,0 +1,18 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = void 0;
+
+var _react = _interopRequireDefault(require("react"));
+
+var _reactDocumentTitle = _interopRequireDefault(require("react-document-title"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var _default = function _default() {
+  return _react.default.createElement(_reactDocumentTitle.default, null);
+};
+
+exports.default = _default;
