ca4d38e3acf570ca8f77e4c41f601e9dd7f12100
ycjcl868 <45808948@qq.com>
2019-10-18 10:14:28 +0800

    fix: bubble client height 0 when body, html not set

--- packages/umi-plugin-ui/src/bubble/utils.js
+++ packages/umi-plugin-ui/src/bubble/utils.js
@@ -37,21 +37,7 @@ export const getScrollOffsets = () => {
   };
 };
 
-export const getClientHeight = () => {
-  let clientHeight = 0;
-  if (document.body.clientHeight && document.documentElement.clientHeight) {
-    clientHeight =
-      document.body.clientHeight < document.documentElement.clientHeight
-        ? document.body.clientHeight
-        : document.documentElement.clientHeight;
-  } else {
-    clientHeight =
-      document.body.clientHeight > document.documentElement.clientHeight
-        ? document.body.clientHeight
-        : document.documentElement.clientHeight;
-  }
-  return clientHeight;
-};
+export const getClientHeight = () => document.documentElement.clientHeight;
 
 export const getClientWidth = () => {
   if (window.innerWidth) {
