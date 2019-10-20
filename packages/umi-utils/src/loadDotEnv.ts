2cf80874aab8ea71462ce020093a6a3033f68d01
wadezhan <wadezhan@tencent.com>
2019-10-20 14:30:38 +0800

    fix: Object.keys(undefined)

--- packages/umi-utils/src/loadDotEnv.ts
+++ packages/umi-utils/src/loadDotEnv.ts
@@ -7,7 +7,7 @@ import { parse } from 'dotenv';
  */
 export default function loadDotEnv(envPath: string): void {
   if (existsSync(envPath)) {
-    const parsed = parse(readFileSync(envPath, 'utf-8'));
+    const parsed = parse(readFileSync(envPath, 'utf-8')) || {};
     Object.keys(parsed).forEach(key => {
       // eslint-disable-next-line no-prototype-builtins
       if (!process.env.hasOwnProperty(key)) {
