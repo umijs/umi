a580492fdecd17b4ea0444107b9f20c441803d34
chencheng <sorrycc@gmail.com>
2019-10-29 14:57:45 +0800

    feat: use esm version of history

--- packages/umi/src/createHistory.js
+++ packages/umi/src/createHistory.js
@@ -1,4 +1,4 @@
-import createHistory from 'history/createBrowserHistory';
+import createHistory from 'history/es/createBrowserHistory';
 import { normalizePath } from './utils';
 
 export default function(opts) {
