f8cceb2711c34406714abd62e3be20b86b11f88e
ycjcl868 <45808948@qq.com>
2019-10-31 14:25:40 +0800

    fix: node-pty use prebuild release version

--- packages/umi-ui/src/initTerminal.ts
+++ packages/umi-ui/src/initTerminal.ts
@@ -1,5 +1,5 @@
 import sockjs, { Connection } from 'sockjs';
-import { spawn } from 'node-pty';
+import { spawn } from 'node-pty-prebuilt-multiarch';
 import { sync as osLocaleSync } from 'os-locale';
 // umiui:UmiUI:terminal
 import { debugTerminal as _log } from './debug';
