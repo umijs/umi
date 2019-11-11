a48e03724d1738c90b8b03e84826d8e06ce4762c
chencheng (云谦) <sorrycc@gmail.com>
2019-11-11 15:19:23 +0800

    feat(umi-ui): support layout block (#3599)
    * feat: support insert block to block
    
    * update
    
    * update
    
    * fix ci
    
    * fix ci under windows

--- packages/umi-build-dev/src/plugins/commands/block/appendBlockToContainer.js
+++ packages/umi-build-dev/src/plugins/commands/block/appendBlockToContainer.js
@@ -7,6 +7,7 @@ export default ({ entryPath, blockFolderName, dryRun, index }) => {
   debug('start to update the entry file for block(s) under the path...');
 
   const oldEntry = readFileSync(entryPath, 'utf-8');
+  debug(`insert component ${blockFolderName} with index ${index}`);
   const newEntry = insertComponent(oldEntry, {
     identifier: blockFolderName,
     relativePath: `./${blockFolderName}`,
