67b3f723213feefc45f95dfbec8a1338b4b43dc4
ycjcl868 <45808948@qq.com>
2019-11-14 16:25:05 +0800

    feat: support dashboard cards enable with order

--- packages/umi-plugin-ui/src/plugins/dashboard/ui/layout.tsx
+++ packages/umi-plugin-ui/src/plugins/dashboard/ui/layout.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import p from 'immer';
 import Context from './context';
 
 const Layout = props => {
   const { api, children } = props;
   // 用户 cards 开关状态
-  const [cardSettings, setCardSettings] = React.useState<object>({});
+  const [cardSettings, setCardSettings] = React.useState<any[]>([]);
   const [loading, setLoading] = React.useState(true);
   const [dbPath, setDbPath] = React.useState('');
   React.useLayoutEffect(() => {
@@ -27,15 +28,28 @@ const Layout = props => {
     };
     getDashboardSettings();
   }, []);
-  const cards = api.getDashboard() || [];
+  const cards = (api.getDashboard() || []).map(card => {
+    const { key } = card;
+    const cardSetting = cardSettings.find(item => item.key === key);
+    const enable = cardSetting ? !!cardSetting.enable : true;
+    return {
+      ...card,
+      enable,
+    };
+  });
+  // 根据开启顺序，排列展示顺序
+  const dashboardCards = p(cards, draft => {
+    draft.sort((next, prev) => {
+      const nextIndex = cardSettings.findIndex(cardSetting => cardSetting.key === next.key);
+      const prevIndex = cardSettings.findIndex(cardSetting => cardSetting.key === prev.key);
+      return nextIndex - prevIndex;
+    });
+  });
 
-  const dashboardCards = cards.map(card => ({
-    ...card,
-    enable: api._.get(cardSettings[`${card.key}`], 'enable', true),
-  }));
   const contextValue = {
     api,
     dbPath,
+    cards,
     dashboardCards,
     loading,
     setCardSettings,
