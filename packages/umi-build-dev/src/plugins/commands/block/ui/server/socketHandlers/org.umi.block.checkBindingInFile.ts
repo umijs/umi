7165e5320e406d564e7adbcd54f7af6b91800d0d
信鑫-King <45808948@qq.com>
2019-12-12 16:05:09 +0800

    fix: umi ui blocks page target path exist (#3781)
    * fix: umi ui blocks page target path exist
    
    * fix: test case
    
    * fix: test case

--- packages/umi-build-dev/src/plugins/commands/block/ui/server/socketHandlers/org.umi.block.checkBindingInFile.ts
+++ packages/umi-build-dev/src/plugins/commands/block/ui/server/socketHandlers/org.umi.block.checkBindingInFile.ts
@@ -2,7 +2,7 @@ import { join } from 'path';
 import { existsSync, readFileSync } from 'fs';
 import haveRootBinding from '../../../sdk/haveRootBinding';
 
-export default function({ success, payload, api, failure }) {
+export default async ({ success, payload, api, failure }) => {
   const { path: targetPath, name } = payload as {
     path: string;
     name: string;
@@ -15,7 +15,11 @@ export default function({ success, payload, api, failure }) {
     ),
   );
   // 有些用户路由下载路径是不在的，这里拦住他们
-  if (!existsSync(absTargetPath)) {
+  if (
+    !existsSync(absTargetPath) &&
+    // 当前路由为单文件
+    !api.findJS(absTargetPath)
+  ) {
     failure({
       message: ` ${absTargetPath} 目录不存在!`,
       success: false,
@@ -31,10 +35,10 @@ export default function({ success, payload, api, failure }) {
     });
     return;
   }
-  haveRootBinding(readFileSync(entryPath, 'utf-8'), name).then(exists => {
-    success({
-      exists,
-      success: true,
-    });
+  const exists = await haveRootBinding(readFileSync(entryPath, 'utf-8'), name);
+
+  success({
+    exists,
+    success: true,
   });
-}
+};
