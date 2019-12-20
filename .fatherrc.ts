52a454a7b2a17c111fa994d055298a61ff499eb1
sorrycc <sorrycc@gmail.com>
2019-12-20 23:48:26 +0800

    chore: fix build order

--- .fatherrc.ts
+++ .fatherrc.ts
@@ -1,5 +1,13 @@
+import { readdirSync } from 'fs';
+import { join } from 'path';
+
+const pkgs = readdirSync(join(__dirname, 'packages')).filter(
+  pkg => pkg.charAt(0) !== '.' && pkg !== 'utils',
+);
+
 export default {
   target: 'node',
   cjs: { type: 'babel', lazy: true },
   disableTypeCheck: true,
+  pkgs: ['utils', ...pkgs],
 };
