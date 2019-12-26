73c6fa1f878b3b21b1a5d9ea019597ab75c429d7
sorrycc <sorrycc@gmail.com>
2019-12-26 20:07:30 +0800

    feat(renderer-react): renderClient

--- packages/renderer-react/src/index.ts
+++ packages/renderer-react/src/index.ts
@@ -1,2 +1,2 @@
-export { default as renderClient } from './renderClient';
+export { default as renderClient } from './renderClient/renderClient';
 export { default as renderRoutes } from './renderRoutes/renderRoutes';
---
+++ packages/renderer-react/src/renderClient/renderClient.tsx
@@ -0,0 +1,67 @@
+import * as ReactDOM from 'react-dom';
+import React, { useEffect } from 'react';
+import { Plugin, ApplyPluginsType, Router } from '@umijs/runtime';
+import { IRoute } from '../types';
+import renderRoutes from '../renderRoutes/renderRoutes';
+
+interface IRouterComponentProps {
+  routes: IRoute[];
+  plugin: Plugin;
+  history: any;
+  ssrProps?: object;
+}
+
+interface IOpts extends IRouterComponentProps {
+  rootElement?: string | HTMLElement;
+}
+
+function RouterComponent(props: IRouterComponentProps) {
+  const { history, ...renderRoutesProps } = props;
+
+  useEffect(() => {
+    function routeChangeHandler(location: any, action?: string) {
+      props.plugin.applyPlugins({
+        key: 'onRouteChange',
+        type: ApplyPluginsType.event,
+        args: {
+          routes: props.routes,
+          location,
+          action,
+        },
+      });
+    }
+
+    routeChangeHandler(history.location, 'POP');
+    return history.listen(routeChangeHandler);
+  }, [history]);
+
+  return <Router history={history}>{renderRoutes(renderRoutesProps)}</Router>;
+}
+
+export default function renderClient(opts: IOpts) {
+  const rootContainer = opts.plugin.applyPlugins({
+    type: ApplyPluginsType.modify,
+    key: 'rootContainer',
+    initialValue: (
+      <RouterComponent
+        history={opts.history}
+        routes={opts.routes}
+        plugin={opts.plugin}
+        ssrProps={opts.ssrProps}
+      />
+    ),
+  });
+
+  if (opts.rootElement) {
+    const rootElement =
+      typeof opts.rootElement === 'string'
+        ? document.getElementById(opts.rootElement)
+        : opts.rootElement;
+    ReactDOM[!!opts.ssrProps ? 'hydrate' : 'render'](
+      rootContainer,
+      rootElement,
+    );
+  } else {
+    return rootContainer;
+  }
+}
