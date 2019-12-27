a1870c34912ab26f6d7d9c12fb3c413973f8cfe0
sorrycc <sorrycc@gmail.com>
2019-12-27 16:58:44 +0800

    chore(preset-built-in): improve directory structure

--- packages/preset-built-in/src/index.ts
+++ packages/preset-built-in/src/index.ts
@@ -2,21 +2,21 @@ export default function() {
   return {
     plugins: [
       // register methods
-      require.resolve('./registerMethods'),
+      require.resolve('./plugins/registerMethods'),
 
       // misc
-      require.resolve('./routes'),
+      require.resolve('./plugins/routes'),
 
       // generate files
-      require.resolve('./generateFiles/core/history'),
-      require.resolve('./generateFiles/core/plugin'),
-      require.resolve('./generateFiles/core/routes'),
-      require.resolve('./generateFiles/core/umiExports'),
-      require.resolve('./generateFiles/umi'),
+      require.resolve('./plugins/generateFiles/core/history'),
+      require.resolve('./plugins/generateFiles/core/plugin'),
+      require.resolve('./plugins/generateFiles/core/routes'),
+      require.resolve('./plugins/generateFiles/core/umiExports'),
+      require.resolve('./plugins/generateFiles/umi'),
 
       // commands
-      require.resolve('./commands/dev/dev'),
-      require.resolve('./commands/build/build'),
+      require.resolve('./plugins/commands/dev/dev'),
+      require.resolve('./plugins/commands/build/build'),
     ],
   };
 }
