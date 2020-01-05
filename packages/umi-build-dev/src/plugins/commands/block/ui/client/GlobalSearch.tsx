5db81471d72c8ae3684ebc1d6055e6093c99b633
信鑫-King <45808948@qq.com>
2020-01-05 18:24:08 +0800

    fix: ui hooks compat (#3869)
    * upd: yarn.lock support father-build optional chain
    
    * fix: dashboard avoid
    
    * fix: action min-height
    
    * :memo: add developer ui plugin asset docs

--- packages/umi-build-dev/src/plugins/commands/block/ui/client/GlobalSearch.tsx
+++ packages/umi-build-dev/src/plugins/commands/block/ui/client/GlobalSearch.tsx
@@ -12,13 +12,18 @@ interface IGlobalSearch {
 
 const GlobalSearch: React.SFC<IGlobalSearch> = props => {
   const { onChange, api } = props;
-  const { intl, hooks } = api;
-  const { useDebounceFn } = hooks;
+  const { intl, hooks, _ } = api;
+  let debounceFn = _.debounce;
+  // compatible with prev version umi ui
+  if (hooks?.useDebounceFn) {
+    debounceFn = hooks.useDebounceFn;
+  }
 
   // 时间太长会造成卡顿的感觉，200-300 比较合适
-  const { run: handleChangeDebounce } = useDebounceFn((value: string) => {
+  const handleChange = debounceFn((value: string) => {
     onChange(value);
   }, 300);
+  const handleChangeDebounce = handleChange?.run || handleChange;
 
   return (
     <Input
