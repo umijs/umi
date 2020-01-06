836f0dd912d7d20983b175242e833632638dea7d
sorrycc <sorrycc@gmail.com>
2020-01-06 22:37:00 +0800

    feat(preset-built-in): integrate @umijs/routes

--- packages/preset-built-in/src/plugins/generateFiles/core/routes.ts
+++ packages/preset-built-in/src/plugins/generateFiles/core/routes.ts
@@ -1,18 +1,25 @@
 import { readFileSync } from 'fs';
 import { join } from 'path';
 import { IApi } from '@umijs/types';
+import { Route } from '@umijs/routes';
 
 export default function(api: IApi) {
   const {
     utils: { Mustache },
+    paths,
   } = api;
 
   api.onGenerateFiles(async args => {
     const routesTpl = readFileSync(join(__dirname, 'routes.tpl'), 'utf-8');
+    const route = new Route();
+    const routes = route.getRoutes({
+      config: api.config as object,
+      root: paths.absPagesPath,
+    });
     api.writeTmpFile({
       path: 'core/routes.ts',
       content: Mustache.render(routesTpl, {
-        routes: `[]`,
+        routes: route.getJSON({ routes }),
       }),
     });
   });
