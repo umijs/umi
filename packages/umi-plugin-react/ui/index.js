69737278dd2661cb9711e4cac38b94546f43ebce
ycjcl868 <45808948@qq.com>
2020-01-24 14:51:36 +0800

    fix: unused code

--- packages/umi-plugin-react/ui/index.js
+++ packages/umi-plugin-react/ui/index.js
@@ -16,7 +16,7 @@ export default api => {
     ),
     component: () => (
       <ConfigForm
-        title="umi-plugin-react 配置-test"
+        title="umi-plugin-react 配置"
         list="org.umi.umi-plugin-react.config.list"
         edit="org.umi.umi-plugin-react.config.edit"
       />
