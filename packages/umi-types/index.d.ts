35fdaf80bd1b3ecbd4ce83b65da7b768860606ec
信鑫-King <45808948@qq.com>
2020-02-04 14:32:17 +0800

    Revert "fix: ui types use @umijs/ui-types (#3952)" (#3955)
    This reverts commit 36dbcec7469e865f08f599aa9805944996fdd334.

--- packages/umi-types/index.d.ts
+++ packages/umi-types/index.d.ts
@@ -1,6 +1,6 @@
 import 'cheerio';
 import IConfig, { IPlugin, IAFWebpackConfig, IRoute } from './config';
-import * as IUi from '@umijs/ui-types';
+import * as IUi from './ui';
 import { DefaultMethods } from 'signale';
 import * as lodash from 'lodash';
 import * as IWebpack from 'webpack';
@@ -22,7 +22,7 @@ export interface MultiStats {
   hash: string;
 }
 // for ui plugin developer
-type IUiApi = IUi.IUiApi;
+type IUiApi = IUi.IApi;
 
 export { IConfig, IPlugin, IRoute, IWebpackChainConfig, IWebpack, IUi, IUiApi };
 
