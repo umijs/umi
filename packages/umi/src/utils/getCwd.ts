f927079f6762cbc22186836b21053b7136f68174
信鑫-King <chaolin.jcl@alibaba-inc.com>
2020-02-23 16:35:32 +0800

    fix: cwd APP_ROOT (#80)

--- packages/umi/src/utils/getCwd.ts
+++ packages/umi/src/utils/getCwd.ts
@@ -1,9 +1,13 @@
-import { join } from 'path';
+import { join, isAbsolute } from 'path';
 
 export default () => {
   let cwd = process.cwd();
   if (process.env.APP_ROOT) {
-    cwd = join(cwd, process.env.APP_ROOT);
+    // avoid repeat cwd path
+    if (!isAbsolute(process.env.APP_ROOT)) {
+      return join(cwd, process.env.APP_ROOT);
+    }
+    return process.env.APP_ROOT;
   }
   return cwd;
 };
