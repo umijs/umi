7a1b07f7cf678d397f0c05cd288c7f557dcbaeb9
Peach <scdzwyxst@gmail.com>
2020-02-25 09:31:01 +0800

    docs: add menu config for docs (#83)

--- .umirc.ts
+++ .umirc.ts
@@ -4,6 +4,98 @@ export default {
     title: 'UmiJS',
     include: ['./docs'],
     previewLangs: [],
+    menus: {
+      '/zh-CN/docs': [
+        {
+          title: '介绍',
+          children: ['/docs/README', '/docs/getting-started'],
+        },
+        {
+          title: 'umi 基础',
+          children: [
+            '/docs/directory-structure',
+            '/docs/config.zh-CN',
+            '/docs/routing',
+            '/docs/convention-routing',
+            '/docs/plugin',
+            '/docs/navigate-between-pages',
+            '/docs/html-template',
+            '/docs/runtime-config',
+            '/docs/mock',
+            '/docs/env-variables',
+          ],
+        },
+        {
+          title: '样式和资源文件',
+          children: ['/docs/assets-css', '/docs/assets-image'],
+        },
+        {
+          title: 'umi 进阶',
+          children: [
+            '/docs/assets-css',
+            '/docs/assets-image',
+            '/docs/deployment',
+          ],
+        },
+        {
+          title: 'API 索引',
+          path: '/docs/api',
+        },
+        {
+          title: '贡献',
+          children: ['/docs/contributing'],
+        },
+        {
+          title: 'FAQ',
+          path: '/docs/faq',
+        },
+      ],
+      '/docs': [
+        {
+          title: '介绍',
+          children: ['/docs/README', '/docs/getting-started'],
+        },
+        {
+          title: 'umi 基础',
+          children: [
+            '/docs/directory-structure',
+            '/docs/config.md',
+            '/docs/routing',
+            '/docs/convention-routing',
+            '/docs/plugin',
+            '/docs/navigate-between-pages',
+            '/docs/html-template',
+            '/docs/runtime-config',
+            '/docs/mock',
+            '/docs/env-variables',
+          ],
+        },
+        {
+          title: '样式和资源文件',
+          children: ['/docs/assets-css', '/docs/assets-image'],
+        },
+        {
+          title: 'umi 进阶',
+          children: [
+            '/docs/assets-css',
+            '/docs/assets-image',
+            '/docs/deployment',
+          ],
+        },
+        {
+          title: 'API 索引',
+          path: '/docs/api',
+        },
+        {
+          title: '贡献',
+          children: ['/docs/contributing'],
+        },
+        {
+          title: 'FAQ',
+          path: '/docs/faq',
+        },
+      ],
+    },
   },
   exportStatic: {},
 };
