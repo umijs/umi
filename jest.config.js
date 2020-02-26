994f4b05916a0fe232a6d0f62f1aa6807e550411
chencheng <sorrycc@gmail.com>
2020-02-26 11:30:57 +0800

    test: improve coverage by skip files, 😢

--- jest.config.js
+++ jest.config.js
@@ -6,11 +6,17 @@ module.exports = {
       '!packages/bundler-webpack/src/getConfig/setPublicPath.ts',
       '!packages/bundler-webpack/src/getConfig/runtimePublicPathEntry.ts',
       '!packages/bundler-webpack/src/webpackHotDevClient/*',
+
+      // cli 入口不测
       '!packages/bundler-webpack/src/cli.ts',
       '!packages/umi/src/cli.ts',
       '!packages/umi/src/forkedDev.ts',
       '!packages/umi/src/ServiceWithBuiltIn.ts',
       '!packages/umi/src/utils/fork.ts',
+      '!packages/create-app/src/cli.ts',
+
+      // dev 在 fork 出来的子进程下，测不了
+      '!packages/preset-built-in/src/plugins/commands/dev/**/*',
     ]);
   },
 };
