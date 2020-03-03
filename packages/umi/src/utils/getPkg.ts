75d9346679d36da26c2b617fcf10c2d88c4e8e3f
Howard.Zuo <leftstick@qq.com>
2020-03-03 23:48:05 +0800

    refactor: handle APP_ROOT scenario in getPkg.ts

--- packages/umi/src/utils/getPkg.ts
+++ packages/umi/src/utils/getPkg.ts
@@ -1,9 +1,24 @@
-import { join } from 'path';
+import { join, isAbsolute } from 'path';
 
 export default (dir: string) => {
   try {
+    if (process.env.APP_ROOT) {
+      // avoid repeat cwd path
+      if (!isAbsolute(process.env.APP_ROOT)) {
+        return require(join(
+          process.cwd(),
+          process.env.APP_ROOT,
+          'package.json',
+        ));
+      }
+      return require(join(process.env.APP_ROOT, 'package.json'));
+    }
     return require(join(dir, 'package.json'));
   } catch (error) {
-    return null;
+    try {
+      return require(join(dir, 'package.json'));
+    } catch (error) {
+      return null;
+    }
   }
 };
