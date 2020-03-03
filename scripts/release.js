7f0d2a14f65c1e2253d0f489af400a08972bae7a
laiguangxiong <laiguangxiong@jianhui.com>
2020-03-03 12:55:38 +0800

    修改 scripts/release.js，只用于发布

--- scripts/release.js
+++ scripts/release.js
@@ -20,26 +20,28 @@ function logStep(name) {
 }
 
 async function release() {
-  // Check git status
-  if (!args.skipGitStatusCheck) {
-    const gitStatus = execa.sync('git', ['status', '--porcelain']).stdout;
-    if (gitStatus.length) {
-      printErrorAndExit(`Your git status is not clean. Aborting.`);
-    }
-  } else {
-    logStep(
-      'git status check is skipped, since --skip-git-status-check is supplied',
-    );
-  }
+  console.log('args', args);
 
-  // Check npm registry
-  logStep('check npm registry');
-  const userRegistry = execa.sync('npm', ['config', 'get', 'registry']).stdout;
-  if (userRegistry.includes('https://registry.yarnpkg.com/')) {
-    printErrorAndExit(
-      `Release failed, please use ${chalk.blue('npm run release')}.`,
-    );
-  }
+  // Check git status
+  // if (!args.skipGitStatusCheck) {
+  //   const gitStatus = execa.sync('git', ['status', '--porcelain']).stdout;
+  //   if (gitStatus.length) {
+  //     printErrorAndExit(`Your git status is not clean. Aborting.`);
+  //   }
+  // } else {
+  //   logStep(
+  //     'git status check is skipped, since --skip-git-status-check is supplied',
+  //   );
+  // }
+  //
+  // // Check npm registry
+  // logStep('check npm registry');
+  // const userRegistry = execa.sync('npm', ['config', 'get', 'registry']).stdout;
+  // if (userRegistry.includes('https://registry.yarnpkg.com/')) {
+  //   printErrorAndExit(
+  //     `Release failed, please use ${chalk.blue('npm run release')}.`,
+  //   );
+  // }
   // 因为是发布到私服，所以不需要是 https://registry.npmjs.org/
   // if (!userRegistry.includes('https://registry.npmjs.org/')) {
   //   const registry = chalk.blue('https://registry.npmjs.org/');
@@ -48,75 +50,76 @@ async function release() {
 
   let updated = null;
 
-  if (!args.publishOnly) {
-    // Get updated packages
-    logStep('check updated packages');
-    const updatedStdout = execa.sync(lernaCli, ['changed']).stdout;
-    updated = updatedStdout
-      .split('\n')
-      .map(pkg => {
-        if (pkg === 'umi') return pkg;
-        else return pkg.split('/')[1];
-      })
-      .filter(Boolean);
-    if (!updated.length) {
-      printErrorAndExit('Release failed, no updated package is updated.');
-    }
-
-    // Clean
-    logStep('clean');
-
-    // Build
-    if (!args.skipBuild) {
-      logStep('build');
-      await exec('npm', ['run', 'build']);
-    } else {
-      logStep('build is skipped, since args.skipBuild is supplied');
-    }
-
-    // Bump version
-    logStep('bump version with lerna version');
-    await exec(lernaCli, [
-      'version',
-      '--exact',
-      '--no-commit-hooks',
-      '--no-git-tag-version',
-      '--no-push',
-    ]);
-
-    const currVersion = require('../lerna').version;
-
-    // Sync version to root package.json
-    logStep('sync version to root package.json');
-    const rootPkg = require('../package');
-    Object.keys(rootPkg.devDependencies).forEach(name => {
-      if (name.startsWith('@umijs/') && name !== '@umijs/preset-dumi') {
-        rootPkg.devDependencies[name] = currVersion;
-      }
-    });
-    writeFileSync(
-      join(__dirname, '..', 'package.json'),
-      JSON.stringify(rootPkg, null, 2) + '\n',
-      'utf-8',
-    );
-
-    // Commit
-    const commitMessage = `release: v${currVersion}`;
-    logStep(`git commit with ${chalk.blue(commitMessage)}`);
-    await exec('git', ['commit', '--all', '--message', commitMessage]);
-
-    // Git Tag
-    logStep(`git tag v${currVersion}`);
-    await exec('git', ['tag', `v${currVersion}`]);
-
-    // Push
-    logStep(`git push`);
-    await exec('git', ['push', 'origin', 'master', '--tags']);
-  }
+  // if (!args.publishOnly) {
+  //   // Get updated packages
+  //   logStep('check updated packages');
+  //   const updatedStdout = execa.sync(lernaCli, ['changed']).stdout;
+  //   updated = updatedStdout
+  //     .split('\n')
+  //     .map(pkg => {
+  //       if (pkg === 'umi') return pkg;
+  //       else return pkg.split('/')[1];
+  //     })
+  //     .filter(Boolean);
+  //   if (!updated.length) {
+  //     printErrorAndExit('Release failed, no updated package is updated.');
+  //   }
+  //
+  //   // Clean
+  //   logStep('clean');
+  //
+  //   // Build
+  //   if (!args.skipBuild) {
+  //     logStep('build');
+  //     await exec('npm', ['run', 'build']);
+  //   } else {
+  //     logStep('build is skipped, since args.skipBuild is supplied');
+  //   }
+  //
+  //   // Bump version
+  //   logStep('bump version with lerna version');
+  //   await exec(lernaCli, [
+  //     'version',
+  //     '--exact',
+  //     '--no-commit-hooks',
+  //     '--no-git-tag-version',
+  //     '--no-push',
+  //   ]);
+  //
+  //   const currVersion = require('../lerna').version;
+  //
+  //   // Sync version to root package.json
+  //   logStep('sync version to root package.json');
+  //   const rootPkg = require('../package');
+  //   Object.keys(rootPkg.devDependencies).forEach(name => {
+  //     if (name.startsWith('@umijs/') && name !== '@umijs/preset-dumi') {
+  //       rootPkg.devDependencies[name] = currVersion;
+  //     }
+  //   });
+  //   writeFileSync(
+  //     join(__dirname, '..', 'package.json'),
+  //     JSON.stringify(rootPkg, null, 2) + '\n',
+  //     'utf-8',
+  //   );
+  //
+  //   // Commit
+  //   const commitMessage = `release: v${currVersion}`;
+  //   logStep(`git commit with ${chalk.blue(commitMessage)}`);
+  //   await exec('git', ['commit', '--all', '--message', commitMessage]);
+  //
+  //   // Git Tag
+  //   logStep(`git tag v${currVersion}`);
+  //   await exec('git', ['tag', `v${currVersion}`]);
+  //
+  //   // Push
+  //   logStep(`git push`);
+  //   await exec('git', ['push', 'origin', 'master', '--tags']);
+  // }
 
   // Publish
   // Umi must be the latest.
-  const pkgs = args.publishOnly ? getPackages() : updated;
+  // const pkgs = args.publishOnly ? getPackages() : updated;
+  const pkgs = getPackages();
   logStep(`publish packages: ${chalk.blue(pkgs.join(', '))}`);
   const currVersion = require('../lerna').version;
   const isNext = isNextVersion(currVersion);
@@ -142,7 +145,7 @@ async function release() {
     });
 
   logStep('done');
-  syncTNPM(pkgs);
+  // syncTNPM(pkgs);
 }
 
 release().catch(err => {
