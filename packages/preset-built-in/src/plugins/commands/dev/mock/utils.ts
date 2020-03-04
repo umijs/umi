c9bd4e5066a093953189e8351c8f5f3db8316a76
ycjcl868 <45808948@qq.com>
2020-03-04 17:21:36 +0800

    fix: mock path

--- packages/preset-built-in/src/plugins/commands/dev/mock/utils.ts
+++ packages/preset-built-in/src/plugins/commands/dev/mock/utils.ts
@@ -3,7 +3,6 @@ import { winPath, createDebug, glob } from '@umijs/utils';
 import { join } from 'path';
 import { existsSync } from 'fs';
 import assert from 'assert';
-import * as url from 'url';
 import bodyParser from 'body-parser';
 import multer from 'multer';
 import { pathToRegexp } from 'path-to-regexp';
@@ -170,18 +169,15 @@ export const normalizeConfig = (config: any) => {
     );
     const { method, path } = parseKey(key);
     const keys: any[] = [];
-    const { pathname } = url.parse(path);
-    if (pathname) {
-      // @ts-ignore
-      const re = pathToRegexp(pathname, keys);
-      memo.push({
-        method,
-        path: pathname,
-        re,
-        keys,
-        handler: createHandler(method, pathname, handler),
-      });
-    }
+    // @ts-ignore
+    const re = pathToRegexp(path, keys);
+    memo.push({
+      method,
+      path,
+      re,
+      keys,
+      handler: createHandler(method, path, handler),
+    });
     return memo;
   }, []);
 };
