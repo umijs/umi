88c9124a6908d1cbe3d0f381f4565695b206d963
Howard.Zuo <leftstick@qq.com>
2020-03-04 13:01:33 +0800

    refactor: handle APP_ROOT scenario in getPkg.ts (#4056)
    * refactor: handle APP_ROOT scenario in getPkg.ts
    
    * refactor: reuse getCwd

--- packages/umi/src/utils/getPkg.ts
+++ packages/umi/src/utils/getPkg.ts
@@ -1,9 +1,14 @@
-import { join } from 'path';
+import { join, isAbsolute } from 'path';
+import getCwd from './getCwd';
 
 export default (dir: string) => {
   try {
-    return require(join(dir, 'package.json'));
+    return require(join(getCwd(), 'package.json'));
   } catch (error) {
-    return null;
+    try {
+      return require(join(dir, 'package.json'));
+    } catch (error) {
+      return null;
+    }
   }
 };
