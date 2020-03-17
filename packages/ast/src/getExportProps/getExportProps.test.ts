56577c7b30e2219c4ec080ad35ff3c52b314d8af
Howard.Zuo <leftstick@qq.com>
2020-03-17 16:46:03 +0800

    feat: assign array, object property on exported Component is supported now (#4154)
    * feat: assign array, object property on exported Component is supported now
    
    * feat: null, undefined shall be supported as well
    
    * refactor: use whitelist instead

--- packages/ast/src/getExportProps/getExportProps.test.ts
+++ packages/ast/src/getExportProps/getExportProps.test.ts
@@ -9,12 +9,24 @@ foo.a = 1;
 foo.b = '2';
 foo.c = function() {};
 
-// TODO: support object and array
-foo.d = {};
-foo.e = [];
+foo.d = {
+  aa: '1',
+  bb: true,
+  cc: {
+    dd: 90
+  },
+  ee: [2],
+  ff: null,
+  gg: undefined,
+  hh: () => {},
+  jj() {}
+};
+foo.e = ['hh', { ff: 66 }, ['gg'], null, undefined, () => {}];
 
 foo.f = true;
 foo.g = false;
+foo.i = null;
+foo.j = undefined;
 bar.h = true;
 export default foo;
     `,
@@ -22,7 +34,29 @@ export default foo;
   expect(props).toEqual({
     a: 1,
     b: '2',
+    d: {
+      aa: '1',
+      bb: true,
+      cc: {
+        dd: 90,
+      },
+      ee: [2],
+      ff: null,
+      gg: undefined,
+    },
+    e: ['hh', { ff: 66 }, ['gg'], null, undefined],
     f: true,
     g: false,
+    i: null,
+    j: undefined,
   });
+});
+
+test('no default export', () => {
+  const props = getExportProps(
+    `
+export function foo () {}
+    `,
+  );
+  expect(props).toEqual({});
 });
---
+++ packages/ast/src/getExportProps/propertyResolver.ts
@@ -0,0 +1,105 @@
+import { t } from '@umijs/utils';
+
+interface IResolver<U> {
+  is(src: any): boolean;
+  get(src: U): any;
+}
+
+const StringResolver: IResolver<t.StringLiteral> = {
+  is(src: any) {
+    return t.isStringLiteral(src);
+  },
+  get(src) {
+    return src.value;
+  },
+};
+
+const NumberResolver: IResolver<t.NumericLiteral> = {
+  is(src: any) {
+    return t.isNumericLiteral(src);
+  },
+  get(src) {
+    return src.value;
+  },
+};
+
+const BooleanResolver: IResolver<t.BooleanLiteral> = {
+  is(src: any) {
+    return t.isBooleanLiteral(src);
+  },
+  get(src) {
+    return src.value;
+  },
+};
+
+const NullResolver: IResolver<t.NullLiteral> = {
+  is(src: any) {
+    return t.isNullLiteral(src);
+  },
+  get(src) {
+    return null;
+  },
+};
+
+const UndefinedResolver: IResolver<t.Identifier> = {
+  is(src: any) {
+    return t.isIdentifier(src) && src.name === 'undefined';
+  },
+  get(src) {
+    return undefined;
+  },
+};
+
+const ObjectResolver: IResolver<t.ObjectExpression> = {
+  is(src: any) {
+    return t.isObjectExpression(src);
+  },
+  get(src) {
+    return findObjectProperties(src);
+  },
+};
+
+const ArrayResolver: IResolver<t.ArrayExpression> = {
+  is(src: any) {
+    return t.isArrayExpression(src);
+  },
+  get(src) {
+    return findArrayProperties(src);
+  },
+};
+
+export const RESOLVABLE_WHITELIST = [
+  StringResolver,
+  NumberResolver,
+  BooleanResolver,
+  NullResolver,
+  UndefinedResolver,
+  ObjectResolver,
+  ArrayResolver,
+];
+
+function findObjectProperties(node: t.ObjectExpression) {
+  const target = {};
+  node.properties.forEach(p => {
+    if (t.isObjectProperty(p) && t.isIdentifier(p.key)) {
+      const resolver = RESOLVABLE_WHITELIST.find(resolver =>
+        resolver.is(p.value),
+      );
+      if (resolver) {
+        target[p.key.name] = resolver.get(p.value as any);
+      }
+    }
+  });
+  return target;
+}
+
+function findArrayProperties(node: t.ArrayExpression) {
+  const target: any[] = [];
+  node.elements.forEach(p => {
+    const resolver = RESOLVABLE_WHITELIST.find(resolver => resolver.is(p));
+    if (resolver) {
+      target.push(resolver.get(p as any));
+    }
+  });
+  return target;
+}
