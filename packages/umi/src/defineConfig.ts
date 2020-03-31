432b91a02f2f5a35732c8479cac9392c28fd97dd
ycjcl868 <45808948@qq.com>
2020-03-31 19:42:03 +0800

    fix: types

--- packages/umi/src/defineConfig.ts
+++ packages/umi/src/defineConfig.ts
@@ -2,6 +2,6 @@ import { IConfig } from '@umijs/types';
 // @ts-ignore
 import { IConfigFromPlugins } from '@@/core/pluginConfig';
 
-export function defineConfig(config: IConfigFromPlugins & IConfig) {
+export function defineConfig(config: IConfig | IConfigFromPlugins) {
   return config;
 }
