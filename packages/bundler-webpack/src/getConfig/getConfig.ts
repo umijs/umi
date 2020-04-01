607515fe10027cd813084f6bffb0a6af3bc372dc
chencheng (云谦) <sorrycc@gmail.com>
2020-04-01 09:05:39 +0800

    feat: chainWebpack config support async (#4344)

--- packages/bundler-webpack/src/getConfig/getConfig.ts
+++ packages/bundler-webpack/src/getConfig/getConfig.ts
@@ -412,7 +412,7 @@ export default async function getConfig(
   }
   // 用户配置的 chainWebpack 优先级最高
   if (config.chainWebpack) {
-    config.chainWebpack(webpackConfig, {
+    await config.chainWebpack(webpackConfig, {
       env,
       webpack: bundleImplementor,
       createCSSRule: createCSSRuleFn,
