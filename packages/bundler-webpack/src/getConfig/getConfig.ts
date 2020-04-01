d0de2130413449d07dc6078b44bd1c3bd4f6d7a3
bibidu <787753295@qq.com>
2020-04-01 11:39:09 +0800

    fix: 修复cwd祖先目录下的文件不触发编译的bug (#4327)
    * fix: 修复cwd祖先目录下的文件不触发编译的bug
    
    在配置了include的情况下，不符合条件的文件不会触发编译。当某文件引用了cwd祖先目录下的js mjs jsx ts等文件时，就会出现不触发编译导致报错的问题。考虑移除include cwd，依赖webpack自动查找引用并编译。
    
    * test: 添加移除webpack include配置项的测试用例

--- packages/bundler-webpack/src/getConfig/getConfig.ts
+++ packages/bundler-webpack/src/getConfig/getConfig.ts
@@ -66,7 +66,7 @@ export default async function getConfig(
 
   // entry
   if (entry) {
-    Object.keys(entry).forEach((key) => {
+    Object.keys(entry).forEach(key => {
       const e = webpackConfig.entry(key);
       if (hot && isDev) {
         e.add(require.resolve('../webpackHotDevClient/webpackHotDevClient'));
@@ -124,7 +124,7 @@ export default async function getConfig(
 
   // resolve.alias
   if (config.alias) {
-    Object.keys(config.alias).forEach((key) => {
+    Object.keys(config.alias).forEach(key => {
       webpackConfig.resolve.alias.set(key, config.alias![key]);
     });
   }
@@ -161,7 +161,8 @@ export default async function getConfig(
   webpackConfig.module
     .rule('js')
       .test(/\.(js|mjs|jsx|ts|tsx)$/)
-      .include.add(cwd).end()
+      // 不配置 include，因为在使用 APP_ROOT 的场景下通常会 import cwd 之外的文件
+      // ref: https://github.com/umijs/umi/pull/4327
       .exclude.add(/node_modules/).end()
       .use('babel-loader')
         .loader(require.resolve('babel-loader'))
@@ -310,7 +311,7 @@ export default async function getConfig(
         to: absOutputPath,
       },
       ...(config.copy
-        ? config.copy.map((from) => ({
+        ? config.copy.map(from => ({
             from: join(cwd, from),
             to: absOutputPath,
           }))
@@ -336,14 +337,14 @@ export default async function getConfig(
 
   webpackConfig.when(
     isDev,
-    (webpackConfig) => {
+    webpackConfig => {
       if (hot) {
         webpackConfig
           .plugin('hmr')
           .use(bundleImplementor.HotModuleReplacementPlugin);
       }
     },
-    (webpackConfig) => {
+    webpackConfig => {
       // don't emit files if have error
       webpackConfig.optimization.noEmitOnErrors(true);
 
