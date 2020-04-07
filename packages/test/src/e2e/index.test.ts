221ef0fb9f0d20995e4aff2b7f2bc0ee84cc67e2
chenshuai2144 <qixian.cs@outlook.com>
2020-04-07 23:06:36 +0800

    add icon test

--- packages/test/src/e2e/index.test.ts
+++ packages/test/src/e2e/index.test.ts
@@ -15,16 +15,9 @@ test('with img', async () => {
 test('home page should has img', async () => {
   await page.goto('https://umijs.org');
   await page.setFamily();
-  const iconText = await page.$('.__dumi-default-navbar-logo');
   const text = await page.$eval(
     '.__dumi-default-navbar-logo',
     (e) => e.textContent,
   );
   expect(text).toEqual('UmiJS');
-  const image = await iconText?.screenshot();
-  expect(image).toMatchImageSnapshot({
-    dumpDiffToConsole: true,
-    failureThreshold: 0.2,
-    blur: 5,
-  });
 });
