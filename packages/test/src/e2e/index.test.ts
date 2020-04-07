05fc80e344bc8c7c6ceff52f240730de6984bea4
chenshuai2144 <qixian.cs@outlook.com>
2020-04-07 22:56:58 +0800

    add icon test

--- packages/test/src/e2e/index.test.ts
+++ packages/test/src/e2e/index.test.ts
@@ -8,6 +8,23 @@ test('with img', async () => {
   expect(image).toMatchImageSnapshot({
     dumpDiffToConsole: true,
     failureThreshold: 0.2,
-    blur: 10,
+    blur: 5,
+  });
+});
+
+test('home page should has img', async () => {
+  await page.goto('https://umijs.org');
+  await page.setFamily();
+  const iconText = await page.$('.__dumi-default-navbar-logo');
+  const text = await page.$eval(
+    '.__dumi-default-navbar-logo',
+    (e) => e.textContent,
+  );
+  expect(text).toEqual('UmiJS');
+  const image = await iconText?.screenshot();
+  expect(image).toMatchImageSnapshot({
+    dumpDiffToConsole: true,
+    failureThreshold: 0.2,
+    blur: 5,
   });
 });
