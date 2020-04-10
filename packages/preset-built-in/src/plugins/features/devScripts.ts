8d089bbff0b8292005df2680473e773eca416abd
chencheng (云谦) <sorrycc@gmail.com>
2020-04-10 11:40:13 +0800

    fix: webpack hot client don't included in some situation (#4419)

--- packages/preset-built-in/src/plugins/features/devScripts.ts
+++ packages/preset-built-in/src/plugins/features/devScripts.ts
@@ -51,52 +51,57 @@ export default (api: IApi) => {
   api.onGenerateFiles(() => {
     api.writeTmpFile({
       path: './core/devScripts.ts',
-      content: `
-window.g_initWebpackHotDevClient({
-  tryApplyUpdates(onHotUpdateSuccess?: Function) {
-    // @ts-ignore
-    if (!module.hot) {
-      window.location.reload();
-      return;
-    }
-
-    function isUpdateAvailable() {
+      content:
+        process.env.HMR !== 'none'
+          ? `
+if (window.g_initWebpackHotDevClient) {
+  window.g_initWebpackHotDevClient({
+    tryApplyUpdates(onHotUpdateSuccess?: Function) {
       // @ts-ignore
-      return window.g_getMostRecentCompilationHash() !== __webpack_hash__;
-    }
-
-    // TODO: is update available?
-    // @ts-ignore
-    if (!isUpdateAvailable() || module.hot.status() !== 'idle') {
-      return;
-    }
-
-    function handleApplyUpdates(err: Error | null, updatedModules: any) {
-      if (err || !updatedModules || window.g_getHadRuntimeError()) {
+      if (!module.hot) {
         window.location.reload();
         return;
       }
 
-      onHotUpdateSuccess?.();
-
-      if (isUpdateAvailable()) {
-        // While we were updating, there was a new update! Do it again.
-        tryApplyUpdates();
+      function isUpdateAvailable() {
+        // @ts-ignore
+        return window.g_getMostRecentCompilationHash() !== __webpack_hash__;
       }
-    }
 
-    // @ts-ignore
-    module.hot.check(true).then(
-      function (updatedModules: any) {
-        handleApplyUpdates(null, updatedModules);
-      },
-      function (err: Error) {
-        handleApplyUpdates(err, null);
-      },
-    );
-  }
-});
-      `,
+      // TODO: is update available?
+      // @ts-ignore
+      if (!isUpdateAvailable() || module.hot.status() !== 'idle') {
+        return;
+      }
+
+      function handleApplyUpdates(err: Error | null, updatedModules: any) {
+        if (err || !updatedModules || window.g_getHadRuntimeError()) {
+          window.location.reload();
+          return;
+        }
+
+        onHotUpdateSuccess?.();
+
+        if (isUpdateAvailable()) {
+          // While we were updating, there was a new update! Do it again.
+          tryApplyUpdates();
+        }
+      }
+
+      // @ts-ignore
+      module.hot.check(true).then(
+        function (updatedModules: any) {
+          handleApplyUpdates(null, updatedModules);
+        },
+        function (err: Error) {
+          handleApplyUpdates(err, null);
+        },
+      );
+    }
+  });
+}
+      `
+          : '',
     });
   });
 
