afe2fae5c353f032c3f3ec0825851b2f8e26b095
pr <pr>
2020-04-14 14:33:05 +0800

    chore: ts checker options

--- packages/preset-built-in/src/plugins/features/forkTSChecker.ts
+++ packages/preset-built-in/src/plugins/features/forkTSChecker.ts
@@ -1,11 +1,23 @@
-import { IApi } from 'umi';
+import { IApi, utils } from 'umi';
 
 export default (api: IApi) => {
+  const { deepmerge } = utils;
   api.describe({
     key: 'forkTSCheker',
     config: {
       schema(joi) {
-        return joi.object();
+        return joi.object({
+          tsconfig: joi.string().description('Path to tsconfig.json file'),
+          compilerOptions: joi.object().description('Allows overriding TypeScript options. Should be specified in the same format as you would do for the compilerOptions property in tsconfig.json.'),
+          eslint: joi.boolean(),
+          eslintOptions: joi.object(),
+          async: joi.boolean(),
+          ignoreDiagnostics: joi.array().items(joi.number()),
+          formatter: joi.valid('default', 'codeframe'),
+          formatterOptions: joi.object(),
+          silent: joi.boolean(),
+          checkSyntacticErrors: joi.boolean(),
+        }).description('More options see https://www.npmjs.com/package/fork-ts-checker-webpack-plugin#options');
       },
     },
     enableBy: () =>
@@ -16,12 +28,12 @@ export default (api: IApi) => {
     webpackConfig
       .plugin('fork-ts-checker')
       .use(require('fork-ts-checker-webpack-plugin'), [
-        {
+        deepmerge({
           formatter: 'codeframe',
           // parallel
           async: false,
           checkSyntacticErrors: true,
-        },
+        }, api.config?.forkTSCheker || {}),
       ]);
     return webpackConfig;
   });
