a4366167f2055fac6083412904bc466af0d43655
pr <pr>
2020-04-14 12:30:12 +0800

    fix: add test case

--- packages/ast/src/isReactComponent/isReactComponent.test.ts
+++ packages/ast/src/isReactComponent/isReactComponent.test.ts
@@ -8,6 +8,10 @@ test('no jsx', () => {
   expect(isReactComponent(`export default () => <p />`)).toEqual(true);
 })
 
-test('no React Fragment', () => {
+test('React Fragment with shorthand', () => {
   expect(isReactComponent(`export default () => <>hello</>`)).toEqual(true);
+})
+
+test('React vars', () => {
+  expect(isReactComponent(`export default () => { const App = (<>hello</>); return App; }`)).toEqual(true);
 })
