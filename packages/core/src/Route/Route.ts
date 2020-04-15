58d45638b5f9d5d63a761193d919db8466f5404a
xg4 <xingor4@gmail.com>
2020-04-15 10:30:52 +0800

    fix: @/pages/404.tsx only matches /404

--- packages/core/src/Route/Route.ts
+++ packages/core/src/Route/Route.ts
@@ -57,8 +57,15 @@ class Route {
         parentRoute: opts.parentRoute,
       });
     }
+    let route404;
     for (const route of routes) {
       await this.patchRoute(route, opts);
+      if (route.path && route.path === '/404') {
+        route404 = route;
+      }
+    }
+    if (opts.isConventional && route404) {
+      routes.push({ component: route404.component });
     }
     if (this.opts.onPatchRoutes) {
       await this.opts.onPatchRoutes({
