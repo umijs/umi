8acdd311905e07e0fcd16bddb13e8a7e5a52d9da
ycjcl868 <45808948@qq.com>
2020-04-26 16:33:33 +0800

    feat: add diff

--- benchmarks/ssr/normal.js
+++ benchmarks/ssr/normal.js
@@ -1,19 +1,37 @@
+const { Stream } = require('stream');
+
 module.exports = (suite) => {
   const render = require('./fixtures/normal/dist/umi.server');
 
   // add tests
-  suite.add('ssr#normal /', {
-    defer: true,
-    fn: (deferred) => {
-      render({
-        path: '/',
-      }).then((res) => {
-        if (res.html) {
-          deferred.resolve();
-        } else {
-          suite.abort();
-        }
-      });
-    },
-  });
+  suite
+    .add('ssr#normal /', {
+      defer: true,
+      fn: (deferred) => {
+        render({
+          path: '/',
+        }).then((res) => {
+          if (res.html) {
+            deferred.resolve();
+          } else {
+            suite.abort();
+          }
+        });
+      },
+    })
+    .add('ssr#normal#stream /', {
+      defer: true,
+      fn: (deferred) => {
+        render({
+          path: '/',
+          stream: true,
+        }).then((res) => {
+          if (res.html instanceof Stream) {
+            deferred.resolve();
+          } else {
+            suite.abort();
+          }
+        });
+      },
+    });
 };
