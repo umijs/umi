6e0b34e4b9c5c860d8a2b4bef95b6b394a42f577
ycjcl868 <45808948@qq.com>
2020-04-26 11:45:51 +0800

    chore: ConfigType

--- packages/bundler-utils/src/enums.ts
+++
@@ -1,6 +0,0 @@
-// 保留，不建议使用
-// 建议使用 import { BundlerConfigType, IBundlerConfigType } from '@umijs/types'
-export enum ConfigType {
-  csr = 'csr',
-  ssr = 'ssr',
-}
--- packages/bundler-utils/src/index.ts
+++ packages/bundler-utils/src/index.ts
@@ -1,3 +1,5 @@
+// 保留，不建议使用
+// 建议使用 import { BundlerConfigType, IBundlerConfigType } from '@umijs/types'
 export { BundlerConfigType as ConfigType } from '@umijs/types';
 
 export * from './getBabelOpts';
