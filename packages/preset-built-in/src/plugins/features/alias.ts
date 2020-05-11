be307da4f7f2d12f182551029bfccbc3ef11e57a
PeachScript <scdzwyxst@gmail.com>
2020-05-11 17:21:28 +0800

    refactor: correct field for @@ path alias

--- packages/preset-built-in/src/plugins/features/alias.ts
+++ packages/preset-built-in/src/plugins/features/alias.ts
@@ -72,7 +72,7 @@ export default (api: IApi) => {
 
     // 选择在 chainWebpack 中进行以上 alias 的初始化，是为了支持用户使用 modifyPaths API 对 paths 进行改写
     memo.resolve.alias.set('@', paths.absSrcPath as string);
-    memo.resolve.alias.set('@@', paths.absSrcPath as string);
+    memo.resolve.alias.set('@@', paths.absTmpPath as string);
 
     return memo;
   });
