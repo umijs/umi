eb8459635f2ee4e7789b0ad7ec65e10e9ffb8650
信鑫-King <chaolinjin@gmail.com>
2020-07-14 09:59:38 +0800

    chore: SSG keep server file when failed (#5023)
    * chore: keep serverFile for ssg
    
    * chore: keep
    
    * chore: failed
    
    * chore: format

--- packages/preset-built-in/src/plugins/features/exportStatic.ts
+++ packages/preset-built-in/src/plugins/features/exportStatic.ts
@@ -84,6 +84,8 @@ export default (api: IApi) => {
     return routeMap;
   });
 
+  // for debug prerender error
+  let serverRenderFailed = false;
   // 不使用 api.modifyHTML 原因是不需要转 cheerio，提高预渲染效率
   api.modifyProdHTMLContent(async (memo, args) => {
     const { route } = args;
@@ -111,9 +113,11 @@ export default (api: IApi) => {
         if (!error) {
           return html;
         } else {
+          serverRenderFailed = true;
           api.logger.error('[SSR]', error);
         }
       } catch (e) {
+        serverRenderFailed = true;
         api.logger.error(`${route.path} render failed`, e);
         throw e;
       }
@@ -122,14 +126,25 @@ export default (api: IApi) => {
   });
 
   api.onBuildComplete(({ err }) => {
-    if (!err && api.config?.ssr && process.env.RM_SERVER_FILE !== 'none') {
-      // remove umi.server.js
-      const serverFilePath = join(
-        api.paths.absOutputPath!,
-        OUTPUT_SERVER_FILENAME,
-      );
-      if (existsSync(serverFilePath)) {
-        rimraf.sync(serverFilePath);
+    if (!err && api.config?.ssr) {
+      if (serverRenderFailed) {
+        // tips: COMPRESS=none to debug
+        api.logger.info('You can use COMPRESS=none to debug.');
+      }
+      // RM_SERVER_FILE prior to serverFailed
+      if (
+        process.env.RM_SERVER_FILE
+          ? process.env.RM_SERVER_FILE !== 'none'
+          : !serverRenderFailed
+      ) {
+        // remove umi.server.js
+        const serverFilePath = join(
+          api.paths.absOutputPath!,
+          OUTPUT_SERVER_FILENAME,
+        );
+        if (existsSync(serverFilePath)) {
+          rimraf.sync(serverFilePath);
+        }
       }
     }
   });
