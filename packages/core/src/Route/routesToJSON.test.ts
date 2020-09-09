2cda1cf7484572aaaf88f7485216294132355c2c
chencheng (云谦) <sorrycc@gmail.com>
2020-09-09 14:16:24 +0800

    feat: use esm for wrappers when dynamicImport is configured (#5387)

--- packages/core/src/Route/routesToJSON.test.ts
+++ packages/core/src/Route/routesToJSON.test.ts
@@ -118,3 +118,53 @@ test('component with function', () => {
   `.trim(),
   );
 });
+
+test('wrappers', () => {
+  const ret = routesToJSON({
+    routes: [
+      {
+        path: '/',
+        component: '@/pages/index.ts',
+        wrappers: ['@/wrappers/foo.ts'],
+      },
+    ],
+    config: {},
+  });
+  expect(ret).toEqual(
+    `
+[
+  {
+    "path": "/",
+    "component": require('@/pages/index.ts').default,
+    "wrappers": [require('@/wrappers/foo.ts').default]
+  }
+]
+  `.trim(),
+  );
+});
+
+test('wrappers with dynamicImport', () => {
+  const ret = routesToJSON({
+    routes: [
+      {
+        path: '/',
+        component: '@/pages/index.ts',
+        wrappers: ['@/wrappers/foo.ts'],
+      },
+    ],
+    config: {
+      dynamicImport: true,
+    },
+  });
+  expect(ret).toEqual(
+    `
+[
+  {
+    "path": "/",
+    "component": dynamic({ loader: () => import(/* webpackChunkName: 'p__index' */'@/pages/index.ts')}),
+    "wrappers": [dynamic({ loader: () => import(/* webpackChunkName: 'wrappers' */'@/wrappers/foo.ts')})]
+  }
+]
+  `.trim(),
+  );
+});
