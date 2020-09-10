0d33e8bd1d77e646bc6fcf6e381ec3791fa2c298
xiaohuoni <448627663@qq.com>
2020-09-10 10:33:51 +0800

    fix: jest window fn not implemented

--- packages/test/helpers/setupFiles/shim.js
+++ packages/test/helpers/setupFiles/shim.js
@@ -4,3 +4,14 @@ require('regenerator-runtime/runtime');
 if (typeof window !== 'undefined') {
   require('whatwg-fetch');
 }
+
+// https://github.com/jsdom/jsdom/issues/1843
+window.document.childNodes.length === 0;
+window.someCoolAPI = () => {
+  /* ... */
+};
+window.alert = (msg) => {
+  console.log(msg);
+};
+window.matchMedia = () => ({});
+window.scrollTo = () => {};
