5f101fc216341f2b2734fe72393f54a3c3d3a20b
ycjcl868 <45808948@qq.com>
2020-09-27 19:19:48 +0800

    chore: comment

--- packages/preset-built-in/src/plugins/features/ssr/ssr.ts
+++ packages/preset-built-in/src/plugins/features/ssr/ssr.ts
@@ -266,6 +266,7 @@ export default (api: IApi) => {
 
       config.output
         .filename(OUTPUT_SERVER_FILENAME)
+        // avoid using `require().default`, just using `require()`
         .libraryExport('default')
         .chunkFilename('[name].server.js')
         .libraryTarget('commonjs2');
