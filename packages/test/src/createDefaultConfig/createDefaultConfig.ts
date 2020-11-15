20995108ebd20a0bce59df53f96c134062a6f077
ycjcl868 <45808948@qq.com>
2020-11-15 23:04:28 +0800

    chore: jest

--- packages/test/src/createDefaultConfig/createDefaultConfig.ts
+++ packages/test/src/createDefaultConfig/createDefaultConfig.ts
@@ -43,7 +43,7 @@ export default function (cwd: string, args: IUmiTestArgs) {
     },
     setupFiles: [require.resolve('../../helpers/setupFiles/shim')],
     setupFilesAfterEnv: [require.resolve('../../helpers/setupFiles/jasmine')],
-    testEnvironment: require.resolve('jest-environment-jsdom'),
+    testEnvironment: require.resolve('jest-environment-jsdom-fourteen'),
     testMatch: [
       `${testMatchPrefix}**/?*.(${testMatchTypes.join('|')}).(j|t)s?(x)`,
     ],
