27bf8e586ecc25c58c27094e453c01d0028fd7e5
信鑫-King <chaolinjin@gmail.com>
2020-11-16 11:26:13 +0800

    chore: upgrade father-build (#5674)
    * chore: upgrade father-build
    
    * chore: babel-core
    
    * chore: esmodule
    
    * chore: babel
    
    * chore: @babel/core

--- packages/utils/src/compatESModuleRequire/compatESModuleRequire.ts
+++ packages/utils/src/compatESModuleRequire/compatESModuleRequire.ts
@@ -1,5 +1,5 @@
-export default function compatESModuleRequire<T extends any>(
-  m: T,
-): T extends { __esModule: true; default: infer U } ? U : T {
+export default function compatESModuleRequire<
+  T extends { __esModule: boolean; default: any }
+>(m: T): T extends { __esModule: true; default: infer U } ? U : T {
   return m.__esModule ? m.default : m;
 }
