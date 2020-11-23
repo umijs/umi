8d047a76bb3031155a258c81047256fd3fb0568a
PeachScript <scdzwyxst@gmail.com>
2020-11-23 17:23:14 +0800

    fix: workaround to resolve core-js path error bug (#5715)

--- packages/umi-build-dev/src/plugins/afwebpack-config/lockCoreJSVersionPlugin.js
+++ packages/umi-build-dev/src/plugins/afwebpack-config/lockCoreJSVersionPlugin.js
@@ -1,21 +1,47 @@
 import { dirname } from 'path';
 import { winPath } from 'umi-utils';
+import * as types from '@babel/types';
 
 const coreJSPath = dirname(require.resolve('core-js/package.json'));
+
+function isUmiCoreJSPolyfill(oFilename, identifier) {
+  const filename = winPath(oFilename);
+
+  return (
+    (filename.endsWith('.umi/polyfills.js') || filename.endsWith('.umi-production/polyfills.js')) &&
+    identifier.startsWith('core-js/')
+  );
+}
+
+function getReplacedCoreJSPath(path) {
+  return path.replace('core-js/', `${coreJSPath}/`);
+}
 
 export default function() {
   return {
     visitor: {
+      // handle import statements
       ImportDeclaration(path, state) {
-        const filename = winPath(state.filename);
+        const callPathNode = path.node;
+
         if (
-          filename.endsWith('.umi/polyfills.js') ||
-          filename.endsWith('.umi-production/polyfills.js')
+          types.isLiteral(callPathNode.source) &&
+          isUmiCoreJSPolyfill(state.filename, callPathNode.source.value)
         ) {
-          const { node } = path;
-          if (node.source.value.startsWith('core-js/')) {
-            node.source.value = node.source.value.replace('core-js/', `${coreJSPath}/`);
-          }
+          callPathNode.source.value = getReplacedCoreJSPath(callPathNode.source.value);
+        }
+      },
+      // handle require statements
+      CallExpression(path, state) {
+        const callPathNode = path.node;
+
+        if (
+          types.isIdentifier(callPathNode.callee) &&
+          callPathNode.callee.name === 'require' &&
+          types.isStringLiteral(callPathNode.arguments[0]) &&
+          isUmiCoreJSPolyfill(state.filename, callPathNode.arguments[0].value)
+        ) {
+          callPathNode.arguments[0].value = getReplacedCoreJSPath(callPathNode.arguments[0].value);
         }
       },
     },
