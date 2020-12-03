4060c03c05e736b5865ffe5f105ff2966dcbd806
信鑫-King <chaolinjin@gmail.com>
2020-12-03 13:53:24 +0800

    fix: box-orient css removed (#5776)

--- packages/bundler-webpack/src/fixtures/less/expect.ts
+++ packages/bundler-webpack/src/fixtures/less/expect.ts
@@ -4,4 +4,6 @@ export default ({ indexCSS }: IExpectOpts) => {
   expect(indexCSS).toContain(`.b___`);
   expect(indexCSS).toContain(`color: #333;`);
   expect(indexCSS).toContain(`.a {`);
+  expect(indexCSS).toContain('.c___');
+  expect(indexCSS).toContain(`-webkit-box-orient: vertical;`)
 }
