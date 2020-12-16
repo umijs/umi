0c411a8ae9ff0a72293146bb1b26e8ed1bb4f5d8
ycjcl868 <45808948@qq.com>
2020-12-16 19:49:06 +0800

    chore: test case

--- packages/bundler-webpack/src/fixtures/css-modules/index.ts
+++ packages/bundler-webpack/src/fixtures/css-modules/index.ts
@@ -1,6 +1,6 @@
 import './a.css';
 import styles from './b.css';
-import style2 from './c.less';
+import empty from './empty.less';
 
-console.log(styles);
-console.log(style2);
+console.log(styles.b);
+console.log(empty.container);
