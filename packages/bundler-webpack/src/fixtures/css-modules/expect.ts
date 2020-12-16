5ef148b3d8c2a0f19d480c10df3f01aa634b5768
信鑫-King <chaolinjin@gmail.com>
2020-12-16 19:52:52 +0800

    chore: css modules global test case (#5834)
    * chore: css modules global test case
    
    * chore: test case

--- packages/bundler-webpack/src/fixtures/css-modules/expect.ts
+++ packages/bundler-webpack/src/fixtures/css-modules/expect.ts
@@ -1,6 +1,7 @@
 import { IExpectOpts } from '../types';
 
-export default ({ indexCSS }: IExpectOpts) => {
+export default ({ indexCSS, indexJS }: IExpectOpts) => {
   expect(indexCSS).toContain(`.b___`);
   expect(indexCSS).toContain(`.a { color: red; }`);
+  expect(indexJS).toMatch(/__webpack_require__\.n\(_empty_less_modules.+\)/i);
 }
--- packages/bundler-webpack/src/fixtures/css-modules/index.ts
+++ packages/bundler-webpack/src/fixtures/css-modules/index.ts
@@ -1,4 +1,6 @@
 import './a.css';
 import styles from './b.css';
+import empty from './empty.less';
 
-console.log(styles);
+console.log(styles.b);
+console.log(empty.container);
