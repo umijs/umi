7e1cc1d4b2e5b5a0099bf0f6851ace28fa2337c6
chencheng <sorrycc@gmail.com>
2021-01-17 11:51:29 +0800

    feat: enable fastRefresh

--- packages/create-umi-app/templates/AppGenerator/src/pages/index.tsx
+++ packages/create-umi-app/templates/AppGenerator/src/pages/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import styles from './index.less';
 
-export default () => {
+export default function IndexPage() {
   return (
     <div>
       <h1 className={styles.title}>Page index</h1>
