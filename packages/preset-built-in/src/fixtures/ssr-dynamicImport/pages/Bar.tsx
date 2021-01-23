db0f32df93140a34ed7cd6e0561572c5a9eebe32
ycjcl868 <45808948@qq.com>
2021-01-23 13:27:52 +0800

    chore: test

--- packages/preset-built-in/src/fixtures/ssr-dynamicImport/pages/Bar.tsx
+++ packages/preset-built-in/src/fixtures/ssr-dynamicImport/pages/Bar.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import styles from './Bar.less';
 
 const Bar = props => {
-  console.log('This is Bar component', props);
   return (
     <h2 className={styles.title}>{props.title}</h2>
   );
