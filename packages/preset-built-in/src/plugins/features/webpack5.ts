d1041308fccee1ec5bdeaf811d3471581cb6ed68
chencheng <sorrycc@gmail.com>
2021-02-26 12:20:54 +0800

    feat: support webpack 5 lazy compilation

--- packages/preset-built-in/src/plugins/features/webpack5.ts
+++ packages/preset-built-in/src/plugins/features/webpack5.ts
@@ -6,13 +6,34 @@ export default (api: IApi) => {
     enableBy: api.EnableBy.config,
     config: {
       schema(joi) {
-        return joi.object();
+        return joi.object().keys({
+          lazyCompilation: joi.object(),
+        });
       },
     },
   });
 
   api.onPluginReady(() => {
-    console.log('Bundle with webpack 5...');
+    const command = process.argv[2];
+    if (['dev', 'build'].includes(command)) {
+      console.log('Bundle with webpack 5...');
+    }
     process.env.USE_WEBPACK_5 = '1';
+  });
+
+  api.modifyBundleConfig((memo) => {
+    if (api.config.webpack5!.lazyCompilation) {
+      // @ts-ignore
+      memo.experiments = {
+        // @ts-ignore
+        ...memo.experiments,
+        lazyCompilation: {
+          // client: '@umijs/deps/compiled/webpack/5/lazy-compilation-web.js',
+          backend: require('@umijs/deps/compiled/webpack/5/lazyCompilationBackend.js'),
+          ...api.config.webpack5!.lazyCompilation,
+        },
+      };
+    }
+    return memo;
   });
 };
