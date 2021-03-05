04089e181c73063427b7798255fab7d48e67c37f
ycjcl868 <45808948@qq.com>
2021-03-05 17:34:58 +0800

    chore: not try catch

--- packages/preset-built-in/src/plugins/features/fastRefresh/babel-plugin-modify-anonymous.ts
+++ packages/preset-built-in/src/plugins/features/fastRefresh/babel-plugin-modify-anonymous.ts
@@ -32,45 +32,41 @@ export default (babel) => {
       // @ts-ignore
       ExportDefaultDeclaration: {
         enter(path, state) {
-          try {
-            const def = path.node.declaration;
-            const { cwd, filename } = state.file.opts;
-            const relativePath = relative(cwd, filename);
+          const def = path.node.declaration;
+          const { cwd, filename } = state.file.opts;
+          const relativePath = relative(cwd, filename);
 
-            if (
-              /^\.(tsx|jsx)$/.test(extname(relativePath)) &&
-              // hidden relativePath
-              !/(^|\/)\.[^\/\.]/g.test(relativePath) &&
-              !relativePath.includes('node_modules')
-            ) {
-              let componentName = path2Component(relativePath);
+          if (
+            /^\.(tsx|jsx)$/.test(extname(relativePath)) &&
+            // hidden relativePath
+            !/(^|\/)\.[^\/\.]/g.test(relativePath) &&
+            !relativePath.includes('node_modules')
+          ) {
+            let componentName = path2Component(relativePath);
 
-              // solve identifier conflict
-              const identifiers = Object.keys(path.scope.bindings || {});
-              // add index if conflict
-              let idx = 0;
-              // loop util componentName conflict
-              while (identifiers.includes(componentName)) {
-                componentName = `${componentName}${idx}`;
-                idx += 1;
-              }
-
-              // generate component name identifier
-              const named = t.identifier(componentName);
-
-              if (t.isArrowFunctionExpression(def)) {
-                const varDec = t.variableDeclaration('const', [
-                  t.variableDeclarator(named, def),
-                ]);
-                const [varDeclPath] = path.insertBefore(varDec);
-                path.scope.registerDeclaration(varDeclPath);
-                path.replaceWith(t.exportDefaultDeclaration(named));
-              } else if (t.isFunctionDeclaration(def) && !def.id) {
-                def.id = named;
-              }
+            // solve identifier conflict
+            const identifiers = Object.keys(path.scope.bindings || {});
+            // add index if conflict
+            let idx = 0;
+            // loop util componentName conflict
+            while (identifiers.includes(componentName)) {
+              componentName = `${componentName}${idx}`;
+              idx += 1;
             }
-          } catch (_) {
-            // slient
+
+            // generate component name identifier
+            const named = t.identifier(componentName);
+
+            if (t.isArrowFunctionExpression(def)) {
+              const varDec = t.variableDeclaration('const', [
+                t.variableDeclarator(named, def),
+              ]);
+              const [varDeclPath] = path.insertBefore(varDec);
+              path.scope.registerDeclaration(varDeclPath);
+              path.replaceWith(t.exportDefaultDeclaration(named));
+            } else if (t.isFunctionDeclaration(def) && !def.id) {
+              def.id = named;
+            }
           }
         },
       },
