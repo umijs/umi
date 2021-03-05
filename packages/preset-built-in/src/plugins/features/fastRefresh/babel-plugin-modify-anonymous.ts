d54c1933df5cec2fc107f055dcf17e49df7c8ad2
ycjcl868 <45808948@qq.com>
2021-03-05 16:27:28 +0800

    chore: test

--- packages/preset-built-in/src/plugins/features/fastRefresh/babel-plugin-modify-anonymous.ts
+++ packages/preset-built-in/src/plugins/features/fastRefresh/babel-plugin-modify-anonymous.ts
@@ -1,32 +1,83 @@
-import { extname } from 'path';
+import { lodash as _ } from '@umijs/utils';
+import { parse, relative, extname, format, sep } from 'path';
+
+/**
+ * convert path into componentName
+ * like:
+ *  - src/index.tsx => SrcIndex
+ *  - components/Header.tsx => ComponentHeader
+ *
+ * @param filePath
+ * @returns {string} componentName
+ */
+export const path2Component = (filePath: string): string => {
+  const { ext } = parse(filePath);
+  const filePathWithoutExt = filePath
+    // remove extension
+    .replace(ext, '')
+    .split(sep)
+    // upperFirst
+    .map(_.upperFirst)
+    .join('');
+  return filePathWithoutExt;
+};
 
 // @ts-ignore
 export default (babel) => {
   const { types: t } = babel;
+
+  const conflictVisitor = {
+    Identifier(identifierPath) {
+      const { componentName } = this;
+      if (identifierPath.node.name === componentName) {
+        identifiers;
+      }
+    },
+  };
+
   return {
+    name: 'babel-plugin-modify-anonymouse',
     visitor: {
       // @ts-ignore
-      ExportDefaultDeclaration(path, state) {
-        const def = path.node.declaration;
-        const named = t.identifier(`Abc1`);
-        const { filename } = state.file.opts;
-        if (
-          /^\.(tsx|jsx)$/.test(extname(filename)) &&
-          // hidden filename
-          !/(^|\/)\.[^\/\.]/g.test(filename) &&
-          !filename.includes('node_modules')
-        ) {
-          if (t.isArrowFunctionExpression(def)) {
-            const varDec = t.variableDeclaration('const', [
-              t.variableDeclarator(named, def),
-            ]);
-            const [varDeclPath] = path.insertBefore(varDec);
-            path.scope.registerDeclaration(varDeclPath);
-            path.replaceWith(t.exportDefaultDeclaration(named));
-          } else if (t.isFunctionDeclaration(def) && !def.id) {
-            def.id = named;
+      ExportDefaultDeclaration: {
+        enter(path, state) {
+          const def = path.node.declaration;
+          const { cwd, filename } = state.file.opts;
+          const relativePath = relative(cwd, filename);
+
+          if (
+            /^\.(tsx|jsx)$/.test(extname(relativePath)) &&
+            // hidden relativePath
+            !/(^|\/)\.[^\/\.]/g.test(relativePath) &&
+            !relativePath.includes('node_modules')
+          ) {
+            let componentName = path2Component(relativePath);
+
+            // solve identifier conflict
+            const identifiers = Object.keys(path.scope.bindings || {});
+            // add index if conflict
+            let idx = 0;
+            // loop util componentName conflict
+            while (identifiers.includes(componentName)) {
+              componentName = `${componentName}${idx}`;
+              idx += 1;
+            }
+
+            // generate component name identifier
+            const named = t.identifier(componentName);
+
+            if (t.isArrowFunctionExpression(def)) {
+              const varDec = t.variableDeclaration('const', [
+                t.variableDeclarator(named, def),
+              ]);
+              const [varDeclPath] = path.insertBefore(varDec);
+              path.scope.registerDeclaration(varDeclPath);
+              path.replaceWith(t.exportDefaultDeclaration(named));
+            } else if (t.isFunctionDeclaration(def) && !def.id) {
+              def.id = named;
+            }
           }
-        }
+        },
       },
     },
   };
