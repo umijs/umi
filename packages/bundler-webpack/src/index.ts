32e875dbfcfaa078fcc28e794d4a664796caacb3
chencheng <sorrycc@gmail.com>
2021-03-10 11:26:53 +0800

    chore: fix build failed, revert any dts

--- packages/bundler-webpack/src/index.ts
+++ packages/bundler-webpack/src/index.ts
@@ -142,7 +142,7 @@ class Bundler {
           invalid.tap('umi-dev-server', () => {
             server.sockWrite({ type: 'invalid' });
           });
-          done.tap('umi-dev-server', (stats: any) => {
+          done.tap('umi-dev-server', (stats) => {
             sendStats({
               server,
               sockets: server.sockets,
