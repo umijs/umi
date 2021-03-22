2fbdc130ec4eb986273be8625ffb7cfc2fc6c0d5
ycjcl868 <45808948@qq.com>
2021-03-22 10:32:33 +0800

    chore: console

--- packages/bundler-webpack/src/fixtures/postcss-flexbugs-fixes/expect.ts
+++ packages/bundler-webpack/src/fixtures/postcss-flexbugs-fixes/expect.ts
@@ -1,6 +1,5 @@
 import { IExpectOpts } from '../types';
 
 export default ({ indexCSS }: IExpectOpts) => {
-  console.log('indexCSS', indexCSS)
   expect(indexCSS).toContain(`.foo { flex: 1 1; }`);
 }
