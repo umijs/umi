cea6e9ac14e7e198c59d9ad2dde1908398839f5d
pigcan <xuanji.jw@taobao.com>
2021-03-24 17:22:36 +0800

    feat: check node version (#6332)
    * feat: check node version
    
    * chore: ignore .idea
    
    * fix: check node version log issue
    
    * Revert "chore: ignore .idea"
    
    This reverts commit 7ca599c74c04c294d166f15e8c64ba411c300692.

--- packages/umi/src/cli.ts
+++ packages/umi/src/cli.ts
@@ -7,6 +7,15 @@ import getCwd from './utils/getCwd';
 import getPkg from './utils/getPkg';
 import initWebpack from './initWebpack';
 
+const v = process.version;
+
+if (v && parseInt(v.slice(1)) < 10) {
+  console.log(chalk.red(`Your node ${v} is not supported by umi, please upgrade to 10 or above.`));
+  process.exit(1);
+} else if (v && parseInt(v.slice(1)) > 14) {
+  console.log(chalk.yellow(`Your node ${v} is not supported by umi, please use node 10, node 12 or node 14.`));
+}
+
 // process.argv: [node, umi.js, command, args]
 const args = yParser(process.argv.slice(2), {
   alias: {
