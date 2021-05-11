9ca363da81427d84b29f102af02f8cff6df774e2
xiaohuoni <448627663@qq.com>
2021-05-11 15:50:04 +0800

    chore: add example for alias

---
+++ examples/config-alias/src/components/Footer.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import type { FC } from 'react';
+
+const Footer: FC = () => {
+  return <div>Footer: Open-source MIT Licensed | Copyright © 2017-present</div>;
+};
+
+export default Footer;
