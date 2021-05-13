7a397533882c1183945f20d6cc9e0bac156701ea
xiaohuoni <448627663@qq.com>
2021-05-13 18:53:00 +0800

    chore: add example for basic wrappers

---
+++ examples/basic-wrappers/pages/login.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const LoginPage = () => {
+  return (
+    <div>
+      <h1>Login Page</h1>
+      <p>Hi,you're here,We have a 'wrappers' in IndexPage.</p>
+    </div>
+  );
+};
+
+export default LoginPage;
