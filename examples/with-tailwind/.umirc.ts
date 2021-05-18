eea9a38a32bc9b0fa6c1e36a1610452ef860a536
wangsong <415613212@qq.com>
2021-05-18 10:55:38 +0800

    add UmiJs examples

---
+++ examples/with-tailwind/.umirc.ts
@@ -0,0 +1,17 @@
+import { defineConfig } from 'umi';
+
+export default defineConfig({
+  nodeModulesTransform: {
+    type: 'none',
+  },
+  routes: [{ path: '/', component: '@/pages/index' }],
+  fastRefresh: {},
+  extraPostCSSPlugins: [
+    require('postcss-import'),
+    require('tailwindcss')({
+      purge: ['./src/**/*.html', './src/**/*.tsx', './src/**/*.jsx'],
+    }),
+    require('postcss-nested'),
+    require('autoprefixer'),
+  ],
+});
