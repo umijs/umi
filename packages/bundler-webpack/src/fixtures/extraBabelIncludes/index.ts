ab3a3bdcd4ea77de7baf3b2e1b4ff08f3a7ef938
xiaohuoni <448627663@qq.com>
2021-05-25 16:50:50 +0800

    chore: add jest test

--- packages/bundler-webpack/src/fixtures/extraBabelIncludes/index.ts
+++ packages/bundler-webpack/src/fixtures/extraBabelIncludes/index.ts
@@ -3,7 +3,9 @@ import bar from 'bar/bar';
 // @ts-ignore
 import foo from 'foo/foo';
 import hoo from '../.extraBabelIncludes/hoo';
+import too from '../.noExtraBabelIncludes/too';
 
 bar();
 foo();
 hoo();
+too();
