a9e2085ca6b6a9e07a54757a9b5cb67264d0cc72
Baoyuan <baoyuan.top@gmail.com>
2021-06-01 21:31:36 +0800

    feat: add config define example

---
+++ examples/config-define/.umirc.ts
@@ -0,0 +1,7 @@
+import { defineConfig } from 'umi';
+
+export default defineConfig({
+  define: {
+    MESSAGE: 'hello world',
+  },
+});
---
+++ examples/config-define/src/pages/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import type { FC } from 'react';
+
+const IndexPage: FC = () => {
+  return <div>{MESSAGE}</div>;
+};
+
+export default IndexPage;
