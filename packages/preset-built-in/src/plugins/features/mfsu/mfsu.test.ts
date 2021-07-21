d08b2ff6ffb1ea0ddbdec350d080b8801eb6e8bc
tangliang <tlyspa@gmail.com>
2021-07-21 21:32:38 +0800

    fix(preset-built-in): fix mfsu assets mismatch with various publicPath

--- packages/preset-built-in/src/plugins/features/mfsu/mfsu.test.ts
+++ packages/preset-built-in/src/plugins/features/mfsu/mfsu.test.ts
@@ -1,6 +1,6 @@
 import { winPath } from '@umijs/utils';
 import { IApi } from 'umi';
-import { getMfsuPath } from './mfsu';
+import { getMfsuPath, normalizeReqPath } from './mfsu';
 
 test('functions: get mfsu path', () => {
   // @ts-ignore
@@ -24,4 +24,103 @@ test('functions: get mfsu path', () => {
   expect(winPath(getMfsuPath(api, { mode: 'production' }))).toEqual(
     '/work/xxx/yyy/zzz',
   );
+});
+
+test('normalizeReqPath', () => {
+  // publicPath should be normalized as '/'
+  expect(normalizeReqPath('/', '/mf-va_xxxxxxxxxxx.js')).toStrictEqual({
+    isMfAssets: true,
+    normalPublicPath: '/',
+    fileRelativePath: 'mf-va_xxxxxxxxxxx.js',
+  });
+
+  expect(normalizeReqPath('./', '/mf-dep_xxxxxx.js')).toStrictEqual({
+    isMfAssets: true,
+    normalPublicPath: '/',
+    fileRelativePath: 'mf-dep_xxxxxx.js',
+  });
+
+  expect(normalizeReqPath('../../../', '/mf-dep_xxxxxx.js')).toStrictEqual({
+    isMfAssets: true,
+    normalPublicPath: '/',
+    fileRelativePath: 'mf-dep_xxxxxx.js',
+  });
+
+  // not mfsu asserts
+  expect(
+    normalizeReqPath('../../../', '/a/b/c/mf-dep_xxxxxx.js'),
+  ).toStrictEqual({
+    isMfAssets: false,
+    normalPublicPath: '/',
+    fileRelativePath: 'a/b/c/mf-dep_xxxxxx.js',
+  });
+
+  // publicPath should be normalized as '/xxx/xxx/'
+  expect(
+    normalizeReqPath('./public_path/', '/public_path/mf-dep_xxxxxx.js'),
+  ).toStrictEqual({
+    isMfAssets: true,
+    normalPublicPath: '/public_path/',
+    fileRelativePath: 'mf-dep_xxxxxx.js',
+  });
+
+  expect(
+    normalizeReqPath('../../public_path/', '/public_path/mf-dep_xxxxxx.js'),
+  ).toStrictEqual({
+    isMfAssets: true,
+    normalPublicPath: '/public_path/',
+    fileRelativePath: 'mf-dep_xxxxxx.js',
+  });
+
+  expect(
+    normalizeReqPath(
+      './public_path/a/b/c/',
+      '/public_path/a/b/c/mf-static/cfile.html',
+    ),
+  ).toStrictEqual({
+    isMfAssets: true,
+    normalPublicPath: '/public_path/a/b/c/',
+    fileRelativePath: 'mf-static/cfile.html',
+  });
+
+  // not mfsu asserts
+  expect(
+    normalizeReqPath('./public_path/', '/public_path/a/b/c/mf-dep_xxxxxx.js'),
+  ).toStrictEqual({
+    isMfAssets: false,
+    normalPublicPath: '/public_path/',
+    fileRelativePath: 'a/b/c/mf-dep_xxxxxx.js',
+  });
+
+  // publicPath with hosts
+  expect(
+    normalizeReqPath(
+      'http://10.0.0.111/public_path/a/b/c/',
+      '/public_path/a/b/c/mf-static/cfile.html',
+    ),
+  ).toStrictEqual({
+    isMfAssets: true,
+    normalPublicPath: '/public_path/a/b/c/',
+    fileRelativePath: 'mf-static/cfile.html',
+  });
+
+  expect(
+    normalizeReqPath(
+      'http://umijs.org/public_path/a/b/c/',
+      '/public_path/a/b/c/mf-static/cfile.html',
+    ),
+  ).toStrictEqual({
+    isMfAssets: true,
+    normalPublicPath: '/public_path/a/b/c/',
+    fileRelativePath: 'mf-static/cfile.html',
+  });
+
+  // not mfsu asserts
+  expect(
+    normalizeReqPath('http://umijs.org/', '/a/b/c/mf-static/cfile.html'),
+  ).toStrictEqual({
+    isMfAssets: false,
+    normalPublicPath: '/',
+    fileRelativePath: 'a/b/c/mf-static/cfile.html',
+  });
 });
