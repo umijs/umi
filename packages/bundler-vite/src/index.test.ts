11a86a14bbf94f3446db9555201015920a2115b2
chencheng (云谦) <sorrycc@gmail.com>
2021-08-20 17:11:55 +0800

    chore: init umi-next (#106)
    * chore: init umi-next
    
    * chore: add github ci build status badge

---
+++ packages/bundler-vite/src/index.test.ts
@@ -0,0 +1,5 @@
+import index from './index';
+
+test('normal', () => {
+  expect(index()).toEqual('@umijs/bundler-vite');
+});
---
+++ packages/bundler-webpack/dist/index.js
@@ -0,0 +1,7 @@
+"use strict";Object.defineProperty(exports, "__esModule", {value: true});// src/index.ts
+var src_default = () => {
+  return "@umijs/bundler-webpack";
+};
+
+
+exports.default = src_default;
