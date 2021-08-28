d6e37ec9ed69c8b0bb6d40b94a57406e3a555683
sorrycc <sorrycc@gmail.com>
2021-08-28 16:47:43 +0800

    fix: tsc

--- scripts/bundleDeps.ts
+++ scripts/bundleDeps.ts
@@ -1,6 +1,7 @@
 // @ts-ignore
 import ncc from '@vercel/ncc';
 // import { Package } from 'dts-packer';
+// @ts-ignore
 import { Package } from '/Users/chencheng/code/github.com/sorrycc/dts-packer/dist/Package.js';
 import resolve from 'resolve';
 import { chalk, fs, path, argv } from 'zx';
