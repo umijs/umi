d6639b7e318b607427dafca585b5df1aa5c1c913
sky <cl98914@alibaba-inc.com>
2021-09-06 16:04:48 +0800

    test: fix ssr prod build load react-router error

--- packages/preset-built-in/src/fixtures/ssr-prerender/.umirc.ts
+++ packages/preset-built-in/src/fixtures/ssr-prerender/.umirc.ts
@@ -4,4 +4,7 @@ export default {
   ssr: {},
   exportStatic: {},
   routes: [{ path: '/', component: 'index' }],
+  alias: {
+    'react-router': require.resolve('react-router/cjs/react-router.js'),
+  },
 } as IConfig;
