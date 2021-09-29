f99dcbe1d4a78fbc8310f5f86821e39d803b3f9a
sorrycc <sorrycc@gmail.com>
2021-09-29 10:01:08 +0800

    feat: don't support node 15

--- packages/umi/src/cli/node.ts
+++ packages/umi/src/cli/node.ts
@@ -1,11 +1,15 @@
 import { logger } from '@umijs/utils';
-import { FRAMEWORK_NAME, MIN_NODE_VERSION } from '../constants';
+import {
+  EXCLUDE_NODE_VERSION,
+  FRAMEWORK_NAME,
+  MIN_NODE_VERSION,
+} from '../constants';
 
 export function checkVersion() {
   const v = parseInt(process.version.slice(1));
-  if (v < MIN_NODE_VERSION) {
+  if (v < MIN_NODE_VERSION || v === EXCLUDE_NODE_VERSION) {
     logger.error(
-      `Your node ${v} is not supported by umi, please upgrade to ${MIN_NODE_VERSION} or above.`,
+      `Your node version ${v} is not supported, please upgrade to ${MIN_NODE_VERSION} or above except ${EXCLUDE_NODE_VERSION}.`,
     );
     process.exit(1);
   }
