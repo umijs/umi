3f2b9833396439a46ffbfaee91cd0028aa4af91a
sorrycc <sorrycc@gmail.com>
2021-10-12 12:11:00 +0800

    chore: code style

--- packages/utils/src/importLazy.ts
+++ packages/utils/src/importLazy.ts
@@ -1,3 +1,9 @@
+/**
+ * ref:
+ * 1. https://rushstack.io/pages/api/node-core-library.import.lazy/
+ * 2. https://github.com/microsoft/rushstack/blob/90301e9/libraries/node-core-library/src/Import.ts#L175
+ */
+
 import importLazyInternal from '../compiled/import-lazy';
 
 export function importLazy(
