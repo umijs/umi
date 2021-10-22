5ad45a4dff91533f8c2b836ba461de15c9c58b5d
sorrycc <sorrycc@gmail.com>
2021-10-22 14:55:42 +0800

    feat: pre bundle react-refresh

--- packages/bundler-webpack/src/config/javaScriptRules.ts
+++ packages/bundler-webpack/src/config/javaScriptRules.ts
@@ -14,7 +14,7 @@ interface IOpts {
 export async function addJavaScriptRules(opts: IOpts) {
   const { config, userConfig, cwd, env } = opts;
   const isDev = opts.env === Env.development;
-  const isFastRefresh = userConfig?.fastRefresh !== false;
+  const useFastRefresh = isDev && userConfig.fastRefresh !== false;
 
   const depPkgs = Object.assign({}, es5ImcompatibleVersionsToPkg());
   const srcRules = [
@@ -97,7 +97,7 @@ export async function addJavaScriptRules(opts: IOpts) {
             ...(userConfig.extraBabelPresets || []).filter(Boolean),
           ],
           plugins: [
-            isDev && isFastRefresh && require.resolve('react-refresh/babel'),
+            useFastRefresh && require.resolve('react-refresh/babel'),
             ...opts.extraBabelPlugins,
             ...(userConfig.extraBabelPlugins || []),
           ].filter(Boolean),
