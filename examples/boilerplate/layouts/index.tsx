ad310b83e94d0a03acfdad4e248e3b1688cdaa07
sorrycc <sorrycc@gmail.com>
2021-10-22 14:55:42 +0800

    feat: pre bundle react-refresh

--- examples/boilerplate/layouts/index.tsx
+++ examples/boilerplate/layouts/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link, Outlet } from 'umi';
 
-export default () => {
+export default function Layout() {
   return (
     <div>
       <h2>global layout</h2>
@@ -19,4 +19,4 @@ export default () => {
       <Outlet />
     </div>
   );
-};
+}
