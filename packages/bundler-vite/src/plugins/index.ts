7b772864a78b6906a3efbe9253063b69d1e20855
lylwanan <lyl444@qq.com>
2021-10-26 10:38:03 +0800

    autoCSSModule & svgr (#136)
    * feat: autoCSSModule plugin
    
    * feat: autoCSSModule plugin
    
    * feat: svgr plugin
    
    * fix: resolve conversation
    
    * fix: resolve conversation
    
    * feat: add regular case
    
    Co-authored-by: lyl444@qq.com <liuyonglang.lyl@alibaba-inc.com>

---
+++ packages/bundler-vite/src/plugins/index.ts
@@ -0,0 +1,19 @@
+import svgrPlugin from './svgr';
+import autoCSSModulePlugin from './autoCSSModule';
+
+import type { IConfig } from '../types';
+import type { InlineConfig as ViteInlineConfig } from 'vite';
+
+/**
+ * config with plugins
+ */
+export default (userConfig: IConfig): ViteInlineConfig => {
+  return {
+    plugins: [
+      svgrPlugin({
+        sourcemap: !!userConfig.devtool
+      }),
+      autoCSSModulePlugin()
+    ],
+  };
+};
