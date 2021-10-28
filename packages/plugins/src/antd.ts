ec97d827c7e6bf4cc4d6f88571141681e20ce682
sorrycc <sorrycc@gmail.com>
2021-10-28 13:29:43 +0800

    chore: update

--- packages/plugins/src/antd.ts
+++ packages/plugins/src/antd.ts
@@ -2,4 +2,11 @@ import { IApi } from 'umi';
 
 export default (api: IApi) => {
   api;
+
+  // antd import
+  // dark mode
+  // compat mode
+  // dayjs (by default?)
+  // babel-plugin-import
+  // antd config provider (HOLD, depends on umi)
 };
--- packages/plugins/src/dva.ts
+++ packages/plugins/src/dva.ts
@@ -2,4 +2,9 @@ import { IApi } from 'umi';
 
 export default (api: IApi) => {
   api;
+
+  // import from dva
+  // model register
+  // container
+  // dva list model
 };
