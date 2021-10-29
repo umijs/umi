05fff18c7b6eab20d8da7ce4ef1f8edf821ef6d4
lylwanan <lyl444@qq.com>
2021-10-29 19:36:14 +0800

    feat: complete bundler-vite config types and schema (#153)
    * feat: complete bundler-vite config types and schema
    
    * fix: resolve conversation

--- packages/bundler-vite/src/schema.ts
+++ packages/bundler-vite/src/schema.ts
@@ -1,35 +1,26 @@
 import type { Root } from '@hapi/joi';
 import { JSMinifier } from './types';
 
-const options = [
-  'cheap-source-map',
-  'cheap-eval-source-map',
-  'cheap-hidden-source-map',
-  'cheap-inline-source-map',
-  'cheap-module-source-map',
-  'cheap-module-eval-source-map',
-  'cheap-module-hidden-source-map',
-  'cheap-module-inline-source-map',
-  'eval',
-  'eval-source-map',
-  'source-map',
-  'hidden-source-map',
-  'inline-source-map',
-];
-
-const DEVTOOL_REGEX = new RegExp(
-  '^' + // start of string
-    '(#@|@|#)?' + // maybe one of the pragmas
-    `(${options.join('$|')})`, // one of the options
-);
-
 export function getSchemas(): Record<string, (Joi: Root) => any> {
   return {
     alias: (Joi) => Joi.object(),
+    analyze: (Joi) => Joi.object(),
+    autoCSSModules: (Joi) => Joi.boolean(),
+    autoprefixer: (Joi) => Joi.object(),
+    copy: (Joi) =>
+      Joi
+        .array()
+        .items(
+          Joi.alternatives().try(
+            Joi.string(),
+            Joi.object().keys({
+              from: Joi.string(),
+              to: Joi.string(),
+            }),
+          )
+        ),
     define: (Joi) => Joi.object(),
-    devtool: (Joi) =>
-      Joi.alternatives().try(Joi.string().regex(DEVTOOL_REGEX), Joi.boolean()),
-    externals: (Joi) =>
+    /*externals: (Joi) =>
       Joi
         .alternatives()
         .try(
@@ -43,7 +34,7 @@ export function getSchemas(): Record<string, (Joi: Root) => any> {
           Joi.string(),
           Joi.func().arity(3),
           Joi.object().regex(),
-        ),
+        ),*/
     extraBabelPlugins: (Joi) =>
       Joi
         .alternatives()
@@ -59,7 +50,10 @@ export function getSchemas(): Record<string, (Joi: Root) => any> {
           Joi.array().items(Joi.alternatives().try(Joi.string(), Joi.object())),
         ),
     extraPostCSSPlugins: (Joi) => Joi.array(),
+    extraVitePlugins: (Joi) => Joi.array(),
     hash: (Joi) => Joi.boolean(),
+    inlineLimit: (Joi) => Joi.number(),
+    manifest: (Joi) => Joi.boolean(),
     jsMinifier: (Joi) =>
       Joi
         .alternatives()
@@ -73,18 +67,27 @@ export function getSchemas(): Record<string, (Joi: Root) => any> {
           Joi.boolean(),
         ),
     jsMinifierOptions: (Joi) => Joi.object(),
+    lessLoader: (Joi) =>
+      Joi.object().keys({
+        lessOptions: Joi.object(),
+      }),
     legacy: (Joi) =>
       Joi.alternatives().try(
         Joi.object(),
         Joi.boolean(),
       ),
     outputPath: (Joi) => Joi.string(),
+    polyfill: (Joi) =>
+      Joi.object().keys({
+        imports: Joi.array().items(Joi.string()),
+      }),
+    postcssLoader: (Joi) =>
+      Joi.object().keys({
+        postcssOptions: Joi.object(),
+      }),
+    proxy: (Joi) => Joi.object(),
     publicPath: (Joi) => Joi.string(),
     svgr: (Joi) => Joi.object(),
-    targets: (Joi) =>
-      Joi.alternatives().try(
-        Joi.string(),
-        Joi.array().items(Joi.alternatives().try(Joi.string())),
-      ),
+    targets: (Joi) => Joi.object(),
   };
 }
