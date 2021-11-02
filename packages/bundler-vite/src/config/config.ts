41624c3664290a2d9f1df02694593e5f2ac42cec
lylwanan <lyl444@qq.com>
2021-11-03 00:11:37 +0800

    feat: support opts config of extraBabelPlugins and extraBabelPresets (#156)

--- packages/bundler-vite/src/config/config.ts
+++ packages/bundler-vite/src/config/config.ts
@@ -1,19 +1,33 @@
 import type { InlineConfig as ViteInlineConfig } from 'vite';
 import { mergeConfig } from 'vite';
 import configPlugins from '../plugins';
-import type { Env, IConfig } from '../types';
 import configTransformer from './transformer';
+import { Env, IConfig, IBabelPlugin } from '../types';
 
 interface IOpts {
   cwd: string;
   env: Env;
   entry: Record<string, string>;
   userConfig: IConfig;
+  extraBabelPlugins?: IBabelPlugin[];
+  extraBabelPresets?: IBabelPlugin[];
 }
 
 export async function getConfig(opts: IOpts): Promise<ViteInlineConfig> {
-  const vitePluginsConfig = configPlugins(opts.userConfig);
-  const viteConfigFromUserConfig = configTransformer(opts.userConfig);
+  const applyOpts = {
+    ...opts.userConfig,
+    extraBabelPlugins: [
+      ...(opts.extraBabelPlugins || []),
+      ...(opts.userConfig.extraBabelPlugins || []),
+    ],
+    extraBabelPresets: [
+      ...(opts.extraBabelPresets || []),
+      ...(opts.userConfig.extraBabelPresets || []),
+    ],
+  };
+
+  const vitePluginsConfig = configPlugins(applyOpts);
+  const viteConfigFromUserConfig = configTransformer(applyOpts);
 
   return mergeConfig(vitePluginsConfig, viteConfigFromUserConfig);
 }
