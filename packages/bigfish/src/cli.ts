80e4597e34e722cb37933a5c6835413dbeb827cf
sorrycc <sorrycc@gmail.com>
2021-11-03 12:21:19 +0800

    chore: rename presets to preset

--- packages/bigfish/src/cli.ts
+++ packages/bigfish/src/cli.ts
@@ -1,7 +1,7 @@
 import { run } from 'umi/dist/cli/cli';
 
 run({
-  presets: [require.resolve('./presets')],
+  presets: [require.resolve('./preset')],
 }).catch((e) => {
   console.error(e);
   process.exit(1);
