55c65492eab133217e67c1dd3f55c4d63865d97b
sorrycc <sorrycc@gmail.com>
2021-11-04 11:28:26 +0800

    feat: api.modifyHTMLFavicon

--- packages/preset-built-in/src/commands/dev/getMarkupArgs.ts
+++ packages/preset-built-in/src/commands/dev/getMarkupArgs.ts
@@ -1,29 +1,33 @@
-import { IApi, IScript } from '../../types';
+import { IApi } from '../../types';
 
 export async function getMarkupArgs(opts: { api: IApi }) {
-  const headScripts = await opts.api.applyPlugins<IScript[]>({
+  const headScripts = await opts.api.applyPlugins({
     key: 'addHTMLHeadScripts',
     initialValue: opts.api.config.headScripts || [],
   });
-  const scripts: any = await opts.api.applyPlugins<IScript[]>({
+  const scripts: any = await opts.api.applyPlugins({
     key: 'addHTMLScripts',
     initialValue: opts.api.config.scripts || [],
   });
-  const metas: any = await opts.api.applyPlugins<IScript[]>({
+  const metas: any = await opts.api.applyPlugins({
     key: 'addHTMLMetas',
     initialValue: opts.api.config.metas || [],
   });
-  const links: any = await opts.api.applyPlugins<IScript[]>({
+  const links: any = await opts.api.applyPlugins({
     key: 'addHTMLLinks',
     initialValue: opts.api.config.links || [],
   });
-  const styles: any = await opts.api.applyPlugins<IScript[]>({
+  const styles: any = await opts.api.applyPlugins({
     key: 'addHTMLStyles',
     initialValue: opts.api.config.styles || [],
   });
+  const favicon = await opts.api.applyPlugins({
+    key: 'modifyHTMLFavicon',
+    initialValue: opts.api.config.favicon,
+  });
   return {
     routes: opts.api.appData.routes,
-    favicon: opts.api.config.favicon,
+    favicon,
     headScripts,
     scripts,
     metas,
