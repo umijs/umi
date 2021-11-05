fd38da445d1080288a1f80d9d378f71f0e01c45b
sorrycc <sorrycc@gmail.com>
2021-11-05 22:07:04 +0800

    feat: export @umijs/utils for umi

--- packages/umi/src/index.ts
+++ packages/umi/src/index.ts
@@ -1,4 +1,6 @@
 import { IServicePluginAPI, PluginAPI } from '@umijs/core';
+import * as utils from '@umijs/utils';
 
 export type IApi = PluginAPI & IServicePluginAPI;
 export * from './service/service';
+export { utils };
