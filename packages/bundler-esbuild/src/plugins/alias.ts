39c1b427b4c37617184d840e3571dc026b3a067c
sorrycc <sorrycc@gmail.com>
2021-11-11 09:48:45 +0800

    fix(bundler-esbuild): improve alias

--- packages/bundler-esbuild/src/plugins/alias.ts
+++ packages/bundler-esbuild/src/plugins/alias.ts
@@ -1,31 +1,12 @@
 import { Plugin } from '@umijs/bundler-utils/compiled/esbuild';
 import { winPath } from '@umijs/utils';
-import assert from 'assert';
 import enhancedResolve from 'enhanced-resolve';
-import { dirname } from 'path';
+import { sortByAffix } from '../utils/sortByAffix';
 
-const getRealFile = async (cwd: string, file: string) => {
-  try {
-    return await resolve(cwd, file);
-  } catch (e) {
-    return null;
-  }
-};
 const resolver = enhancedResolve.create({
   mainFields: ['module', 'browser', 'main'],
-  extensions: [
-    '.json',
-    '.js',
-    '.jsx',
-    '.ts',
-    '.tsx',
-    '.cjs',
-    '.mjs',
-    '.cjsx',
-    '.mjsx',
-  ],
+  extensions: ['.json', '.js', '.jsx', '.ts', '.tsx', '.cjs', '.mjs'],
   // TODO: support exports
-  // tried to add exports, but it don't work with swr
   exportsFields: [],
 });
 
@@ -38,35 +19,20 @@ async function resolve(context: string, path: string): Promise<string> {
 }
 
 // https://esbuild.github.io/plugins/#resolve-callbacks
-export default (options?: Record<string, string>): Plugin => {
+export default (options: Record<string, string> = {}): Plugin => {
   return {
     name: 'alias',
     setup({ onResolve }) {
-      if (!options || Object.keys(options).length === 0) {
-        return;
-      }
-      Object.keys(options).forEach((key) => {
+      const keys = sortByAffix({ arr: Object.keys(options), affix: '$' });
+      keys.forEach((key) => {
         onResolve({ filter: new RegExp(`^${key}`) }, async (args) => {
-          const dir = dirname(args.path);
-          try {
-            const realFile = await getRealFile(
-              options.cwd,
-              winPath(args.path).replace(new RegExp(`^${key}`), options[key]),
-            );
-            assert(realFile, `filePath not found of ${args.path}`);
-            return {
-              path: realFile,
-            };
-          } catch (error: any) {
-            return {
-              errors: [
-                {
-                  text: error.message,
-                },
-              ],
-              resolveDir: dir,
-            };
-          }
+          const path = await resolve(
+            args.importer,
+            winPath(args.path).replace(new RegExp(`^${key}`), options[key]),
+          );
+          return {
+            path,
+          };
         });
       });
     },
