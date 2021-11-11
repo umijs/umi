aff5fbe6064cc4b361e53398ac2168e1fdf901bf
sorrycc <sorrycc@gmail.com>
2021-11-11 14:38:49 +0800

    example: add tmp-srcTranspiler-swc

---
+++ examples/tmp-swcTranspiler-swc/pages/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import './global.less';
+// @ts-ignore
+// import styles from './index.less';
+
+export default function HomePage() {
+  return <div /*className={styles.title}*/>HomePage</div>;
+}
