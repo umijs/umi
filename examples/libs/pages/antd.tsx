5b5212e677c426f9ca932059fb88f5d121337e83
sorrycc <sorrycc@gmail.com>
2021-11-11 14:38:49 +0800

    example: add tmp-srcTranspiler-swc

--- examples/libs/pages/antd.tsx
+++ examples/libs/pages/antd.tsx
@@ -1,5 +1,6 @@
 import 'antd/dist/antd.css';
 import Button from 'antd/es/button';
+import DatePicker from 'antd/es/date-picker';
 import Input from 'antd/es/input';
 import React from 'react';
 
@@ -9,6 +10,7 @@ export default () => {
       <h1>Ant Design</h1>
       <Button type="primary">Button</Button>
       <Input />
+      <DatePicker />
     </div>
   );
 };
