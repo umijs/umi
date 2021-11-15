4629af85ce6e43a31ad19045f99d0b66c5f7866d
sorrycc <sorrycc@gmail.com>
2021-11-15 14:54:04 +0800

    feat(preset-umi): add cjsExports to api.checkCode

---
+++ packages/preset-umi/src/features/transform/babelPlugin.test.ts
@@ -0,0 +1,77 @@
+import { transform } from '@umijs/bundler-utils/compiled/babel/core';
+
+interface IOpts {
+  code: string;
+  filename?: string;
+  opts?: any;
+}
+
+function doTransform(opts: IOpts): any {
+  let ret = null;
+  transform(opts.code, {
+    filename: opts.filename || 'foo.js',
+    plugins: [
+      [
+        require.resolve('./babelPlugin.ts'),
+        {
+          cwd: '/foo',
+          absTmpPath: '/foo/bar',
+          onCheckCode({ args }: any) {
+            ret = args;
+          },
+        },
+      ],
+    ],
+  });
+  return ret;
+}
+
+function formatImport(item: any) {
+  const ret: any = {
+    source: item.source,
+  };
+  if (item.default) ret.default = item.default;
+  if (item.specifiers) ret.specifiers = item.specifiers;
+  if (item.namespace) ret.namespace = item.namespace;
+  return ret;
+}
+
+test('import default', () => {
+  const { imports } = doTransform({ code: `import a from 'a';` });
+  expect(imports.map(formatImport)).toEqual([
+    {
+      default: 'a',
+      source: 'a',
+    },
+  ]);
+});
+
+test('import namespace', () => {
+  const { imports } = doTransform({ code: `import * as a from 'a';` });
+  expect(imports.map(formatImport)).toEqual([
+    {
+      namespace: 'a',
+      source: 'a',
+    },
+  ]);
+});
+
+test('import specifiers', () => {
+  const { imports } = doTransform({ code: `import { a, b } from 'a';` });
+  expect(imports.map(formatImport)).toEqual([
+    {
+      specifiers: {
+        a: 'a',
+        b: 'b',
+      },
+      source: 'a',
+    },
+  ]);
+});
+
+test('cjs exports', () => {
+  const { cjsExports } = doTransform({
+    code: `module.exports = 'a';exports.b = 'b';`,
+  });
+  expect(cjsExports).toEqual(['default', 'b']);
+});
--- packages/preset-umi/src/features/transform/transform.ts
+++ packages/preset-umi/src/features/transform/transform.ts
@@ -1,90 +1,24 @@
-import * as Babel from '@umijs/bundler-utils/compiled/babel/core';
-import * as t from '@umijs/bundler-utils/compiled/babel/types';
-import { winPath } from '@umijs/utils';
-import { join } from 'path';
 import { IApi } from '../../types';
+import babelPlugin from './babelPlugin';
 
 export default (api: IApi) => {
   api.addBeforeBabelPresets(() => {
     return [
       {
         plugins: [
-          () => {
-            return {
-              pre() {
-                // @ts-ignore
-                this.cache = new Map<string, any>();
+          [
+            babelPlugin,
+            {
+              cwd: api.cwd,
+              absTmpPath: api.paths.absTmpPath,
+              onCheckCode({ args }: any) {
+                api.applyPlugins({
+                  key: 'onCheckCode',
+                  args: args,
+                });
               },
-              post(state: any) {
-                // @ts-ignore
-                const { cache } = this;
-                const filename = winPath(state.opts.filename);
-                if (
-                  cache.has(filename) &&
-                  !filename.includes('bundler-webpack/client') &&
-                  !filename.startsWith(winPath(join(api.cwd, 'node_modules')))
-                ) {
-                  api.applyPlugins({
-                    key: 'onCheckCode',
-                    args: {
-                      ...cache.get(state.opts.filename),
-                      file: state.opts.filename,
-                      isFromTmp: state.opts.filename.startsWith(
-                        api.paths.absTmpPath,
-                      ),
-                    },
-                  });
-                }
-              },
-              visitor: {
-                Program: {
-                  enter(path: Babel.NodePath<t.Program>) {
-                    // @ts-ignore
-                    const file = path?.hub.file.opts.filename;
-                    // @ts-ignore
-                    const cache = this.cache;
-                    // reset cache
-                    cache.set(file, {
-                      code: path.hub.getCode(),
-                      imports: [],
-                    });
-                    path.node.body.forEach((node) => {
-                      // import x from 'x'; { default: 'x' }
-                      // import * as x2 from 'x'; { namespace: 'x2' }
-                      // import x3, * as xx from 'x'; { default: 'x3', namespace: 'xx' }
-                      // import { x4, a as b } from 'x'; { specifiers: { x4: 'x4', a: 'b' } }
-                      if (t.isImportDeclaration(node)) {
-                        const ret: Record<string, any> = {
-                          source: node.source.value,
-                          loc: node.loc,
-                        };
-                        node.specifiers.forEach((specifier) => {
-                          if (t.isImportDefaultSpecifier(specifier)) {
-                            ret.default = specifier.local.name;
-                          } else if (t.isImportNamespaceSpecifier(specifier)) {
-                            ret.namespace = specifier.local.name;
-                          } else if (t.isImportSpecifier(specifier)) {
-                            ret.specifiers ||= {};
-                            ret.specifiers[
-                              t.isIdentifier(specifier.imported)
-                                ? specifier.imported.name
-                                : specifier.imported.value
-                            ] = specifier.local.name;
-                          }
-                        });
-                        cache.get(file).imports.push(ret);
-                      }
-
-                      // TODO:
-                      // exports
-                      // callExpression
-                      // newExpression
-                    });
-                  },
-                },
-              },
-            };
-          },
+            },
+          ],
         ],
       },
     ];
