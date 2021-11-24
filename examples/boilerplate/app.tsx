7e96c8453b3d7405dfe1b4142471f1f75ba53a94
sorrycc <sorrycc@gmail.com>
2021-11-24 17:46:02 +0800

    feat: more providers

--- examples/boilerplate/app.tsx
+++ examples/boilerplate/app.tsx
@@ -4,10 +4,26 @@ export function rootContainer(container: any, opts: any) {
   return React.createElement(Foo, opts, container);
 }
 
+export function innerProvider(container: any) {
+  return React.createElement(Foo, { title: 'innerProvider' }, container);
+}
+
+export function i18nProvider(container: any) {
+  return React.createElement(Foo, { title: 'i18nProvider' }, container);
+}
+
+export function dataflowProvider(container: any) {
+  return React.createElement(Foo, { title: 'dataflowProvider' }, container);
+}
+
+export function outerProvider(container: any) {
+  return React.createElement(Foo, { title: 'outerProvider' }, container);
+}
+
 function Foo(props: any) {
   return (
     <div>
-      <h1>Foo</h1>
+      <h1>{props.title || 'Foo'}</h1>
       {props.children}
     </div>
   );
