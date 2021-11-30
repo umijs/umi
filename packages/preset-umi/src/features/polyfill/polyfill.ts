c3e7b79dd49f8cff83fb4255f02a3276643852ae
sorrycc <sorrycc@gmail.com>
2021-11-30 11:32:09 +0800

    feat(preset-umi): include current-script-polyfill only when publicPath is auto

--- packages/preset-umi/src/features/polyfill/polyfill.ts
+++ packages/preset-umi/src/features/polyfill/polyfill.ts
@@ -2,7 +2,7 @@ import { IApi } from '../../types';
 
 export default (api: IApi) => {
   api.addPolyfillImports(() => {
-    return api.config.targets?.ie
+    return api.config.publicPath === 'auto' && api.config.targets?.ie
       ? [
           {
             source: require.resolve('current-script-polyfill'),
