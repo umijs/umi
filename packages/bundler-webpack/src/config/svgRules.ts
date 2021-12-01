82214a357432ed0fc3bb413e1b5dea607e69bab3
小虎oni <xiaohuoni@users.noreply.github.com>
2021-12-01 11:05:31 +0800

    feat: upgrade svgr to 6 (#195)
    * feat: upgrade svgr to 6
    
    * chore: change fixtures config

--- packages/bundler-webpack/src/config/svgRules.ts
+++ packages/bundler-webpack/src/config/svgRules.ts
@@ -11,32 +11,21 @@ interface IOpts {
 
 export async function addSVGRules(opts: IOpts) {
   const { config, userConfig } = opts;
-  const { svgr, svgo } = userConfig;
+  const { svgr, svgo = {} } = userConfig;
   if (svgr) {
-    // https://react-svgr.com/docs/webpack/#handle-svg-in-css-sass-or-less
-    // https://github.com/gregberge/svgr/issues/551#issuecomment-883073902
-    // https://github.com/webpack/webpack/issues/9309
     const svgrRule = config.module.rule('svgr');
     svgrRule
-      .test(/\.svg$/)
+      .test(/\.svg$/i)
       .issuer(/\.[jt]sx?$/)
-      .type('javascript/auto')
-      .use('babel-loader')
-      .loader(require.resolve('@umijs/bundler-webpack/compiled/babel-loader'))
-      .end()
       .use('svgr-loader')
       .loader(require.resolve('@umijs/bundler-webpack/compiled/@svgr/webpack'))
       .options({
         svgoConfig: {
-          plugins: [{ removeViewBox: false }],
+          ...(svgo || {}),
         },
         ...svgr,
-        svgo,
-      })
-      .end()
-      .use('url-loader')
-      .loader(require.resolve('@umijs/bundler-webpack/compiled/url-loader'))
-      .end();
+        svgo: !!svgo,
+      });
   }
   if (svgo === false) {
     const svgRule = config.module.rule('svg');
