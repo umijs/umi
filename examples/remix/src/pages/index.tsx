354ab2241e966a52a03880e918cfd9b8fdc2d8b9
Ydream <43460525+YdreamW@users.noreply.github.com>
2021-12-02 17:48:25 +0800

    fix(example/remix): Modify css to make the style adaptive (#200)
    * feat(examples/remix): Add a demo
    
    * fix(examples/remix): Modify the css to make the style adaptive.
    
    Co-authored-by: Ydream <moda.wmy@alibaba-inc.com>

--- examples/remix/src/pages/index.tsx
+++ examples/remix/src/pages/index.tsx
@@ -7,14 +7,14 @@ export default () => {
     <div className={styles.container}>
       <div className={styles.leftContainer}>
         <h2>Welcome to Remix!</h2>
-        <p>We're stoked that you're here. 🥳</p>
-        <p>
+        <p className={styles.p}>We're stoked that you're here. 🥳</p>
+        <p className={styles.p}>
           Feel free to take a look around the code to see how Remix does things,
           it might be a bit different than what you’re used to. When you're
           ready to dive deeper, we've got plenty of resources to get you
           up-and-running quickly.
         </p>
-        <p>
+        <p className={styles.p}>
           Check out all the demos in this starter, and then just delete the
           app/routes/demos and app/styles/demos folders when you're ready to
           turn this into your next project.
