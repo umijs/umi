e867fbef4dbfa82cedbca5ed354c3d9a0fe170d4
sorrycc <sorrycc@gmail.com>
2021-12-03 12:19:28 +0800

    feat(plugin/dva): basic dva plugin

--- packages/umi/src/index.ts
+++ packages/umi/src/index.ts
@@ -1,6 +1,4 @@
 import { IServicePluginAPI, PluginAPI } from '@umijs/core';
-import * as utils from '@umijs/utils';
 
 export type IApi = PluginAPI & IServicePluginAPI;
 export * from './service/service';
-export { utils };
