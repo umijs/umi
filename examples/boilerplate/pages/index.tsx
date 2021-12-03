33c5746a1db323f3b6c75ed170ab9aa7b32f9a17
小虎oni <xiaohuoni@users.noreply.github.com>
2021-12-03 15:32:53 +0800

    feat: add custom svgr-loader (#199)
    * feat: add custom svgr-loader
    
    * chore: update pnpm-lock
    
    * feat: remove svg asset
    
    * chore:code style
    
    * feat: use esbuild
    
    * chore: update pnpm-lock
    
    * chore: code style
    
    * chore: install dep

--- examples/boilerplate/pages/index.tsx
+++ examples/boilerplate/pages/index.tsx
@@ -1,8 +1,17 @@
 import React from 'react';
+// @ts-ignore
+import Smileurl, { ReactComponent as SvgSmile } from '../smile.svg';
 import './global.less';
 // @ts-ignore
 import styles from './index.less';
 
 export default function HomePage() {
-  return <div className={styles.title}>HomePage</div>;
+  return (
+    <div className={styles.title}>
+      HomePage
+      <div className={styles.smile}></div>
+      <img src={Smileurl} alt="" />
+      <SvgSmile />
+    </div>
+  );
 }
