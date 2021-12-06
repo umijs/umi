b31c50d144db91e8099c863bf3ce773c079bfe16
sorrycc <sorrycc@gmail.com>
2021-12-06 14:41:03 +0800

    chore: improve webstorm setup script, add .mfsu and .umi dir to exclude

--- scripts/setupWebStorm.ts
+++ scripts/setupWebStorm.ts
@@ -1,7 +1,22 @@
+import { join } from 'path';
 import { eachPkg, getPkgs, setExcludeFolder } from './utils';
 
-const pkgs = getPkgs();
 const cwd = process.cwd();
-eachPkg(pkgs, ({ pkg }) => {
+eachPkg(getPkgs(), ({ pkg }) => {
   setExcludeFolder({ pkg, cwd });
 });
+
+eachPkg(
+  getPkgs({ base: join(__dirname, '../examples') }),
+  ({ pkg }) => {
+    setExcludeFolder({
+      pkg,
+      cwd,
+      dirName: 'examples',
+      folders: ['.mfsu', '.umi'],
+    });
+  },
+  {
+    base: join(__dirname, '../examples'),
+  },
+);
