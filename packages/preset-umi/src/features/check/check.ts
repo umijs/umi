45f5c89b80daa88c32a44cd1228db899c1c09303
sorrycc <sorrycc@gmail.com>
2021-12-09 21:22:40 +0800

    feat(preset-umi): fixed version import is not allowed

--- packages/preset-umi/src/features/check/check.ts
+++ packages/preset-umi/src/features/check/check.ts
@@ -23,4 +23,16 @@ export default (api: IApi) => {
       },
     });
   });
+
+  api.onCheckCode((args) => {
+    // Fixed version import is not allowed
+    // e.g. import { X } from '_@ant-design_icons@4.7.0@ant-design/icons'
+    if (['cnpm', 'tnpm'].includes(api.appData.npmClient)) {
+      args.imports.forEach(({ source }) => {
+        if (/@\d/.test(source)) {
+          throw new Error(`${source} is not allowed to import.`);
+        }
+      });
+    }
+  });
 };
