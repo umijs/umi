c6c3f2449f922c68883c552fa57985f5297408ec
Peach <scdzwyxst@gmail.com>
2021-12-10 14:20:27 +0800

    feat: local cache & improvements for esmi (#207)
    * feat(preset-umi): add local cache for esmi
    
    * feat(preset-umi): add deps compile info for esmi
    
    * refactor(preset-umi): force include react & react-dom for esmi
    
    * feat(preset-umi): add modulepreload for importmap url in esmi
    
    * feat(preset-umi): support to custom shim script for esmi
    
    * refactor(preset-umi): improve dep log for esmi
    
    * style(preset-umi): useless import in esmi plugin
    
    * refactor(preset-umi): use chalk for colorful esmi info

--- packages/preset-umi/src/features/esmi/Service.ts
+++ packages/preset-umi/src/features/esmi/Service.ts
@@ -1,4 +1,7 @@
-import { axios } from '@umijs/utils';
+import fs from 'fs';
+import path from 'path';
+import { createHash } from 'crypto';
+import { axios, logger, chalk } from '@umijs/utils';
 import type { IApi } from '../../types';
 
 export interface IImportmapData {
@@ -45,9 +48,63 @@ export interface IPkgData {
  */
 export default class ESMIService {
   cdnOrigin: string = '';
+  cacheDir: string = '';
+  cache: Record<string, IImportmapData> = {};
 
-  constructor(opts: { cdnOrigin: string }) {
+  constructor(opts: { cdnOrigin: string; cacheDir: string }) {
     this.cdnOrigin = opts.cdnOrigin;
+    this.cacheDir = opts.cacheDir;
+
+    // restore local cache
+    const cacheFilePath = path.join(this.cacheDir, 'importmap.json');
+
+    if (fs.existsSync(cacheFilePath)) {
+      try {
+        this.cache = require(cacheFilePath);
+      } catch {
+        /* nothing */
+      }
+    }
+  }
+
+  /**
+   * get cache file path by cache key
+   * @param data  pkg data
+   */
+  static getCacheKey(data: IPkgData) {
+    const hash = createHash('md4');
+
+    hash.update(JSON.stringify(data.pkgInfo.exports));
+
+    return hash.digest('hex');
+  }
+
+  /**
+   * get importmap cache by cache key
+   * @param key cache key
+   */
+  getCache(key: string) {
+    return this.cache[key];
+  }
+
+  /**
+   * set importmap cache
+   * @param key   cache key
+   * @param data  importmap data
+   */
+  setCache(key: string, data: IImportmapData) {
+    this.cache[key] = data;
+
+    // create cache dir
+    if (!fs.existsSync(this.cacheDir)) {
+      fs.mkdirSync(this.cacheDir, { recursive: true });
+    }
+
+    // write cache to file system
+    fs.writeFileSync(
+      path.join(this.cacheDir, 'importmap.json'),
+      JSON.stringify(this.cache, null, 2),
+    );
   }
 
   /**
@@ -70,9 +127,25 @@ export default class ESMIService {
    * @returns importmap
    */
   async getImportmap(data: IPkgData) {
+    const cacheKey = ESMIService.getCacheKey(data);
+    const cache = this.getCache(cacheKey);
+    const stamp = +new Date();
+
+    // use valid cache first
+    if (cache) {
+      logger.info('ESMi cache used');
+      return cache;
+    }
+
+    // log dependency list
+    logger.info(chalk.greenBright('Pre-compiling dependencies on esmi:'));
+    data.pkgInfo.exports[0].deps.forEach((dep) => {
+      console.log(chalk.yellow(`  ${dep.name}`));
+    });
+
     // get the build ticket id
     const ticketId = await this.build(data);
-    // continue to the next request after 1s
+    // continue to the next request after 2s
     const next = () =>
       new Promise<IImportmapData | undefined>((resolve) =>
         setTimeout(() => resolve(deferrer()), 2000),
@@ -82,10 +155,19 @@ export default class ESMIService {
         .get<{ success: boolean; data?: IImportmapData }>(
           `${this.cdnOrigin}/api/v1/esm/importmap/${ticketId}`,
         )
-        .then((res) => (res.data.success ? res.data.data : next()), next);
-    };
+        .then((res) => {
+          if (res.data.success) {
+            this.setCache(cacheKey, res.data.data!);
+            logger.info(
+              `Done, took ${((+new Date() - stamp) / 1000).toFixed(1)}s`,
+            );
 
-    // TODO: timeout + time spend log
+            return res.data.data;
+          }
+
+          return next();
+        }, next);
+    };
 
     return deferrer();
   }
