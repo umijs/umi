4d9bfb818ac36c1a860f4d9da168c1509d3bf3a3
sorrycc <sorrycc@gmail.com>
2021-12-13 17:40:09 +0800

    feat(umi): export more from react-router-dom

--- packages/renderer-react/src/index.ts
+++ packages/renderer-react/src/index.ts
@@ -1,3 +1,18 @@
-export { Link, Outlet, useNavigate } from 'react-router-dom';
+export {
+  createSearchParams,
+  Link,
+  matchPath,
+  matchRoutes,
+  NavLink,
+  Outlet,
+  useLocation,
+  useMatch,
+  useNavigate,
+  useOutlet,
+  useParams,
+  useResolvedPath,
+  useRoutes,
+  useSearchParams,
+} from 'react-router-dom';
 export { useAppContext } from './appContext';
 export { renderClient } from './browser';
--- packages/umi/index.esm.js
+++ packages/umi/index.esm.js
@@ -1,8 +1,21 @@
 export {
+  createSearchParams,
   Link,
+  matchPath,
+  matchRoutes,
+  NavLink,
   Outlet,
+  // renderer-react
   renderClient,
+  // renderer-react
   useAppContext,
+  useLocation,
+  useMatch,
   useNavigate,
+  useOutlet,
+  useParams,
+  useResolvedPath,
+  useRoutes,
+  useSearchParams,
 } from '@umijs/renderer-react';
 export { PluginManager } from './client/client/plugin';
