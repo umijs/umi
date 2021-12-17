e548447c64dc678949be7349fab0da2e4bf6081f
sorrycc <sorrycc@gmail.com>
2021-12-17 15:08:44 +0800

    chore(example): code style

--- examples/bigfish/app.ts
+++ examples/bigfish/app.ts
@@ -1,9 +1,10 @@
+// @ts-ignore
 async function delay(ms: number) {
   return new Promise((resolve) => setTimeout(resolve, ms));
 }
 
 export async function getInitialState() {
-  await delay(1000);
+  // await delay(1000);
   return {
     name: 'Big Fish',
     size: 'big',
