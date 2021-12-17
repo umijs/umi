a1f9403ee5cf47857b582f7f96a2a485866a5fc5
sorrycc <sorrycc@gmail.com>
2021-12-17 15:00:24 +0800

    feat(plugin/initial-state): complete initial-state plugin

--- examples/bigfish/.umirc.ts
+++ examples/bigfish/.umirc.ts
@@ -25,6 +25,7 @@ export default {
     style: 'less',
     // dark: true,
   },
+  initialState: {},
   dva: {},
   model: {},
   analytics: {
--- examples/bigfish/pages/index.tsx
+++ examples/bigfish/pages/index.tsx
@@ -1,8 +1,12 @@
 // @ts-ignore
+import { useModel } from '@@/plugin-model';
+// @ts-ignore
 import { Button, DatePicker, Input } from 'antd';
 import React from 'react';
 
 export default function HomePage() {
+  const { initialState } = useModel('@@initialState');
+  console.log('initialState', initialState);
   return (
     <div>
       <h2>index page</h2>
