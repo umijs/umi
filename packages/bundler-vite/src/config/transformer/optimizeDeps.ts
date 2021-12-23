b963e205e3d645e421d1899a72f1311a91297484
Peach <scdzwyxst@gmail.com>
2021-12-23 18:08:56 +0800

    fix(bundler-vite): set optimize entries for pre-bundling deps (#224)

--- packages/bundler-vite/src/config/transformer/optimizeDeps.ts
+++ packages/bundler-vite/src/config/transformer/optimizeDeps.ts
@@ -4,7 +4,10 @@ import type { IConfigProcessor } from '.';
  * transform user config to vite optimizeDeps config
  */
 export default (function optimizeDeps(userConfig) {
-  const config: ReturnType<IConfigProcessor> = { optimizeDeps: {} };
+  const config: ReturnType<IConfigProcessor> = {
+    // configure pre-bundling entries
+    optimizeDeps: { entries: Object.values(userConfig.entry) },
+  };
 
   // include alias which within node_modules for optimize dependencies
   if (typeof userConfig.alias === 'object') {
