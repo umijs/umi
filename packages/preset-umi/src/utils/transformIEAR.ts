a151b1e548ec834f0e69d04bcc38aaa971110c5d
sorrycc <sorrycc@gmail.com>
2021-12-29 16:49:55 +0800

    chore: code style

--- packages/preset-umi/src/utils/transformIEAR.ts
+++ packages/preset-umi/src/utils/transformIEAR.ts
@@ -4,6 +4,13 @@ import type { IApi } from '../types';
 
 /**
  * import/export/await-import/require match regular expression
+ *
+ * WHY: REGEXP
+ * ref: https://github.com/umijs/umi-next/pull/230
+ *
+ * TODO: more choices
+ * 1. fork es-module-lexer, support jsx
+ * 2. use sourcemap of esbuild
  */
 export const IEAR_REG_EXP = new RegExp(
   [
