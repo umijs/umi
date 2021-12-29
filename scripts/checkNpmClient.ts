79e9c0e6b20d4566ab4c86ae64d53e4a1122242a
Yingci <59400654+fz6m@users.noreply.github.com>
2021-12-29 11:59:36 +0800

    chore: only allow pnpm install (#232)

--- scripts/checkNpmClient.ts
+++
@@ -1,6 +0,0 @@
-import * as logger from '@umijs/utils/src/logger';
-
-if (!/pnpm/.test(process.env.npm_execpath || '')) {
-  logger.error(`This repository requires using pnpm to work properly.`);
-  process.exit(1);
-}
