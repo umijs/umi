69d63781f7162f9d23acbfa6aeaf90e5b677d09c
sorrycc <sorrycc@gmail.com>
2022-01-04 17:12:13 +0800

    fix: tsc check

--- packages/bundler-webpack/src/config/forkTSCheckerPlugin.ts
+++ packages/bundler-webpack/src/config/forkTSCheckerPlugin.ts
@@ -1,3 +1,4 @@
+// @ts-ignore
 import ForkTSCheckerPlugin from '@umijs/bundler-webpack/compiled/fork-ts-checker-webpack-plugin';
 import Config from '@umijs/bundler-webpack/compiled/webpack-5-chain';
 import { Env, IConfig } from '../types';
