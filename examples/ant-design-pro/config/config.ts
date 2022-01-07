00edbc5f420d8bbeec81c89887d3f7e7660cc049
sorrycc <sorrycc@gmail.com>
2022-01-07 17:09:21 +0800

    fix(preset-umi): entry path not found in vite mode

--- examples/ant-design-pro/config/config.ts
+++ examples/ant-design-pro/config/config.ts
@@ -1,9 +1,9 @@
 // https://umijs.org/config/
-// import { defineConfig } from 'umi';
+import { defineConfig } from 'umi';
 import defaultSettings from './defaultSettings';
 import proxy from './proxy';
 const { REACT_APP_ENV } = process.env;
-export default {
+export default defineConfig({
   hash: true,
   model: {},
   antd: {},
@@ -349,4 +349,4 @@ export default {
   //   type: 'none',
   // },
   // exportStatic: {},
-};
+});
