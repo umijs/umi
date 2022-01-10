fd003032fdf7a567fc49e68f93f3ceb455c06486
sorrycc <sorrycc@gmail.com>
2022-01-10 17:01:07 +0800

    fix(preset-umi): umi.css is lost

--- packages/preset-umi/src/commands/dev/createRouteMiddleware.ts
+++ packages/preset-umi/src/commands/dev/createRouteMiddleware.ts
@@ -26,6 +26,7 @@ export function createRouteMiddleware(opts: { api: IApi }): RequestHandler {
     // @ts-ignore
     const requestHandler = await createRequestHandler({
       ...markupArgs,
+      styles: ['/umi.css'].concat(markupArgs.styles),
       scripts: (vite ? viteScripts : ['/umi.js']).concat(markupArgs.scripts!),
       esmScript: vite,
     });
