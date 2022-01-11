838540833eb5bb3f29b3fd92a88607389b812b8f
sorrycc <sorrycc@gmail.com>
2022-01-11 14:36:10 +0800

    test(preset-umi): fix mock test

--- packages/preset-umi/src/features/mock/getMockData.test.ts
+++ packages/preset-umi/src/features/mock/getMockData.test.ts
@@ -6,6 +6,7 @@ const fixtures = join(__dirname, './fixtures');
 test('normal', () => {
   const ret = getMockData({
     cwd: join(fixtures, 'normal'),
+    mockConfig: {},
   });
   expect(Object.keys(ret)).toEqual(['GET /api/a', 'GET /api/b', 'GET /api/c']);
 });
