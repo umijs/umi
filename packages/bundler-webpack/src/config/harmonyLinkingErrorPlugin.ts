08e58c9ca73e01add14eace90fca57c53d4ff31b
sorrycc <sorrycc@gmail.com>
2022-01-11 16:35:24 +0800

    fix(bundler-webpack): harmony error exclude css modules

--- packages/bundler-webpack/src/config/harmonyLinkingErrorPlugin.ts
+++ packages/bundler-webpack/src/config/harmonyLinkingErrorPlugin.ts
@@ -10,6 +10,8 @@ interface IOpts {
 
 // ref: https://github.com/webpack/webpack/blob/ccecc17c01af96edddb931a76e7a3b21ef2969d8/lib/dependencies/HarmonyImportDependency.js#L164
 const LINKING_ERROR_TAG = 'was not found in';
+// build 时会出现 css modules 的引用警告，但这应该是需要忽略的
+const CSS_NO_EXPORTS = /\.(css|sass|scss|styl|less)' \(module has no exports\)/;
 
 class HarmonyLinkingErrorPlugin {
   apply(compiler: Compiler) {
@@ -23,7 +25,8 @@ class HarmonyLinkingErrorPlugin {
           return (
             w.name === 'ModuleDependencyWarning' &&
             !(w.module as NormalModule).resource.includes('node_modules') &&
-            w.message.includes(LINKING_ERROR_TAG)
+            w.message.includes(LINKING_ERROR_TAG) &&
+            !CSS_NO_EXPORTS.test(w.message)
           );
         });
         if (!harmonyLinkingErrors.length) {
