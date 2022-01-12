7e129cb56f783da42bdaebeb8141f1cb182b2be9
sorrycc <sorrycc@gmail.com>
2022-01-12 12:12:49 +0800

    example(boilerplate): try script externalsType

--- examples/boilerplate/.umirc.ts
+++ examples/boilerplate/.umirc.ts
@@ -1,10 +1,20 @@
 import extraConfig from './extraConfig';
 
 export default {
-  // externals: {
-  //   react: 'React',
-  //   'react-dom': 'ReactDOM',
-  // },
+  externals: {
+    marked: [
+      'script https://gw.alipayobjects.com/os/lib/marked/2.0.0/marked.min.js',
+      'marked',
+    ],
+    '@antv/g2': [
+      'script https://gw.alipayobjects.com/os/lib/antv/g2/3.5.19/dist/g2.min.js',
+      'G2',
+    ],
+    '@antv/g6': [
+      'script https://gw.alipayobjects.com/os/lib/antv/g6/4.1.16/dist/g6.min.js',
+      'G6',
+    ],
+  },
   chainWebpack(memo: any) {
     memo;
   },
