af24567d3c0ff53e62b12bb0f4ef7a198c2f5517
sorrycc <sorrycc@gmail.com>
2022-01-13 15:47:04 +0800

    fix(plugin/layout): menu route not match

--- examples/ant-design-pro/config/config.ts
+++ examples/ant-design-pro/config/config.ts
@@ -329,6 +329,7 @@ export default defineConfig({
   // esbuild: {},
   // title: false,
   ignoreMomentLocale: true,
+  // @ts-ignore
   proxy: proxy[REACT_APP_ENV || 'dev'],
   manifest: {
     basePath: '/',
