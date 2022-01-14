d75357759bfe3aac9720d0b5299dd2e5f07f1824
sorrycc <sorrycc@gmail.com>
2022-01-14 11:26:12 +0800

    feat(utils): add logger.debug and fix types

--- packages/utils/src/logger.ts
+++ packages/utils/src/logger.ts
@@ -7,28 +7,35 @@ export const prefixes = {
   ready: chalk.green('ready') + ' -',
   info: chalk.cyan('info') + '  -',
   event: chalk.magenta('event') + ' -',
+  debug: chalk.gray('debug') + ' -',
 };
 
-export function wait(...message: string[]) {
+export function wait(...message: any[]) {
   console.log(prefixes.wait, ...message);
 }
 
-export function error(...message: string[]) {
+export function error(...message: any[]) {
   console.error(prefixes.error, ...message);
 }
 
-export function warn(...message: string[]) {
+export function warn(...message: any[]) {
   console.warn(prefixes.warn, ...message);
 }
 
-export function ready(...message: string[]) {
+export function ready(...message: any[]) {
   console.log(prefixes.ready, ...message);
 }
 
-export function info(...message: string[]) {
+export function info(...message: any[]) {
   console.log(prefixes.info, ...message);
 }
 
-export function event(...message: string[]) {
+export function event(...message: any[]) {
   console.log(prefixes.event, ...message);
+}
+
+export function debug(...message: any[]) {
+  if (process.env.DEBUG) {
+    console.log(prefixes.debug, ...message);
+  }
 }
