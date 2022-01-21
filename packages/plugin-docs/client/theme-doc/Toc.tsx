c69ede359beb11889d0f1cac90f669dcf090028c
sorrycc <sorrycc@gmail.com>
2022-01-21 13:22:45 +0800

    fix(plugin-docs): toc route not found

--- packages/plugin-docs/client/theme-doc/Toc.tsx
+++ packages/plugin-docs/client/theme-doc/Toc.tsx
@@ -2,8 +2,13 @@ import React from 'react';
 import { useThemeContext } from './context';
 
 export default () => {
-  const { location, appData } = useThemeContext();
+  const { location, appData } = useThemeContext()!;
   const route = appData.routes[location.pathname.slice(1)];
+
+  if (!route) {
+    return null;
+  }
+
   const titles = route.titles.filter((t: any) => t.level > 1);
   return (
     <div>
