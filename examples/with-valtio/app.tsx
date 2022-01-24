459073742ac6ddedd4dcb57e2533d663bad6db1e
小虎oni <xiaohuoni@users.noreply.github.com>
2022-01-24 21:25:50 +0800

    chore: code style (#293)
    * chore: code style
    
    * chore: prettier ignore wailwind outcss

--- examples/with-valtio/app.tsx
+++ examples/with-valtio/app.tsx
@@ -6,19 +6,19 @@ export function rootContainer(container: any, opts: any) {
 }
 
 export function innerProvider(container: any) {
-  return React.createElement(Foo, {title: 'innerProvider'}, container);
+  return React.createElement(Foo, { title: 'innerProvider' }, container);
 }
 
 export function i18nProvider(container: any) {
-  return React.createElement(Foo, {title: 'i18nProvider'}, container);
+  return React.createElement(Foo, { title: 'i18nProvider' }, container);
 }
 
 export function dataflowProvider(container: any) {
-  return React.createElement(Foo, {title: 'dataflowProvider'}, container);
+  return React.createElement(Foo, { title: 'dataflowProvider' }, container);
 }
 
 export function outerProvider(container: any) {
-  return React.createElement(Foo, {title: 'outerProvider'}, container);
+  return React.createElement(Foo, { title: 'outerProvider' }, container);
 }
 
 function Foo(props: any) {
--- examples/with-valtio/components/DisplayCount.tsx
+++ examples/with-valtio/components/DisplayCount.tsx
@@ -1,10 +1,10 @@
-import React from "react";
-import {useSnapshot} from "valtio";
-import state from "../state";
+import React from 'react';
+import { useSnapshot } from 'valtio';
+import state from '../state';
 
 function DisplayCount() {
   const snap = useSnapshot(state);
-  return <p className="display-count">{snap.number}</p>
+  return <p className="display-count">{snap.number}</p>;
 }
 
 export default DisplayCount;
