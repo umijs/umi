50902865604cf496f646b8f0c038fcc06381c934
Yuanlin Lin <im.yuanlinlin@gmail.com>
2022-01-24 23:09:17 +0800

    feat(plugin-docs): Added dark mode (#302)
    * feat(plugin-docs): Added dark mode
    
    * feat(plugin-docs): Fixed some component not support dark mode

--- packages/plugin-docs/client/theme-doc/Search.tsx
+++ packages/plugin-docs/client/theme-doc/Search.tsx
@@ -31,7 +31,8 @@ export default () => {
       <div
         className="rounded-lg w-40 lg:w-64 flex items-center pr-2 flex-row hover:bg-gray-50
      transition duration-300 bg-gray-100 border border-white focus-within:border-gray-100
-     focus-within:bg-white"
+     focus-within:bg-white dark:bg-gray-700 dark:border-gray-700
+     dark:focus-within:border-gray-700 dark:focus-within:bg-gray-800 dark:text-gray-100"
       >
         <input
           onFocus={() => setIsFocused(true)}
@@ -44,34 +45,39 @@ export default () => {
         />
         <div
           className="bg-gray-200 rounded w-6 h-6 flex flex-row text-gray-400
-       items-center justify-center border border-gray-300"
+       focus-within:bg-white"
         >
-          /
-        </div>
-      </div>
-      <div
-        className={cx(
-          'absolute transition-all duration-500 top-16 w-96 rounded-lg',
-          'cursor-pointer shadow overflow-hidden',
-          result.length > 0 && isFocused ? 'max-h-80' : 'max-h-0',
-        )}
-      >
-        {result.map((r, i) => (
-          <a
-            href={r.href}
-            key={i}
-            className="group outline-none"
-            onFocus={() => setIsFocused(true)}
-            onBlur={() => setIsFocused(false)}
+          <div
+            className="bg-gray-200 rounded w-6 h-6 flex flex-row text-gray-400
+         items-center justify-center border border-gray-300"
           >
-            <p
-              className="p-2 bg-white hover:bg-gray-50 transition
-               duration-300 group-focus:bg-blue-200"
+            /
+          </div>
+        </div>
+        <div
+          className={cx(
+            'absolute transition-all duration-500 top-16 w-96 rounded-lg',
+            'cursor-pointer shadow overflow-hidden',
+            result.length > 0 && isFocused ? 'max-h-80' : 'max-h-0',
+          )}
+        >
+          {result.map((r, i) => (
+            <a
+              href={r.href}
+              key={i}
+              className="group outline-none"
+              onFocus={() => setIsFocused(true)}
+              onBlur={() => setIsFocused(false)}
             >
-              {r.path}
-            </p>
-          </a>
-        ))}
+              <p
+                className="p-2 bg-white hover:bg-gray-50 transition
+                 duration-300 group-focus:bg-blue-200 dark:bg-gray-700"
+              >
+                {r.path}
+              </p>
+            </a>
+          ))}
+        </div>
       </div>
     </Fragment>
   );
--- packages/plugin-docs/tailwind.config.js
+++ packages/plugin-docs/tailwind.config.js
@@ -1,3 +1,4 @@
 module.exports = {
   content: ['./client/theme-doc/*.tsx'],
+  darkMode: 'class'
 };
