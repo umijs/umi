f226b86f6ea33ffc47015935c65ed3b0ce24e05a
Yuanlin Lin <im.yuanlinlin@gmail.com>
2022-01-24 21:20:58 +0800

    feat(plugin-docs): using classnames package to organize tailwind classnames (#296)

--- packages/plugin-docs/client/theme-doc/ThemeSwitch.tsx
+++ packages/plugin-docs/client/theme-doc/ThemeSwitch.tsx
@@ -1,3 +1,4 @@
+import cx from 'classnames';
 import React, { useState } from 'react';
 import MoonIcon from './icons/moon.png';
 import SunIcon from './icons/sun.png';
@@ -5,22 +6,21 @@ import SunIcon from './icons/sun.png';
 export default () => {
   const [toggle, setToggle] = useState(true);
 
-  const toggleClass = ' transform translate-x-6';
-
   return (
     <div
-      className={
-        'md:w-12 md:h-6 w-12 h-4 flex items-center bg-gray-300 ' +
-        'rounded-full p-1 cursor-pointer' +
-        (toggle ? ' bg-blue-300' : ' bg-gray-700')
-      }
+      className={cx(
+        'md:w-12 md:h-6 w-12 h-4 flex items-center bg-gray-300 rounded-full ',
+        'py-1 px-1.5  cursor-pointer',
+        toggle ? 'bg-blue-300' : 'bg-gray-700',
+      )}
       onClick={() => setToggle(!toggle)}
     >
       <div
-        className={
-          'md:w-4 md:h-4 h-3 w-3 rounded-full shadow-md transition transform' +
-          (toggle ? null : toggleClass)
-        }
+        className={cx(
+          'md:w-4 md:h-4 h-3 w-3 rounded-full shadow-md ',
+          'transition transform',
+          toggle && 'translate-x-5',
+        )}
       >
         <img
           src={toggle ? SunIcon : MoonIcon}
