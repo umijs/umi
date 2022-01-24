ad74ce3ed04a0cd143ea54bb9f92b3e8dc6442d0
Yuanlin Lin <im.yuanlinlin@gmail.com>
2022-01-24 21:21:45 +0800

    feat(plugin-docs): Added search function (#298)
    * feat(plugin-docs-search): Added search function
    
    * feat(plugin-docs-search): Added first level title into search result
    
    * feat(plugin-docs-search): Improving search function for better performance

--- packages/plugin-docs/client/theme-doc/Search.tsx
+++ packages/plugin-docs/client/theme-doc/Search.tsx
@@ -1,32 +1,115 @@
-import React, { useEffect } from 'react';
+import cx from 'classnames';
+import React, { Fragment, useEffect, useState } from 'react';
+import { useThemeContext } from './context';
 
 export default () => {
+  const [isFocused, setIsFocused] = useState(false);
+  const [keyword, setKeyword] = useState('');
+
+  const { appData } = useThemeContext()!;
+
   useEffect(() => {
     document.addEventListener('keydown', function (event) {
+      // 在页面中按下 '/' 键可以打开搜索框
       if (event.key === '/') {
         event.preventDefault();
         document.getElementById('search-input')?.focus();
       }
+
+      // 在搜索框中按下 'Escape' 键可以关闭搜索框
+      if (event.key === 'Escape') {
+        document.getElementById('search-input')?.blur();
+        (document.activeElement as HTMLElement).blur();
+      }
     });
   }, []);
 
+  const result = search(appData.routes, keyword);
+
   return (
-    <div
-      className="rounded-lg w-64 flex items-center pr-2 flex-row hover:bg-gray-50
+    <Fragment>
+      <div
+        className="rounded-lg w-64 flex items-center pr-2 flex-row hover:bg-gray-50
      transition duration-300 bg-gray-100 border border-white focus-within:border-gray-100
      focus-within:bg-white"
-    >
-      <input
-        id="search-input"
-        className="w-full bg-transparent outline-0 text-sm px-4 py-2 "
-        placeholder="Search anything ..."
-      />
-      <div
-        className="bg-gray-200 rounded w-6 h-6 flex flex-row text-gray-400
-       items-center justify-center border border-gray-300"
       >
-        /
+        <input
+          onFocus={() => setIsFocused(true)}
+          onBlur={() => setIsFocused(false)}
+          value={keyword}
+          onChange={(e) => setKeyword(e.target.value)}
+          id="search-input"
+          className="w-full bg-transparent outline-0 text-sm px-4 py-2 "
+          placeholder="Search anything ..."
+        />
+        <div
+          className="bg-gray-200 rounded w-6 h-6 flex flex-row text-gray-400
+       items-center justify-center border border-gray-300"
+        >
+          /
+        </div>
       </div>
-    </div>
+      <div
+        className={cx(
+          'absolute transition-all duration-500 top-16 w-96 rounded-lg',
+          'cursor-pointer shadow overflow-hidden',
+          result.length > 0 && isFocused ? 'max-h-80' : 'max-h-0',
+        )}
+      >
+        {result.map((r, i) => (
+          <a
+            href={r.href}
+            key={i}
+            className="group outline-none"
+            onFocus={() => setIsFocused(true)}
+            onBlur={() => setIsFocused(false)}
+          >
+            <p
+              className="p-2 bg-white hover:bg-gray-50 transition
+               duration-300 group-focus:bg-blue-200"
+            >
+              {r.path}
+            </p>
+          </a>
+        ))}
+      </div>
+    </Fragment>
   );
 };
+
+interface SearchResultItem {
+  path: string;
+  href: string;
+}
+
+function search(routes: any, keyword: string): SearchResultItem[] {
+  if (!keyword) return [];
+
+  const result: SearchResultItem[] = [];
+
+  Object.keys(routes).map((path) => {
+    if (path.toLowerCase().includes(keyword.toLowerCase())) {
+      result.push({
+        path: path.split('/').slice(1).join(' > '),
+        href: '/' + path,
+      });
+    }
+
+    const route = routes[path];
+    if (!route.titles) return;
+    route.titles
+      .filter((t: any) => t.level <= 2)
+      .map((title: any) => {
+        if (title.title.toLowerCase().includes(keyword.toLowerCase())) {
+          result.push({
+            path: path.split('/').slice(1).join(' > ') + ' > ' + title.title,
+            href: '/' + path + '#' + title.title,
+          });
+        }
+      });
+  });
+
+  if (result.length > 8) return result.slice(0, 8);
+
+  return result;
+}
