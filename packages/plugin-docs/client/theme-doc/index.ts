09225d936390d575987079de8a99740d7a2ac84c
Yuanlin Lin <im.yuanlinlin@gmail.com>
2022-01-26 12:25:12 +0800

    feat(plugin-docs): Added mdx Message component for docs (#310)

--- packages/plugin-docs/client/theme-doc/index.ts
+++ packages/plugin-docs/client/theme-doc/index.ts
@@ -1,2 +1,5 @@
 import './tailwind.out.css';
 export { default as Layout } from './Layout';
+
+// Components
+export { default as Message } from './components/Message';
--- packages/plugin-docs/tailwind.config.js
+++ packages/plugin-docs/tailwind.config.js
@@ -1,4 +1,4 @@
 module.exports = {
-  content: ['./client/theme-doc/*.tsx'],
+  content: ['./client/theme-doc/**/*.tsx'],
   darkMode: 'class'
 };
