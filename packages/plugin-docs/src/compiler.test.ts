6190cf62a5b654c6d699f0bdf52f132109290820
sorrycc <sorrycc@gmail.com>
2022-01-26 11:03:44 +0800

    fix(plugin-umi): route.file exception

--- packages/plugin-docs/src/compiler.test.ts
+++ packages/plugin-docs/src/compiler.test.ts
@@ -2,6 +2,5 @@ import { compile } from './compiler';
 
 test('normal', async () => {
   const { result } = await compile({ content: '# Hello' });
-  console.log(result);
   expect(result).toContain('{`Hello`}</h1>');
 });
