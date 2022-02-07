1b883b421f2d59d7f0b78be29664ed4bfd2f2b4e
Yuanlin Lin <im.yuanlinlin@gmail.com>
2022-02-07 18:13:17 +0800

    feat(plugin-docs): Added <Features /> component for landing page (#341)

--- packages/plugin-docs/client/theme-doc/index.ts
+++ packages/plugin-docs/client/theme-doc/index.ts
@@ -3,4 +3,6 @@ import './tailwind.out.css';
 // Components
 export { default as Hero } from './components/Hero';
 export { default as Message } from './components/Message';
+export { default as Features } from './components/Features';
+export { default as FeatureItem } from './components/FeatureItem';
 export { default as Layout } from './Layout';
--- packages/plugin-docs/src/index.ts
+++ packages/plugin-docs/src/index.ts
@@ -68,7 +68,7 @@ export default (api: IApi) => {
     api.writeTmpFile({
       path: 'index.ts',
       content: `
-export { Message, Hero } from '${require.resolve('../client/theme-doc')}';
+export { Message, Hero, Features, FeatureItem } from '${require.resolve('../client/theme-doc')}';
     `,
     });
 
