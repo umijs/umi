cdf45ac9ab9a25206b80b77e58b3fda43849962b
sorrycc <sorrycc@gmail.com>
2022-02-14 10:13:32 +0800

    feat(plugin-docs): use compiled @mdx-js/mdx and rehype plugins

--- packages/plugin-docs/src/compiler.test.ts
+++ packages/plugin-docs/src/compiler.test.ts
@@ -2,5 +2,5 @@ import { compile } from './compiler';
 
 test('normal', async () => {
   const { result } = await compile({ content: '# Hello' });
-  expect(result).toContain('{`Hello`}</h1>');
+  expect(result).toContain('{"Hello"}</_components.h1>');
 });
