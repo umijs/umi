9cecc02fe6839f5fba0c906e58d035e0791c38fe
Yuanlin Lin <im.yuanlinlin@gmail.com>
2022-02-14 14:04:31 +0800

    Feature/plugin: Adjusted <Featurs /> component layout (#359)
    * feat(plugin-docs): Adjusted <Features /> layout
    
    * feat(plugin-docs): Improved <Features /> for better RWD
    
    * feat(plugin-docs): Removed unused ref in <Features />

--- packages/plugin-docs/client/theme-doc/components/FeatureItem.tsx
+++ packages/plugin-docs/client/theme-doc/components/FeatureItem.tsx
@@ -1,10 +1,10 @@
-import React from "react";
+import React from 'react';
 
 interface Feature {
   icon: string;
   title: string;
   description: string;
-  link?: string
+  link?: string;
 }
 
 /**
@@ -12,33 +12,37 @@ interface Feature {
  * 从 docs/README.md 中使用 MDX 语法调用，必须被包含在 <Features /> 组件内
  * */
 function FeatureItem(props: Feature) {
-  return <div
-    className="w-3/4 lg:w-1/2 shrink-0 flex flex-row items-center
-    snap-always snap-center justify-center">
-    <div
-      className="flex flex-col w-5/6 lg:w-3/4 px-4 lg:px-16 items-center
-      bg-white dark:bg-gray-800 py-16
-      border-gray-300 dark:border-gray-500 border
-      rounded-xl shadow-xl h-96 lg:h-auto dark:shadow-gray-700">
-      <img src={props.icon} className="w-8 h-8" alt="feature-icon" />
-      <p
-        className="text-3xl lg:text-5xl font-extrabold
-         mt-4 mb-8 text-gray-900 dark:text-gray-200">
-        {props.title}
-      </p>
-      <p
-        className="text-center text-gray-800 dark:text-gray-400">
-        {props.description}
-      </p>
-      {props.link && <a
-        href={props.link}
-        target="_blank"
-        rel="noreferrer"
-        className="mt-8 link-with-underline">
-        深入了解
-      </a>}
+  return (
+    <div className="w-full md:w-1/2 lg:w-1/3 flex flex-row items-center justify-center mb-8 lg:mb-16">
+      <div
+        className="flex flex-col w-5/6 lg:w-3/4 items-center
+      bg-white dark:bg-gray-800 py-12 px-6 justify-center
+      border-gray-300 dark:border-gray-500 border transition-all hover:scale-105
+      rounded-xl shadow-lg hover:shadow-2xl h-72 lg:h-96 dark:shadow-gray-700"
+      >
+        <img src={props.icon} className="w-8 h-8" alt="feature-icon" />
+        <p
+          className="text-3xl font-extrabold
+         mt-4 mb-8 text-gray-900 dark:text-gray-200"
+        >
+          {props.title}
+        </p>
+        <p className="text-center text-gray-600 text-sm lg:text-base dark:text-gray-400">
+          {props.description}
+        </p>
+        {props.link && (
+          <a
+            href={props.link}
+            target="_blank"
+            rel="noreferrer"
+            className="mt-8 link-with-underline"
+          >
+            深入了解
+          </a>
+        )}
+      </div>
     </div>
-  </div>
+  );
 }
 
 export default FeatureItem;
--- packages/plugin-docs/client/theme-doc/components/Features.tsx
+++ packages/plugin-docs/client/theme-doc/components/Features.tsx
@@ -1,41 +1,44 @@
-import React, { PropsWithChildren, useEffect, useRef } from "react"
+import React, { PropsWithChildren } from 'react';
 
 /**
  * Features 组件是文档首页第二个 Feature 区块的容器，
  * 从 docs/README.md 中使用 MDX 语法调用，可在内部传入 <FeatureItem /> 组件
  * */
-function Features(props: PropsWithChildren<{ title?: string, subtitle?: string }>) {
-
-  const ref = useRef<HTMLDivElement>(null)
-
-  useEffect(() => {
-    /** 组件加载完毕后，移动滑动条到某个 Feature Item 上，否则初始状态是歪的*/
-    if (ref) ref.current?.scrollTo(600, 0)
-  }, [ref])
-
-  return <div
-    className="w-screen py-36 features dark:features-dark min-h-screen">
-    {(props.title || props.subtitle) && <div className="mb-24 px-4">
-      {props.title && <p
-        className="text-4xl lg:text-5xl font-extrabold mb-4 text-center
-          text-gray-700 dark:text-gray-300">
-        {props.title}
-      </p>}
-      {props.subtitle && <p
-        className="text-lg lg:text-xl text-center
-          text-gray-500 dark:text-gray-400">
-        {props.subtitle}
-      </p>}
-    </div>}
-    <div
-      ref={ref}
-      className="w-full flex flex-row snap-x overflow-x-scroll
-       features pb-12 dark:features-dark">
-      <div className="w-1/2 shrink-0" />
-      {props.children}
-      <div className="w-1/2 shrink-0" />
+function Features(
+  props: PropsWithChildren<{ title?: string; subtitle?: string }>,
+) {
+  return (
+    <div className="w-screen py-36 features dark:features-dark min-h-screen">
+      {(props.title || props.subtitle) && (
+        <div className="mb-24 px-4">
+          {props.title && (
+            <p
+              className="text-4xl lg:text-5xl font-extrabold mb-4 text-center
+          text-gray-700 dark:text-gray-300"
+            >
+              {props.title}
+            </p>
+          )}
+          {props.subtitle && (
+            <p
+              className="text-lg lg:text-xl text-center
+          text-gray-500 dark:text-gray-400"
+            >
+              {props.subtitle}
+            </p>
+          )}
+        </div>
+      )}
+      <div className="w-full flex flex-row justify-center">
+        <div
+          className="w-full flex flex-row flex-wrap
+       features pb-12 dark:features-dark container"
+        >
+          {props.children}
+        </div>
+      </div>
     </div>
-  </div>
+  );
 }
 
-export default Features
+export default Features;
