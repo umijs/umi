7a67ca70230e620d659d041ff2561e57ec2a88b1
sorrycc <sorrycc@gmail.com>
2022-02-18 16:01:46 +0800

    fix(plugin-docs): use Link to navigate

--- packages/plugin-docs/client/theme-doc/Logo.tsx
+++ packages/plugin-docs/client/theme-doc/Logo.tsx
@@ -2,17 +2,17 @@ import React from 'react';
 import { useThemeContext } from './context';
 
 export default () => {
-  const { themeConfig } = useThemeContext()!;
+  const { themeConfig, components } = useThemeContext()!;
   // @ts-ignore
   const { logo } = themeConfig;
   return (
-    <a href="/">
+    <components.Link to="/">
       <div className="flex flex-row items-center">
         <img src={logo} className="w-8 h-8" alt="logo" />
         <div className="text-xl font-extrabold ml-2 dark:text-white">
           {themeConfig.title}
         </div>
       </div>
-    </a>
+    </components.Link>
   );
 };
