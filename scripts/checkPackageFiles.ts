cad69fff8909a9dd2e4cfe809c1fa2eda715a4b9
Shu Pengfei <stormslowly@gmail.com>
2022-03-04 18:17:55 +0800

    feat: ✨ implement npm package files using blacklist way (#426)
    Co-authored-by: pshu <pishu.spf@antfin.com>

--- scripts/checkPackageFiles.ts
+++ scripts/checkPackageFiles.ts
@@ -1,11 +1,38 @@
+import * as logger from '@umijs/utils/src/logger';
+import { readdirSync } from 'fs';
+import { isMatch } from 'matcher';
 import path from 'path';
 import { eachPkg, getPkgs } from './utils';
 
+const COMMON_IGNORES = [
+  'src',
+  '*.md',
+  'tsconfig*.json',
+  'fixtures',
+  'node_modules',
+  'package.json',
+];
+
 const cwd = process.cwd();
+let missingDetected = false;
 eachPkg(getPkgs(), (opts) => {
-  console.log('Checking package:', opts.pkg);
   const pkg = require(path.join(cwd, 'packages', opts.pkg, 'package.json'));
-  pkg.files;
-  // TODO
-  // 检测 pkg.files 是否包含必要的和当前 pkg 下的文件和目录
+  const base = path.join(cwd, 'packages', opts.pkg);
+
+  const files = readdirSync(base).filter((f) => !isMatch(f, COMMON_IGNORES));
+  const missingAddFiles = files.filter((f) => !isMatch(f, pkg.files));
+
+  if (missingAddFiles.length > 0) {
+    logger.error('Checking package:', opts.pkg);
+    logger.error(
+      `  "${missingAddFiles.join(
+        ', ',
+      )}"  missing in the package.json files field`,
+    );
+    missingDetected = true;
+  }
 });
+
+if (missingDetected) {
+  process.exit(1);
+}
