f00ae4389f3c6c3938bfe021ffd421a48bab53bd
cnyballk(云剪者) <584518260@qq.com>
2022-03-09 14:44:09 +0800

    example: add redux-toolkit (#447)
    * example: add redux-toolkit
    
    * chore: rename with-redux-toolkit
    
    Co-authored-by: chencheng (云谦) <sorrycc@gmail.com>

---
+++ examples/with-redux-toolkit/pages/index.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { useAppDispatch, useAppSelector } from '../redux/hook';
+import {
+  selectCount,
+  incrementAction,
+  decrementAction,
+} from '../redux/reducer/counterSlice';
+import '../style.less';
+
+export default function HomePage() {
+  const count = useAppSelector(selectCount);
+  const dispatch = useAppDispatch();
+
+  return (
+    <div className="container">
+      <p className="title">UmiJS x redux-toolkit</p>
+      <p className="display-count">{count}</p>
+      <div>
+        <button onClick={() => dispatch(incrementAction())}>+</button>
+        <button onClick={() => dispatch(decrementAction())}>-</button>
+      </div>
+    </div>
+  );
+}
---
+++ examples/with-redux-toolkit/readme.md
@@ -0,0 +1,3 @@
+# with-redux-toolkit
+
+An example of using [UmiJS](https://umijs.org/zh-CN) with [redux-toolkit](https://redux-toolkit.js.org/).
