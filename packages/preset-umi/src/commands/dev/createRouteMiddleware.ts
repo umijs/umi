ccdbfceaef9f5f1be85b0648b3a5e37ae2c66061
sorrycc <sorrycc@gmail.com>
2022-03-10 14:40:25 +0800

    fix(preset-umi): assets path problem

--- packages/preset-umi/src/commands/dev/createRouteMiddleware.ts
+++ packages/preset-umi/src/commands/dev/createRouteMiddleware.ts
@@ -1,7 +1,8 @@
-import type { RequestHandler } from '@umijs/bundler-webpack';
+import type { RequestHandler, Compiler, Stats } from '@umijs/bundler-webpack';
 import { createRequestHandler, IOpts } from '@umijs/server';
 import { IApi } from '../../types';
 import { getMarkupArgs } from './getMarkupArgs';
+import { getAssetsMap } from './getAssetsMap';
 
 // TODO: extract to bundler-vite
 // refer: https://vitejs.dev/guide/backend-integration.html#backend-integration
@@ -13,24 +14,58 @@ window.$RefreshSig$ = () => (type) => type
 window.__vite_plugin_react_preamble_installed__ = true
 `;
 
-export function createRouteMiddleware(opts: { api: IApi }): RequestHandler {
-  return async (req, res, next) => {
+function createRouteMiddleware(opts: { api: IApi }) {
+  return ({ compiler }: { compiler: Compiler }): RequestHandler => {
     const { vite } = opts.api.config;
-    const viteScripts: IOpts['scripts'] = [
-      // add noshim attr for skip importmap shim logic for this modules
-      { content: viteRefreshScript, noshim: '' },
-      { src: '/@vite/client', noshim: '' },
-      opts.api.appData.hasSrcDir ? '/src/.umi/umi.ts' : '/.umi/umi.ts',
-    ];
-    const markupArgs = await getMarkupArgs(opts);
-    // @ts-ignore
-    const requestHandler = await createRequestHandler({
-      ...markupArgs,
-      // css will be injected with style tag in vite mode
-      styles: (vite ? [] : ['/umi.css']).concat(markupArgs.styles),
-      scripts: (vite ? viteScripts : ['/umi.js']).concat(markupArgs.scripts!),
-      esmScript: vite,
-    });
-    requestHandler(req, res, next);
+    let webpackStats: Stats | null = null;
+    let onStats: Function | null = null;
+
+    if (!vite) {
+      compiler.hooks.done.tap('umiRouteMiddleware', (stats: any) => {
+        webpackStats = stats;
+        onStats?.(stats);
+      });
+    }
+
+    async function getStats() {
+      if (webpackStats) return Promise.resolve(webpackStats);
+      return new Promise((resolve) => {
+        onStats = (stats: any) => {
+          resolve(stats);
+        };
+      });
+    }
+
+    return async (req, res, next) => {
+      const viteScripts: IOpts['scripts'] = [
+        // add noshim attr for skip import-maps shim logic for this modules
+        { content: viteRefreshScript, noshim: '' },
+        { src: '/@vite/client', noshim: '' },
+        opts.api.appData.hasSrcDir ? '/src/.umi/umi.ts' : '/.umi/umi.ts',
+      ];
+      const markupArgs = await getMarkupArgs(opts);
+      let assetsMap: Record<string, string[]> = {};
+      if (!vite) {
+        const stats: any = await getStats();
+        assetsMap = getAssetsMap({
+          stats,
+          publicPath: opts.api.config.publicPath,
+        });
+      }
+      const requestHandler = await createRequestHandler({
+        ...markupArgs,
+        // css will be injected with style tag in vite mode
+        styles: (vite ? [] : assetsMap['umi.css'] || []).concat(
+          markupArgs.styles,
+        ),
+        scripts: (vite ? viteScripts : assetsMap['umi.js'] || []).concat(
+          markupArgs.scripts!,
+        ),
+        esmScript: vite,
+      });
+      requestHandler(req, res, next);
+    };
   };
 }
+
+export { createRouteMiddleware };
---
+++ packages/preset-umi/src/commands/dev/getAssetsMap.ts
@@ -0,0 +1,21 @@
+const UMI_ASSETS_REG = {
+  js: /^umi(\..+)?\.js$/,
+  css: /^umi(\..+)?\.css$/,
+};
+
+export function getAssetsMap(opts: { stats: any; publicPath: string }) {
+  const { stats, publicPath } = opts;
+
+  let ret: Record<string, string[]> = {};
+  let json = stats.toJson();
+  const entrypoints = json.entrypoints || json.children[0].entrypoints;
+  for (const asset of entrypoints['umi'].assets) {
+    if (UMI_ASSETS_REG.js.test(asset.name)) {
+      ret['umi.js'] = [`${publicPath}${asset.name}`];
+    }
+    if (UMI_ASSETS_REG.css.test(asset.name)) {
+      ret['umi.css'] = [`${publicPath}${asset.name}`];
+    }
+  }
+  return ret;
+}
