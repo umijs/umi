245924a733da8fab6acdf3edf564a7e775d2353e
sorrycc <sorrycc@gmail.com>
2022-03-14 13:33:26 +0800

    feat(plugins/dva): support immer and dva-loading, Close #399, Close #398

--- examples/with-dva/pages/index.tsx
+++ examples/with-dva/pages/index.tsx
@@ -7,22 +7,35 @@ import { connect } from 'umi';
 function mapStateToProps(state: any) {
   return {
     count: state.count,
+    loading: state.loading,
   };
 }
 
 export default connect(mapStateToProps)(function Page(props: any) {
   return (
     <div>
-      <h1 className={styles.title}>Count {props.count}</h1>
-      <button
-        onClick={() => {
-          props.dispatch({
-            type: 'count/add',
-          });
-        }}
-      >
-        Add
-      </button>
+      <h1 className={styles.title}>Count {props.count.num}</h1>
+      <div>
+        <button
+          onClick={() => {
+            props.dispatch({
+              type: 'count/add',
+            });
+          }}
+        >
+          Add
+        </button>
+        <button
+          onClick={() => {
+            props.dispatch({
+              type: 'count/addAsync',
+            });
+          }}
+        >
+          Add Async
+        </button>
+      </div>
+      <div>{props.loading.global ? 'loading... ' : ''}</div>
     </div>
   );
 });
