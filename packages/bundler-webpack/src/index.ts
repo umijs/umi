13237321436967354ea02396ef18b9a467dda35a
sorrycc <sorrycc@gmail.com>
2022-03-15 14:52:05 +0800

    fix: export webpack type for umi and @umijs/max

--- packages/bundler-webpack/src/index.ts
+++ packages/bundler-webpack/src/index.ts
@@ -1,6 +1,9 @@
+import type webpack from '../compiled/webpack';
+
 export type { RequestHandler } from '../compiled/express';
 export type { Compiler, Stats } from '../compiled/webpack';
 export * from './build';
 export * from './config/config';
 export * from './dev';
 export * from './schema';
+export { webpack };
--- packages/umi/index.d.ts
+++ packages/umi/index.d.ts
@@ -1,4 +1,8 @@
 export * from '@@/exports';
-export type { IApi } from '@umijs/preset-umi';
-export type { UmiApiRequest, UmiApiResponse, IRoute } from '@umijs/preset-umi/src/types';
+export type { IApi, webpack } from '@umijs/preset-umi';
+export type {
+  IRoute,
+  UmiApiRequest,
+  UmiApiResponse,
+} from '@umijs/preset-umi/src/types';
 export * from './dist';
