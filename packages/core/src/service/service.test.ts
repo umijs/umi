75d25652ee4c8aba756ab79f8de6dd08d4005c6c
sorrycc <sorrycc@gmail.com>
2022-03-22 14:49:26 +0800

    chore(core): code style

--- packages/core/src/service/service.test.ts
+++ packages/core/src/service/service.test.ts
@@ -1,3 +1,4 @@
+import { existsSync } from 'fs';
 import { join } from 'path';
 import { Env } from '../types';
 import { Service } from './service';
@@ -5,10 +6,13 @@ import { Service } from './service';
 const base = join(__dirname, '../../fixtures/service');
 
 function buildService(opts: { name: string }) {
+  const cwd = join(base, opts.name);
+  const pluginPath = join(cwd, 'plugin.ts');
   return new Service({
-    cwd: join(base, opts.name),
+    cwd,
     env: Env.development,
-    defaultConfigFiles: ['.umirc.ts'],
+    defaultConfigFiles: ['.umirc.ts', 'config/config.ts'],
+    plugins: [(existsSync(pluginPath) && pluginPath) as string].filter(Boolean),
   });
 }
 
@@ -17,4 +21,44 @@ test('plugin should not return', async () => {
   await expect(service.run({ name: 'test' })).rejects.toThrow(
     /plugin should return nothing/,
   );
+});
+
+test('config normal', async () => {
+  const service = buildService({ name: 'umi-env' });
+  const userConfig = await service.run({ name: 'userConfig' });
+  expect(userConfig).toEqual({
+    foo: 1,
+    bar: 1,
+    nest: {
+      foo: {
+        bar: 2,
+      },
+    },
+  });
+});
+
+test('config umi-env', async () => {
+  process.env.UMI_ENV = 'cloud';
+  const service = buildService({ name: 'umi-env' });
+  const userConfig = await service.run({ name: 'userConfig' });
+  expect(userConfig).toEqual({
+    foo: 2,
+    bar: 1,
+    nest: {
+      foo: {
+        bar: 123,
+      },
+    },
+  });
+  process.env.UMI_ENV = '';
+});
+
+test('config umi-env-dot-env', async () => {
+  const service = buildService({ name: 'umi-env-dot-env' });
+  const userConfig = await service.run({ name: 'userConfig' });
+  expect(userConfig).toEqual({
+    bar: 2,
+    foo: 3,
+    nest: 4,
+  });
 });
