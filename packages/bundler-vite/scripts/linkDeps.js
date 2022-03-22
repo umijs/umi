1a7830b6490c1578ba0ebe28b9622a1987175222
Peach <scdzwyxst@gmail.com>
2022-03-22 12:24:43 +0800

    fix(bundler-vite): install failed bug with yarn (#525)

--- packages/bundler-vite/scripts/linkDeps.js
+++ packages/bundler-vite/scripts/linkDeps.js
@@ -2,8 +2,15 @@
 const fs = require('fs');
 const path = require('path');
 const target = path.dirname(require.resolve('@umijs/bundler-utils/compiled/less/package.json'));
-const symlink = path.join(__dirname, '../node_modules/less');
+const installDir = path.join(__dirname, '../node_modules');
+const symlink = path.join(installDir, 'less');
 const symlinkType = process.platform === 'win32' ? 'junction' : 'dir';
 
-if (fs.existsSync(symlink)) fs.unlinkSync(symlink);
+if (fs.existsSync(symlink)) {
+  // for multi-times install, in dev
+  fs.unlinkSync(symlink);
+} else if (!fs.existsSync(installDir)) {
+  // for yarn-like install, no node_modules dir
+  fs.mkdirSync(installDir);
+}
 fs.symlinkSync(target, symlink, symlinkType);
