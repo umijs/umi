e57a717bb4079f4357859f37f6fe47dff8df4f8f
sorrycc <sorrycc@gmail.com>
2022-03-28 12:33:56 +0800

    feat: add prettier config

---
+++ packages/umi/prettier.js
@@ -0,0 +1,12 @@
+module.exports = {
+  printWidth: 80,
+  singleQuote: true,
+  trailingComma: 'all',
+  proseWrap: 'never',
+  endOfLine: 'lf',
+  overrides: [{ files: '.prettierrc', options: { parser: 'json' } }],
+  plugins: [
+    require.resolve('prettier-plugin-packagejson'),
+    require.resolve('prettier-plugin-organize-imports'),
+  ],
+};
