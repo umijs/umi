d92e44ebe804419025f1355f6b2532635cc2afc6
Shu Pengfei aka pshu <stormslowly@gmail.com>
2022-03-30 16:42:43 +0800

    fix: 🐛 useEffect missing deps (#551)
    Co-authored-by: pshu <pishu.spf@antfin.com>

---
+++ packages/preset-umi/devToolAppDist/278.js
@@ -0,0 +1,8 @@
+"use strict";(self.webpackChunk=self.webpackChunk||[]).push([[278],{3838:function(s,t,r){var l;/** @license React v17.0.2
+ * react-jsx-runtime.production.min.js
+ *
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */r(1625);var o=r(2406),u=60103;if(l=60107,typeof Symbol=="function"&&Symbol.for){var c=Symbol.for;u=c("react.element"),l=c("react.fragment")}var a=o.__SECRET_INTERNALS_DO_NOT_USE_OR_YOU_WILL_BE_FIRED.ReactCurrentOwner,v=Object.prototype.hasOwnProperty,E={key:!0,ref:!0,__self:!0,__source:!0};function i(f,e,p){var n,_={},d=null,y=null;p!==void 0&&(d=""+p),e.key!==void 0&&(d=""+e.key),e.ref!==void 0&&(y=e.ref);for(n in e)v.call(e,n)&&!E.hasOwnProperty(n)&&(_[n]=e[n]);if(f&&f.defaultProps)for(n in e=f.defaultProps,e)_[n]===void 0&&(_[n]=e[n]);return{$$typeof:u,type:f,key:d,ref:y,props:_,_owner:a.current}}t.jsx=i,t.jsxs=i},567:function(s,t,r){s.exports=r(3838)},3278:function(s,t,r){r.r(t),r.d(t,{default:function(){return u}});var l=r(2406),o=r(567);function u(){return(0,o.jsx)("div",{children:(0,o.jsx)("h2",{children:"Routes"})})}}}]);
