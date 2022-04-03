3959e3ed1767c4e19381fcb2501ad0250e5ab183
阿平 <xierenyuan@qq.com>
2022-04-03 18:08:45 +0800

    feat(create-umi): template support vite (#571)
    Co-authored-by: 谢仁洪 <xierenhong@renrenche.com>

--- packages/create-umi/templates/app/pages/index.tsx
+++ packages/create-umi/templates/app/pages/index.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
+import yayJpg from '../assets/yay.jpg';
 
 export default function HomePage() {
   return (
     <div>
       <h2>Yay! Welcome to umi!</h2>
       <p>
-        <img src={require('../assets/yay.jpg')} width="388" />
+        <img src={yayJpg} width="388" />
       </p>
       <p>
         To get started, edit <code>pages/index.tsx</code> and save to reload.
