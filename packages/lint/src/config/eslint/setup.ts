f5f66cbcf2e1734179f703023c15849055a9e46e
Peach <scdzwyxst@gmail.com>
2022-04-14 14:44:04 +0800

    feat(lint): use forked eslint-patch (#608)
    * refactor(lint): pre-bundle eslint-patch
    
    * feat(lint): fork eslint-path to resolve builtin plugins

--- packages/lint/src/config/eslint/setup.ts
+++ packages/lint/src/config/eslint/setup.ts
@@ -1,2 +1,2 @@
 // patch eslint plugin resolve logic
-require('@rushstack/eslint-patch/modern-module-resolution');
+require('../../../compiled/@rushstack/eslint-patch/lib/modern-module-resolution.js');
