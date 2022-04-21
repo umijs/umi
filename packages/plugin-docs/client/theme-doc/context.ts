5fe5aa433cd08dd5bc9816ec1faa3679b1c57d49
Yuanlin Lin <im.yuanlinlin@gmail.com>
2022-04-21 14:55:31 +0800

    feat(plugin-docs): Add configs for customize head of plugin-docs (#643)
    * feat(plugin-docs): Add configs for customize head of plugin-docs
    
    * feat(plugin-docs): Change rightNavComponents to extraNavRight
    
    * feat(plugin-docs): Change React.FC to React.ComponentType

--- packages/plugin-docs/client/theme-doc/context.ts
+++ packages/plugin-docs/client/theme-doc/context.ts
@@ -9,12 +9,16 @@ interface IContext {
     github: string;
     // 键盘搜索的快捷键，参考 https://github.com/madrobby/keymaster
     searchHotKey?: string | { macos: string; windows: string };
-    logo: string;
+    logo: string | React.ComponentType;
     // 在设置文件中声明该项目的国际化功能支持的语言
     i18n?: { locale: string; text: string }[];
     // 插件会从 docs/locales 内将所有 json 文件注入到 themeConfig 中
     // 供 useLanguage 使用
     locales: { [locale: string]: { [key: string]: string } };
+    // 顶部导航栏右侧自定义组件
+    extraNavRight?: React.ComponentType;
+    // 底部导航栏左侧自定义组件
+    extraNavLeft?: React.ComponentType;
     navs: {
       path: string;
       title: string;
