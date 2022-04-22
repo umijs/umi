7a2bce0afea2bcd3ec322aa805c4985f976a5523
Yingci <59400654+fz6m@users.noreply.github.com>
2022-04-22 15:08:37 +0800

    chore: remove dead code (#656)

--- packages/bundler-webpack/src/server/server.ts
+++ packages/bundler-webpack/src/server/server.ts
@@ -29,21 +29,6 @@ export async function createServer(opts: IOpts) {
   const { proxy } = userConfig;
   const app = express();
 
-  // basename middleware
-  app.use((req, _res, next) => {
-    const { url, path } = req;
-    const { basename, history } = userConfig;
-    if (
-      history?.type === 'browser' &&
-      basename !== '/' &&
-      url.startsWith(basename)
-    ) {
-      req.url = url.slice(basename.length);
-      req.path = path.slice(basename.length);
-    }
-    next();
-  });
-
   // cros
   app.use((_req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
