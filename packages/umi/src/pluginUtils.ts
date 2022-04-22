7b577ce5a8d0424dc068347741b5287195db48aa
Peach <scdzwyxst@gmail.com>
2022-04-22 20:56:35 +0800

    fix(umi): correct export for http-proxy-middleware (#663)

--- packages/umi/src/pluginUtils.ts
+++ packages/umi/src/pluginUtils.ts
@@ -1,4 +1,4 @@
 import express from '@umijs/bundler-utils/compiled/express';
-import httpProxyMiddleware from '@umijs/bundler-webpack/compiled/http-proxy-middleware';
+import * as httpProxyMiddleware from '@umijs/bundler-webpack/compiled/http-proxy-middleware';
 export * from '@umijs/utils';
 export { httpProxyMiddleware, express };
