4b6143e8daeb66b1604a8e72047b4c08a7d3d06e
Yuanlin Lin <im.yuanlinlin@gmail.com>
2022-04-24 16:06:52 +0800

    feat(plugin-docs): Add id attribute of search input component for style override (#674)

--- packages/plugin-docs/client/theme-doc/Search.tsx
+++ packages/plugin-docs/client/theme-doc/Search.tsx
@@ -63,6 +63,7 @@ export default () => {
   return (
     <Fragment>
       <div
+        id="search-input-wrapper"
         className="rounded-lg w-40 lg:w-64 flex items-center pr-2 flex-row hover:bg-gray-50
      transition duration-300 bg-gray-100 border border-white focus-within:border-gray-100
      focus-within:bg-white dark:bg-gray-700 dark:border-gray-700 relative
@@ -84,6 +85,7 @@ export default () => {
           {isMac ? macSearchKey : windowsSearchKey}
         </div>
         <div
+          id="search-results-wrapper"
           className={cx(
             'absolute transition-all duration-500 top-12 w-96 rounded-lg',
             'cursor-pointer shadow overflow-hidden',
