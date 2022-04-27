68372d6626f3413e25b4a6df51ba62be3f6dd0e2
JasonSun <42314340+LolipopJ@users.noreply.github.com>
2022-04-27 13:59:53 +0800

    fix(plugin-docs): fix toc behavior (#700)
    * fix(plugin-docs): fix toc href
    
    * fix(plugin-docs): fix toc href with html tags
    
    * style(plugin-docs): update toc style
    
    * fix(plugin-docs): fix toc title display

--- packages/plugin-docs/client/theme-doc/Toc.tsx
+++ packages/plugin-docs/client/theme-doc/Toc.tsx
@@ -3,6 +3,7 @@ import { Helmet } from 'react-helmet';
 import { useThemeContext } from './context';
 import useLanguage from './useLanguage';
 import getLinkFromTitle from './utils/getLinkFromTitle';
+import getTocTitle from './utils/getTocTitle';
 
 export default () => {
   const { location, appData, themeConfig } = useThemeContext()!;
@@ -24,7 +25,7 @@ export default () => {
   return (
     <div
       className="w-full lg:m-12 mb-12 border
-      border-gray-100 p-4 rounded-xl z-20"
+      border-gray-200 dark:border-neutral-700 py-4 rounded-lg z-20"
     >
       {/* @ts-ignore */}
       <Helmet>
@@ -32,22 +33,24 @@ export default () => {
           {route.titles[0].title} | {themeConfig.title}
         </title>
       </Helmet>
-      <p className="text-lg font-extrabold dark:text-white">
-        {route.titles[0].title}
+      <p className="text-lg font-extrabold text-gray-800 dark:text-neutral-50 pb-2 border-b border-gray-200 dark:border-neutral-700">
+        <span className="px-4">{route.titles[0].title}</span>
       </p>
-      <ul className="max-h-[calc(100vh-360px)] overflow-y-auto py-2">
+      <ul className="max-h-[calc(100vh-360px)] overflow-y-auto px-4">
         {titles.map((item: any) => {
           return (
             <li
               style={{ paddingLeft: `${item.level - 2}rem` }}
-              className="mt-3 text-gray-600 cursor-pointer dark:text-gray-400
+              className="mt-3 text-gray-600 cursor-pointer dark:text-neutral-400
               hover:text-blue-500 transition duration-300 dark:hover:text-blue-500"
             >
               <a
-                className={item.level > 2 ? 'text-sm' : ''}
+                className={`${
+                  item.level > 2 ? 'text-sm' : 'text-base'
+                } break-all 2xl:break-words`}
                 href={'#' + getLinkFromTitle(item.title)}
               >
-                {item.title.replace(/\\{/g, '{').replace(/\\}/g, '}')}
+                {getTocTitle(item.title)}
               </a>
             </li>
           );
--- packages/plugin-docs/client/theme-doc/utils/getLinkFromTitle.ts
+++ packages/plugin-docs/client/theme-doc/utils/getLinkFromTitle.ts
@@ -1,6 +1,15 @@
 export default function getLinkFromTitle(title: string) {
-  return title
-    .toLowerCase()
-    .replace(/\s/g, '-')
-    .replace(/[（）()\\{},]/g, '');
+  return (
+    title
+      .toLowerCase()
+      .trim()
+      // not remove html tags
+      // .replace(/<[!\/a-z].*?>/gi, '')
+      // remove unwanted chars
+      .replace(
+        /[\u2000-\u206F\u2E00-\u2E7F\\'!！"#$%&()（）*+,，.。/:：;；<=>?？@[\]^`{|}~]/g,
+        '',
+      )
+      .replace(/\s/g, '-')
+  );
 }
