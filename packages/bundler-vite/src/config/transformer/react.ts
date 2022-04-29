7413c2a8b00d3fd3ee466b17e1828b4a2cd4d15a
sorrycc <sorrycc@gmail.com>
2022-04-29 16:53:55 +0800

    chore: fix tsc

--- packages/bundler-vite/src/config/transformer/react.ts
+++ packages/bundler-vite/src/config/transformer/react.ts
@@ -10,7 +10,6 @@ export default (function react(userConfig) {
   config.plugins?.push(
     // pre-compiled rollup type is different with installed rollup type
     // so this plugin type is not compatible with config.plugins
-    // @ts-expect-error
     reactPlugin({
       // jsxRuntime: 'automatic',
       babel: {
