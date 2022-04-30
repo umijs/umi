13a08c7c84db8e5bf5569b60bda5ee007a25a573
Yingci <59400654+fz6m@users.noreply.github.com>
2022-04-30 09:57:55 +0800

    fix(scripts): process exit when status code 1 (#724)

--- scripts/.internal/utils.ts
+++ scripts/.internal/utils.ts
@@ -65,9 +65,14 @@ export function setExcludeFolder(opts: {
 }
 
 export function spawnSync(cmd: string, opts: SpawnSyncOptions) {
-  return spawn.sync(cmd, {
+  const result = spawn.sync(cmd, {
     shell: true,
     stdio: 'inherit',
     ...opts,
   });
+  if (result.status !== 0) {
+    logger.error(`Execute command error (${cmd})`);
+    process.exit(1);
+  }
+  return result;
 }
