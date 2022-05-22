d463403f220e20cfe69a792c4d14749201ef0fba
阿平 <xierenyuan@qq.com>
2022-05-23 00:06:03 +0800

    feat(preset-umi): router layout support the vue suffix (#802)
    Co-authored-by: xierenhong <xierenhong@kuaishou.com>

---
+++ examples/boilerplate-vue/pages/docs.vue
@@ -0,0 +1,14 @@
+<template>
+  <h2>DocPage</h2>
+  <div ref="content"></div>
+</template>
+<script lang="ts" setup>
+import { ref, onMounted } from 'vue'
+import marked from 'marked'
+
+const content = ref()
+
+onMounted(() => {
+  content.value.innerHTML = marked.parse('# Marked in the browser\n\nRendered by **marked**.')
+})
+</script>
