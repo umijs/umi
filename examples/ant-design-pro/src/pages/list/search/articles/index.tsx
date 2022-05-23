59adf80700eb72d16be3c36ce8dcd426ca4b205a
pshu <stormslowly@gmail.com>
2022-05-23 12:20:21 +0800

    chore: 🎨 蚂蚁金服 -> 蚂蚁集团 (#804)
    Co-authored-by: pshu <pishu.spf@antfin.com>

--- examples/ant-design-pro/src/pages/list/search/articles/index.tsx
+++ examples/ant-design-pro/src/pages/list/search/articles/index.tsx
@@ -225,7 +225,7 @@ const Articles: FC = () => {
                   <span>
                     <Tag>Ant Design</Tag>
                     <Tag>设计语言</Tag>
-                    <Tag>蚂蚁金服</Tag>
+                    <Tag>蚂蚁集团</Tag>
                   </span>
                 }
               />
