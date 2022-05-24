2e1c73a1a166d1b09e26ca1716385f1f1934c55f
sorrycc <sorrycc@gmail.com>
2022-05-24 11:54:21 +0800

    example(libs): add master-css

--- examples/libs/.umirc.ts
+++ examples/libs/.umirc.ts
@@ -3,4 +3,9 @@ export default {
     esbuild: true,
   },
   manifest: {},
+  styles: ['https://unpkg.com/@master/normal.css'],
+  scripts: [
+    'https://unpkg.com/@master/style',
+    'https://unpkg.com/@master/styles',
+  ],
 };
---
+++ examples/libs/pages/master-css.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default () => {
+  return (
+    <h1 className="font:40 font:heavy font:italic m:50 text:center">
+      Hello World
+    </h1>
+  );
+};
