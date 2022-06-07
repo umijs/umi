b80d55740f70ff47a4e39dc316d3892b032c9f02
sorrycc <sorrycc@gmail.com>
2022-06-07 17:40:00 +0800

    feat: improve typigns

--- packages/umi/src/index.ts
+++ packages/umi/src/index.ts
@@ -2,6 +2,6 @@ import { IServicePluginAPI, PluginAPI } from '@umijs/core';
 
 export { run } from './cli/cli';
 export { defineConfig } from './defineConfig';
-export * from './mock/helper';
+export { defineMock } from './defineMock';
 export * from './service/service';
 export type IApi = PluginAPI & IServicePluginAPI;
