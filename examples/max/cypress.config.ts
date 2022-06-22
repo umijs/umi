edc1e4ba01fa013668b7b34632f1e4594b54d4e6
pshu <stormslowly@gmail.com>
2022-06-22 23:32:10 +0800

    chore: 🔧 actions 相关配置更新 (#961)
    cypress 上传  cypres dashborad 方便 debug
    codecov 移除 annotation
    
    Co-authored-by: pshu <pishu.spf@antfin.com>

--- examples/max/cypress.config.ts
+++ examples/max/cypress.config.ts
@@ -3,10 +3,12 @@ import { defineConfig } from 'cypress';
 const PORT = process.env.PORT || '8000';
 
 export default defineConfig({
+  projectId: 'qikpat',
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
     baseUrl: `http://localhost:${PORT}`,
+    defaultCommandTimeout: 8000,
   },
 });
