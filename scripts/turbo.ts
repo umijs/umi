6ad626cf304c64d1b15e574e6e8b8ed4c081c3ea
fz6m <59400654+fz6m@users.noreply.github.com>
2022-06-23 22:05:21 +0800

    wip: turbo pass arg

--- scripts/turbo.ts
+++ scripts/turbo.ts
@@ -23,7 +23,7 @@ function turbo(opts: {
   cache?: boolean;
   parallel?: boolean;
 }) {
-  const extraCmd = opts.extra ? `-- -- ${opts.extra}` : '';
+  const extraCmd = opts.extra ? `-- ${opts.extra}` : '';
   const cacheCmd = opts.cache === false ? '--no-cache --force' : '';
   const parallelCmd = opts.parallel ? '--parallel' : '';
   const filters: string[] = toArray(opts.filter);
