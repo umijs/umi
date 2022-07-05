8e71b95bf1b0d924d8c4ab1d70f52f5dd3f5c7ec
DaoYuan <txp1035@gmail.com>
2022-07-05 23:56:00 +0000

    refactor: less code

--- packages/utils/src/logger.ts
+++ packages/utils/src/logger.ts
@@ -1,5 +1,5 @@
 import { join } from 'path';
-import pino from 'pino';
+import pino, { type Logger } from 'pino';
 import chalk from '../compiled/chalk';
 import fsExtra from '../compiled/fs-extra';
 
@@ -16,7 +16,7 @@ const customLevels = {
   debug: 30,
 };
 
-let logger:any;
+let logger: Logger<{ customLevels: typeof customLevels }>;
 function init() {
   // 不存在目录就创建它
   if (!logger) {
@@ -41,7 +41,7 @@ function init() {
 
 }
 
-export const prefixes = {
+const prefixesBase = {
   wait: chalk.cyan('wait') + '  -',
   error: chalk.red('error') + ' -',
   fatal: chalk.red('fatal') + ' -',
@@ -52,44 +52,44 @@ export const prefixes = {
   debug: chalk.gray('debug') + ' -',
 };
 
-export function wait(...message: any[]) {
-  init();
+const prefixes = new Proxy(prefixesBase, {
+  get(target, key, receiver) {
+    init();
+    return Reflect.get(target, key, receiver);
+  },
+});
+
+export function wait(...message: any[]) { 
   console.log(prefixes.wait, ...message);
   logger.wait(message[0]);
 }
 
-export function error(...message: any[]) {
-  init();
+export function error(...message: any[]) { 
   console.error(prefixes.error, ...message);
   logger.error(message[0]);
 }
 
-export function warn(...message: any[]) {
-  init();
+export function warn(...message: any[]) { 
   console.warn(prefixes.warn, ...message);
   logger.warn(message[0]);
 }
 
-export function ready(...message: any[]) {
-  init();
+export function ready(...message: any[]) { 
   console.log(prefixes.ready, ...message);
   logger.ready(message[0]);
 }
 
 export function info(...message: any[]) {
-  init();
   console.log(prefixes.info, ...message);
   logger.info(message[0]);
 }
 
 export function event(...message: any[]) {
-  init();
   console.log(prefixes.event, ...message);
   logger.event(message[0]);
 }
 
 export function debug(...message: any[]) {
-  init();
   if (process.env.DEBUG) {
     console.log(prefixes.debug, ...message);
   }
@@ -97,7 +97,6 @@ export function debug(...message: any[]) {
 }
 
 export function fatal(...message: any[]) {
-  init();
   console.error(prefixes.fatal, ...message);
   logger.fatal(message[0]);
 }
