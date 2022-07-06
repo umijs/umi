1eb7ec74436e1995f8cf959db8746d4942bb90ed
sorrycc <sorrycc@gmail.com>
2022-07-06 16:14:44 +0800

    chore: code style

--- packages/max/src/plugins/maxChecker.ts
+++ packages/max/src/plugins/maxChecker.ts
@@ -4,7 +4,6 @@ export default (api: IApi) => {
   api.onCheckPkgJSON(({ current }) => {
     const hasUmi =
       current.dependencies?.['umi'] || current.devDependencies?.['umi'];
-    console.log('hasUmi', hasUmi);
     if (hasUmi) {
       throw new Error(
         `You are using @umijs/max, please remove umi from your dependencies in package.json.`,
