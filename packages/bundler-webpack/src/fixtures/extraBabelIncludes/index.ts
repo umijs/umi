672e0d3a5b68869d0a6d83d3bb811f92ad1e76ac
hanzebang <573729333@qq.com>
2022-07-08 23:59:45 +0800

    fix(bundler-webpack): 修复 extraBabelIncludes 不能支持 htmlparser2 特例的问题 - prettier

--- packages/bundler-webpack/src/fixtures/extraBabelIncludes/index.ts
+++ packages/bundler-webpack/src/fixtures/extraBabelIncludes/index.ts
@@ -2,6 +2,7 @@
 import bar from 'bar/bar';
 // @ts-ignore
 import foo from 'foo/foo';
+// @ts-ignore
 import pun from 'pkg-up-name';
 import hoo from '../.extraBabelIncludes/hoo';
 import too from '../.noExtraBabelIncludes/too';
