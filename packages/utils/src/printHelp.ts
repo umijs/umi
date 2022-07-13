effe6ee735647f3a300551b6e78258f7f5cc3767
sorrycc <sorrycc@gmail.com>
2022-07-13 15:12:14 +0800

    feat(utils): allow disable fs logger with FS_LOGGER=none

--- packages/utils/src/printHelp.ts
+++ packages/utils/src/printHelp.ts
@@ -3,8 +3,11 @@ import * as logger from './logger';
 const FEEDBACK_MESSAGE = '如果你需要进交流群，请访问 https://fb.umijs.org';
 
 export function exit() {
-  logger.fatal('A complete log of this run can be found in:');
-  logger.fatal(logger.getLatestLogFilePath());
+  const loggerPath = logger.getLatestLogFilePath();
+  if (loggerPath) {
+    logger.fatal('A complete log of this run can be found in:');
+    logger.fatal(loggerPath);
+  }
   logger.fatal(
     'Consider reporting a GitHub issue on https://github.com/umijs/umi/issues',
   );
