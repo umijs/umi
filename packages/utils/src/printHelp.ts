6f1ee5cd12c5cab6fddd79ba8a73c7081f77f8c9
sorrycc <sorrycc@gmail.com>
2022-07-14 14:50:20 +0800

    chore(utils): comment out fb link

--- packages/utils/src/printHelp.ts
+++ packages/utils/src/printHelp.ts
@@ -1,6 +1,7 @@
 import * as logger from './logger';
 
-const FEEDBACK_MESSAGE = '如果你需要进交流群，请访问 https://fb.umijs.org';
+// TODO: 先注释，因为这里要考虑比如 @alipay/bigfish 不应该走这个提示
+// const FEEDBACK_MESSAGE = '如果你需要进交流群，请访问 https://fb.umijs.org';
 
 export function exit() {
   const loggerPath = logger.getLatestLogFilePath();
@@ -11,10 +12,10 @@ export function exit() {
   logger.fatal(
     'Consider reporting a GitHub issue on https://github.com/umijs/umi/issues',
   );
-  logger.fatal(FEEDBACK_MESSAGE);
+  // logger.fatal(FEEDBACK_MESSAGE);
 }
 
 export function runtime(e: Error) {
   logger.error(e);
-  logger.fatal(FEEDBACK_MESSAGE);
+  // logger.fatal(FEEDBACK_MESSAGE);
 }
