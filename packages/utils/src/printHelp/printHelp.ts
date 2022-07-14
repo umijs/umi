f476d83e0ef92c34fa3211ce809e753e4f4912a5
sorrycc <sorrycc@gmail.com>
2022-07-14 15:49:19 +0800

    chore: disable fb tips in some situations

--- packages/utils/src/printHelp/printHelp.ts
+++ packages/utils/src/printHelp/printHelp.ts
@@ -1,6 +1,9 @@
 import { chalk } from '../index';
 
 export function feedback() {
+  // don't print feedback in bigfish framework
+  if (process.env.BIGFISH_VERSION) return;
+  if (process.env.FB_TIPS === 'none') return;
   console.info(
     chalk.black.bgGreen.bold(' info '),
     chalk.green.bold('如果你需要进交流群，请访问 https://fb.umijs.org'),
