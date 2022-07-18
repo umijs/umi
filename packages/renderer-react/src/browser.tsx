2b5aa7f0950bca2f89e9e7011105d076986b65fe
期贤 <qixian.cs@antgroup.com>
2022-07-18 17:18:31 +0800

    feat(test): support test mock RenderClient

--- packages/renderer-react/src/browser.tsx
+++ packages/renderer-react/src/browser.tsx
@@ -7,6 +7,11 @@ import { AppContext, useAppData } from './appContext';
 import { createClientRoutes } from './routes';
 import { ILoaderData, IRouteComponents, IRoutesById } from './types';
 
+/**
+ * 这个组件的功能是 history 发生改变的时候重新触发渲染
+ * @param props
+ * @returns
+ */
 function BrowserRoutes(props: {
   routes: any;
   clientRoutes: any;
@@ -53,20 +58,81 @@ export function Routes() {
   return useRoutes(clientRoutes);
 }
 
-export function renderClient(opts: {
+/**
+ * umi 渲染需要的配置，在node端调用的哦
+ */
+export type RenderClientOpts = {
+  /**
+   * 配置 webpack 的 publicPath。
+   * @doc https://umijs.org/docs/api/config#publicpath
+   */
   publicPath?: string;
+  /**
+   * 是否是 runtimePublicPath
+   * @doc https://umijs.org/docs/api/config#runtimepublicpath
+   */
   runtimePublicPath?: boolean;
+  /**
+   * react dom 渲染的的目标 dom
+   * @doc 一般不需要改，微前端的时候会变化
+   */
   rootElement?: HTMLElement;
+  /**
+   * 当前的路由配置
+   */
   routes: IRoutesById;
+  /**
+   * 当前的路由对应的dom组件
+   */
   routeComponents: IRouteComponents;
+  /**
+   * 插件的执行实例
+   */
   pluginManager: any;
+  /**
+   * 设置路由 base，部署项目到非根目录下时使用。
+   * @doc https://umijs.org/docs/api/config#base
+   */
   basename?: string;
+  /**
+   * loading 中展示的组件 dom
+   */
   loadingComponent?: React.ReactNode;
+  /**
+   * react router 的 history，用于控制列表渲染
+   * @doc https://umijs.org/docs/api/config#history
+   * 有多种不同的类型，测试的时候建议用 内存路由，默认是 browserHistory
+   */
   history: History;
+  /**
+   * ssr 的配置
+   */
   hydrate?: boolean;
-}) {
+};
+/**
+ * umi max 所需要的所有插件列表，用于获取provide
+ */
+const UMI_CLIENT_RENDER_REACT_PLUGIN_LIST = [
+  // Lowest to the highest priority
+  'innerProvider',
+  'i18nProvider',
+  'accessProvider',
+  'dataflowProvider',
+  'outerProvider',
+  'rootContainer',
+];
+
+/**
+ *
+ * @param {RenderClientOpts} opts - 插件相关的配置
+ * @param {React.ReactElement} routesElement 需要渲染的 routers，为了方便测试注入才导出
+ * @returns @returns A function that returns a React component.
+ */
+const getBrowser = (
+  opts: RenderClientOpts,
+  routesElement: React.ReactElement,
+) => {
   const basename = opts.basename || '/';
-  const rootElement = opts.rootElement || document.getElementById('root')!;
   const clientRoutes = createClientRoutes({
     routesById: opts.routes,
     routeComponents: opts.routeComponents,
@@ -79,6 +145,7 @@ export function renderClient(opts: {
       routes: clientRoutes,
     },
   });
+
   let rootContainer = (
     <BrowserRoutes
       basename={basename}
@@ -87,18 +154,12 @@ export function renderClient(opts: {
       clientRoutes={clientRoutes}
       history={opts.history}
     >
-      <Routes />
+      {routesElement}
     </BrowserRoutes>
   );
-  for (const key of [
-    // Lowest to the highest priority
-    'innerProvider',
-    'i18nProvider',
-    'accessProvider',
-    'dataflowProvider',
-    'outerProvider',
-    'rootContainer',
-  ]) {
+
+  // 加载所有需要的插件
+  for (const key of UMI_CLIENT_RENDER_REACT_PLUGIN_LIST) {
     rootContainer = opts.pluginManager.applyPlugins({
       type: 'modify',
       key: key,
@@ -107,6 +168,10 @@ export function renderClient(opts: {
     });
   }
 
+  /**
+   * umi 增加完 Provide 的 react dom，可以直接交给 react-dom 渲染
+   * @returns {React.ReactElement}
+   */
   const Browser = () => {
     const [clientLoaderData, setClientLoaderData] = useState<ILoaderData>({});
     const [serverLoaderData, setServerLoaderData] = useState<ILoaderData>(
@@ -160,6 +225,7 @@ export function renderClient(opts: {
               .then((data) => {
                 // setServerLoaderData when startTransition because if ssr is enabled,
                 // the component may being hydrated and setLoaderData will break the hydration
+                // @ts-ignore
                 React.startTransition(() => {
                   setServerLoaderData((d) => ({ ...d, [id]: data }));
                 });
@@ -204,15 +270,26 @@ export function renderClient(opts: {
       </AppContext.Provider>
     );
   };
+  return Browser;
+};
 
+/**
+ *  执行 react dom 的 render 方法
+ * @param {RenderClientOpts} opts - 插件相关的配置
+ * @returns void
+ */
+export function renderClient(opts: RenderClientOpts) {
+  const rootElement = opts.rootElement || document.getElementById('root')!;
+  const Browser = getBrowser(opts, <Routes />);
   if (opts.hydrate) {
     ReactDOM.hydrateRoot(rootElement, <Browser />);
-  } else {
-    if (ReactDOM.createRoot) {
-      ReactDOM.createRoot(rootElement).render(<Browser />);
-    } else {
-      // @ts-ignore
-      ReactDOM.render(<Browser />, rootElement);
-    }
+    return;
   }
+
+  if (ReactDOM.createRoot) {
+    ReactDOM.createRoot(rootElement).render(<Browser />);
+    return;
+  }
+  // @ts-ignore
+  ReactDOM.render(<Browser />, rootElement);
 }
--- packages/testing/src/index.ts
+++ packages/testing/src/index.ts
@@ -5,12 +5,31 @@ export type JSTransformer = 'esbuild' | 'swc' | 'ts-jest';
 
 export type { Config };
 
+/**
+ * 返回给定 jsTransformer 的 transformer 配置
+ * @param {JSTransformer} JSTransformer 要使用的JS transformer
+ * @param {config}  传递给 build 的配置
+ * @returns 转下去 transformer
+ */
 function getJSTransformer(
   jsTransformer: JSTransformer,
+  config?: {
+    esBuildConfig?: {
+      jsxFactory: string;
+      jsxFragment: string;
+      jsxInject: string;
+    };
+  },
 ): TransformerConfig | Path {
   switch (jsTransformer) {
     case 'esbuild':
-      return [require.resolve('esbuild-jest'), { sourcemap: true }];
+      return [
+        require.resolve('esbuild-jest'),
+        {
+          sourcemap: true,
+          ...config?.esBuildConfig,
+        },
+      ];
     case 'swc':
       return require.resolve('@swc-node/jest');
     case 'ts-jest':
@@ -20,10 +39,27 @@ function getJSTransformer(
   }
 }
 
-export function createConfig(opts?: {
+/**
+ * createConfig 的配置
+ */
+export type CreateConfigType = {
+  /**
+   * 转化 js 的配置
+   * @type {'esbuild' | 'swc' | 'ts-jest'}
+   */
   jsTransformer?: JSTransformer;
+  /**
+   * 运行环境，node 和 浏览器
+   * @type {'node' | 'browser'}
+   */
   target?: 'node' | 'browser';
-}): Config.InitialOptions {
+};
+/**
+ * 生成 jest.config.js 的配置
+ * @param {CreateConfigType} opts
+ * @returns
+ */
+export function createConfig(opts?: CreateConfigType): Config.InitialOptions {
   const config: Config.InitialOptions = {
     testMatch: ['**/*.test.(t|j)s(x)?'],
     transform: {
@@ -46,3 +82,14 @@ export function createConfig(opts?: {
   }
   return config;
 }
+
+/**
+ * 等待一段时间
+ * @param time 等待时间
+ * @returns
+ */
+export const waitTime = (time: number) => {
+  return new Promise((res) => {
+    setTimeout(res, time);
+  });
+};
