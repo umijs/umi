ce81dd67543680b5bcbfede1a8d3514f95f6a311
xierenyuan <xierenyuan@qq.com>
2022-07-21 23:40:02 +0800

    feat: update vue cache type

--- packages/preset-vue/src/features/vueBabelLoaderCustomize.ts
+++ packages/preset-vue/src/features/vueBabelLoaderCustomize.ts
@@ -1,10 +1,12 @@
+import { parse } from 'querystring';
+
 export default function vueBabelLoaderCustomize() {
   return {
     config(config: any) {
+      const context = this as any;
       if (/\.vue$/.test(config.options.filename)) {
-        config.options.filename = `${config.options.filename}${
-          (this as any).resourceQuery
-        }`;
+        const query = parse(context.resourceQuery.slice(1));
+        config.options.filename = `${config.options.filename}?type=${query.type}`;
       }
       return config.options;
     },
