cf6cfd3d32df968ad3cbd0484a401fbf2f08b072
chencheng (云谦) <sorrycc@gmail.com>
2022-07-27 16:13:28 +0800

    Update packages/preset-vue/src/features/vueBabelLoaderCustomize.ts

--- packages/preset-vue/src/features/vueBabelLoaderCustomize.ts
+++ packages/preset-vue/src/features/vueBabelLoaderCustomize.ts
@@ -8,7 +8,7 @@ export default function vueBabelLoaderCustomize() {
       if (config.options.filename) {
         if (/\.vue$/.test(config.options.filename)) {
           const query = parse(context.resourceQuery.slice(1));
-          // 仅template 执行区分
+          // 仅 template 执行区分
           if (query.type === 'template') {
             config.options.filename = `${config.options.filename}?type=${query.type}`;
           }
