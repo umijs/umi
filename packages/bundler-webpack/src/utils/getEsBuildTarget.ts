12f0738bf372db4450a10d0669b8bd5a78fad404
fz6m <59400654+fz6m@users.noreply.github.com>
2022-07-28 14:12:46 +0800

    chore(jsMinifier): ie is not supported esbuild

--- packages/bundler-webpack/src/utils/getEsBuildTarget.ts
+++ packages/bundler-webpack/src/utils/getEsBuildTarget.ts
@@ -1,10 +1,24 @@
+import { chalk, logger } from '@umijs/utils';
 import { DEFAULT_ESBUILD_TARGET_KEYS } from '../constants';
+import { JSMinifier } from '../types';
 
 interface IOpts {
   targets: Record<string, any>;
+  jsMinifier: `${JSMinifier}`;
 }
 
-export function getEsBuildTarget({ targets }: IOpts) {
+export function getEsBuildTarget({ targets, jsMinifier }: IOpts) {
+  if (targets?.['ie'] && jsMinifier === JSMinifier.esbuild) {
+    logger.error(
+      `${chalk.red(
+        `jsMinifier: esbuild`,
+      )} does not support ie as targets, You can use ${chalk.green(
+        `jsMinifier: 'terser'`,
+      )}`,
+    );
+    throw new Error('IE is not supported');
+  }
+
   return Object.keys(targets)
     .filter((key) => DEFAULT_ESBUILD_TARGET_KEYS.includes(key))
     .map((key) => {
