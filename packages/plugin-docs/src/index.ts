216fd7d8ffb80bd8f76d70bf79c27cd2475a8ff8
xierenyuan <xierenyuan@qq.com>
2022-08-11 14:36:31 +0800

    fix(plugin-docs): compiler error

--- packages/plugin-docs/src/index.ts
+++ packages/plugin-docs/src/index.ts
@@ -143,12 +143,14 @@ export default () => {
 
   // 支持文档热更新
   api.chainWebpack((memo) => {
-    memo.plugin('fastRefresh').tap(([params]) => [
-      {
-        ...params,
-        include: /\.([cm]js|[jt]sx?|flow|md)$/i,
-      },
-    ]);
+    if (api.env === 'development') {
+      memo.plugin('fastRefresh').tap(([params]) => [
+        {
+          ...params,
+          include: /\.([cm]js|[jt]sx?|flow|md)$/i,
+        },
+      ]);
+    }
     return memo;
   });
 };
