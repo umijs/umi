3683e6ddff7c89d019db706553dafd494bbe6fcf
sorrycc <sorrycc@gmail.com>
2022-08-22 16:55:39 +0800

    feat: support react 18

--- packages/preset-umi/src/features/mpa/mpa.ts
+++ packages/preset-umi/src/features/mpa/mpa.ts
@@ -27,12 +27,16 @@ export default (api: IApi) => {
 
   api.onGenerateFiles(() => {
     // TODO: support react 18
-    // const isReact18 = api.appData.react.version.startsWith('18.');
+    const isReact18 = api.appData.react.version.startsWith('18.');
     (api.appData.mpa.entry as Entry[]).forEach((entry) => {
       const layoutImport = entry.layout
         ? `import Layout from '${entry.layout}';`
         : '';
       const layoutJSX = entry.layout ? `<Layout><App /></Layout>` : `<App />`;
+      const rootElement = `document.getElementById('${entry.mountElementId}')`;
+      const renderer = isReact18
+        ? `ReactDOM.createRoot(${rootElement}).render(${layoutJSX});`
+        : `ReactDOM.render(${layoutJSX}, ${rootElement});`;
       api.writeTmpFile({
         path: entry.tmpFilePath,
         noPluginDir: true,
@@ -41,7 +45,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from '${entry.file}';
 ${layoutImport}
-ReactDOM.render(${layoutJSX}, document.getElementById('${entry.mountElementId}'));
+${renderer}
         `.trimStart(),
       });
     });
