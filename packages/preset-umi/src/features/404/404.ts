9e8f143229d6f5d8547e951c23cbb2c66cbfd190
Zeno <513005627@qq.com>
2022-08-30 15:27:45 +0800

    feat: add 404 plugin (#9145)
    * feat: add 404 plugin
    
    * Update packages/preset-umi/src/features/404/404.ts
    
    Co-authored-by: chencheng (云谦) <sorrycc@gmail.com>

---
+++ packages/preset-umi/src/features/404/404.ts
@@ -0,0 +1,25 @@
+import { IApi, IRoute } from '../../types';
+
+type Routes = Record<string, IRoute>;
+
+export function patchRoutes(routes: Routes): Routes {
+  Object.keys(routes).forEach((key) => {
+    if (routes[key].path === '404') {
+      routes[key].path = '*';
+    }
+  });
+  return routes;
+}
+
+export default (api: IApi) => {
+  api.describe({
+    key: '404',
+  });
+  api.modifyRoutes(async (routes: Routes) => {
+    // 仅支持约定式路由
+    if (api.config.routes) {
+      return routes;
+    }
+    return patchRoutes(routes);
+  });
+};
