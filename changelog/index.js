29dfeca23d6d381e93bbc89b502fe67ea4424893
DaoYuan <txp1035@gmail.com>
2022-09-04 08:58:01 +0800

    perf: smallest change

--- changelog/index.js
+++ changelog/index.js
@@ -2,13 +2,11 @@
 // 原始模板参考
 // https://github.com/conventional-changelog/conventional-changelog/tree/master/packages/conventional-changelog-angular
 const Q = require('q')
-const conventionalChangelog = require('./conventional-changelog')
-const parserOpts = require('./parser-opts')
-const recommendedBumpOpts = require('./conventional-recommended-bump')
+
 const writerOpts = require('./writer-opts')
 const gitRawCommitsOpts = require('./git-raw-commit');
 
-module.exports = Q.all([conventionalChangelog, parserOpts, recommendedBumpOpts, writerOpts, gitRawCommitsOpts])
-  .spread((conventionalChangelog, parserOpts, recommendedBumpOpts, writerOpts, gitRawCommitsOpts) => {
-    return { conventionalChangelog, parserOpts, recommendedBumpOpts, writerOpts, gitRawCommitsOpts }
+module.exports = Q.all([writerOpts, gitRawCommitsOpts])
+  .spread((writerOpts, gitRawCommitsOpts) => {
+    return { writerOpts, gitRawCommitsOpts }
   })
