4cb4a0f6f43584786969b1d68d95a4d28b34fa9f
Yingci <59400654+fz6m@users.noreply.github.com>
2022-09-07 20:41:40 +0800

    fix(tailwind): bin path (#9255)

--- packages/plugins/src/tailwindcss.ts
+++ packages/plugins/src/tailwindcss.ts
@@ -1,4 +1,4 @@
-import { join } from 'path';
+import { join, dirname } from 'path';
 import { IApi } from 'umi';
 import { crossSpawn, winPath } from 'umi/plugin-utils';
 
@@ -22,7 +22,7 @@ export default (api: IApi) => {
   api.onBeforeCompiler(() => {
     const inputPath = join(api.cwd, 'tailwind.css');
     const generatedPath = join(api.paths.absTmpPath, outputPath);
-    const binPath = join(api.cwd, 'node_modules/.bin/tailwind');
+    const binPath = getTailwindBinPath({ cwd: api.cwd });
     const configPath = join(
       process.env.APP_ROOT || api.cwd,
       'tailwind.config.js',
@@ -67,3 +67,11 @@ export default (api: IApi) => {
     return [{ source: generatedPath }];
   });
 };
+
+function getTailwindBinPath(opts: { cwd: string }) {
+  const pkgPath = require.resolve('tailwindcss/package.json', {
+    paths: [opts.cwd],
+  });
+  const tailwindPath = require(pkgPath).bin['tailwind'];
+  return join(dirname(pkgPath), tailwindPath);
+}
