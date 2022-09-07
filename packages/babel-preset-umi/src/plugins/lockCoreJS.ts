3dedb46a409599610f4b0ab03bb2dce601d2880b
2239559319 <w2239559319@outlook.com>
2022-09-07 21:58:56 +0800

    fix: lockCoreJS plugin support dynamic import

--- packages/babel-preset-umi/src/plugins/lockCoreJS.ts
+++ packages/babel-preset-umi/src/plugins/lockCoreJS.ts
@@ -1,4 +1,5 @@
 import * as t from '@umijs/bundler-utils/compiled/babel/types';
+import * as Babel from '@umijs/bundler-utils/compiled/babel/core';
 import { winPath } from '@umijs/utils';
 import { dirname } from 'path';
 
@@ -8,8 +9,24 @@ function addLastSlash(path: string) {
 
 export default function () {
   return {
-    post({ path }: any) {
-      path.node.body.forEach((node: any) => {
+    visitor: {
+      CallExpression(path: Babel.NodePath<t.CallExpression>) {
+        if (
+          t.isImport(path.node.callee) &&
+          t.isStringLiteral(path.node.arguments?.[0]) &&
+          path.node.arguments[0].value.startsWith('core-js/')
+        ) {
+          path.node.arguments[0].value = path.node.arguments[0].value.replace(
+            /^core-js\//,
+            addLastSlash(
+              winPath(dirname(require.resolve('core-js/package.json'))),
+            ),
+          );
+        }
+      },
+    },
+    post({ path }: { path: Babel.NodePath<t.Program> }) {
+      path.node.body.forEach((node) => {
         if (t.isImportDeclaration(node)) {
           if (node.source.value.startsWith('core-js/')) {
             node.source.value = node.source.value.replace(
