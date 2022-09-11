8cb825a281bbeca77764b2be05719a9d87f0138b
2239559319 <w2239559319@outlook.com>
2022-09-11 23:43:08 +0800

    fix: use DEFAULT_BROWSER_TARGETS from @umijs/bundler-webpack

--- packages/preset-umi/src/constants.ts
+++ packages/preset-umi/src/constants.ts
@@ -4,8 +4,4 @@ export const DEFAULT_PORT = '8000';
 export const DEFAULT_HOST = '0.0.0.0';
 export const CACHE_DIR_NAME = '.cache';
 export const TEMPLATES_DIR = join(__dirname, '../templates');
-
-// see ../../bundler-webpack/src/constant.ts
-export const DEFAULT_BROWSER_TARGETS = {
-  chrome: 80,
-};
+export { DEFAULT_BROWSER_TARGETS } from '@umijs/bundler-webpack/dist/constants';
