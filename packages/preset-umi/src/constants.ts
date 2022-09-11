e1d94656733bfd5ec0c6d102e39207c97a218533
Xiao Chuan <w2239559319@outlook.com>
2022-09-11 23:53:28 +0800

    import DEFAULT_BROWSER_TARGETS from bundler-webpack
    Co-authored-by: Yingci <59400654+fz6m@users.noreply.github.com>

--- packages/preset-umi/src/constants.ts
+++ packages/preset-umi/src/constants.ts
@@ -4,4 +4,3 @@ export const DEFAULT_PORT = '8000';
 export const DEFAULT_HOST = '0.0.0.0';
 export const CACHE_DIR_NAME = '.cache';
 export const TEMPLATES_DIR = join(__dirname, '../templates');
-export { DEFAULT_BROWSER_TARGETS } from '@umijs/bundler-webpack/dist/constants';
--- packages/preset-umi/src/features/polyfill/polyfill.ts
+++ packages/preset-umi/src/features/polyfill/polyfill.ts
@@ -1,7 +1,7 @@
 import { transform } from '@umijs/bundler-utils/compiled/babel/core';
 import { getCorejsVersion, winPath } from '@umijs/utils';
 import { dirname, join } from 'path';
-import { DEFAULT_BROWSER_TARGETS } from '../../constants';
+import { DEFAULT_BROWSER_TARGETS } from '@umijs/bundler-webpack/dist/constants';
 import { IApi } from '../../types';
 
 export default (api: IApi) => {
