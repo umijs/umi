5f04ded9778fa7f500e2988beafd90a9ea224bef
chencheng (云谦) <sorrycc@gmail.com>
2022-09-22 11:53:40 +0800

    feat: mpa support config entry globally (#9395)

--- examples/mpa/.umirc.ts
+++ examples/mpa/.umirc.ts
@@ -3,5 +3,9 @@ export default {
     template: 'templates/default.html',
     getConfigFromEntryFile: true,
     layout: '@/layouts/basic',
+    entry: {
+      foo: { description: 'hello foo' },
+      bar: { description: 'hello bar' },
+    },
   },
 };
