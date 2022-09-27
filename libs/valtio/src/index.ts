46b4f47accf9c314772b29158f97442e275751c0
sorrycc <sorrycc@gmail.com>
2022-09-27 17:43:59 +0800

    feat: valtio plugin

---
+++ libs/valtio/src/index.ts
@@ -0,0 +1,15 @@
+export { proxy, useSnapshot, snapshot, subscribe } from 'valtio';
+export {
+  proxyWithComputed,
+  proxyWithHistory,
+  devtools as proxyWithDevtools,
+  proxyMap,
+  proxySet,
+} from 'valtio/utils';
+
+// TODO:
+// - proxyWithPersistent
+// - proxyWithAdapter
+// - proxyWithLoading
+// - proxyWithLogger
+// - proxyWithErrorHandler
