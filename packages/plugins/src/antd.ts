25e379693f5f3aa25dfa633dffcfce33e5724242
PeachScript <scdzwyxst@gmail.com>
2022-10-12 11:29:55 +0800

    refactor(plugins): correct config logic in antd plugin

--- packages/plugins/src/antd.ts
+++ packages/plugins/src/antd.ts
@@ -67,7 +67,8 @@ export default (api: IApi) => {
     // defaultConfig 的取值在 config 之后，所以改用环境变量传默认值
     if (process.env.UMI_PLUGIN_ANTD_ENABLE) {
       const { defaultConfig } = JSON.parse(process.env.UMI_PLUGIN_ANTD_ENABLE);
-      antd = Object.assign(defaultConfig, antd);
+      // 通过环境变量启用时，保持 memo.antd 与局部变量 antd 的引用关系，方便后续修改
+      memo.antd = antd = Object.assign(defaultConfig, antd);
     }
 
     // antd import
@@ -104,7 +105,10 @@ export default (api: IApi) => {
 
     // allow use `antd.theme` as the shortcut of `antd.configProvider.theme`
     if (antd.theme) {
-      assert(antdVersion.startsWith('5'), `antd.theme is only valid when antd is 5`);
+      assert(
+        antdVersion.startsWith('5'),
+        `antd.theme is only valid when antd is 5`,
+      );
       antd.configProvider ??= {};
       // priority: antd.theme > antd.configProvider.theme
       antd.configProvider.theme = deepmerge(
