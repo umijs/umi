39ff49556ec0d36c3cd8b8b43c3500d496df0250
pshu <pishu.spf@antfin.com>
2022-10-21 17:07:02 +0800

    test: ✅ add cypress project id

--- examples/antd-pro-create/cypress.config.ts
+++ examples/antd-pro-create/cypress.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from "cypress";
 const PORT = process.env.PORT || "8000";
 
 export default defineConfig({
+  projectId: 'qikpat',
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
--- examples/create-umi-pro/cypress.config.ts
+++ examples/create-umi-pro/cypress.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from 'cypress';
 const PORT = process.env.PORT || '8000';
 
 export default defineConfig({
+  projectId: 'qikpat',
   e2e: {
     // eslint-disable-next-line  @typescript-eslint/no-unused-vars
     setupNodeEvents(on, config) {
