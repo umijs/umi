c14c574b32e6c929b22aa0998d9c42138a1f7e3f
fz6m <59400654+fz6m@users.noreply.github.com>
2022-10-28 05:15:52 +0900

    refactor: code style

--- packages/preset-umi/src/features/tmpFiles/tmpFiles.ts
+++ packages/preset-umi/src/features/tmpFiles/tmpFiles.ts
@@ -41,6 +41,54 @@ export default (api: IApi) => {
     const umiTempDir = `${srcPrefix}.umi`;
     const baseUrl = api.appData.hasSrcDir ? '../../' : '../';
 
+    const defaultTsConfig = {
+      compilerOptions: {
+        target: 'esnext',
+        module: 'esnext',
+        moduleResolution: 'node',
+        importHelpers: true,
+        jsx: api.appData.framework === 'vue' ? 'preserve' : 'react-jsx',
+        esModuleInterop: true,
+        sourceMap: true,
+        baseUrl,
+        strict: true,
+        resolveJsonModule: true,
+        allowSyntheticDefaultImports: true,
+
+        // Supported by vue only
+        ...(api.appData.framework === 'vue'
+          ? {
+              // TODO Actually, it should be vite mode, but here it is written as vue only
+              // Required in Vite https://vitejs.dev/guide/features.html#typescript
+              isolatedModules: true,
+              // For `<script setup>`
+              // See <https://devblogs.microsoft.com/typescript/announcing-typescript-4-5-beta/#preserve-value-imports>
+              preserveValueImports: true,
+            }
+          : {}),
+
+        paths: {
+          '@/*': [`${srcPrefix}*`],
+          '@@/*': [`${umiTempDir}/*`],
+          [`${api.appData.umi.importSource}`]: [umiDir],
+          [`${api.appData.umi.importSource}/typings`]: [
+            `${umiTempDir}/typings`,
+          ],
+          ...(api.config.vite
+            ? {
+                '@fs/*': ['*'],
+              }
+            : {}),
+        },
+      },
+      include: [
+        `${baseUrl}.umirc.ts`,
+        `${baseUrl}**/*.d.ts`,
+        `${baseUrl}**/*.ts`,
+        `${baseUrl}**/*.tsx`,
+      ],
+    };
+
     api.writeTmpFile({
       noPluginDir: true,
       path: 'tsconfig.json',
@@ -49,56 +97,7 @@ export default (api: IApi) => {
       // 3、language service platform
       // 4、typing
       content: JSON.stringify(
-        deepmerge(
-          {
-            compilerOptions: {
-              target: 'esnext',
-              module: 'esnext',
-              moduleResolution: 'node',
-              importHelpers: true,
-              jsx: api.appData.framework === 'vue' ? 'preserve' : 'react-jsx',
-              esModuleInterop: true,
-              sourceMap: true,
-              baseUrl,
-              strict: true,
-              resolveJsonModule: true,
-              allowSyntheticDefaultImports: true,
-
-              // Supported by vue only
-              ...(api.appData.framework === 'vue'
-                ? {
-                    // TODO Actually, it should be vite mode, but here it is written as vue only
-                    // Required in Vite https://vitejs.dev/guide/features.html#typescript
-                    isolatedModules: true,
-                    // For `<script setup>`
-                    // See <https://devblogs.microsoft.com/typescript/announcing-typescript-4-5-beta/#preserve-value-imports>
-                    preserveValueImports: true,
-                  }
-                : {}),
-
-              paths: {
-                '@/*': [`${srcPrefix}*`],
-                '@@/*': [`${umiTempDir}/*`],
-                [`${api.appData.umi.importSource}`]: [umiDir],
-                [`${api.appData.umi.importSource}/typings`]: [
-                  `${umiTempDir}/typings`,
-                ],
-                ...(api.config.vite
-                  ? {
-                      '@fs/*': ['*'],
-                    }
-                  : {}),
-              },
-            },
-            include: [
-              `${baseUrl}.umirc.ts`,
-              `${baseUrl}**/*.d.ts`,
-              `${baseUrl}**/*.ts`,
-              `${baseUrl}**/*.tsx`,
-            ],
-          },
-          api.config.tsconfig?.overrides || {},
-        ),
+        deepmerge(defaultTsConfig, api.config.tsconfig?.overrides || {}),
         null,
         2,
       ),
