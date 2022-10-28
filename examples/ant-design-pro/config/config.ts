69b7530607181fe0f61ad760b49336e25fea47fe
陈帅 <qixian.cs@antgroup.com>
2022-10-28 15:35:30 +0800

    chore: remove unuse code (#9645)

--- examples/ant-design-pro/config/config.ts
+++ examples/ant-design-pro/config/config.ts
@@ -286,41 +286,6 @@ export default defineConfig({
       ],
     },
     {
-      name: 'editor',
-      icon: 'highlight',
-      path: '/editor',
-      routes: [
-        {
-          path: '/editor',
-          redirect: '/editor/flow',
-        },
-        {
-          name: 'flow',
-          icon: 'smile',
-          path: '/editor/flow',
-          component: './editor/flow',
-        },
-        {
-          name: 'mind',
-          icon: 'smile',
-          path: '/editor/mind',
-          component: './editor/mind',
-        },
-        {
-          name: 'koni',
-          icon: 'smile',
-          path: '/editor/koni',
-          component: './editor/koni',
-        },
-        {
-          name: 'monaco-editor',
-          icon: 'smile',
-          path: '/editor/monaco-editor',
-          component: './editor/monaco-editor',
-        },
-      ],
-    },
-    {
       path: '/',
       redirect: '/dashboard/analysis',
     },
