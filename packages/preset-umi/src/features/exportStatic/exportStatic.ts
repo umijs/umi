fad19a06b9ecfa9e9235ac25212de2693e31f237
Peach <scdzwyxst@gmail.com>
2022-11-03 11:46:16 +0800

    feat: allow extraRoutePaths for exportStatic (#9683)
    * fix(preset-umi): export static for 404 route
    
    * feat(preset-umi): allow extraRoutePaths for exportStatic
    
    * docs: describe extraRoutePaths of exportStatic

--- packages/preset-umi/src/features/exportStatic/exportStatic.ts
+++ packages/preset-umi/src/features/exportStatic/exportStatic.ts
@@ -27,9 +27,14 @@ function getExportHtmlData(routes: Record<string, IRoute>): IExportHtmlItem[] {
       // skip dynamic route for win, because `:` is not allowed in file name
       (!IS_WIN || !route.path.includes('/:')) &&
       // skip `*` route, because `*` is not working for most site serve services
-      !route.path.includes('*')
+      (!route.path.includes('*') ||
+        // except `404.html`
+        route.absPath === '/*')
     ) {
-      const file = join('.', route.absPath, 'index.html');
+      const file =
+        route.absPath === '/*'
+          ? '404.html'
+          : join('.', route.absPath, 'index.html');
 
       map.set(file, {
         route: {
@@ -45,9 +50,33 @@ function getExportHtmlData(routes: Record<string, IRoute>): IExportHtmlItem[] {
 }
 
 export default (api: IApi) => {
+  /**
+   * convert user `exportStatic.extraRoutePaths` config to routes
+   */
+  async function getRoutesFromUserExtraPaths(
+    routePaths: string[] | (() => string[] | Promise<string[]>),
+  ) {
+    const paths =
+      typeof routePaths === 'function' ? await routePaths() : routePaths;
+
+    return paths.reduce<Record<string, IRoute>>(
+      (acc, p) => ({
+        ...acc,
+        [p]: { id: p, absPath: p, path: p.slice(1), file: '' },
+      }),
+      {},
+    );
+  }
+
   api.describe({
     config: {
-      schema: (Joi) => Joi.object(),
+      schema: (Joi) =>
+        Joi.object({
+          extraRoutePaths: Joi.alternatives(
+            Joi.function(),
+            Joi.array().items(Joi.string()),
+          ),
+        }),
     },
     enableBy: api.EnableBy.config,
   });
@@ -58,8 +87,16 @@ export default (api: IApi) => {
 
   // export routes to html files
   api.modifyExportHTMLFiles(async (_defaultFiles, opts) => {
-    const { publicPath } = api.config;
-    const htmlData = getExportHtmlData(api.appData.routes);
+    const {
+      publicPath,
+      exportStatic: { extraRoutePaths = [] },
+    } = api.config;
+    const extraHtmlData = getExportHtmlData(
+      await getRoutesFromUserExtraPaths(extraRoutePaths),
+    );
+    const htmlData = getExportHtmlData(api.appData.routes).concat(
+      extraHtmlData,
+    );
     const htmlFiles: { path: string; content: string }[] = [];
 
     for (const { file } of htmlData) {
