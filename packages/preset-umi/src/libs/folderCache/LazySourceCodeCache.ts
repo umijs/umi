66ce273cc1a067f61e0da97cb87557666e1d789e
pshu <pishu.spf@antfin.com>
2022-11-04 17:38:55 +0800

    feat: ✨ extract folder watch class

--- packages/preset-umi/src/libs/folderCache/LazySourceCodeCache.ts
+++ packages/preset-umi/src/libs/folderCache/LazySourceCodeCache.ts
@@ -10,20 +10,23 @@ import fg from 'fast-glob';
 import { readFileSync } from 'fs';
 import { extname, join, relative } from 'path';
 import { FileChangeEvent } from './AutoUpdateFolderCache';
-import { LazyFolderCache } from './LazyFolderCache';
+import { FolderWatch } from './FolderWatch';
 
 export type MergedCodeInfo = {
   code: string;
   imports: readonly ImportSpecifier[];
-  events: FileChangeEvent[];
 };
+
+type AbsPath = string;
+type FileContent = string;
+type FileContentCache = Record<AbsPath, FileContent>;
 
 export type Listener = (info: MergedCodeInfo) => void;
 
 export class LazySourceCodeCache {
   private readonly srcPath: string;
   private readonly cachePath: string;
-  folderCache: LazyFolderCache;
+  folderWatch: FolderWatch;
   private listeners: Listener[] = [];
 
   private ignores: string[] = [
@@ -40,43 +43,41 @@ export class LazySourceCodeCache {
     '**/jest-setup.{ts,js}',
   ];
 
+  fileContentCache: FileContentCache = {};
+  private pendingNewFiles = new Set<string>();
+
   constructor(opts: { cwd: string; cachePath: string }) {
     this.srcPath = opts.cwd;
     this.cachePath = opts.cachePath;
 
-    this.folderCache = new LazyFolderCache({
+    this.folderWatch = new FolderWatch({
       cwd: this.srcPath,
       exts: ['ts', 'js', 'jsx', 'tsx'],
       ignored: this.ignores,
-      debouncedTimeout: 200,
-      filesLoader: async (files: string[]) => {
-        const loaded: Record<string, string> = {};
-        await this.batchProcess(files);
-
-        for (const f of files) {
-          let newFile = join(this.cachePath, relative(this.srcPath, f));
-
-          newFile = newFile.replace(new RegExp(`${extname(newFile)}$`), '.js');
-
-          loaded[f] = readFileSync(newFile, 'utf-8');
-        }
-        return loaded;
-      },
-      onCacheUpdate: async (_cache, events) => {
-        const merged = this.getMergedCode();
-        const info = { ...merged, events };
-
-        for (const l of this.listeners) {
-          await l(info);
-        }
-      },
+      events: ['add'],
+    });
+    this.folderWatch.listen((e) => {
+      this.pendingNewFiles.add(e.path);
     });
   }
 
   async init() {
-    const [files] = await Promise.all([this.initFileList(), esModuleLexerInit]);
+    const [files] = await Promise.all([
+      this.initFileList(),
+      esModuleLexerInit,
+      this.folderWatch.init(),
+    ]);
 
-    await this.folderCache.loadFiles(files);
+    await this.loadFiles(files);
+  }
+
+  public async loadFiles(files: string[]) {
+    const loaded = await this.filesLoader(files);
+
+    for (const f of Object.keys(loaded)) {
+      console.log('file', f);
+      this.fileContentCache[f] = loaded[f];
+    }
   }
 
   private async initFileList(): Promise<string[]> {
@@ -93,43 +94,8 @@ export class LazySourceCodeCache {
     return files;
   }
 
-  async batchProcess(files: string[]) {
-    try {
-      await esBuild({
-        entryPoints: files,
-        bundle: false,
-        outdir: this.cachePath,
-        outbase: this.srcPath,
-        loader: {
-          // in case some js using some feature, eg: decorator
-          '.js': 'tsx',
-          '.jsx': 'tsx',
-        },
-        logLevel: 'error',
-      });
-    } catch (e) {
-      // error ignored due to user have to update code to fix then trigger another batchProcess;
-      // @ts-ignore
-      if (e.errors?.length || e.warnings?.length) {
-        logger.warn(
-          'transpile code with esbuild got ',
-          // @ts-ignore
-          e.errors?.length || 0,
-          'errors,',
-          // @ts-ignore
-          e.warnings?.length || 0,
-          'warnings',
-        );
-        logger.debug('esbuild transpile code with error', e);
-      } else {
-        logger.warn('transpile code with esbuild error', e);
-      }
-    }
-  }
-
   getMergedCode() {
-    const fileContentCache = this.folderCache.getFileCache();
-    const code = Object.values(fileContentCache).join('\n');
+    const code = Object.values(this.fileContentCache).join('\n');
     const [imports] = parse(code);
     const merged = {
       code,
@@ -138,8 +104,39 @@ export class LazySourceCodeCache {
     return merged;
   }
 
-  async handleFileChangeEvents(e: FileChangeEvent[]) {
-    return this.folderCache.handleFileChangeEvents(e);
+  public consumePendingNewFiles() {
+    const files = Array.from(this.pendingNewFiles);
+    this.pendingNewFiles.clear();
+    return files;
+  }
+
+  public async handleFileChangeEvents(events: FileChangeEvent[]) {
+    const eventsCopy = events.slice();
+    const modifiedFiles: string[] = [];
+    while (eventsCopy.length > 0) {
+      const c = eventsCopy.pop()!;
+
+      switch (c.event) {
+        case 'unlink':
+          delete this.fileContentCache[c.path];
+          break;
+        case 'change':
+        case 'add':
+          modifiedFiles.push(c.path);
+          break;
+        default:
+          ((_n: never) => {})(c.event);
+      }
+    }
+    await this.loadFiles(modifiedFiles);
+
+    this.notify();
+  }
+
+  notify() {
+    const merged = this.getMergedCode();
+    const info = { ...merged };
+    this.listeners.forEach((l) => l(info));
   }
 
   register(l: Listener) {
@@ -153,5 +150,59 @@ export class LazySourceCodeCache {
     };
   }
 
+  async filesLoader(files: string[]) {
+    const loaded: Record<string, string> = {};
+    await esbuildTransform(files, {
+      srcPath: this.srcPath,
+      cachePath: this.cachePath,
+    });
+
+    for (const f of files) {
+      let newFile = join(this.cachePath, relative(this.srcPath, f));
+
+      newFile = newFile.replace(new RegExp(`${extname(newFile)}$`), '.js');
+
+      loaded[f] = readFileSync(newFile, 'utf-8');
+    }
+    return loaded;
+  }
+
   unwatch() {}
+}
+
+async function esbuildTransform(
+  files: string[],
+  opts: { cachePath: string; srcPath: string },
+) {
+  try {
+    await esBuild({
+      entryPoints: files,
+      bundle: false,
+      outdir: opts.cachePath,
+      outbase: opts.srcPath,
+      loader: {
+        // in case some js using some feature, eg: decorator
+        '.js': 'tsx',
+        '.jsx': 'tsx',
+      },
+      logLevel: 'error',
+    });
+  } catch (e) {
+    // error ignored due to user have to update code to fix then trigger another batchProcess;
+    // @ts-ignore
+    if (e.errors?.length || e.warnings?.length) {
+      logger.warn(
+        'transpile code with esbuild got ',
+        // @ts-ignore
+        e.errors?.length || 0,
+        'errors,',
+        // @ts-ignore
+        e.warnings?.length || 0,
+        'warnings',
+      );
+      logger.debug('esbuild transpile code with error', e);
+    } else {
+      logger.warn('transpile code with esbuild error', e);
+    }
+  }
 }
