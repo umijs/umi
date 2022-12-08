c4465e0e9a92d05e482255cac61e5204c631b72f
聪小陈 <xiaohuoni@users.noreply.github.com>
2022-12-08 15:27:00 +0800

    fix: history is undefined on render (#9871)

--- packages/plugins/libs/qiankun/slave/slaveRuntimePlugin.ts
+++ packages/plugins/libs/qiankun/slave/slaveRuntimePlugin.ts
@@ -1,13 +1,21 @@
 // @ts-nocheck
 import qiankunRender, { contextOptsStack } from './lifecycles';
+import { createHistory } from '@@/core/history';
 
 export function render(oldRender: any) {
   return qiankunRender().then(oldRender);
 }
 
-export function modifyContextOpts(memo: any) {
+export function modifyClientRenderOpts(memo: any) {
   // 每次应用 render 的时候会调 modifyClientRenderOpts，这时尝试从队列中取 render 的配置
   const clientRenderOpts = contextOptsStack.shift();
+  if (clientRenderOpts) {
+    clientRenderOpts.history = createHistory({
+      type: clientRenderOpts.historyType,
+      basename: clientRenderOpts.basename,
+      ...clientRenderOpts.historyOpts,
+    });
+  }
   return {
     ...memo,
     ...clientRenderOpts,
