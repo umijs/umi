d8153b14b1c6274697478e9f5ab6850402fb4c57
xierenyuan <xierenyuan@qq.com>
2022-12-09 17:41:54 +0800

    test: update max cypress.config

--- examples/max/cypress.config.ts
+++ examples/max/cypress.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'cypress';
 
 const PORT = process.env.PORT || '8000';
+const isWin = process.platform === 'win32';
 
 export default defineConfig({
   projectId: 'qikpat',
@@ -9,6 +10,9 @@ export default defineConfig({
       // implement node event listeners here
     },
     baseUrl: `http://localhost:${PORT}`,
-    defaultCommandTimeout: 8000,
   },
+  retries: {
+    runMode: 3,
+  },
+  defaultCommandTimeout: isWin ? 60000 : 8000,
 });
