02fedf132da0bd62836d0e79089ce0426958044c
xierenyuan <xierenyuan@qq.com>
2022-12-12 23:06:52 +0800

    feat(preset-vue): upgrade @vitejs/plugin-vue

--- examples/bundler-vite-demo/src/index.tsx
+++ examples/bundler-vite-demo/src/index.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import logoSrc, { ReactComponent as LogoComponent } from './logo.svg';
+import styles from './index.less';
 
 function App() {
   return (
-    <div>
+    <div className={styles['u-hello']}>
       <LogoComponent style={{ fontSize: 100 }} />
       <br />
       <LogoComponent width={50} height={50} />
