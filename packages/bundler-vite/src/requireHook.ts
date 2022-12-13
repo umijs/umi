0445fc2c4443f3fb5c2bbf9e95f7d91c76c5fed5
xierenyuan <xierenyuan@qq.com>
2022-12-13 15:35:37 +0800

    types: fix bundler-vite

--- packages/bundler-vite/src/requireHook.ts
+++ packages/bundler-vite/src/requireHook.ts
@@ -1,9 +1,13 @@
+// @ts-expect-error 这里不能删除, 不然 ts 声明会有问题 TODO 需要 father 看看
+import path from 'path';
+
 const hookPropertyMap = new Map([
   ['less', '@umijs/bundler-utils/compiled/less'],
 ]);
 
 const mod = require('module');
 const resolveFilename = mod._resolveFilename;
+
 mod._resolveFilename = function (
   request: string,
   parent: any,
