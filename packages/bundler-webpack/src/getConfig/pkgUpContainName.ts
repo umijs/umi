e7a173864653b946464884bb655497c9da6892c1
hanzebang <46307343+hanzebang@users.noreply.github.com>
2022-12-14 20:24:43 +0800

    Update packages/bundler-webpack/src/getConfig/pkgUpContainName.ts
    add comment
    
    Co-authored-by: Peach <scdzwyxst@gmail.com>

--- packages/bundler-webpack/src/getConfig/pkgUpContainName.ts
+++ packages/bundler-webpack/src/getConfig/pkgUpContainName.ts
@@ -1,6 +1,11 @@
 import { pkgUp } from '@umijs/utils';
 import path from 'path';
 
+/**
+ * find the closet package.json which contains `name`
+ * why not use pkg-up directly? some package (such as htmlparser2@8) put a `package.json` with
+ * `{ "type": "module" }` to provide pure esm dist, but it is not the npm `package.json` we want
+ */
 export const pkgUpContainName = (dir: string): string | null => {
   let pkgPath = pkgUp.sync({ cwd: dir });
   if (!pkgPath) return pkgPath;
