e3ccdf442705be9697819b9800576cb8457bb177
ice-zjchen <ice.zjchen@gmail.com>
2022-12-27 14:47:40 +0800

    feat: 简化props

--- examples/qiankun-slave/pages/home.tsx
+++ examples/qiankun-slave/pages/home.tsx
@@ -5,7 +5,7 @@ export default function HomePage() {
   return (
     <div>
       Slave Home Page{' '}
-      <MicroAppLink name="slave-app2" to="hello">
+      <MicroAppLink name="slave-app2" to="/hello">
         goto slave-app2
       </MicroAppLink>
     </div>
