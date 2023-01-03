70dfcb9fde2dd528e610b6ff574e66bbb08f1c24
chencheng (云谦) <sorrycc@gmail.com>
2023-01-03 23:01:40 +0800

    Revert "fix: 🐛  mfsu 错误的去读取通过 npm link 过的包的源码文件 (#10194)"
    This reverts commit 12b37c4bbed2996f338f476b6010159ba0b6f864.

--- packages/preset-umi/src/libs/folderCache/LazySourceCodeCache.ts
+++ packages/preset-umi/src/libs/folderCache/LazySourceCodeCache.ts
@@ -56,10 +56,6 @@ export class LazySourceCodeCache {
     await this.loadFiles(files);
   }
 
-  getSrcPath() {
-    return this.srcPath;
-  }
-
   public async loadFiles(files: string[]) {
     const loaded = await this.filesLoader(files);
 
