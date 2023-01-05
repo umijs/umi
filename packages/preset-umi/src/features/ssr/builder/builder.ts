19bcc0fd7620769e48c50d71e6625d4e9eda97b6
AkaraChen <85140972+AkaraChen@users.noreply.github.com>
2023-01-05 12:03:43 +0800

    feat(preset-umi): external dependencies in ssr builder

--- packages/preset-umi/src/features/ssr/builder/builder.ts
+++ packages/preset-umi/src/features/ssr/builder/builder.ts
@@ -1,5 +1,6 @@
 import esbuild from '@umijs/bundler-utils/compiled/esbuild';
 import { logger } from '@umijs/utils';
+import { existsSync, readFileSync } from 'fs';
 import { resolve } from 'path';
 import { IApi } from '../../../types';
 import { absServerBuildPath, esbuildUmiPlugin } from '../utils';
@@ -12,6 +13,14 @@ export async function build(opts: { api: IApi; watch?: boolean }) {
   const { api, watch } = opts;
   logger.wait('[SSR] Compiling...');
   const now = new Date().getTime();
+
+  let external = (function () {
+    const pkgJSONPath = resolve(api.paths.cwd, 'package.json');
+    if (existsSync(pkgJSONPath)) {
+      const pkg = JSON.parse(readFileSync(pkgJSONPath, 'utf-8').toString());
+      if (pkg.dependencies) return Object.keys(pkg.dependencies);
+    }
+  })();
 
   // TODO: 支持通用的 alias
   // TODO: external all import from package.json.dependencies
@@ -43,6 +52,7 @@ export async function build(opts: { api: IApi; watch?: boolean }) {
       assetsLoader({ cwd: api.cwd }),
     ],
     outfile: absServerBuildPath(api),
+    external,
   });
   const diff = new Date().getTime() - now;
   logger.info(`[SSR] Compiled in ${diff}ms`);
