b631869f30188c5c16023246ba0b47f76853e25b
fz6m <59400654+fz6m@users.noreply.github.com>
2023-02-03 01:59:19 +0800

    fix: ci

--- examples/mf-host/tests/bad-remote-config.spec.ts
+++ examples/mf-host/tests/bad-remote-config.spec.ts
@@ -3,11 +3,7 @@ import { expect, test } from '@playwright/test';
 test.describe('safe MF load', () => {
   test('can fallback with bad remote', async ({ page }) => {
     await Promise.all([
-      page.waitForResponse((res) => {
-        return (
-          res.url() === 'http://1.2.3.4:404/bad_file.js' && res.status() !== 200
-        );
-      }),
+      page.waitForRequest('http://1.2.3.4:404/bad_file.js'),
       page.goto('/bad-remote'),
     ]);
 
