bb695e1b5dbe93a7e72233f33fbc18b147fd2665
fz6m <59400654+fz6m@users.noreply.github.com>
2023-02-03 02:20:45 +0800

    fix: ci

--- playwright.base.config.ts
+++ playwright.base.config.ts
@@ -33,6 +33,8 @@ const config: PlaywrightTestConfig = {
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
+
+    serviceWorkers: 'block',
   },
 };
 
