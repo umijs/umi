e379b6a2d293852b9f41168e92ccf108869a5bba
xierenyuan <xierenyuan@qq.com>
2023-02-06 14:33:26 +0800

    feat: refer to nextjs to complete the webpack plugin

--- packages/bundler-webpack/bundles/webpack/bundle.js
+++ packages/bundler-webpack/bundles/webpack/bundle.js
@@ -9,9 +9,15 @@ const UseEffectRulePlugin = require('webpack/lib/rules/UseEffectRulePlugin')
 const ObjectMatcherRulePlugin = require('webpack/lib/rules/ObjectMatcherRulePlugin')
 const RuleSetCompiler = require('webpack/lib/rules/RuleSetCompiler')
 
-// workerize-loader
+// refer to nextjs https://github.com/vercel/next.js/blob/canary/packages/next/src/bundles/webpack/bundle5.js
+const BasicEvaluatedExpression =require('webpack/lib/javascript/BasicEvaluatedExpression')
 const NodeTargetPlugin = require('webpack/lib/node/NodeTargetPlugin')
+const NodeTemplatePlugin = require('webpack/lib/node/NodeTemplatePlugin')
+const LimitChunkCountPlugin = require('webpack/lib/optimize/LimitChunkCountPlugin')
 const WebWorkerTemplatePlugin = require('webpack/lib/webworker/WebWorkerTemplatePlugin')
+const FetchCompileAsyncWasmPlugin = require('webpack/lib/web/FetchCompileAsyncWasmPlugin')
+const FetchCompileWasmPlugin = require('webpack/lib/web/FetchCompileWasmPlugin')
+const StringXor = require('webpack/lib/util/StringXor')
 
 export {
   BasicEffectRulePlugin,
@@ -20,6 +26,12 @@ export {
   ObjectMatcherRulePlugin,
   RuleSetCompiler,
 
+  BasicEvaluatedExpression,
   NodeTargetPlugin,
-  WebWorkerTemplatePlugin
+  NodeTemplatePlugin,
+  LimitChunkCountPlugin,
+  WebWorkerTemplatePlugin,
+  FetchCompileAsyncWasmPlugin,
+  FetchCompileWasmPlugin,
+  StringXor
 }
