85349e07f720f978020dbe3c10989e0cc7875d82
fz6m <59400654+fz6m@users.noreply.github.com>
2023-02-15 05:20:46 +0800

    dep: upgrade

--- packages/utils/compiled/@clack/prompts/dist/index.d.ts
+++ packages/utils/compiled/@clack/prompts/dist/index.d.ts
@@ -3,6 +3,7 @@ export { isCancel } from '@clack/core';
 interface TextOptions {
     message: string;
     placeholder?: string;
+    initialValue?: string;
     validate?: (value: string) => string | void;
 }
 declare const text: (opts: TextOptions) => Promise<string | symbol>;
@@ -12,19 +13,20 @@ interface ConfirmOptions {
     inactive?: string;
     initialValue?: boolean;
 }
-declare const confirm: (opts: ConfirmOptions) => Promise<string | symbol>;
-interface Option {
-    value: any;
+declare const confirm: (opts: ConfirmOptions) => Promise<boolean | symbol>;
+interface Option<Value extends Readonly<string>> {
+    value: Value;
     label?: string;
     hint?: string;
 }
-interface SelectOptions<Options extends Option[]> {
+interface SelectOptions<Options extends Option<Value>[], Value extends Readonly<string>> {
     message: string;
     options: Options;
     initialValue?: Options[number]["value"];
 }
-declare const select: <Options extends Option[]>(opts: SelectOptions<Options>) => Promise<string | symbol>;
-declare const multiselect: <Options extends Option[]>(opts: SelectOptions<Options>) => Promise<string | symbol>;
+declare const select: <Options extends Option<Value>[], Value extends string>(opts: SelectOptions<Options, Value>) => Promise<symbol | Options[number]["value"]>;
+declare const multiselect: <Options extends Option<Value>[], Value extends string>(opts: SelectOptions<Options, Value>) => Promise<symbol | Options[number]["value"][]>;
+declare const note: (message?: string, title?: string) => void;
 declare const cancel: (message?: string) => void;
 declare const intro: (title?: string) => void;
 declare const outro: (message?: string) => void;
@@ -33,4 +35,4 @@ declare const spinner: () => {
     stop(message?: string): void;
 };
 
-export { ConfirmOptions, SelectOptions, TextOptions, cancel, confirm, intro, multiselect, outro, select, spinner, text };
+export { ConfirmOptions, SelectOptions, TextOptions, cancel, confirm, intro, multiselect, note, outro, select, spinner, text };
