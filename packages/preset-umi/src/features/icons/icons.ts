3ef598635c450274188c46c94ac7c379a08cba41
pshu <pishu.spf@antfin.com>
2023-02-20 15:25:09 +0800

    feat: ✨ icons 支持 include 配置

--- packages/preset-umi/src/features/icons/icons.ts
+++ packages/preset-umi/src/features/icons/icons.ts
@@ -19,6 +19,7 @@ export default (api: IApi) => {
           defaultComponentConfig: Joi.object(),
           // e.g. alias: { home: 'fa:home' }
           alias: Joi.object(),
+          include: Joi.array().items(Joi.string()),
         });
       },
     },
@@ -51,7 +52,10 @@ export default (api: IApi) => {
   });
 
   api.onPrepareBuildSuccess(async () => {
-    if (!icons.size) {
+    const extraIcons = api.config.icons.include || [];
+    const allIcons = new Set([...icons, ...extraIcons]);
+
+    if (!allIcons.size) {
       logger.info(`[icons] no icons was found`);
       return;
     }
@@ -60,7 +64,8 @@ export default (api: IApi) => {
     const code: string[] = [];
     const { generateIconName, generateSvgr }: typeof import('./svgr') =
       importLazy(require.resolve('./svgr'));
-    for (const iconStr of icons) {
+
+    for (const iconStr of allIcons) {
       const [collect, icon] = iconStr.split(':');
       const iconName = generateIconName({ collect, icon });
       let svgr;
@@ -147,8 +152,8 @@ interface IUmiIconProps extends React.SVGAttributes<SVGElement> {
   flip?: 'vertical' | 'horizontal' | 'horizontal,vertical' | 'vertical,horizontal';
 }
 
-export const Icon = React.forwardRef((props: IUmiIconProps, ref) => {
-  const { icon, hover, style, className, rotate, flip, ...extraProps } = props;
+export const Icon = React.forwardRef<HTMLSpanElement, IUmiIconProps>((props, ref) => {
+  const { icon, hover, style, className = '' , rotate, flip, ...extraProps } = props;
   const iconName = normalizeIconName(alias[icon] || icon);
   const Component = iconsMap[iconName];
   if (!Component) {
@@ -180,8 +185,12 @@ export const Icon = React.forwardRef((props: IUmiIconProps, ref) => {
     svgStyle.msTransform = transformStr;
     svgStyle.transform = transformStr;
   }
+  
+  const spanClassName = HoverComponent ? 'umiIconDoNotUseThis ' : '' + className;
+  const spanClass = spanClassName ? { className: spanClassName } : {};
+  
   return (
-    <span role="img" ref={ref} className={HoverComponent ? 'umiIconDoNotUseThis ' : '' + className} style={style}>
+    <span role="img" ref={ref} {...spanClass} style={style}>
       <Component {...extraProps} className={cls} style={svgStyle} />
       {
         HoverComponent ? <HoverComponent {...extraProps} className={'umiIconDoNotUseThisHover ' + cls} style={svgStyle} /> : null
