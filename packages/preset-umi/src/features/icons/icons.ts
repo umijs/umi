7597fa34d0e36c696b6d6a1fd23178fde9643d0c
chencheng (云谦) <sorrycc@gmail.com>
2023-02-20 18:05:49 +0800

    feat(preset-umi): improve icons type, support collection prefix and local icons (#10554)
    * feat(preset-umi): improve icons type, support collection prefix
    
    * feat: support derive local icon keys

--- packages/preset-umi/src/features/icons/icons.ts
+++ packages/preset-umi/src/features/icons/icons.ts
@@ -4,6 +4,7 @@ import {
   installWithNpmClient,
   logger,
 } from '@umijs/utils';
+import fs from 'fs';
 import path from 'path';
 
 import type { IApi } from '../../types';
@@ -93,7 +94,7 @@ export default (api: IApi) => {
                 });
               }),
           },
-          localIconDir: path.join(api.paths.absSrcPath, 'icons'),
+          localIconDir: getLocalIconDir(),
         });
       } catch (e) {
         logger.error(e);
@@ -124,6 +125,16 @@ export default (api: IApi) => {
         content: EMPTY_ICONS_FILE,
       });
     }
+    const localIconDir = getLocalIconDir();
+    const localIcons: string[] = [];
+    if (fs.existsSync(localIconDir)) {
+      localIcons.push(
+        ...fs
+          .readdirSync(localIconDir)
+          .filter((file) => file.endsWith('.svg'))
+          .map((file) => file.replace(/\.svg$/, '')),
+      );
+    }
     api.writeTmpFile({
       path: 'index.tsx',
       content: `
@@ -133,9 +144,160 @@ import './index.css';
 
 const alias = ${JSON.stringify(api.config.icons.alias || {})};
 type AliasKeys = keyof typeof alias;
+const localIcons = ${JSON.stringify(localIcons)} as const;
+type LocalIconsKeys = typeof localIcons[number];
+
+type IconCollections = 'academicons' |
+  'akar-icons' |
+  'ant-design' |
+  'arcticons' |
+  'basil' |
+  'bi' |
+  'bpmn' |
+  'brandico' |
+  'bx' |
+  'bxl' |
+  'bxs' |
+  'bytesize' |
+  'carbon' |
+  'charm' |
+  'ci' |
+  'cib' |
+  'cif' |
+  'cil' |
+  'circle-flags' |
+  'circum' |
+  'clarity' |
+  'codicon' |
+  'cryptocurrency-color' |
+  'cryptocurrency' |
+  'dashicons' |
+  'ei' |
+  'el' |
+  'emblemicons' |
+  'emojione-monotone' |
+  'emojione-v1' |
+  'emojione' |
+  'entypo-social' |
+  'entypo' |
+  'eos-icons' |
+  'ep' |
+  'et' |
+  'eva' |
+  'fa-brands' |
+  'fa-regular' |
+  'fa-solid' |
+  'fa' |
+  'fa6-brands' |
+  'fa6-regular' |
+  'fa6-solid' |
+  'fad' |
+  'fe' |
+  'feather' |
+  'file-icons' |
+  'flag' |
+  'flagpack' |
+  'flat-color-icons' |
+  'flat-ui' |
+  'fluent-emoji-flat' |
+  'fluent-emoji-high-contrast' |
+  'fluent-emoji' |
+  'fluent-mdl2' |
+  'fluent' |
+  'fontelico' |
+  'fontisto' |
+  'foundation' |
+  'fxemoji' |
+  'gala' |
+  'game-icons' |
+  'geo' |
+  'gg' |
+  'gis' |
+  'gridicons' |
+  'grommet-icons' |
+  'healthicons' |
+  'heroicons-outline' |
+  'heroicons-solid' |
+  'heroicons' |
+  'humbleicons' |
+  'ic' |
+  'icomoon-free' |
+  'icon-park-outline' |
+  'icon-park-solid' |
+  'icon-park-twotone' |
+  'icon-park' |
+  'iconoir' |
+  'icons8' |
+  'il' |
+  'ion' |
+  'iwwa' |
+  'jam' |
+  'la' |
+  'line-md' |
+  'logos' |
+  'ls' |
+  'lucide' |
+  'majesticons' |
+  'maki' |
+  'map' |
+  'material-symbols' |
+  'mdi-light' |
+  'mdi' |
+  'medical-icon' |
+  'memory' |
+  'mi' |
+  'mingcute' |
+  'mono-icons' |
+  'nimbus' |
+  'nonicons' |
+  'noto-v1' |
+  'noto' |
+  'octicon' |
+  'oi' |
+  'ooui' |
+  'openmoji' |
+  'pajamas' |
+  'pepicons-pop' |
+  'pepicons-print' |
+  'pepicons' |
+  'ph' |
+  'pixelarticons' |
+  'prime' |
+  'ps' |
+  'quill' |
+  'radix-icons' |
+  'raphael' |
+  'ri' |
+  'si-glyph' |
+  'simple-icons' |
+  'simple-line-icons' |
+  'skill-icons' |
+  'subway' |
+  'svg-spinners' |
+  'system-uicons' |
+  'tabler' |
+  'teenyicons' |
+  'topcoat' |
+  'twemoji' |
+  'typcn' |
+  'uil' |
+  'uim' |
+  'uis' |
+  'uit' |
+  'uiw' |
+  'vaadin' |
+  'vs' |
+  'vscode-icons' |
+  'websymbol' |
+  'whh' |
+  'wi' |
+  'wpf' |
+  'zmdi' |
+  'zondicons';
+type Icon = \`\${IconCollections}:\${string}\`;
 
 interface IUmiIconProps extends React.SVGAttributes<SVGElement> {
-  icon: AliasKeys | string;
+  icon: AliasKeys | Icon | \`local:\${LocalIconsKeys}\`;
   hover?: AliasKeys | string;
   className?: string;
   viewBox?: string;
@@ -247,4 +409,12 @@ function normalizeIconName(name: string) {
       `,
     });
   });
+
+  api.addTmpGenerateWatcherPaths(() => {
+    return [getLocalIconDir()];
+  });
+
+  function getLocalIconDir() {
+    return path.join(api.paths.absSrcPath, 'icons');
+  }
 };
