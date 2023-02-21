e6e08902ff6decaeb449056abf40a10fafebd15e
Crazy Urus <crazyurus@vip.qq.com>
2023-02-21 12:45:34 +0800

    chore: replace next.umijs.org into current domain

--- packages/create-umi/templates/max/src/app.ts
+++ packages/create-umi/templates/max/src/app.ts
@@ -1,7 +1,7 @@
 // 运行时配置
 
 // 全局初始化数据配置，用于 Layout 用户信息和权限初始化
-// 更多信息见文档：https://next.umijs.org/docs/api/runtime-config#getinitialstate
+// 更多信息见文档：https://umijs.org/docs/api/runtime-config#getinitialstate
 export async function getInitialState(): Promise<{ name: string }> {
   return { name: '@umijs/max' };
 }
