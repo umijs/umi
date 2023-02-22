10c4828854a75d36bbfe210ff3dbb6b6abe6bd5b
pshu <pishu.spf@antfin.com>
2023-02-23 00:50:09 +0800

    chore: 🔥 widnows debug2

--- packages/preset-umi/src/utils/projectFileList.ts
+++ packages/preset-umi/src/utils/projectFileList.ts
@@ -5,8 +5,24 @@ export function getProjectFileListPromise(api: IApi): Promise<string[]> {
   return new Promise<string[]>((resolve) => {
     api.onPrepareBuildSuccess(({ result, isWatch }) => {
       if (!isWatch) {
+        const debugfs = Object.keys(result.metafile!.inputs);
+
+        console.log(
+          '------ Object.keys(result.metafile!.inputs) ->',
+          Object.keys(result.metafile!.inputs),
+        );
+
+        console.log(
+          'resolved',
+          debugfs.map((f) => resolvePath(api.paths.cwd, f)),
+        );
+        console.log(
+          '------ api.paths.cwd ->',
+          api.paths.cwd,
+          api.paths.absSrcPath,
+        );
+
         const files = Object.keys(result.metafile!.inputs)
-          .sort()
           .map((f) => resolvePath(api.paths.cwd, f))
           .filter((f) => f.startsWith(api.paths.absSrcPath));
 
