9ead0bb1f4c614b808b88f851cab3ba8d8d79760
pshu <pishu.spf@antfin.com>
2023-02-22 22:29:41 +0800

    refactor: 🎨 rename resolve

--- packages/preset-umi/src/utils/projectFileList.ts
+++ packages/preset-umi/src/utils/projectFileList.ts
@@ -1,16 +1,16 @@
-import { resolve } from 'path';
+import { resolve as resolvePath } from 'path';
 import { IApi } from '../types';
 
 export function getProjectFileListPromise(api: IApi): Promise<string[]> {
-  return new Promise<string[]>((rslv) => {
+  return new Promise<string[]>((resolve) => {
     api.onPrepareBuildSuccess(({ result, isWatch }) => {
       if (!isWatch) {
         const files = Object.keys(result.metafile!.inputs)
           .sort()
-          .map((f) => resolve(api.paths.cwd, f))
+          .map((f) => resolvePath(api.paths.cwd, f))
           .filter((f) => f.startsWith(api.paths.absSrcPath));
 
-        rslv(files);
+        resolve(files);
       }
     });
   });
