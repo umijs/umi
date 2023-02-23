d2e96a518b968b33b786aeef35fe567a5fccc163
pshu <pishu.spf@antfin.com>
2023-02-23 10:20:00 +0800

    fix: 🐛 normalize and winpath src path

--- packages/preset-umi/src/utils/projectFileList.ts
+++ packages/preset-umi/src/utils/projectFileList.ts
@@ -1,12 +1,13 @@
 import { winPath } from '@umijs/utils';
-import { resolve as resolvePath } from 'path';
+import { normalize, resolve as resolvePath } from 'path';
 import { IApi } from '../types';
 
 export function getProjectFileListPromise(api: IApi): Promise<string[]> {
   return new Promise<string[]>((resolve) => {
     api.onPrepareBuildSuccess(({ result, isWatch }) => {
       if (!isWatch) {
-        const debugfs = Object.keys(result.metafile!.inputs);
+        const debugfs = Object.keys(result.metafile!.inputs).sort();
+        const srcPath = winPath(normalize(api.paths.absSrcPath));
 
         console.log(
           '------ Object.keys(result.metafile!.inputs) ->',
@@ -17,15 +18,11 @@ export function getProjectFileListPromise(api: IApi): Promise<string[]> {
           'resolved',
           debugfs.map((f) => winPath(resolvePath(api.paths.cwd, f))),
         );
-        console.log(
-          '------ api.paths.cwd ->',
-          api.paths.cwd,
-          api.paths.absSrcPath,
-        );
+        console.log('------ api.paths.cwd ->', api.paths.cwd, srcPath);
 
         const files = Object.keys(result.metafile!.inputs)
           .map((f) => winPath(resolvePath(api.paths.cwd, f)))
-          .filter((f) => f.startsWith(api.paths.absSrcPath));
+          .filter((f) => f.startsWith(srcPath));
 
         resolve(files);
       }
