bc03e895a545e2f3537219786dcbad9903815f81
pshu <pishu.spf@antfin.com>
2023-02-26 20:52:00 +0800

    chore: ✅ cypress use electron back

--- scripts/cypress.ts
+++ scripts/cypress.ts
@@ -5,10 +5,7 @@ const CYPRESS_RECORD_KEY = process.env.CYPRESS_RECORD_KEY as string;
 spawnSync('npx cypress install', {});
 
 if (CYPRESS_RECORD_KEY) {
-  spawnSync(
-    `npx cypress run --browser chrome --record --key ${CYPRESS_RECORD_KEY}`,
-    {},
-  );
+  spawnSync(`npx cypress run --record --key ${CYPRESS_RECORD_KEY}`, {});
 } else {
   spawnSync('npx cypress run', {});
 }
