878255ebb68cd78d2db9af9525115652340004ad
DaoYuan <txp1035@gmail.com>
2023-02-28 21:10:19 +0800

    chore: add type (#10627)
    * chore: add type
    
    * chore: string

--- packages/plugins/src/request.ts
+++ packages/plugins/src/request.ts
@@ -9,9 +9,7 @@ export default (api: IApi) => {
       schema: (Joi) => {
         return Joi.alternatives().try(
           Joi.object({
-            dataField: Joi.string()
-              .pattern(/^[a-zA-Z]*$/)
-              .allow(''),
+            dataField: Joi.string(),
           }),
           Joi.boolean().invalid(true),
         );
