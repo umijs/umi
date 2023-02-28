12ab3cafeaf1711da9a85fe37f0f8d18233705bb
PeachScript <scdzwyxst@gmail.com>
2023-02-28 16:27:22 +0800

    chore(preset-umi): stub commit

--- packages/preset-umi/src/commands/build.ts
+++ packages/preset-umi/src/commands/build.ts
@@ -3,15 +3,15 @@ import { chalk, fsExtra, logger, rimraf } from '@umijs/utils';
 import { writeFileSync } from 'fs';
 import { dirname, join, resolve } from 'path';
 import type { IApi, IOnGenerateFiles } from '../types';
+import {
+  measureFileSizesBeforeBuild,
+  printFileSizesAfterBuild,
+} from '../utils/fileSizeReporter';
 import { lazyImportFromCurrentPkg } from '../utils/lazyImportFromCurrentPkg';
 import { getAssetsMap } from './dev/getAssetsMap';
 import { getBabelOpts } from './dev/getBabelOpts';
 import { getMarkupArgs } from './dev/getMarkupArgs';
 import { printMemoryUsage } from './dev/printMemoryUsage';
-import {
-  measureFileSizesBeforeBuild,
-  printFileSizesAfterBuild,
-} from '../utils/fileSizeReporter';
 
 const bundlerWebpack: typeof import('@umijs/bundler-webpack') =
   lazyImportFromCurrentPkg('@umijs/bundler-webpack');
@@ -113,6 +113,7 @@ umi build --clean
           api.applyPlugins({
             key: 'onBuildComplete',
             args: opts,
+            // sync: true,
           });
         },
         clean: true,
