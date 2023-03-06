7c7fd3ff779d0a98e43c874342ca6c6243c086a2
折木 <1076849402@qq.com>
2023-03-06 15:04:16 +0800

    feat: did-you-know prompt strategy (#10663)
    * feat: did-you-know first show tip did not prompt, then random, must be different from prev
    
    * fix: change max reset count
    
    * fix: use faketimer to prevent get the same timestamp
    
    * refactor: use closure to update, use tip index as id
    
    * fix: typo
    
    * fix: resolve review
    
    * fix: write file when first onGen, resolve review
    
    * fix: records move to .cache
    
    * fix: mkdir when no exist

--- did-you-know/src/plugin.ts
+++ did-you-know/src/plugin.ts
@@ -1,3 +1,5 @@
+import fs from 'fs';
+import path from 'path';
 // @ts-ignore
 import data from '../package.json';
 // @ts-ignore
@@ -8,8 +10,20 @@ enum EFramework {
   max = '@umijs/max',
   bigfish = '@alipay/bigfish',
 }
+const MAX_RESET_COUNT = 5;
+const RECORD_FILE = 'did-you-know.json';
 
 export default (api: any) => {
+  const recordJSONPath = path.join(
+    api.paths.absNodeModulesPath,
+    '.cache',
+    RECORD_FILE,
+  );
+  // did_you_know 触发记录
+  let records: Record<string, ITipRecord> = fs.existsSync(recordJSONPath)
+    ? JSON.parse(fs.readFileSync(recordJSONPath, 'utf-8'))
+    : {};
+
   api.onStart(() => {
     const isUmi3 = !!api.utils;
     let logger = console;
@@ -41,6 +55,7 @@ export default (api: any) => {
     }
 
     const item = getDidYouKnow(data.didYouKnow, framework, majorVersion);
+
     if (!item) return;
     const { text, url } = item;
     const info = [
@@ -51,6 +66,12 @@ export default (api: any) => {
       url ? formatLink(url) : '。',
     ];
     logger.info(chalk.yellow(info.join('')));
+
+    const cacheDir = path.join(api.paths.absNodeModulesPath, '.cache');
+    if (!fs.existsSync(cacheDir)) {
+      fs.mkdirSync(cacheDir);
+    }
+    fs.writeFileSync(recordJSONPath, JSON.stringify(records), 'utf-8');
   });
 
   function getDidYouKnow(
@@ -59,7 +80,7 @@ export default (api: any) => {
     majorVersion: string,
   ) {
     // 1、get matched
-    const matched = items.filter(item => {
+    const matched = items.filter((item) => {
       return (
         (!item.framework || item.framework.includes(framework)) &&
         (!item.majorVersion || majorVersion === `${item.majorVersion}`)
@@ -67,8 +88,55 @@ export default (api: any) => {
     });
     // 2、matched.length ? random : null
     if (matched.length) {
-      const luck = Math.floor(Math.random() * matched.length);
-      return matched[luck];
+      // 未提示过、已提示数小于最大值，作为待选项
+      const available = matched.filter(({ text }) => {
+        const record = records[encodeURI(text)];
+        return !record || record.count < MAX_RESET_COUNT;
+      });
+
+      // 只剩一个选项，直接返回并重置记录
+      if (available.length === 1) {
+        const tip = available[0];
+        records = {
+          [encodeURI(tip.text)]: {
+            lastTime: Date.now(),
+            count: 1,
+          },
+        };
+        return tip;
+      }
+
+      // 存在未曾提示过的，或新增的提示，优先提示
+      for (const tip of available) {
+        const recordKey = encodeURI(tip.text);
+        if (!records[recordKey]) {
+          records = {
+            ...records,
+            [recordKey]: {
+              count: 1,
+              lastTime: Date.now(),
+            },
+          };
+          return tip;
+        }
+      }
+
+      available.sort(
+        (l, r) =>
+          records[encodeURI(l.text)].lastTime -
+          records[encodeURI(r.text)].lastTime,
+      );
+      // 末位为上次输出的提示，不取
+      const rIndex = Math.floor(Math.random() * (available.length - 1));
+
+      const luckTip = available[rIndex];
+      const recordKey = encodeURI(luckTip.text);
+      records[recordKey] = {
+        lastTime: Date.now(),
+        count: records[recordKey].count + 1,
+      };
+
+      return luckTip;
     }
     return null;
   }
@@ -81,6 +149,11 @@ interface ITip {
   url?: string;
   majorVersion?: number;
   framework?: Framework[];
+}
+
+interface ITipRecord {
+  lastTime: number;
+  count: number;
 }
 
 function formatLink(url: string) {
