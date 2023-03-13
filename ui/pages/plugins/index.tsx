76ef20c39643bfaaffb4c6a688c119fa33aa1f3a
yuyuehui.yyh <yuyuehui.yyh@digital-engine.com>
2023-03-14 01:28:29 +0800

    feat(ui): delete console.log

--- ui/pages/plugins/index.tsx
+++ ui/pages/plugins/index.tsx
@@ -4,8 +4,6 @@ import { PluginList } from '@/pages/plugins/components/PluginList';
 export default function Page() {
   const { data } = useAppData();
   if (!data) return <div>Loading...</div>;
-
-  console.log('plugins', data.plugins);
   return (
     <div>
       <PluginList plugins={data.plugins} />
