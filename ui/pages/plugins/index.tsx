579e8ffda63019e4e113ac1bb39901d04b3f54bd
goo-yyh <45666106+goo-yyh@users.noreply.github.com>
2023-03-14 10:42:47 +0800

    Feature/lyy umi UI plugins (#10739)
    * feat(ui): umi ui plugin 页面开发
    
    * feat(ui): delete console.log
    
    * Update package.json
    
    ---------
    
    Co-authored-by: yuyuehui.yyh <yuyuehui.yyh@digital-engine.com>
    Co-authored-by: chencheng (云谦) <sorrycc@gmail.com>

--- ui/pages/plugins/index.tsx
+++ ui/pages/plugins/index.tsx
@@ -1,15 +1,12 @@
-import { CodeBlock } from '@/components/CodeBlock';
 import { useAppData } from '@/hooks/useAppData';
+import { PluginList } from '@/pages/plugins/components/PluginList';
 
 export default function Page() {
   const { data } = useAppData();
   if (!data) return <div>Loading...</div>;
   return (
     <div>
-      <h2>Plugins</h2>
-      <pre>
-        <CodeBlock code={data.plugins} />
-      </pre>
+      <PluginList plugins={data.plugins} />
     </div>
   );
 }
