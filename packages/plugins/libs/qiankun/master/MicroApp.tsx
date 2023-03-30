e93842c1f9e6854e7d85577b02da6f1ca760bf03
Kuitos <kuitos.lau@gmail.com>
2023-03-30 17:12:17 +0800

    fix(qiankun): microapp error when unmount in updating (#10877)
    * fix(qiankun): 兼容微应用 updating 阶段触发了 unmount 的场景
    
    * chore: optimize
    
    ---------
    
    Co-authored-by: PeachScript <scdzwyxst@gmail.com>

--- packages/plugins/libs/qiankun/master/MicroApp.tsx
+++ packages/plugins/libs/qiankun/master/MicroApp.tsx
@@ -58,9 +58,21 @@ export type Props = {
   className?: string;
 } & Record<string, any>;
 
-function unmountMicroApp(microApp?: MicroAppType) {
+function unmountMicroApp(microApp?: MicroAppType, updatingPromise?: Promise<void>) {
   if (microApp) {
-    microApp.mountPromise.then(() => microApp.unmount());
+    microApp.mountPromise.then(() => {
+      switch (microApp.getStatus()) {
+        case "MOUNTED":
+          microApp.unmount();
+          break;
+        case "UPDATING":
+          // UPDATING 阶段 updatingPromise 一定存在
+          updatingPromise!.then(() => microApp.unmount());
+          break;
+        default:
+          break;
+      }
+    });
   }
 }
 
@@ -213,7 +225,7 @@ export const MicroApp = forwardRef(
         },
       );
 
-      return () => unmountMicroApp(microAppRef.current);
+      return () => unmountMicroApp(microAppRef.current, updatingPromise.current);
     }, [name]);
 
     useEffect(() => {
