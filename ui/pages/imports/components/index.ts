8072d3418082bf23f7d94eac19c17556891952b6
goo-yyh <45666106+goo-yyh@users.noreply.github.com>
2023-04-01 14:36:40 +0800

    feat(ui): routes第一版开发 & 目录修改 (#10882)
    * feat(ui): routes第一版开发 & 目录修改
    
    * feat(ui): 删除console
    
    * feat(ui): 修改codeBlock
    
    ---------
    
    Co-authored-by: yuyuehui.yyh <yuyuehui.yyh@digital-engine.com>

--- ui/pages/imports/components/index.ts
+++
@@ -1,3 +0,0 @@
-export { ViewButton } from '@/pages/imports/components/ViewButton';
-export { ViewChart } from '@/pages/imports/components/ViewChart';
-export { ViewList } from '@/pages/imports/components/ViewList';
