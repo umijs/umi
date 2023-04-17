986f723e14100e9fb136edba1371a37e5f014fb7
DroganCen <32896700+DroganC@users.noreply.github.com>
2023-04-17 15:18:22 +0800

    export DEV_COMMAND from umi
    增加一个DEV_COMMAND的导出

--- packages/umi/src/index.ts
+++ packages/umi/src/index.ts
@@ -1,7 +1,7 @@
 import { IServicePluginAPI, PluginAPI } from '@umijs/core';
 
 export { run } from './cli/cli';
-export { RUNTIME_TYPE_FILE_NAME } from './constants';
+export { RUNTIME_TYPE_FILE_NAME, DEV_COMMAND  } from './constants';
 export { defineConfig } from './defineConfig';
 export { defineMock } from './defineMock';
 export * from './service/service';
