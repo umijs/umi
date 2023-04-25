2663a5cf34aeb2a082ccb4d1912d49c6eba77d88
Ydream <43460525+YdreamW@users.noreply.github.com>
2023-04-25 18:29:42 +0800

    feat(server/routes): 扩展 createServerRoutes 方法，允许定制 route 的内容。 (#11033)

--- packages/server/src/routes.ts
+++ packages/server/src/routes.ts
@@ -3,12 +3,14 @@ import { IRoute, IRoutesById } from './types';
 export function createServerRoutes(opts: {
   routesById: IRoutesById;
   parentId?: string;
+  createRoute?: (opts: { route: IRoute }) => any;
 }) {
-  const { routesById, parentId } = opts;
+  const { routesById, parentId, createRoute } = opts;
+  const createRouteFn = createRoute || createServerRoute;
   return Object.keys(routesById)
     .filter((id) => routesById[id].parentId === parentId)
     .map((id) => {
-      const route = createServerRoute({
+      const route = createRouteFn({
         route: routesById[id],
       });
       const children = createServerRoutes({
