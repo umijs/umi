a781026870b0298aa8150105fd150b1874c42a53
hualigushi <hualigushi@163.com>
2023-06-25 16:39:04 +0800

    docs: migrate to dumi

--- docs/.dumirc.ts
+++ docs/.dumirc.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'dumi';
 import { version } from '../lerna.json';
 
-const OG_HOME = 'https://umi-api-route-demo.vercel.app/images/og-home.png';
+const OG_HOME = 'https://umijs.org/images/og-home.png';
 
 const UMI_DESCRIPTION =
   'Umi是可扩展的企业级前端应用框架。Umi 以路由为基础的，同时支持配置式路由和约定式路由，保证路由的功能完备，并以此进行功能扩展。然后配以生命周期完善的插件体系，覆盖从源码到构建产物的每个生命周期，支持各种功能扩展和业务需求。';
