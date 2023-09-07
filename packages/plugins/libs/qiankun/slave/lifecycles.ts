a7e994e56ef8a97796f48910b45a33da0c6c8be4
gaopeng <gaopeng19960108@gmail.com>
2023-09-07 10:06:01 +0800

    fix: 增加 autoSetLoading 判断

--- packages/plugins/libs/qiankun/slave/lifecycles.ts
+++ packages/plugins/libs/qiankun/slave/lifecycles.ts
@@ -82,8 +82,8 @@ export function genMount(mountElementId: string) {
       const clientRenderOpts = {
         callback: () => {
           // 默认开启
-          // 不再判断 autoSetLoading 的值，因为通过组件模式可以直接传递 loader
-          if (typeof props.setLoading === 'function') {
+          // 如果需要手动控制 loading，通过主应用配置 props.autoSetLoading false 可以关闭
+          if (props.autoSetLoading && typeof props.setLoading === 'function') {
             props.setLoading(false);
           }
 
