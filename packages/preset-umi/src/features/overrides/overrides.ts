32809c735ecfcf709885204bad848ca02019dfd0
Peach <scdzwyxst@gmail.com>
2023-09-26 21:15:09 +0800

    refactor: update

--- packages/preset-umi/src/features/overrides/overrides.ts
+++ packages/preset-umi/src/features/overrides/overrides.ts
@@ -32,8 +32,8 @@ export default (api: IApi) => {
                 // special :first-child to promote html selector priority
                 return `html:first-child`;
               } else if (/^body([\s+~>[:]|$)/.test(selector)) {
-                // use :root parent to promote body selector priority
-                return `:root ${selector}`;
+                // use html parent to promote body selector priority
+                return `html ${selector}`;
               }
 
               return prefixedSelector;
