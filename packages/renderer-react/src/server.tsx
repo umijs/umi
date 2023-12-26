87f3ccff7fb1b70105f5ee05ce4f5d8c6e090d63
MadCcc <1075746765@qq.com>
2023-12-26 14:19:19 +0800

    chore: add DOCTYPE

--- packages/renderer-react/src/server.tsx
+++ packages/renderer-react/src/server.tsx
@@ -68,7 +68,8 @@ export function getServerHTMLStart({
   manifest: any;
   metadata?: IMetadata;
 }) {
-  return `<html lang={metadata?.lang || 'en'}>
+  return `<!DOCTYPE html>
+<html lang="${metadata?.lang || 'en'}">
   <head>
     <meta charSet="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />${
