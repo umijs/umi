70cdd9bfd54f7c52931db3327c9dd6c6a4ed9e6c
fz6m <59400654+fz6m@users.noreply.github.com>
2024-01-07 03:24:28 +0800

    ci: fix type

--- docs/plugin.ts
+++ docs/plugin.ts
@@ -1,4 +1,6 @@
-export default (api: any) => {
+import { IApi } from 'dumi';
+
+export default (api: IApi) => {
   api.modifyBabelPresetOpts((memo) => {
     // we do not write UI library so enable it
     // https://github.com/umijs/dumi/commit/c5e6c8877f5b57f649e47b664e2a6d2701961f73
