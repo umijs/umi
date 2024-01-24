935d0ef743420c22080527cbf24c564dc4ee38ae
PeachScript <scdzwyxst@gmail.com>
2024-01-24 17:49:41 +0800

    feat: add bundler info when enable okam

--- packages/preset-umi/src/features/okam/okam.ts
+++ packages/preset-umi/src/features/okam/okam.ts
@@ -2,11 +2,21 @@ import { checkVersion } from '@umijs/utils';
 import { IApi } from '../../types';
 
 export default (api: IApi) => {
+  api.describe({
+    enableBy: () => Boolean(process.env.OKAM),
+  });
+
   api.onCheck(() => {
     // mako 仅支持 node 16+
     // ref: https://github.com/umijs/mako/issues/300
     if (api.userConfig.mako) {
       checkVersion(16, `Node 16 is required when using mako.`);
     }
+  });
+
+  api.modifyAppData((memo) => {
+    memo.bundler = 'mako';
+
+    return memo;
   });
 };
