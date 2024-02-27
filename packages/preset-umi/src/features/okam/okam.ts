55f1d4fca5ca2eb09e9001587103a075edbc064a
PeachScript <scdzwyxst@gmail.com>
2024-02-27 09:46:44 +0800

    refactor: update enableBy for okam

--- packages/preset-umi/src/features/okam/okam.ts
+++ packages/preset-umi/src/features/okam/okam.ts
@@ -3,15 +3,13 @@ import { IApi } from '../../types';
 
 export default (api: IApi) => {
   api.describe({
-    enableBy: () => Boolean(process.env.OKAM),
+    enableBy: api.EnableBy.config,
   });
 
   api.onCheck(() => {
     // mako 仅支持 node 16+
     // ref: https://github.com/umijs/mako/issues/300
-    if (api.userConfig.mako) {
-      checkVersion(16, `Node 16 is required when using mako.`);
-    }
+    checkVersion(16, `Node 16 is required when using mako.`);
   });
 
   api.modifyAppData((memo) => {
