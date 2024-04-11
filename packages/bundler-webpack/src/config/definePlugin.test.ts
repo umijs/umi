54a84c0310f5859c95dbc1ab0d1332b1766cd15c
shidong.wh <hfut.haow@gmail.com>
2024-04-11 21:50:42 +0800

    feat: socket-server 根据 env.HOST 生成

--- packages/bundler-webpack/src/config/definePlugin.test.ts
+++ packages/bundler-webpack/src/config/definePlugin.test.ts
@@ -29,3 +29,35 @@ test('env variables', () => {
     },
   });
 });
+
+test('should get SOCKET_SERVER if SOCKET_SERVER exists', () => {
+  process.env.SOCKET_SERVER = 'socket.server';
+  process.env.HOST = 'test.host';
+  expect(
+    resolveDefine({
+      define: {},
+    })['process.env']['SOCKET_SERVER'],
+  ).toEqual('"socket.server"');
+});
+
+test('should get SOCKET_SERVER if HOST exists', () => {
+  delete process.env.SOCKET_SERVER;
+  process.env.HOST = 'test.host';
+  expect(
+    resolveDefine({
+      define: {},
+    })['process.env']['SOCKET_SERVER'],
+  ).toEqual('"http://test.host:8000"');
+});
+
+test('should get https SOCKET_SERVER if exists', () => {
+  delete process.env.SOCKET_SERVER;
+  process.env.HOST = 'test.host';
+  process.env.PORT = '6666';
+  expect(
+    resolveDefine({
+      define: {},
+      https: true,
+    })['process.env']['SOCKET_SERVER'],
+  ).toEqual('"https://test.host:6666"');
+});
--- packages/bundler-webpack/src/config/definePlugin.ts
+++ packages/bundler-webpack/src/config/definePlugin.ts
@@ -1,6 +1,7 @@
+import type { HttpsServerOptions } from '@umijs/bundler-utils';
 import { DefinePlugin } from '@umijs/bundler-webpack/compiled/webpack';
 import Config from '@umijs/bundler-webpack/compiled/webpack-5-chain';
-import { Env, IConfig } from '../types';
+import type { Env, IConfig } from '../types';
 
 interface IOpts {
   config: Config;
@@ -9,15 +10,49 @@ interface IOpts {
   env: Env;
 }
 
+interface IResolveDefineOpts {
+  define: any;
+  publicPath?: string;
+  https?: HttpsServerOptions;
+}
+
 const prefixRE = /^UMI_APP_/;
 const ENV_SHOULD_PASS = ['NODE_ENV', 'HMR', 'SOCKET_SERVER', 'ERROR_OVERLAY'];
 
-export function resolveDefine(opts: { define: any; publicPath?: string }) {
-  const env: Record<string, any> = {};
-  Object.keys(process.env).forEach((key) => {
-    if (prefixRE.test(key) || ENV_SHOULD_PASS.includes(key)) {
-      env[key] = process.env[key];
+// 环境变量传递自定义逻辑，默认直接透传
+const EnvGetter: Record<
+  string,
+  (opts: IResolveDefineOpts) => string | undefined
+> = {
+  SOCKET_SERVER: (opts: IResolveDefineOpts) => {
+    // 如果当前有 process.env.SOCKET_SERVER，则使用当前值
+    if (process.env.SOCKET_SERVER) {
+      return process.env.SOCKET_SERVER;
     }
+    // 如果当前无 process.env.SOCKET_SERVER
+    // 则判断是否有 process.env.HOST 且不为 0.0.0.0/127.0.0.1/localhost
+    if (
+      process.env.HOST &&
+      !['0.0.0.0', '127.0.0.1', 'localhost'].includes(process.env.HOST)
+    ) {
+      const protocol = opts.https ? 'https:' : 'http:';
+      return `${protocol}//${process.env.HOST}:${process.env.PORT || 8000}`;
+    }
+    return;
+  },
+};
+
+export function resolveDefine(opts: IResolveDefineOpts) {
+  const env: Record<string, any> = {};
+  // 带 UMI_APP_ 前缀的和 ENV_SHOULD_PASS 定义的环境变量需要透传
+  ENV_SHOULD_PASS.concat(
+    Object.keys(process.env).filter((k) => prefixRE.test(k)),
+  ).forEach((key: string) => {
+    const envValue = EnvGetter[key] ? EnvGetter[key](opts) : process.env[key];
+    if (typeof envValue === 'undefined') {
+      return;
+    }
+    env[key] = envValue;
   });
 
   // Useful for resolving the correct path to static assets in `public`.
@@ -47,6 +82,7 @@ export async function addDefinePlugin(opts: IOpts) {
     resolveDefine({
       define: userConfig.define || {},
       publicPath: userConfig.publicPath,
+      https: userConfig.https,
     }),
   ] as any);
 }
