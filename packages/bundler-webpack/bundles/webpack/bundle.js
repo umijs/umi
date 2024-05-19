feb1848b8fb970c6e20ad11f8920df411f2a8d31
fz6m <59400654+fz6m@users.noreply.github.com>
2024-05-19 10:00:07 +0900

    feat: hook mf v2 using webpack exports

--- packages/bundler-webpack/bundles/webpack/bundle.js
+++ packages/bundler-webpack/bundles/webpack/bundle.js
@@ -23,6 +23,42 @@ const StringXor = require('webpack/lib/util/StringXor')
 const TemplatedPathPlugin = require('webpack/lib/TemplatedPathPlugin')
 const createHash = require('webpack/lib/util/createHash')
 
+// mf v2
+// const JavascriptModulesPlugin = require('webpack/lib/javascript/JavascriptModulesPlugin')
+// const Module = require('webpack/lib/Module')
+// const WebpackError = require('webpack/lib/WebpackError')
+// const RuntimeGlobals = require('webpack/lib/RuntimeGlobals')
+// const Dependency = require('webpack/lib/Dependency')
+// const RuntimeModule = require('webpack/lib/RuntimeModule')
+// const ExternalModule = require('webpack/lib/ExternalModule')
+const StartupChunkDependenciesPlugin = require('webpack/lib/runtime/StartupChunkDependenciesPlugin')
+const identifier = require('webpack/lib/util/identifier')
+const compileBooleanMatcher = require('webpack/lib/util/compileBooleanMatcher')
+const SortableSet = require('webpack/lib/util/SortableSet')
+const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin')
+const NodeEnvironmentPlugin = require('webpack/lib/node/NodeEnvironmentPlugin')
+const makeSerializable = require('webpack/lib/util/makeSerializable')
+const StaticExportsDependency = require('webpack/lib/dependencies/StaticExportsDependency')
+const EntryDependency = require('webpack/lib/dependencies/EntryDependency')
+const ModuleFactory = require('webpack/lib/ModuleFactory')
+const ModuleDependency = require('webpack/lib/dependencies/ModuleDependency')
+const ResolverFactory = require('webpack/lib/ResolverFactory')
+const FileSystemInfo = require('webpack/lib/FileSystemInfo')
+const ObjectMiddleware = require('webpack/lib/serialization/ObjectMiddleware')
+const extractUrlAndGlobal = require('webpack/lib/util/extractUrlAndGlobal')
+const semver = require('webpack/lib/util/semver')
+const ModuleNotFoundError = require('webpack/lib/ModuleNotFoundError')
+const LazySet = require('webpack/lib/util/LazySet')
+const fs = require('webpack/lib/util/fs')
+const comparators = require('webpack/lib/util/comparators')
+const ConsumeSharedModule = require('webpack/lib/sharing/ConsumeSharedModule')
+
+const JavascriptHotModuleReplacementRuntime = require('webpack/lib/hmr/JavascriptHotModuleReplacement.runtime.js')
+const createSchemaValidation = require('webpack/lib/util/create-schema-validation')
+
+const ExternalsTypeCheck = require("webpack/schemas/plugins/container/ExternalsType.check.js")
+const ConsumeSharedPluginCheck = require("webpack/schemas/plugins/sharing/ConsumeSharedPlugin.check.js")
+
 export {
   BasicEffectRulePlugin,
   BasicMatcherRulePlugin,
@@ -41,4 +77,30 @@ export {
 
   TemplatedPathPlugin,
   createHash,
+
+  StartupChunkDependenciesPlugin,
+  identifier,
+  compileBooleanMatcher,
+  SortableSet,
+  ModuleFederationPlugin,
+  NodeEnvironmentPlugin,
+  makeSerializable,
+  StaticExportsDependency,
+  EntryDependency,
+  ModuleFactory,
+  ModuleDependency,
+  ResolverFactory,
+  FileSystemInfo,
+  ObjectMiddleware,
+  extractUrlAndGlobal,
+  semver,
+  ModuleNotFoundError,
+  LazySet,
+  fs,
+  comparators,
+  ConsumeSharedModule,
+  JavascriptHotModuleReplacementRuntime,
+  createSchemaValidation,
+  ExternalsTypeCheck,
+  ConsumeSharedPluginCheck,
 }
