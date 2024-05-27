7cc85428a408ad988733042cab4ca0f553b96dca
SANmq <50514663+SANmq@users.noreply.github.com>
2024-05-27 22:13:34 +0800

    修复一个三方依赖exports导出__esModule=true引起的导入问题
    webpack本来可以正常处理模块，因为依赖导出带有__esModule.webpack导入的时候当成es模块处理去取default，结果模块里没有default，已经被esbuild导出的时候处理了，结果导致webpack 默认导入失败，

--- packages/mfsu/src/depBuilder/getESBuildEntry.ts
+++ packages/mfsu/src/depBuilder/getESBuildEntry.ts
@@ -6,10 +6,10 @@ const ES_INTEROP_FUNC = `__exportStar`;
 const ES_INTEROP_HELPER = `
 var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
   if (k2 === undefined) k2 = k;
-  Object.defineProperty(o, k2, { enumerable: true, get: function() { return m[k]; } });
+  if (k !== '__esModule') Object.defineProperty(o, k2, { enumerable: true, get: function() { return m[k]; } });
 }) : (function(o, m, k, k2) {
   if (k2 === undefined) k2 = k;
-  o[k2] = m[k];
+  if (k !== '__esModule') o[k2] = m[k];
 }));
 var ${ES_INTEROP_FUNC} = (this && this.__exportStar) || function(m, exports) {
   for (var p in m) if (p !== "default" && !Object.prototype.hasOwnProperty.call(exports, p)) __createBinding(exports, m, p);
