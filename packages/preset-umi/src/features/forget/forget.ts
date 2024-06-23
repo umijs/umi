ec7dd79a860a1e0d5406aaac33fe445c59521273
BBBigCat <liuooonihaoa@163.com>
2024-06-23 09:51:00 +0800

    fix: writing error

--- packages/preset-umi/src/features/forget/forget.ts
+++ packages/preset-umi/src/features/forget/forget.ts
@@ -54,7 +54,7 @@ export default (api: IApi) => {
       ...memo,
       extraBabelPlugins: [
         ...(memo.extraBabelPlugins || []),
-        [[require.resolve(libPath), ReactCompilerConfig], ReactCompilerConfig],
+        [require.resolve(libPath), ReactCompilerConfig],
       ],
     };
   });
