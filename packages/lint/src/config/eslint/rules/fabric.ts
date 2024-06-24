e8ca3e1d82fb9768d632c5f5c268458b81bbbb0f
huangkairan <18757128360@163.com>
2024-06-24 17:12:31 +0800

    fix: typo

--- packages/lint/src/config/eslint/rules/fabric.ts
+++ packages/lint/src/config/eslint/rules/fabric.ts
@@ -1,5 +1,5 @@
 /**
- * legay enabled/disabled rules from @umijs/fabric tsEslintConfig
+ * legacy enabled/disabled rules from @umijs/fabric tsEslintConfig
  * @see https://github.com/umijs/fabric/blob/master/src/tsEslintConfig.ts
  */
 export const typescript = {
@@ -69,7 +69,7 @@ export const typescript = {
 };
 
 /**
- * legay enabled/disabled rules from @umijs/fabric
+ * legacy enabled/disabled rules from @umijs/fabric
  * @see https://github.com/umijs/fabric/blob/master/src/eslint.ts#L54
  */
 export default {
