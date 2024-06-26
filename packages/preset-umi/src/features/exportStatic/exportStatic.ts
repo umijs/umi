b04143c6bc505343f1d6edbb646418a0a3cd1e5f
feirlau <njulf04@163.com>
2024-06-26 17:25:36 +0800

    feat: 支持htmlSuffix、dynamicRoot
    htmlSuffix 会生成 xxx.html，非xxx/index.html
    dynamicRoot可不用做任何修改部署在任意目录下

--- packages/preset-umi/src/features/exportStatic/exportStatic.ts
+++ packages/preset-umi/src/features/exportStatic/exportStatic.ts
@@ -19,26 +19,47 @@ interface IExportHtmlItem {
 
 type IUserExtraRoute = string | { path: string; prerender: boolean };
 
+function isHtmlRoute(route: IRoute): boolean {
+  const is404 = route.absPath === '/*';
+
+  if (
+    // skip layout
+    !route.isLayout &&
+    // skip duplicate route
+    !route.path.endsWith('.html') &&
+    // skip dynamic route for win, because `:` is not allowed in file name
+    (!IS_WIN || !route.path.includes('/:')) &&
+    // skip `*` route, because `*` is not working for most site serve services
+    (!route.path.includes('*') ||
+      // except `404.html`
+      is404)
+  ) {
+    return true;
+  }
+  return false;
+}
+function getHtmlPath(path: string, htmlSuffix: boolean): string {
+  if (!path) return path;
+  if (path === '/*') return '/404.html';
+  if (path === '/') return '/index.html';
+
+  if (path.endsWith('/')) path = path.slice(0, -1);
+  return htmlSuffix ? `${path}.html` : `${path}/index.html`;
+}
 /**
  * get export html data from routes
  */
-function getExportHtmlData(routes: Record<string, IRoute>): IExportHtmlItem[] {
+function getExportHtmlData(
+  routes: Record<string, IRoute>,
+  htmlSuffix: boolean,
+): IExportHtmlItem[] {
   const map = new Map<string, IExportHtmlItem>();
 
-  Object.values(routes).forEach((route) => {
+  for (const route of Object.values(routes)) {
     const is404 = route.absPath === '/*';
 
-    if (
-      // skip layout
-      !route.isLayout &&
-      // skip dynamic route for win, because `:` is not allowed in file name
-      (!IS_WIN || !route.path.includes('/:')) &&
-      // skip `*` route, because `*` is not working for most site serve services
-      (!route.path.includes('*') ||
-        // except `404.html`
-        is404)
-    ) {
-      const file = is404 ? '404.html' : join('.', route.absPath, 'index.html');
+    if (isHtmlRoute(route)) {
+      const file = join('.', getHtmlPath(route.absPath, htmlSuffix));
 
       map.set(file, {
         route: {
@@ -49,7 +70,7 @@ function getExportHtmlData(routes: Record<string, IRoute>): IExportHtmlItem[] {
         file,
       });
     }
-  });
+  }
 
   return Array.from(map.values());
 }
@@ -128,6 +149,8 @@ export default (api: IApi) => {
       schema: ({ zod }) =>
         zod
           .object({
+            htmlSuffix: zod.boolean(),
+            dynamicRoot: zod.boolean(),
             extraRoutePaths: zod.union([
               zod.function(),
               zod.array(zod.string()),
@@ -145,7 +168,11 @@ export default (api: IApi) => {
 
   // export routes to html files
   api.modifyExportHTMLFiles(async (_defaultFiles, opts) => {
-    const { publicPath } = api.config;
+    const {
+      publicPath,
+      base,
+      exportStatic: { htmlSuffix, dynamicRoot },
+    } = api.config;
     const htmlData = api.appData.exportHtmlData;
     const htmlFiles: { path: string; content: string }[] = [];
     const { markupArgs: defaultMarkupArgs } = opts;
@@ -164,65 +191,93 @@ export default (api: IApi) => {
           ),
         };
       }
-
-      // handle relative publicPath, such as `./`
-      if (publicPath.startsWith('.')) {
+      let routerBaseStr = JSON.stringify(base || '/');
+      let publicPathStr = JSON.stringify(publicPath || '/');
+      // handle relative publicPath, such as `./`, same with dynamicRoot
+      if (publicPath.startsWith('.') || dynamicRoot) {
         assert(
           api.config.runtimePublicPath,
-          '`runtimePublicPath` should be enable when `publicPath` is relative!',
+          '`runtimePublicPath` should be enable when `publicPath` is relative or `exportStatic.dynamicRoot` is true!',
         );
 
-        const rltPrefix = relative(dirname(file), '.');
-
-        // prefix for all assets
-        if (rltPrefix) {
-          // HINT: clone for keep original markupArgs unmodified
-          const picked = lodash.cloneDeep(
-            lodash.pick(markupArgs, [
-              'favicons',
-              'links',
-              'styles',
-              'headScripts',
-              'scripts',
-            ]),
-          );
-
-          // handle favicons
-          picked.favicons.forEach((item: string, i: number) => {
-            if (item.startsWith(publicPath)) {
-              picked.favicons[i] = winPath(join(rltPrefix, item));
-            }
-          });
-
-          // handle links
-          picked.links.forEach((link: { href: string }) => {
-            if (link.href?.startsWith(publicPath)) {
-              link.href = winPath(join(rltPrefix, link.href));
-            }
-          });
-
-          // handle scripts
-          [picked.headScripts, picked.scripts, picked.styles].forEach(
-            (group: ({ src: string } | string)[]) => {
-              group.forEach((script, i) => {
-                if (
-                  typeof script === 'string' &&
-                  script.startsWith(publicPath)
-                ) {
-                  group[i] = winPath(join(rltPrefix, script));
-                } else if (
-                  typeof script === 'object' &&
-                  script.src?.startsWith(publicPath)
-                ) {
-                  script.src = winPath(join(rltPrefix, script.src));
-                }
-              });
-            },
-          );
-
-          // update markupArgs
-          markupArgs = Object.assign({}, markupArgs, picked);
+        let pathS = route.path;
+        const isSlash = pathS.endsWith('/');
+        if (pathS === '/404') {
+          //do nothing
         }
+        // keep the relative path same for route /xxx and /xxx.html
+        else if (htmlSuffix && isSlash) {
+          pathS = pathS.slice(0, -1);
+        }
+        // keep the relative path same for route /xxx/ and /xxx/index.html
+        else if (!htmlSuffix && !isSlash) {
+          pathS = pathS + '/';
+        }
+
+        const pathN = Math.max(pathS.split('/').length - 1, 1);
+        routerBaseStr = `location.pathname.split('/').slice(0, -${pathN}).concat('').join('/')`;
+        publicPathStr = `location.protocol + '//' + location.hostname + (location.port ? ':' + location.port : '') + window.routerBase`;
+
+        const rltPrefix = relative(dirname(file), '.');
+        const joinRltPrefix = (path: string) => {
+          if (!rltPrefix || rltPrefix == '.') {
+            return `.${path.startsWith('/') ? '' : '/'}${path}`;
+          }
+          return winPath(join(rltPrefix, path));
+        };
+        // prefix for all assets
+        // HINT: clone for keep original markupArgs unmodified
+        const picked = lodash.cloneDeep(
+          lodash.pick(markupArgs, [
+            'favicons',
+            'links',
+            'styles',
+            'headScripts',
+            'scripts',
+          ]),
+        );
+
+        // handle favicons
+        picked.favicons.forEach((item: string, i: number) => {
+          if (item.startsWith(publicPath)) {
+            picked.favicons[i] = joinRltPrefix(item);
+          }
+        });
+
+        // handle links
+        picked.links.forEach((link: { href: string }) => {
+          if (link.href?.startsWith(publicPath)) {
+            link.href = joinRltPrefix(link.href);
+          }
+        });
+
+        // handle scripts
+        [picked.headScripts, picked.scripts, picked.styles].forEach(
+          (group: ({ src: string } | string)[]) => {
+            group.forEach((script, i) => {
+              if (typeof script === 'string' && script.startsWith(publicPath)) {
+                group[i] = joinRltPrefix(script);
+              } else if (
+                typeof script === 'object' &&
+                script.src?.startsWith(publicPath)
+              ) {
+                script.src = joinRltPrefix(script.src);
+              }
+            });
+          },
+        );
+
+        picked.headScripts.unshift(
+          `window.routerBase = ${routerBaseStr};`,
+          `
+if(!window.publicPath) {
+window.publicPath = ${publicPathStr};
+}
+          `,
+        );
+
+        // update markupArgs
+        markupArgs = Object.assign({}, markupArgs, picked);
       }
 
       // append html file
@@ -246,12 +301,13 @@ export default (api: IApi) => {
 
   api.onGenerateFiles(async () => {
     const {
-      exportStatic: { extraRoutePaths = [] },
+      exportStatic: { extraRoutePaths = [], htmlSuffix },
     } = api.config;
     const extraHtmlData = getExportHtmlData(
       await getRoutesFromUserExtraPaths(extraRoutePaths),
+      htmlSuffix,
     );
-    const htmlData = getExportHtmlData(api.appData.routes).concat(
+    const htmlData = getExportHtmlData(api.appData.routes, htmlSuffix).concat(
       extraHtmlData,
     );
 
@@ -269,6 +325,13 @@ export function modifyClientRenderOpts(memo: any) {
     hydrate: hydrate && !{{{ ignorePaths }}}.includes(history.location.pathname),
   };
 }
+
+export function modifyContextOpts(memo: any) {
+  return {
+    ...memo,
+    basename: window.routerBase || memo.basename,
+  }
+}
       `.trim(),
         {
           ignorePaths: JSON.stringify(
@@ -281,7 +344,22 @@ export function modifyClientRenderOpts(memo: any) {
       noPluginDir: true,
     });
   });
-
+  api.modifyRoutes((routes: Record<string, IRoute>) => {
+    const {
+      exportStatic: { htmlSuffix },
+    } = api.config;
+    // copy / to /index.html and /xxx to /xxx.html or /xxx/index.html
+    for (const key of Object.keys(routes)) {
+      const route = routes[key];
+      if (isHtmlRoute(route)) {
+        key = `${key}.html`;
+        routes[key] = {
+          ...route,
+          path: getHtmlPath(route.path, htmlSuffix),
+        };
+      }
+    }
+  });
   api.addRuntimePlugin(() => {
     return [`@@/core/exportStaticRuntimePlugin.ts`];
   });
