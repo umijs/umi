ad3abccca178002ad75d761122afc151ec2ec68c
阿菜 Cai <1064425721@qq.com>
2024-07-17 12:33:28 +0800

    chore(example): add global 'Loading' to ant design pro (#12570)

--- examples/ant-design-pro/src/app.tsx
+++ examples/ant-design-pro/src/app.tsx
@@ -10,12 +10,6 @@ import { currentUser as queryCurrentUser } from './services/ant-design-pro/api';
 const isDev = process.env.NODE_ENV === 'development';
 const loginPath = '/user/login';
 
-// TODO: 不知道这是啥？
-/** 获取用户信息比较慢的时候会展示一个 loading */
-// export const initialStateConfig = {
-//   loading: <PageLoading />,
-// };
-
 /**
  * @see  https://umijs.org/zh-CN/plugins/plugin-initial-state
  * */
