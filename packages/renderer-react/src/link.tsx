062f3ff2501433929f608794f983b2e51a18ddb7
sorrycc <sorrycc@gmail.com>
2024-12-12 11:20:09 +0800

    feat: improve link with prefetch

--- packages/renderer-react/src/link.tsx
+++ packages/renderer-react/src/link.tsx
@@ -5,20 +5,47 @@ import { useAppData } from './appContext';
 export function LinkWithPrefetch(
   props: PropsWithChildren<
     {
-      prefetch?: boolean;
+      prefetch?: boolean | 'intent' | 'render' | 'viewport' | 'none';
     } & LinkProps &
       React.RefAttributes<HTMLAnchorElement>
   >,
 ) {
-  const { prefetch, ...linkProps } = props;
+  const { prefetch: prefetchProp, ...linkProps } = props;
+  const prefetch = prefetchProp === true ? 'intent' : prefetchProp;
   const appData = useAppData();
   const to = typeof props.to === 'string' ? props.to : props.to?.pathname;
+
+  const handleMouseEnter = (e: React.MouseEvent<HTMLAnchorElement>) => {
+    if (prefetch !== 'intent') return;
+    const eventTarget = (e.target || {}) as HTMLElement & {
+      preloadTimeout?: NodeJS.Timeout | null;
+    };
+    if (eventTarget.preloadTimeout) return;
+    eventTarget.preloadTimeout = setTimeout(() => {
+      eventTarget.preloadTimeout = null;
+      appData.preloadRoute?.(to!);
+    }, 50);
+  };
+
+  const handleMouseLeave = (e: React.MouseEvent<HTMLAnchorElement>) => {
+    if (prefetch !== 'intent') return;
+    const eventTarget = (e.target || {}) as HTMLElement & {
+      preloadTimeout?: NodeJS.Timeout | null;
+    };
+    if (eventTarget.preloadTimeout) {
+      clearTimeout(eventTarget.preloadTimeout);
+      eventTarget.preloadTimeout = null;
+    }
+  };
+
   // compatible with old code
   // which to might be undefined
   if (!to) return null;
+
   return (
     <Link
-      onMouseEnter={() => prefetch && to && appData.preloadRoute?.(to)}
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}
       {...linkProps}
     >
       {props.children}
