a59824a139656458c6a4ffd6f7402cab5962aeb4
chencheng (云谦) <sorrycc@gmail.com>
2024-12-12 15:31:59 +0800

    feat: improve link with prefetch (#12844)
    * feat: improve link with prefetch
    
    * feat: support preload=render
    
    * feat: support preload=viewport
    
    * chore: throw error when prefetch props is invalid
    
    * chore: docs and default prefetch config
    
    * ci: fix

--- packages/preset-umi/src/features/routePrefetch/routePrefetch.ts
+++ packages/preset-umi/src/features/routePrefetch/routePrefetch.ts
@@ -4,13 +4,27 @@ export default (api: IApi) => {
   api.describe({
     config: {
       schema({ zod }) {
-        return zod.object({});
+        return zod.object({
+          defaultPrefetch: zod
+            .enum(['none', 'intent', 'render', 'viewport'])
+            .optional(),
+          defaultPrefetchTimeout: zod.number().optional(),
+        });
       },
     },
     enableBy: api.EnableBy.config,
   });
 
   api.addEntryCodeAhead(() => {
-    return `if(typeof window !== 'undefined') window.__umi_route_prefetch__ = true;`;
+    return `if(typeof window !== 'undefined') window.__umi_route_prefetch__ =
+      {
+        defaultPrefetch: ${JSON.stringify(
+          api.config.routePrefetch.defaultPrefetch || 'none',
+        )},
+        defaultPrefetchTimeout: ${JSON.stringify(
+          api.config.routePrefetch.defaultPrefetchTimeout || 50,
+        )},
+      };
+    `;
   });
 };
--- packages/renderer-react/src/link.tsx
+++ packages/renderer-react/src/link.tsx
@@ -1,27 +1,111 @@
-import React, { PropsWithChildren } from 'react';
+import React, { PropsWithChildren, useLayoutEffect } from 'react';
 import { Link, LinkProps } from 'react-router-dom';
 import { useAppData } from './appContext';
+import { useIntersectionObserver } from './useIntersectionObserver';
 
-export function LinkWithPrefetch(
-  props: PropsWithChildren<
-    {
-      prefetch?: boolean;
-    } & LinkProps &
-      React.RefAttributes<HTMLAnchorElement>
-  >,
-) {
-  const { prefetch, ...linkProps } = props;
-  const appData = useAppData();
-  const to = typeof props.to === 'string' ? props.to : props.to?.pathname;
-  // compatible with old code
-  // which to might be undefined
-  if (!to) return null;
-  return (
-    <Link
-      onMouseEnter={() => prefetch && to && appData.preloadRoute?.(to)}
-      {...linkProps}
-    >
-      {props.children}
-    </Link>
-  );
+function useForwardedRef<T>(ref?: React.ForwardedRef<T>) {
+  const innerRef = React.useRef<T>(null);
+  React.useEffect(() => {
+    if (!ref) return;
+    if (typeof ref === 'function') {
+      ref(innerRef.current);
+    } else {
+      ref.current = innerRef.current;
+    }
+  });
+  return innerRef;
 }
+
+export const LinkWithPrefetch = React.forwardRef(
+  (
+    props: PropsWithChildren<
+      {
+        prefetch?: boolean | 'intent' | 'render' | 'viewport' | 'none';
+        prefetchTimeout?: number;
+      } & LinkProps &
+        React.RefAttributes<HTMLAnchorElement>
+    >,
+    forwardedRef,
+  ) => {
+    const { prefetch: prefetchProp, ...linkProps } = props;
+    const { defaultPrefetch, defaultPrefetchTimeout } = (typeof window !==
+      'undefined' && // @ts-ignore
+      window.__umi_route_prefetch__) || {
+      defaultPrefetch: 'none',
+      defaultPrefetchTimeout: 50,
+    };
+
+    const prefetch =
+      (prefetchProp === true
+        ? 'intent'
+        : prefetchProp === false
+        ? 'none'
+        : prefetchProp) || defaultPrefetch;
+    if (!['intent', 'render', 'viewport', 'none'].includes(prefetch)) {
+      throw new Error(
+        `Invalid prefetch value ${prefetch} found in Link component`,
+      );
+    }
+    const appData = useAppData();
+    const to = typeof props.to === 'string' ? props.to : props.to?.pathname;
+    const hasRenderFetched = React.useRef(false);
+    const ref = useForwardedRef(forwardedRef);
+    // prefetch intent
+    const handleMouseEnter = (e: React.MouseEvent<HTMLAnchorElement>) => {
+      if (prefetch !== 'intent') return;
+      const eventTarget = (e.target || {}) as HTMLElement & {
+        preloadTimeout?: NodeJS.Timeout | null;
+      };
+      if (eventTarget.preloadTimeout) return;
+      eventTarget.preloadTimeout = setTimeout(() => {
+        eventTarget.preloadTimeout = null;
+        appData.preloadRoute?.(to!);
+      }, props.prefetchTimeout || defaultPrefetchTimeout);
+    };
+    const handleMouseLeave = (e: React.MouseEvent<HTMLAnchorElement>) => {
+      if (prefetch !== 'intent') return;
+      const eventTarget = (e.target || {}) as HTMLElement & {
+        preloadTimeout?: NodeJS.Timeout | null;
+      };
+      if (eventTarget.preloadTimeout) {
+        clearTimeout(eventTarget.preloadTimeout);
+        eventTarget.preloadTimeout = null;
+      }
+    };
+
+    // prefetch render
+    useLayoutEffect(() => {
+      if (prefetch === 'render' && !hasRenderFetched.current) {
+        appData.preloadRoute?.(to!);
+        hasRenderFetched.current = true;
+      }
+    }, [prefetch, to]);
+
+    // prefetch viewport
+    useIntersectionObserver(
+      ref as React.RefObject<HTMLAnchorElement>,
+      (entry) => {
+        if (entry?.isIntersecting) {
+          appData.preloadRoute?.(to!);
+        }
+      },
+      { rootMargin: '100px' },
+      { disabled: prefetch !== 'viewport' },
+    );
+
+    // compatible with old code
+    // which to might be undefined
+    if (!to) return null;
+
+    return (
+      <Link
+        onMouseEnter={handleMouseEnter}
+        onMouseLeave={handleMouseLeave}
+        ref={ref as React.RefObject<HTMLAnchorElement>}
+        {...linkProps}
+      >
+        {props.children}
+      </Link>
+    );
+  },
+);
