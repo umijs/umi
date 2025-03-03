3b8d58fb40d2b24d4e92410d8ad5787ee7543203
野迂迂 <yangwei1@outlook.com>
2025-03-03 11:44:15 +0800

    feat: support onExit hook for plugins

--- packages/core/src/service/service.ts
+++ packages/core/src/service/service.ts
@@ -698,6 +698,7 @@ export interface IServicePluginAPI {
 
   onCheck: IEvent<null>;
   onStart: IEvent<null>;
+  onExit: IEvent<null>;
   modifyAppData: IModify<typeof Service.prototype.appData, null>;
   modifyConfig: IModify<
     typeof Service.prototype.config,
