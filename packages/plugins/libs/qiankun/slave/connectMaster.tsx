50c5decc177acd32e7442a7aeaa1408eb635013a
lvboda <lvboda705@gmail.com>
2026-01-05 18:21:25 +0800

    refactor(qiankun): replace hardcoded '@@qiankunStateFromMaster' string with qiankunStateFromMasterModelNamespace constant in connectMaster.

--- packages/plugins/libs/qiankun/slave/connectMaster.tsx
+++ packages/plugins/libs/qiankun/slave/connectMaster.tsx
@@ -1,12 +1,14 @@
 // @ts-nocheck
 __USE_MODEL__;
 import React from 'react';
+import { qiankunStateFromMasterModelNamespace } from './constants';
 
 const noop = () => {};
 
 const connectMaster = <T extends object>(Component: React.ComponentType<T>) => {
   return (props: T, ...rest: any[]) => {
-    const masterProps = (useModel || noop)('@@qiankunStateFromMaster') || {};
+    const masterProps =
+      (useModel || noop)(qiankunStateFromMasterModelNamespace) || {};
     return <Component {...props} {...rest} {...masterProps} />;
   };
 };
