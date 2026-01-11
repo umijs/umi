17cf5b143d1a5b47144f7ddb4716118d2fddb499
gin-lsl <gin_lsl@outlook.com>
2026-01-11 20:23:41 +0800

    fix: ssg 模式下，meta 无法设置 property 属性 (#13241)

--- examples/ssg-basename/.umirc.ts
+++ examples/ssg-basename/.umirc.ts
@@ -3,4 +3,10 @@ export default {
   ssr: {},
   base: '/base/',
   publicPath: '/base/', // 布署时需要布署在 base 文件夹下.
+  metas: [
+    {
+      property: 'og:image',
+      content: 'https://example.com/example.png',
+    },
+  ],
 };
