5ffde50775d245ec1c4686e0aae23e4756486c6a
xusd320 <xusd320@gmail.com>
2026-04-11 15:29:26 +0800

    test: bug report for static output path

--- examples/with-antd/pages/index.tsx
+++ examples/with-antd/pages/index.tsx
@@ -2,6 +2,7 @@ import { Button } from 'antd';
 import React from 'react';
 
 export default function Page() {
+  console.log(new URL('./plain.txt', import.meta.url).href);
   return (
     <div>
       <h1>with antd</h1>
