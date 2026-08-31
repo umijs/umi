a0839af7419226880703402ea861f947e4d8ac0f
zoomdong <1344492820@qq.com>
2026-09-01 00:02:52 +0800

    fix: align esbuild with Vite 7

--- packages/testing/src/transformers/esbuild/type.ts
+++ packages/testing/src/transformers/esbuild/type.ts
@@ -1,4 +1,4 @@
-import type { TransformOptions } from 'esbuild';
+import type { TransformOptions } from '@umijs/bundler-utils/compiled/esbuild';
 
 export interface UserOptions extends TransformOptions {}
 export interface ResolvedOptions extends TransformOptions {}
