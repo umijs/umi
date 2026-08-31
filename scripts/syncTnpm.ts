6be1c9a09241aa964167903931fb3ff847184913
zoomdong <1344492820@qq.com>
2026-08-31 17:25:56 +0800

    ci: sync packages to TNPM after publish (#13424)

---
+++ scripts/syncTnpm.ts
@@ -0,0 +1,20 @@
+import * as logger from '@umijs/utils/src/logger';
+import 'zx/globals';
+import { PATHS } from './.internal/constants';
+import { getPkgs } from './.internal/utils';
+
+(async () => {
+  const pkgs = getPkgs();
+  logger.info(`pkgs: ${pkgs.join(', ')}`);
+
+  logger.event('sync tnpm');
+  $.verbose = false;
+  await Promise.all(
+    pkgs.map(async (pkg) => {
+      const { name } = require(path.join(PATHS.PACKAGES, pkg, 'package.json'));
+      logger.info(`sync ${name}`);
+      await $`tnpm sync ${name}`;
+    }),
+  );
+  $.verbose = true;
+})();
